2c207c3ba6b0a9427cabf477982af6fad614534d
Ryan Blue <blue@apache.org>
2019-03-24 15:50:04 -0700

    Fix tests for package rename.

--- core/src/test/java/org/apache/iceberg/TestScansAndSchemaEvolution.java
+++ core/src/test/java/org/apache/iceberg/TestScansAndSchemaEvolution.java
@@ -17,27 +17,26 @@
  * under the License.
  */
 
-package com.netflix.iceberg;
+package org.apache.iceberg;
 
 import com.google.common.collect.Lists;
-import com.netflix.iceberg.avro.Avro;
-import com.netflix.iceberg.avro.RandomAvroData;
-import com.netflix.iceberg.expressions.Expressions;
-import com.netflix.iceberg.io.FileAppender;
-import com.netflix.iceberg.types.Types;
 import org.apache.avro.generic.GenericData;
+import org.apache.iceberg.avro.Avro;
+import org.apache.iceberg.avro.RandomAvroData;
+import org.apache.iceberg.expressions.Expressions;
+import org.apache.iceberg.io.FileAppender;
+import org.apache.iceberg.types.Types;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 
-import static com.netflix.iceberg.types.Types.NestedField.required;
+import static org.apache.iceberg.types.Types.NestedField.required;
 
 public class TestScansAndSchemaEvolution {
   private static final Schema SCHEMA = new Schema(
