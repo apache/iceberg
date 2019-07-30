bdebffd20a0a1d7279ff3701c3cc33617e469999
Edgar Rodriguez <edgar.rodriguez@airbnb.com>
2019-07-30 15:44:10 -0700

    Restore imports

--- core/src/test/java/org/apache/iceberg/TestMetrics.java
+++ core/src/test/java/org/apache/iceberg/TestMetrics.java
@@ -35,7 +35,22 @@ import org.apache.iceberg.avro.AvroSchemaUtil;
 import org.apache.iceberg.io.InputFile;
 import org.apache.iceberg.types.Conversions;
 import org.apache.iceberg.types.Type;
-import org.apache.iceberg.types.Types;
+import org.apache.iceberg.types.Types.BinaryType;
+import org.apache.iceberg.types.Types.BooleanType;
+import org.apache.iceberg.types.Types.DateType;
+import org.apache.iceberg.types.Types.DecimalType;
+import org.apache.iceberg.types.Types.DoubleType;
+import org.apache.iceberg.types.Types.FixedType;
+import org.apache.iceberg.types.Types.FloatType;
+import org.apache.iceberg.types.Types.IntegerType;
+import org.apache.iceberg.types.Types.ListType;
+import org.apache.iceberg.types.Types.LongType;
+import org.apache.iceberg.types.Types.MapType;
+import org.apache.iceberg.types.Types.StringType;
+import org.apache.iceberg.types.Types.StructType;
+import org.apache.iceberg.types.Types.TimeType;
+import org.apache.iceberg.types.Types.TimestampType;
+import org.apache.iceberg.types.Types.UUIDType;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -59,19 +74,19 @@ public abstract class TestMetrics {
   @Test
   public void testMetricsForTopLevelFields() throws IOException {
     Schema schema = new Schema(
-        optional(1, "booleanCol", Types.BooleanType.get()),
-        required(2, "intCol", Types.IntegerType.get()),
-        optional(3, "longCol", Types.LongType.get()),
-        required(4, "floatCol", Types.FloatType.get()),
-        optional(5, "doubleCol", Types.DoubleType.get()),
-        optional(6, "decimalCol", Types.DecimalType.of(10, 2)),
-        required(7, "stringCol", Types.StringType.get()),
-        optional(8, "dateCol", Types.DateType.get()),
-        required(9, "timeCol", Types.TimeType.get()),
-        required(10, "timestampCol", Types.TimestampType.withoutZone()),
-        optional(11, "uuidCol", Types.UUIDType.get()),
-        required(12, "fixedCol", Types.FixedType.ofLength(4)),
-        required(13, "binaryCol", Types.BinaryType.get())
+        optional(1, "booleanCol", BooleanType.get()),
+        required(2, "intCol", IntegerType.get()),
+        optional(3, "longCol", LongType.get()),
+        required(4, "floatCol", FloatType.get()),
+        optional(5, "doubleCol", DoubleType.get()),
+        optional(6, "decimalCol", DecimalType.of(10, 2)),
+        required(7, "stringCol", StringType.get()),
+        optional(8, "dateCol", DateType.get()),
+        required(9, "timeCol", TimeType.get()),
+        required(10, "timestampCol", TimestampType.withoutZone()),
+        optional(11, "uuidCol", UUIDType.get()),
+        required(12, "fixedCol", FixedType.ofLength(4)),
+        required(13, "binaryCol", BinaryType.get())
     );
 
     GenericData.Record firstRecord = new GenericData.Record(AvroSchemaUtil.convert(schema.asStruct()));
@@ -109,41 +124,41 @@ public abstract class TestMetrics {
     Metrics metrics = getMetrics(Files.localInput(recordsFile));
     Assert.assertEquals(2L, (long) metrics.recordCount());
     assertCounts(1, 2L, 0L, metrics);
-    assertBounds(1, Types.BooleanType.get(), false, true, metrics);
+    assertBounds(1, BooleanType.get(), false, true, metrics);
     assertCounts(2, 2L, 0L, metrics);
-    assertBounds(2, Types.IntegerType.get(), Integer.MIN_VALUE, 3, metrics);
+    assertBounds(2, IntegerType.get(), Integer.MIN_VALUE, 3, metrics);
     assertCounts(3, 2L, 1L, metrics);
-    assertBounds(3, Types.LongType.get(), 5L, 5L, metrics);
+    assertBounds(3, LongType.get(), 5L, 5L, metrics);
     assertCounts(4, 2L, 0L, metrics);
-    assertBounds(4, Types.FloatType.get(), 1.0F, 2.0F, metrics);
+    assertBounds(4, FloatType.get(), 1.0F, 2.0F, metrics);
     assertCounts(5, 2L, 1L, metrics);
-    assertBounds(5, Types.DoubleType.get(), 2.0D, 2.0D, metrics);
+    assertBounds(5, DoubleType.get(), 2.0D, 2.0D, metrics);
     assertCounts(6, 2L, 1L, metrics);
-    assertBounds(6, Types.DecimalType.of(10, 2), new BigDecimal("3.50"), new BigDecimal("3.50"), metrics);
+    assertBounds(6, DecimalType.of(10, 2), new BigDecimal("3.50"), new BigDecimal("3.50"), metrics);
     assertCounts(7, 2L, 0L, metrics);
-    assertBounds(7, Types.StringType.get(), CharBuffer.wrap("AAA"), CharBuffer.wrap("ZZZ"), metrics);
+    assertBounds(7, StringType.get(), CharBuffer.wrap("AAA"), CharBuffer.wrap("ZZZ"), metrics);
     assertCounts(8, 2L, 1L, metrics);
-    assertBounds(8, Types.DateType.get(), 1500, 1500, metrics);
+    assertBounds(8, DateType.get(), 1500, 1500, metrics);
     assertCounts(9, 2L, 0L, metrics);
-    assertBounds(9, Types.TimeType.get(), 2000L, 3000L, metrics);
+    assertBounds(9, TimeType.get(), 2000L, 3000L, metrics);
     assertCounts(10, 2L, 0L, metrics);
-    assertBounds(10, Types.TimestampType.withoutZone(), 0L, 1000L, metrics);
+    assertBounds(10, TimestampType.withoutZone(), 0L, 1000L, metrics);
     assertCounts(11, 2L, 1L, metrics);
-    assertBounds(11, Types.UUIDType.get(), uuid, uuid, metrics);
+    assertBounds(11, UUIDType.get(), uuid, uuid, metrics);
     assertCounts(12, 2L, 0L, metrics);
-    assertBounds(12, Types.FixedType.ofLength(4),
+    assertBounds(12, FixedType.ofLength(4),
         ByteBuffer.wrap(fixed.bytes()), ByteBuffer.wrap(fixed.bytes()), metrics);
     assertCounts(13, 2L, 0L, metrics);
-    assertBounds(13, Types.BinaryType.get(),
+    assertBounds(13, BinaryType.get(),
         ByteBuffer.wrap("S".getBytes()), ByteBuffer.wrap("W".getBytes()), metrics);
   }
 
   @Test
   public void testMetricsForDecimals() throws IOException {
     Schema schema = new Schema(
-        required(1, "decimalAsInt32", Types.DecimalType.of(4, 2)),
-        required(2, "decimalAsInt64", Types.DecimalType.of(14, 2)),
-        required(3, "decimalAsFixed", Types.DecimalType.of(22, 2))
+        required(1, "decimalAsInt32", DecimalType.of(4, 2)),
+        required(2, "decimalAsInt64", DecimalType.of(14, 2)),
+        required(3, "decimalAsFixed", DecimalType.of(22, 2))
     );
 
     GenericData.Record record = new GenericData.Record(AvroSchemaUtil.convert(schema.asStruct()));
@@ -156,25 +171,25 @@ public abstract class TestMetrics {
     Metrics metrics = getMetrics(Files.localInput(recordsFile));
     Assert.assertEquals(1L, (long) metrics.recordCount());
     assertCounts(1, 1L, 0L, metrics);
-    assertBounds(1, Types.DecimalType.of(4, 2), new BigDecimal("2.55"), new BigDecimal("2.55"), metrics);
+    assertBounds(1, DecimalType.of(4, 2), new BigDecimal("2.55"), new BigDecimal("2.55"), metrics);
     assertCounts(2, 1L, 0L, metrics);
-    assertBounds(2, Types.DecimalType.of(14, 2), new BigDecimal("4.75"), new BigDecimal("4.75"), metrics);
+    assertBounds(2, DecimalType.of(14, 2), new BigDecimal("4.75"), new BigDecimal("4.75"), metrics);
     assertCounts(3, 1L, 0L, metrics);
-    assertBounds(3, Types.DecimalType.of(22, 2), new BigDecimal("5.80"), new BigDecimal("5.80"), metrics);
+    assertBounds(3, DecimalType.of(22, 2), new BigDecimal("5.80"), new BigDecimal("5.80"), metrics);
   }
 
   @Test
   public void testMetricsForNestedStructFields() throws IOException {
-    Types.StructType leafStructType = Types.StructType.of(
-        optional(5, "leafLongCol", Types.LongType.get()),
-        optional(6, "leafBinaryCol", Types.BinaryType.get())
+    StructType leafStructType = StructType.of(
+        optional(5, "leafLongCol", LongType.get()),
+        optional(6, "leafBinaryCol", BinaryType.get())
     );
-    Types.StructType nestedStructType = Types.StructType.of(
-        required(3, "longCol", Types.LongType.get()),
+    StructType nestedStructType = StructType.of(
+        required(3, "longCol", LongType.get()),
         required(4, "leafStructCol", leafStructType)
     );
     Schema schema = new Schema(
-        required(1, "intCol", Types.IntegerType.get()),
+        required(1, "intCol", IntegerType.get()),
         required(2, "nestedStructCol", nestedStructType)
     );
 
@@ -193,25 +208,25 @@ public abstract class TestMetrics {
     Metrics metrics = getMetrics(Files.localInput(recordsFile));
     Assert.assertEquals(1L, (long) metrics.recordCount());
     assertCounts(1, 1L, 0L, metrics);
-    assertBounds(1, Types.IntegerType.get(), Integer.MAX_VALUE, Integer.MAX_VALUE, metrics);
+    assertBounds(1, IntegerType.get(), Integer.MAX_VALUE, Integer.MAX_VALUE, metrics);
     assertCounts(3, 1L, 0L, metrics);
-    assertBounds(3, Types.LongType.get(), 100L, 100L, metrics);
+    assertBounds(3, LongType.get(), 100L, 100L, metrics);
     assertCounts(5, 1L, 0L, metrics);
-    assertBounds(5, Types.LongType.get(), 20L, 20L, metrics);
+    assertBounds(5, LongType.get(), 20L, 20L, metrics);
     assertCounts(6, 1L, 0L, metrics);
-    assertBounds(6, Types.BinaryType.get(),
+    assertBounds(6, BinaryType.get(),
         ByteBuffer.wrap("A".getBytes()), ByteBuffer.wrap("A".getBytes()), metrics);
   }
 
   @Test
   public void testMetricsForListAndMapElements() throws IOException {
-    Types.StructType structType = Types.StructType.of(
-        required(1, "leafIntCol", Types.IntegerType.get()),
-        optional(2, "leafStringCol", Types.StringType.get())
+    StructType structType = StructType.of(
+        required(1, "leafIntCol", IntegerType.get()),
+        optional(2, "leafStringCol", StringType.get())
     );
     Schema schema = new Schema(
-        optional(3, "intListCol", Types.ListType.ofRequired(4, Types.IntegerType.get())),
-        optional(5, "mapCol", Types.MapType.ofRequired(6, 7, Types.StringType.get(), structType))
+        optional(3, "intListCol", ListType.ofRequired(4, IntegerType.get())),
+        optional(5, "mapCol", MapType.ofRequired(6, 7, StringType.get(), structType))
     );
 
     GenericData.Record record = new GenericData.Record(AvroSchemaUtil.convert(schema.asStruct()));
@@ -228,19 +243,19 @@ public abstract class TestMetrics {
     Metrics metrics = getMetrics(Files.localInput(recordsFile));
     Assert.assertEquals(1L, (long) metrics.recordCount());
     assertCounts(1, 1, 0, metrics);
-    assertBounds(1, Types.IntegerType.get(), null, null, metrics);
+    assertBounds(1, IntegerType.get(), null, null, metrics);
     assertCounts(2, 1, 0, metrics);
-    assertBounds(2, Types.StringType.get(), null, null, metrics);
+    assertBounds(2, StringType.get(), null, null, metrics);
     assertCounts(4, 3, 0, metrics);
-    assertBounds(4, Types.IntegerType.get(), null, null, metrics);
+    assertBounds(4, IntegerType.get(), null, null, metrics);
     assertCounts(6, 1, 0, metrics);
-    assertBounds(6, Types.StringType.get(), null, null, metrics);
+    assertBounds(6, StringType.get(), null, null, metrics);
   }
 
   @Test
   public void testMetricsForNullColumns() throws IOException {
     Schema schema = new Schema(
-        optional(1, "intCol", Types.IntegerType.get())
+        optional(1, "intCol", IntegerType.get())
     );
     GenericData.Record firstRecord = new GenericData.Record(AvroSchemaUtil.convert(schema.asStruct()));
     firstRecord.put("intCol", null);
@@ -252,7 +267,7 @@ public abstract class TestMetrics {
     Metrics metrics = getMetrics(Files.localInput(recordsFile));
     Assert.assertEquals(2L, (long) metrics.recordCount());
     assertCounts(1, 2, 2, metrics);
-    assertBounds(1, Types.IntegerType.get(), null, null, metrics);
+    assertBounds(1, IntegerType.get(), null, null, metrics);
   }
 
   private void assertCounts(int fieldId, long valueCount, long nullValueCount, Metrics metrics) {
