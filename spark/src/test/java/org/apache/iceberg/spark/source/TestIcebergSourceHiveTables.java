0f318ba862f2c00a266a312d64cf305e0288dfc0
wangmiao1981 <wm624@hotmail.com>
2019-09-11 09:36:43 -0700

    dummy commit for test

--- spark/src/test/java/org/apache/iceberg/spark/source/TestIcebergSourceHiveTables.java
+++ spark/src/test/java/org/apache/iceberg/spark/source/TestIcebergSourceHiveTables.java
@@ -136,6 +136,7 @@ public class TestIcebergSourceHiveTables {
     }
   }
 
+  
   @Test
   public synchronized void testHiveEntriesTable() throws Exception {
     TableIdentifier tableIdentifier = TableIdentifier.of("db", "entries_test");
