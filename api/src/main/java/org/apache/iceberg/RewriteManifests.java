70c1940209b9c444363322bf547b65d9320e0cea
Anton Okolnychyi <aokolnychyi@apple.com>
2019-11-12 20:01:44 +0200

    Extend RewriteManifests to delete/add manifests directly (#512)

--- api/src/main/java/org/apache/iceberg/RewriteManifests.java
+++ api/src/main/java/org/apache/iceberg/RewriteManifests.java
@@ -29,6 +29,10 @@ import java.util.function.Predicate;
  * described only by the manifest files that were added, and commits that snapshot as the
  * current.
  * <p>
+ * This API can be used to rewrite matching manifests according to a clustering function as well as
+ * to replace specific manifests. Manifests that are deleted or added directly are ignored during
+ * the rewrite process. The set of active files in replaced manifests must be the same as in new manifests.
+ * <p>
  * When committing, these changes will be applied to the latest table snapshot. Commit conflicts
  * will be resolved by applying the changes to the new latest snapshot and reattempting the commit.
  */
@@ -37,7 +41,8 @@ public interface RewriteManifests extends SnapshotUpdate<RewriteManifests> {
    * Groups an existing {@link DataFile} by a cluster key produced by a function. The cluster key
    * will determine which data file will be associated with a particular manifest. All data files
    * with the same cluster key will be written to the same manifest (unless the file is large and
-   * split into multiple files).
+   * split into multiple files). Manifests deleted via {@link #deleteManifest(ManifestFile)} or
+   * added via {@link #addManifest(ManifestFile)} are ignored during the rewrite process.
    *
    * @param func Function used to cluster data files to manifests.
    * @return this for method chaining
@@ -54,4 +59,21 @@ public interface RewriteManifests extends SnapshotUpdate<RewriteManifests> {
    * @return this for method chaining
    */
   RewriteManifests rewriteIf(Predicate<ManifestFile> predicate);
+
+  /**
+   * Deletes a {@link ManifestFile manifest file} from the table.
+   *
+   * @param manifest a manifest to delete
+   * @return this for method chaining
+   */
+  RewriteManifests deleteManifest(ManifestFile manifest);
+
+  /**
+   * Adds a {@link ManifestFile manifest file} to the table. The added manifest cannot contain new
+   * or deleted files.
+   *
+   * @param manifest a manifest to add
+   * @return this for method chaining
+   */
+  RewriteManifests addManifest(ManifestFile manifest);
 }
