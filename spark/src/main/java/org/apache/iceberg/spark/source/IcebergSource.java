b52b1b2c4ad15535e9342a9bbcd654d22e9e1629
Xabriel J Collazo Mojica <xcollazo@adobe.com>
2019-11-13 10:53:46 -0800

    Allow spark.read.schema() to be set

--- spark/src/main/java/org/apache/iceberg/spark/source/IcebergSource.java
+++ spark/src/main/java/org/apache/iceberg/spark/source/IcebergSource.java
@@ -64,11 +64,23 @@ public class IcebergSource implements DataSourceV2, ReadSupport, WriteSupport, D
 
   @Override
   public DataSourceReader createReader(DataSourceOptions options) {
+    return createReader(null, options);
+  }
+
+  @Override
+  public DataSourceReader createReader(StructType readSchema, DataSourceOptions options) {
     Configuration conf = new Configuration(lazyBaseConf());
     Table table = getTableAndResolveHadoopConfiguration(options, conf);
     String caseSensitive = lazySparkSession().conf().get("spark.sql.caseSensitive", "true");
 
-    return new Reader(table, Boolean.valueOf(caseSensitive), options);
+    Reader reader = new Reader(table, Boolean.valueOf(caseSensitive), options);
+    if (readSchema != null) {
+      // convert() will fail if readSchema contains fields not in table.schema()
+      SparkSchemaUtil.convert(table.schema(), readSchema);
+      reader.pruneColumns(readSchema);
+    }
+
+    return reader;
   }
 
   @Override
