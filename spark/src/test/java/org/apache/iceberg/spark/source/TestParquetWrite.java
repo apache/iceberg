e43ceae2af61e053004d1f802cb71164eafbb8f8
Andrei Ionescu <aionescu@adobe.com>
2019-12-05 15:10:56 +0200

    Add day partition in nested partition unit test

--- spark/src/test/java/org/apache/iceberg/spark/source/TestParquetWrite.java
+++ spark/src/test/java/org/apache/iceberg/spark/source/TestParquetWrite.java
@@ -22,6 +22,8 @@ package org.apache.iceberg.spark.source;
 import com.google.common.collect.Lists;
 import java.io.File;
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.List;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.iceberg.DataFile;
@@ -418,7 +420,8 @@ public class TestParquetWrite {
         optional(2, "data", Types.StringType.get()),
         optional(3, "nestedData", Types.StructType.of(
             optional(4, "id", Types.IntegerType.get()),
-            optional(5, "moreData", Types.StringType.get())))
+            optional(5, "moreData", Types.StringType.get()))),
+        optional(6, "timestamp", Types.TimestampType.withZone())
     );
 
     File parent = temp.newFolder("parquet");
@@ -427,21 +430,26 @@ public class TestParquetWrite {
     HadoopTables tables = new HadoopTables(new Configuration());
     PartitionSpec spec = PartitionSpec.builderFor(nestedSchema)
         .identity("id")
+        .day("timestamp")
         .identity("nestedData.moreData")
         .build();
     Table table = tables.create(nestedSchema, spec, location.toString());
 
     List<String> jsons = Lists.newArrayList(
-        "{ \"id\": 1, \"data\": \"a\", \"nestedData\": { \"id\": 100, \"moreData\": \"p1\"} }",
-        "{ \"id\": 2, \"data\": \"b\", \"nestedData\": { \"id\": 200, \"moreData\": \"p1\"} }",
-        "{ \"id\": 3, \"data\": \"c\", \"nestedData\": { \"id\": 300, \"moreData\": \"p2\"} }",
-        "{ \"id\": 4, \"data\": \"d\", \"nestedData\": { \"id\": 400, \"moreData\": \"p2\"} }"
+        "{ \"id\": 1, \"data\": \"a\", \"nestedData\": { \"id\": 100, \"moreData\": \"p1\"}, " +
+            "\"timestamp\": \"2017-12-01T10:12:55.034Z\" }",
+        "{ \"id\": 2, \"data\": \"b\", \"nestedData\": { \"id\": 200, \"moreData\": \"p1\"}, " +
+            "\"timestamp\": \"2017-12-02T10:12:55.034Z\" }",
+        "{ \"id\": 3, \"data\": \"c\", \"nestedData\": { \"id\": 300, \"moreData\": \"p2\"}, " +
+            "\"timestamp\": \"2017-12-03T10:12:55.034Z\" }",
+        "{ \"id\": 4, \"data\": \"d\", \"nestedData\": { \"id\": 400, \"moreData\": \"p2\"}, " +
+            "\"timestamp\": \"2017-12-04T10:12:55.034Z\" }"
     );
     Dataset<Row> df = spark.read().schema(SparkSchemaUtil.convert(nestedSchema))
         .json(spark.createDataset(jsons, Encoders.STRING()));
 
     // TODO: incoming columns must be ordered according to the table's schema
-    df.select("id", "data", "nestedData").write()
+    df.select("id", "data", "nestedData", "timestamp").write()
         .format("iceberg")
         .mode("append")
         .save(location.toString());
@@ -458,17 +466,25 @@ public class TestParquetWrite {
     Assert.assertEquals("Row 1 col 2 is a", "a", actual.get(0).getString(1));
     Assert.assertEquals("Row 1 col 3,1 is 100", 100, actual.get(0).getStruct(2).getInt(0));
     Assert.assertEquals("Row 1 col 3,2 is p1", "p1", actual.get(0).getStruct(2).getString(1));
+    Assert.assertEquals("Row 1 col 4 is 2017-12-01T10:12:55.034+00:00",
+        0, actual.get(0).getTimestamp(3).compareTo(Timestamp.from(Instant.parse("2017-12-01T10:12:55.034Z"))));
     Assert.assertEquals("Row 2 col 1 is 2", 2, actual.get(1).getInt(0));
     Assert.assertEquals("Row 2 col 2 is b", "b", actual.get(1).getString(1));
     Assert.assertEquals("Row 2 col 3,1 is 200", 200, actual.get(1).getStruct(2).getInt(0));
     Assert.assertEquals("Row 2 col 3,2 is p1", "p1", actual.get(1).getStruct(2).getString(1));
+    Assert.assertEquals("Row 2 col 4 is 2017-12-02 12:12:55.034",
+        0, actual.get(1).getTimestamp(3).compareTo(Timestamp.from(Instant.parse("2017-12-02T10:12:55.034Z"))));
     Assert.assertEquals("Row 3 col 1 is 3", 3, actual.get(2).getInt(0));
     Assert.assertEquals("Row 3 col 2 is c", "c", actual.get(2).getString(1));
     Assert.assertEquals("Row 3 col 3,1 is 300", 300, actual.get(2).getStruct(2).getInt(0));
     Assert.assertEquals("Row 3 col 3,2 is p2", "p2", actual.get(2).getStruct(2).getString(1));
+    Assert.assertEquals("Row 3 col 4 is 2017-12-03 12:12:55.034",
+        0, actual.get(2).getTimestamp(3).compareTo(Timestamp.from(Instant.parse("2017-12-03T10:12:55.034Z"))));
     Assert.assertEquals("Row 4 col 1 is 4", 4, actual.get(3).getInt(0));
     Assert.assertEquals("Row 4 col 2 is d", "d", actual.get(3).getString(1));
     Assert.assertEquals("Row 4 col 3,1 is 400", 400, actual.get(3).getStruct(2).getInt(0));
     Assert.assertEquals("Row 4 col 3,2 is p2", "p2", actual.get(3).getStruct(2).getString(1));
+    Assert.assertEquals("Row 4 col 4 is 2017-12-04 12:12:55.034",
+        0, actual.get(3).getTimestamp(3).compareTo(Timestamp.from(Instant.parse("2017-12-04T10:12:55.034Z"))));
   }
 }
