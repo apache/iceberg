768e051fe8a4ed1931481c4f59c92d6e3c1cb802
Gautam Kowshik <kowshik@adobe.com>
2019-12-06 17:51:17 -0800

    Removed redundant methods and changed method name

--- api/src/main/java/org/apache/iceberg/CherryPick.java
+++ api/src/main/java/org/apache/iceberg/CherryPick.java
@@ -2,29 +2,6 @@ package org.apache.iceberg;
 
 public interface CherryPick extends PendingUpdate<Snapshot> {
 
-  public CherryPick fromWapId(long wapId);
-
-  /**
-   * Apply the pending changes and return the uncommitted changes for validation.
-   * <p>
-   * This does not result in a permanent update.
-   *
-   * @return the uncommitted changes that would be committed by calling {@link #commit()}
-   * @throws ValidationException      If the pending changes cannot be applied to the current metadata
-   * @throws IllegalArgumentException If the pending changes are conflicting or invalid
-   */
-  @Override
-  public Snapshot apply();
-  /**
-   * Apply the pending changes and commit.
-   * <p>
-   * Changes are committed by calling the underlying table's commit method.
-   * <p>
-   * Once the commit is successful, the updated table will be refreshed.
-   *
-   * @throws ValidationException   If the update cannot be applied to the current table metadata.
-   * @throws CommitFailedException If the update cannot be committed due to conflicts.
-   */
-  @Override
-  public void commit();
+  CherryPick fromSnapshotId(long wapId);
+  
 }
