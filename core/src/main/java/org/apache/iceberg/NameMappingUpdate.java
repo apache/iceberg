32434acfae517b981eecd4aeb5c42afe2124a968
Sundaram Ananthanarayanan <sananthanarayanan@netflix.com>
2020-02-17 14:46:24 -0800

    Fixing checkstyle issues

--- core/src/main/java/org/apache/iceberg/NameMappingUpdate.java
+++ core/src/main/java/org/apache/iceberg/NameMappingUpdate.java
@@ -1,16 +1,37 @@
-package org.apache.iceberg;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
-import static org.apache.iceberg.SchemaUpdate.findParentField;
+package org.apache.iceberg;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
-import org.apache.iceberg.mapping.*;
-import org.apache.iceberg.types.*;
-
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import org.apache.iceberg.mapping.MappedField;
+import org.apache.iceberg.mapping.MappedFields;
+import org.apache.iceberg.mapping.NameMapping;
+import org.apache.iceberg.mapping.NameMappingParser;
+import org.apache.iceberg.types.Type;
+import org.apache.iceberg.types.TypeUtil;
+import org.apache.iceberg.types.Types;
 
 /**
  * NameMapping evolution API implementation.
@@ -38,6 +59,16 @@ class NameMappingUpdate implements UpdateNameMapping {
     this.updateProperties = updateProperties;
   }
 
+  private void addAliases(int fieldId, Iterable<String> aliases) {
+    adds.compute(fieldId, (dontCare, oldValue) -> {
+      if (oldValue == null) {
+        return aliases;
+      } else {
+        return ImmutableList.<String>builder().addAll(aliases).build();
+      }
+    });
+  }
+
   @Override
   public UpdateNameMapping addAliases(String name, Iterable<String> aliases) {
     Preconditions.checkArgument(schema.findField(name) != null, "Cannot find column: %s", name);
@@ -48,7 +79,7 @@ class NameMappingUpdate implements UpdateNameMapping {
 
   @Override
   public UpdateNameMapping addAliases(String parent, String name, Iterable<String> aliases) {
-    Types.NestedField parentField = findParentField(parent, schema);
+    Types.NestedField parentField = SchemaUpdate.findParentField(parent, schema);
     Preconditions.checkArgument(schema.findField(parent + "." + name) != null,
         "Cannot add alias, name doesn't exists: %s.%s", parent, name);
     addAliases(schema.findField(parent + "." + name).fieldId(), aliases);
@@ -67,16 +98,6 @@ class NameMappingUpdate implements UpdateNameMapping {
     updateProperties
         .set(TableProperties.DEFAULT_NAME_MAPPING, NameMappingParser.toJson(goalState))
         .commit();
-  }
-
-  private void addAliases(int fieldId, Iterable<String> aliases) {
-    adds.compute(fieldId, (dontCare, oldValue) -> {
-      if (oldValue == null) {
-        return aliases;
-      } else {
-        return ImmutableList.<String>builder().addAll(aliases).build();
-      }
-    });
   }
 
   private static class UpdateNameMappingVisitor extends TypeUtil.SchemaVisitor<MappedFields> {
@@ -143,18 +164,18 @@ class NameMappingUpdate implements UpdateNameMapping {
       return null; // no mapping because primitives have no nested fields
     }
 
-    private static Iterable<String> merge(Iterable<String> a, Iterable<String> b) {
-      if (a == null) {
-        return b;
+    private static Iterable<String> merge(Iterable<String> first, Iterable<String> second) {
+      if (first == null) {
+        return second;
       }
 
-      if (b == null) {
-        return a;
+      if (second == null) {
+        return first;
       }
 
       return ImmutableList.<String>builder()
-          .addAll(a)
-          .addAll(b)
+          .addAll(first)
+          .addAll(second)
           .build();
     }
   }
--- core/src/test/java/org/apache/iceberg/TestNameMappingUpdate.java
+++ core/src/test/java/org/apache/iceberg/TestNameMappingUpdate.java
@@ -1,7 +1,23 @@
-package org.apache.iceberg;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
+package org.apache.iceberg;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
@@ -10,7 +26,11 @@ import org.apache.iceberg.mapping.NameMapping;
 import org.apache.iceberg.types.Types;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
 public class TestNameMappingUpdate {
+
   private final UpdateProperties updateProperties = mock(UpdateProperties.class);
   private static final Schema SCHEMA = mkSchema();
 
@@ -29,7 +49,8 @@ public class TestNameMappingUpdate {
             .apply();
 
     int rowIdFieldId = SCHEMA.findField("metadata" + "." + "row_id").fieldId();
-    int ingestionTimestampFieldId = SCHEMA.findField("metadata" + "." + "ingestion_timestamp_ms").fieldId();
+    int ingestionTimestampFieldId = SCHEMA.findField("metadata" + "." + "ingestion_timestamp_ms")
+        .fieldId();
 
     assertEquals(mapping.find(rowIdFieldId).names(), ImmutableSet.of("rowId", "row_id"));
     assertEquals(mapping.find(ingestionTimestampFieldId).names(),
