1d07d8c8a88ef9c9ee56c750c519fc13e455d697
Ratandeep Ratt <rratti@linkedin.com>
2020-03-17 23:32:37 -0700

    Address review comments

--- mr/src/test/java/org/apache/iceberg/mr/TestIcebergInputFormat.java
+++ mr/src/test/java/org/apache/iceberg/mr/TestIcebergInputFormat.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.iceberg.mr;
 
 import java.io.File;
@@ -5,12 +24,13 @@ import java.io.IOException;
 import java.util.List;
 import org.apache.avro.generic.GenericData;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapred.JobConf;
-import org.apache.hadoop.mapred.TaskAttemptContextImpl;
-import org.apache.hadoop.mapred.TaskAttemptID;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.TaskAttemptID;
+import org.apache.hadoop.mapreduce.task.TaskAttemptContextImpl;
 import org.apache.iceberg.DataFile;
 import org.apache.iceberg.DataFiles;
 import org.apache.iceberg.Files;
@@ -36,7 +56,6 @@ public class TestIcebergInputFormat {
   private static final Configuration CONF = new Configuration();
   private static final HadoopTables TABLES = new HadoopTables(CONF);
 
-  private Table table;
   private File tableLocation;
 
   private static final Schema SCHEMA = new Schema(
@@ -77,14 +96,16 @@ public class TestIcebergInputFormat {
 
     table.newAppend().appendFile(dataFile).commit();
 
-    Configuration conf = new Configuration();
-    conf = IcebergInputFormat
-        .updateConf(conf, tableLocation.getAbsolutePath(), TestReadSupport.class)
-        .updatedConf();
-    TaskAttemptContext context = new TaskAttemptContextImpl(new JobConf(conf), new TaskAttemptID());
+    Job job = Job.getInstance(new Configuration());
+    IcebergInputFormat
+        .configure(job)
+        .readFrom(tableLocation.getAbsolutePath());
+
+    TaskAttemptContext context = new TaskAttemptContextImpl(new JobConf(job.getConfiguration()), new TaskAttemptID());
     icebergInputFormat = new IcebergInputFormat<>();
     List<InputSplit> splits = icebergInputFormat.getSplits(context);
-    final RecordReader<Void, GenericData.Record> recordReader = icebergInputFormat.createRecordReader(splits.get(0), context);
+    final RecordReader<Void, GenericData.Record> recordReader =
+        icebergInputFormat.createRecordReader(splits.get(0), context);
     recordReader.initialize(splits.get(0), context);
     while (recordReader.nextKeyValue()) {
       System.out.println(recordReader.getCurrentValue());
@@ -108,13 +129,5 @@ public class TestIcebergInputFormat {
       public <T> void set(int pos, T value) {
       }
     };
-  }
-
-  public static class TestReadSupport implements ReadSupport<GenericData.Record> {
-    @Override
-    public GenericData.Record withPartitionColumns(
-        GenericData.Record row, Schema partitionSchema, PartitionSpec spec, StructLike partitionData) {
-      return row;
-    }
   }
 }
