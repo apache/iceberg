5f5ffbd30f0ae7881225d886a27bdcb87c69078c
Ratandeep Ratt <rratti@linkedin.com>
2020-03-17 23:32:37 -0700

    Address review comments

--- mr/src/main/java/org/apache/iceberg/mr/IcebergInputFormat.java
+++ mr/src/main/java/org/apache/iceberg/mr/IcebergInputFormat.java
@@ -32,21 +32,28 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.InputFormat;
 import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.JobContext;
 import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.iceberg.CombinedScanTask;
 import org.apache.iceberg.DataFile;
 import org.apache.iceberg.FileScanTask;
+import org.apache.iceberg.PartitionField;
 import org.apache.iceberg.PartitionSpec;
 import org.apache.iceberg.Schema;
 import org.apache.iceberg.SchemaParser;
+import org.apache.iceberg.StructLike;
 import org.apache.iceberg.Table;
+import org.apache.iceberg.TableProperties;
 import org.apache.iceberg.TableScan;
 import org.apache.iceberg.avro.Avro;
 import org.apache.iceberg.catalog.Catalog;
 import org.apache.iceberg.catalog.TableIdentifier;
-import org.apache.iceberg.common.DynClasses;
+import org.apache.iceberg.data.GenericRecord;
+import org.apache.iceberg.data.Record;
+import org.apache.iceberg.data.avro.DataReader;
+import org.apache.iceberg.data.parquet.GenericParquetReaders;
 import org.apache.iceberg.exceptions.RuntimeIOException;
 import org.apache.iceberg.expressions.Expression;
 import org.apache.iceberg.hadoop.HadoopInputFile;
@@ -57,6 +64,7 @@ import org.apache.iceberg.io.InputFile;
 import org.apache.iceberg.orc.ORC;
 import org.apache.iceberg.parquet.Parquet;
 import org.apache.iceberg.types.TypeUtil;
+import org.apache.iceberg.types.Types;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -64,16 +72,89 @@ import org.slf4j.LoggerFactory;
 public class IcebergInputFormat<T> extends InputFormat<Void, T> {
   private static final Logger LOG = LoggerFactory.getLogger(IcebergInputFormat.class);
 
+  static final String AS_OF_TIMESTAMP = "iceberg.mr.as.of.time";
+  static final String CASE_SENSITIVE = "iceberg.mr.case.sensitive";
   static final String FILTER_EXPRESSION = "iceberg.mr.filter.expression";
-  static final String TABLE_SCHEMA = "iceberg.mr.table.schema";
-  static final String TABLE_PATH = "iceberg.mr.table.path";
+  static final String IN_MEMORY_DATA_MODEL = "iceberg.mr.in.memory.data.model";
   static final String READ_SCHEMA = "iceberg.mr.read.schema";
-  static final String READ_SUPPORT = "iceberg.mr.read.support";
+  static final String REUSE_CONTAINERS = "iceberg.mr.case.sensitive";
+  static final String SNAPSHOT_ID = "iceberg.mr.snapshot.id";
+  static final String SPLIT_SIZE = "iceberg.mr.split.size";
+  static final String TABLE_PATH = "iceberg.mr.table.path";
+  static final String TABLE_SCHEMA = "iceberg.mr.table.schema";
 
-  private transient Table table;
   private transient List<InputSplit> splits;
 
-  public IcebergInputFormat() {
+  public enum InMemoryDataModel {
+    PIG,
+    HIVE,
+    DEFAULT
+  }
+
+  /**
+   * Configures the {@code Job} to use the {@code IcebergInputFormat} and
+   * returns a helper to add further configuration.
+   *
+   * @param job the {@code Job} to configure
+   */
+  public static ConfigBuilder configure(Job job) {
+    job.setInputFormatClass(IcebergInputFormat.class);
+    return new ConfigBuilder(job.getConfiguration());
+  }
+
+  public static class ConfigBuilder {
+    private final Configuration conf;
+
+    public ConfigBuilder(Configuration conf) {
+      this.conf = conf;
+    }
+
+    public ConfigBuilder readFrom(String path) {
+      conf.set(TABLE_PATH, path);
+      Table table = getTable(conf);
+      conf.set(TABLE_SCHEMA, SchemaParser.toJson(table.schema()));
+      return this;
+    }
+
+    public ConfigBuilder filter(Expression expression) {
+      conf.set(FILTER_EXPRESSION, SerializationUtil.serializeToBase64(expression));
+      return this;
+    }
+
+    public ConfigBuilder project(Schema schema) {
+      conf.set(READ_SCHEMA, SchemaParser.toJson(schema));
+      return this;
+    }
+
+    public ConfigBuilder reuseContainers(boolean reuse) {
+      conf.setBoolean(REUSE_CONTAINERS, reuse);
+      return this;
+    }
+
+    public ConfigBuilder caseSensitive(boolean caseSensitive) {
+      conf.setBoolean(CASE_SENSITIVE, caseSensitive);
+      return this;
+    }
+
+    public ConfigBuilder snapshotId(long snapshotId) {
+      conf.setLong(SNAPSHOT_ID, snapshotId);
+      return this;
+    }
+
+    public ConfigBuilder asOfTime(long asOfTime) {
+      conf.setLong(AS_OF_TIMESTAMP, asOfTime);
+      return this;
+    }
+
+    public ConfigBuilder splitSize(long splitSize) {
+      conf.setLong(SPLIT_SIZE, splitSize);
+      return this;
+    }
+
+    public ConfigBuilder inMemoryDataModel(InMemoryDataModel inMemoryDataModel) {
+      conf.set(IN_MEMORY_DATA_MODEL, inMemoryDataModel.name());
+      return this;
+    }
   }
 
   @Override
@@ -84,19 +165,31 @@ public class IcebergInputFormat<T> extends InputFormat<Void, T> {
     }
 
     Configuration conf = context.getConfiguration();
-    table = getTable(conf);
-    TableScan scan = table.newScan();
-    //TODO add caseSensitive, snapshot id etc..
+    Table table = getTable(conf);
+    TableScan scan = table.newScan()
+                          .caseSensitive(conf.getBoolean(CASE_SENSITIVE, true));
 
+    long snapshotId = conf.getLong(SNAPSHOT_ID, -1);
+    if (snapshotId != -1) {
+      scan = scan.useSnapshot(snapshotId);
+    }
+    long asOfTime = conf.getLong(AS_OF_TIMESTAMP, -1);
+    if (asOfTime != -1) {
+      scan = scan.asOfTime(asOfTime);
+    }
+    long splitSize = conf.getLong(SPLIT_SIZE, -1);
+    if (splitSize != -1) {
+      scan = scan.option(TableProperties.SPLIT_SIZE, String.valueOf(splitSize));
+    }
+    String schemaStr = conf.get(READ_SCHEMA);
+    if (schemaStr != null) {
+      scan.project(SchemaParser.fromJson(schemaStr));
+    }
+
+    // TODO add a filter parser to get rid of Serialization
     Expression filterExpression = SerializationUtil.deserializeFromBase64(conf.get(FILTER_EXPRESSION));
     if (filterExpression != null) {
       scan = scan.filter(filterExpression);
-    }
-
-    final String schemaStr = conf.get(READ_SCHEMA);
-    if (schemaStr != null) {
-      // Not sure if this is having any effect?
-      scan.project(SchemaParser.fromJson(schemaStr));
     }
 
     splits = Lists.newArrayList();
@@ -111,39 +204,7 @@ public class IcebergInputFormat<T> extends InputFormat<Void, T> {
 
   @Override
   public RecordReader<Void, T> createRecordReader(InputSplit split, TaskAttemptContext context) {
-    return new IcebergRecordReader();
-  }
-
-  public static ConfBuilder updateConf(
-      Configuration conf, String path, Class<? extends ReadSupport<?>> readSupportClass) {
-    return new ConfBuilder(conf, path, readSupportClass);
-  }
-
-  public static class ConfBuilder {
-    private final Configuration conf;
-
-    public ConfBuilder(Configuration conf, String path, Class<? extends ReadSupport<?>> readSupportClass) {
-      this.conf = conf;
-      conf.set(TABLE_PATH, path);
-      conf.set(READ_SUPPORT, readSupportClass.getName());
-      Table table = getTable(conf);
-      conf.set(TABLE_SCHEMA, SchemaParser.toJson(table.schema()));
-    }
-
-    public ConfBuilder filterExpression(Expression expression) throws IOException {
-      conf.set(FILTER_EXPRESSION, SerializationUtil.serializeToBase64(expression));
-      return this;
-    }
-
-    public ConfBuilder project(Schema schema) {
-      conf.set(READ_SCHEMA, SchemaParser.toJson(schema));
-      return this;
-    }
-
-    //TODO: other options split-size, snapshotid etc..
-    public Configuration updatedConf() {
-      return conf;
-    }
+    return new IcebergRecordReader<>();
   }
 
   private static final class IcebergRecordReader<T> extends RecordReader<Void, T> {
@@ -153,21 +214,25 @@ public class IcebergInputFormat<T> extends InputFormat<Void, T> {
     private T currentRow;
     private Schema expectedSchema;
     private Schema tableSchema;
-    private ReadSupport<T> readSupport;
+    private InMemoryDataModel inMemoryDataModel;
     private Closeable currentCloseable;
+    private boolean reuseContainers;
+    private boolean caseSensitive;
 
     @Override
-    public void initialize(InputSplit split, TaskAttemptContext context) {
-      Configuration conf = context.getConfiguration();
+    public void initialize(InputSplit split, TaskAttemptContext newContext) {
+      Configuration conf = newContext.getConfiguration();
       CombinedScanTask task = ((IcebergSplit) split).task;
-      this.context = context;
+      this.context = newContext;
       this.tasks = task.files().iterator();
       this.tableSchema = SchemaParser.fromJson(conf.get(TABLE_SCHEMA));
       String readSchemaStr = conf.get(READ_SCHEMA);
       if (readSchemaStr != null) {
         this.expectedSchema = SchemaParser.fromJson(readSchemaStr);
       }
-      this.readSupport = readSupport(conf);
+      this.reuseContainers = conf.getBoolean(REUSE_CONTAINERS, false);
+      this.caseSensitive = conf.getBoolean(CASE_SENSITIVE, true);
+      this.inMemoryDataModel = conf.getEnum(IN_MEMORY_DATA_MODEL, InMemoryDataModel.DEFAULT);
       this.currentIterator = open(tasks.next());
     }
 
@@ -206,19 +271,6 @@ public class IcebergInputFormat<T> extends InputFormat<Void, T> {
       currentCloseable.close();
     }
 
-    private ReadSupport<T> readSupport(Configuration conf) {
-      String readSupportClassName = conf.get(READ_SUPPORT);
-      try {
-        return DynClasses
-            .builder()
-            .impl(readSupportClassName)
-            .<ReadSupport<T>>buildChecked()
-            .newInstance();
-      } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-        throw new RuntimeException(String.format("Unable to instantiate read support %s", readSupportClassName), e);
-      }
-    }
-
     private Iterator<T> open(FileScanTask currentTask) {
       DataFile file = currentTask.file();
       // schema of rows returned by readers
@@ -229,10 +281,10 @@ public class IcebergInputFormat<T> extends InputFormat<Void, T> {
       Schema readSchema = expectedSchema != null ? expectedSchema : tableSchema;
       if (hasJoinedPartitionColumns) {
         readSchema = TypeUtil.selectNot(tableSchema, idColumns);
-        Schema partitionSchema = TypeUtil.select(tableSchema, idColumns);
+        Schema identityPartitionSchema = TypeUtil.select(tableSchema, idColumns);
         return Iterators.transform(
             open(currentTask, readSchema),
-            row -> readSupport.withPartitionColumns(row, partitionSchema, spec, file.partition()));
+            row -> withPartitionColumns(row, identityPartitionSchema, spec, file.partition()));
       } else {
         return open(currentTask, readSchema);
       }
@@ -261,34 +313,103 @@ public class IcebergInputFormat<T> extends InputFormat<Void, T> {
       return iterable.iterator();
     }
 
+    @SuppressWarnings("unchecked")
+    private T withPartitionColumns(T row, Schema identityPartitionSchema, PartitionSpec spec, StructLike partition) {
+      switch (inMemoryDataModel) {
+        case PIG:
+        case HIVE:
+          // TODO implement adding partition columns to records for Pig and Hive
+          throw new UnsupportedOperationException();
+        case DEFAULT:
+          return (T) icebergRecordWithPartitionsColumns((Record) row, identityPartitionSchema, spec, partition);
+      }
+      return row;
+    }
+
+    private static Record icebergRecordWithPartitionsColumns(
+        Record record, Schema identityPartitionSchema, PartitionSpec spec, StructLike partition) {
+      List<Types.NestedField> fields = Lists.newArrayList(record.struct().fields());
+      fields.addAll(identityPartitionSchema.asStruct().fields());
+      GenericRecord row = GenericRecord.create(Types.StructType.of(fields));
+      int size = record.struct().fields().size();
+      for (int i = 0; i < size; i++) {
+        row.set(i, record.get(i));
+      }
+      List<PartitionField> partitionFields = spec.fields();
+      List<Types.NestedField> identityColumns = identityPartitionSchema.columns();
+      for (int i = 0; i < identityColumns.size(); i++) {
+        Types.NestedField identityColumn = identityColumns.get(i);
+
+        for (int j = 0; j < partitionFields.size(); j++) {
+          PartitionField partitionField = partitionFields.get(j);
+          if (identityColumn.fieldId() == partitionField.sourceId() &&
+              "identity".equals(partitionField.transform().toString())) {
+            row.set(size + i, partition.get(j, spec.javaClasses()[i]));
+          } else {
+            row.set(size + i, null);
+          }
+        }
+      }
+      return row;
+    }
+
     private CloseableIterable<T> newAvroIterable(InputFile inputFile, FileScanTask task, Schema readSchema) {
       Avro.ReadBuilder avroReadBuilder = Avro.read(inputFile)
-                                             .createReaderFunc(readSupport.avroReadBiFunction())
-                                             .createReaderFunc(readSupport.avroReadFunction())
                                              .project(readSchema)
                                              .split(task.start(), task.length());
-      //.reuseContainers(reuseContainers);
+
+      if (reuseContainers) {
+        avroReadBuilder.reuseContainers();
+      }
+
+      switch (inMemoryDataModel) {
+        case PIG:
+        case HIVE:
+          //TODO implement value readers for Pig and Hive
+          throw new UnsupportedOperationException();
+        case DEFAULT:
+          avroReadBuilder.createReaderFunc(DataReader::create);
+      }
       return avroReadBuilder.build();
     }
 
-
     private CloseableIterable<T> newParquetIterable(InputFile inputFile, FileScanTask task, Schema readSchema) {
       Parquet.ReadBuilder parquetReadBuilder = Parquet.read(inputFile)
-                                                      .createBatchedReaderFunc(readSupport.parquetBatchReadFunction())
-                                                      .createReaderFunc(readSupport.parquetReadFunction())
-                                                      .createBatchedReaderFunc(readSupport.parquetBatchReadFunction())
                                                       .project(readSchema)
-                                                      //.caseSensitive(caseSensitive)
+                                                      .filter(task.residual())
+                                                      .caseSensitive(caseSensitive)
                                                       .split(task.start(), task.length());
+      if (reuseContainers) {
+        parquetReadBuilder.reuseContainers();
+      }
+
+      switch (inMemoryDataModel) {
+        case PIG:
+        case HIVE:
+          //TODO implement value readers for Pig and Hive
+          throw new UnsupportedOperationException();
+        case DEFAULT:
+          parquetReadBuilder.createReaderFunc(
+              fileSchema -> GenericParquetReaders.buildReader(readSchema, fileSchema));
+      }
       return parquetReadBuilder.build();
     }
 
     private CloseableIterable<T> newOrcIterable(InputFile inputFile, FileScanTask task, Schema readSchema) {
       ORC.ReadBuilder orcReadBuilder = ORC.read(inputFile)
-                                          .createReaderFunc(readSupport.orcReadFunction())
                                           .schema(readSchema)
-                                          //.caseSensitive(caseSensitive)
+                                          .caseSensitive(caseSensitive)
                                           .split(task.start(), task.length());
+      // ORC does not support reuse containers yet
+      switch (inMemoryDataModel) {
+        case PIG:
+        case HIVE:
+          //TODO implement value readers for Pig and Hive
+          throw new UnsupportedOperationException();
+        case DEFAULT:
+          //TODO: We do not have support for Iceberg generics for ORC
+          throw new UnsupportedOperationException();
+      }
 
       return orcReadBuilder.build();
     }
@@ -308,13 +429,10 @@ public class IcebergInputFormat<T> extends InputFormat<Void, T> {
 
   private static class IcebergSplit extends InputSplit implements Writable {
     private static final String[] ANYWHERE = new String[]{"*"};
-    CombinedScanTask task;
+    private CombinedScanTask task;
 
     IcebergSplit(CombinedScanTask task) {
       this.task = task;
-    }
-
-    public IcebergSplit() {
     }
 
     @Override
--- mr/src/main/java/org/apache/iceberg/mr/SerializationUtil.java
+++ mr/src/main/java/org/apache/iceberg/mr/SerializationUtil.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.iceberg.mr;
 
 import java.io.ByteArrayInputStream;
@@ -17,12 +36,14 @@ public class SerializationUtil {
   private SerializationUtil() {
   }
 
-  public static byte[] serializeToBytes(Object obj) throws IOException {
+  public static byte[] serializeToBytes(Object obj) {
     try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
         GZIPOutputStream gos = new GZIPOutputStream(baos);
         ObjectOutputStream oos = new ObjectOutputStream(gos)) {
       oos.writeObject(obj);
       return baos.toByteArray();
+    } catch (IOException e) {
+      throw new RuntimeIOException("Failed to serialize object", e);
     }
   }
 
@@ -37,18 +58,17 @@ public class SerializationUtil {
         ObjectInputStream ois = new ObjectInputStream(gis)) {
       return (T) ois.readObject();
     } catch (IOException e) {
-      throw new RuntimeIOException(e);
+      throw new RuntimeIOException("Failed to deserialize object", e);
     } catch (ClassNotFoundException e) {
       throw new RuntimeException("Could not read object ", e);
     }
   }
 
-  public static String serializeToBase64(Object obj) throws IOException {
+  public static String serializeToBase64(Object obj) {
     byte[] bytes = serializeToBytes(obj);
     return new String(Base64.getMimeEncoder().encode(bytes), StandardCharsets.UTF_8);
   }
 
-  @SuppressWarnings("unchecked")
   public static <T> T deserializeFromBase64(String base64) {
     if (base64 == null) {
       return null;
