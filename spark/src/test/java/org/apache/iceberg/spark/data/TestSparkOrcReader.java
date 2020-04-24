69caf5c9ddcf0ac2b5cef407ce729af054939693
Shardul Mahadik <smahadik@linkedin.com>
2020-04-23 18:14:23 -0700

    Move test for repeated values into a separate test

--- spark/src/test/java/org/apache/iceberg/spark/data/TestSparkOrcReader.java
+++ spark/src/test/java/org/apache/iceberg/spark/data/TestSparkOrcReader.java
@@ -23,15 +23,19 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 import org.apache.iceberg.Files;
 import org.apache.iceberg.Schema;
 import org.apache.iceberg.io.CloseableIterable;
 import org.apache.iceberg.io.FileAppender;
 import org.apache.iceberg.orc.ORC;
+import org.apache.iceberg.types.Types;
 import org.apache.spark.sql.catalyst.InternalRow;
 import org.junit.Assert;
+import org.junit.Test;
 
 import static org.apache.iceberg.spark.data.TestHelpers.assertEquals;
+import static org.apache.iceberg.types.Types.NestedField.required;
 
 public class TestSparkOrcReader extends AvroDataTest {
   @Override
@@ -39,6 +43,22 @@ public class TestSparkOrcReader extends AvroDataTest {
     final Iterable<InternalRow> expected = RandomData
         .generateSpark(schema, 100, 0L);
 
+    writeAndValidateRecords(schema, expected);
+  }
+
+  @Test
+  public void writeAndValidateRepeatingRecords() throws IOException {
+    Schema structSchema = new Schema(
+        required(100, "id", Types.LongType.get()),
+        required(101, "data", Types.StringType.get())
+    );
+    List<InternalRow> expectedRepeating = Collections.nCopies(100,
+        RandomData.generateSpark(structSchema, 1, 0L).iterator().next());
+
+    writeAndValidateRecords(structSchema, expectedRepeating);
+  }
+
+  private void writeAndValidateRecords(Schema schema, Iterable<InternalRow> expected) throws IOException {
     final File testFile = temp.newFile();
     Assert.assertTrue("Delete should succeed", testFile.delete());
 
@@ -55,30 +75,6 @@ public class TestSparkOrcReader extends AvroDataTest {
         .build()) {
       final Iterator<InternalRow> actualRows = reader.iterator();
       final Iterator<InternalRow> expectedRows = expected.iterator();
-      while (expectedRows.hasNext()) {
-        Assert.assertTrue("Should have expected number of rows", actualRows.hasNext());
-        assertEquals(schema, expectedRows.next(), actualRows.next());
-      }
-      Assert.assertFalse("Should not have extra rows", actualRows.hasNext());
-    }
-
-    // Also write and validate where all rows are the same (repeating)
-    final Iterable<InternalRow> expectedRepeating = Collections.nCopies(100, expected.iterator().next());
-    Assert.assertTrue("Delete should succeed", testFile.delete());
-
-    try (FileAppender<InternalRow> writer = ORC.write(Files.localOutput(testFile))
-        .createWriterFunc(SparkOrcWriter::new)
-        .schema(schema)
-        .build()) {
-      writer.addAll(expectedRepeating);
-    }
-
-    try (CloseableIterable<InternalRow> reader = ORC.read(Files.localInput(testFile))
-        .project(schema)
-        .createReaderFunc(SparkOrcReader::new)
-        .build()) {
-      final Iterator<InternalRow> actualRows = reader.iterator();
-      final Iterator<InternalRow> expectedRows = expectedRepeating.iterator();
       while (expectedRows.hasNext()) {
         Assert.assertTrue("Should have expected number of rows", actualRows.hasNext());
         assertEquals(schema, expectedRows.next(), actualRows.next());
