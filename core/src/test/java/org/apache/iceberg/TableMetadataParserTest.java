53c10a2df905be74c2d950989dcfef0147fc2480
Ryan Blue <blue@apache.org>
2020-04-29 16:34:21 -0700

    Add back deprecated methods used by Presto (#986)

--- core/src/test/java/org/apache/iceberg/TableMetadataParserTest.java
+++ core/src/test/java/org/apache/iceberg/TableMetadataParserTest.java
@@ -30,6 +30,7 @@ import java.util.Map;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.ZipException;
 import org.apache.iceberg.TableMetadataParser.Codec;
+import org.apache.iceberg.io.FileIO;
 import org.apache.iceberg.io.OutputFile;
 import org.apache.iceberg.types.Types.BooleanType;
 import org.junit.After;
@@ -74,7 +75,7 @@ public class TableMetadataParserTest {
     TableMetadata metadata = newTableMetadata(SCHEMA, unpartitioned(), location, properties);
     TableMetadataParser.write(metadata, outputFile);
     Assert.assertEquals(codec == Codec.GZIP, isCompressed(fileName));
-    TableMetadata actualMetadata = TableMetadataParser.read(null, Files.localInput(new File(fileName)));
+    TableMetadata actualMetadata = TableMetadataParser.read((FileIO) null, Files.localInput(new File(fileName)));
     verifyMetadata(metadata, actualMetadata);
   }
 
