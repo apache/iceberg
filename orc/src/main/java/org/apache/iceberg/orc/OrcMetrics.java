3ecb21c020d626b71992679a5ad5429af199b7ad
Xingyuan Lin <linxingyuan1102@gmail.com>
2020-06-12 18:50:00 -0700

    Reduce compute complexity in OrcMetrics::findColumnsInContainers
    Reduce compute complexity from O(N*R) to O(N), where N is the number of
    columns (including all child columns in LIST, MAP and STRUCT) in an ORC
    schema, R is the average number of LIST-or-MAP ancestors per node.

--- orc/src/main/java/org/apache/iceberg/orc/OrcMetrics.java
+++ orc/src/main/java/org/apache/iceberg/orc/OrcMetrics.java
@@ -23,10 +23,9 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.sql.Timestamp;
 import java.time.temporal.ChronoUnit;
-import java.util.List;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Queue;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import org.apache.hadoop.conf.Configuration;
@@ -35,10 +34,7 @@ import org.apache.iceberg.Schema;
 import org.apache.iceberg.exceptions.RuntimeIOException;
 import org.apache.iceberg.hadoop.HadoopInputFile;
 import org.apache.iceberg.io.InputFile;
-import org.apache.iceberg.relocated.com.google.common.collect.ImmutableList;
-import org.apache.iceberg.relocated.com.google.common.collect.ImmutableSet;
 import org.apache.iceberg.relocated.com.google.common.collect.Maps;
-import org.apache.iceberg.relocated.com.google.common.collect.Queues;
 import org.apache.iceberg.relocated.com.google.common.collect.Sets;
 import org.apache.iceberg.types.Conversions;
 import org.apache.iceberg.types.Type;
@@ -86,7 +82,7 @@ public class OrcMetrics {
   private static Metrics buildOrcMetrics(final long numOfRows, final TypeDescription orcSchema,
                                          final ColumnStatistics[] colStats) {
     final Schema schema = ORCSchemaUtil.convert(orcSchema);
-    final Set<TypeDescription> columnsInContainers = findColumnsInContainers(schema, orcSchema);
+    final Set<TypeDescription> columnsInContainers = findColumnsInContainers(orcSchema);
     Map<Integer, Long> columnSizes = Maps.newHashMapWithExpectedSize(colStats.length);
     Map<Integer, Long> valueCounts = Maps.newHashMapWithExpectedSize(colStats.length);
     Map<Integer, Long> nullCounts = Maps.newHashMapWithExpectedSize(colStats.length);
@@ -214,64 +210,33 @@ public class OrcMetrics {
     return Optional.ofNullable(Conversions.toByteBuffer(column.type(), max));
   }
 
-  private static Set<TypeDescription> findColumnsInContainers(Schema schema,
-                                                              TypeDescription orcSchema) {
-    ColumnsInContainersVisitor visitor = new ColumnsInContainersVisitor();
-    OrcSchemaWithTypeVisitor.visit(schema, orcSchema, visitor);
-    return visitor.getColumnsInContainers();
+  private static Set<TypeDescription> findColumnsInContainers(TypeDescription orcSchema) {
+    Set<TypeDescription> columnsInContainers = Sets.newHashSet();
+    findColumnsInContainers(orcSchema, columnsInContainers, false);
+    return Collections.unmodifiableSet(columnsInContainers);
   }
 
-  private static class ColumnsInContainersVisitor extends OrcSchemaWithTypeVisitor<TypeDescription> {
-
-    private final Set<TypeDescription> columnsInContainers;
-
-    private ColumnsInContainersVisitor() {
-      columnsInContainers = Sets.newHashSet();
+  private static void findColumnsInContainers(TypeDescription column,
+                                              Set<TypeDescription> columnsInContainers,
+                                              boolean isInContainers) {
+    if (isInContainers) {
+      columnsInContainers.add(column);
     }
-
-    public Set<TypeDescription> getColumnsInContainers() {
-      return columnsInContainers;
-    }
-
-    private Set<TypeDescription> flatten(TypeDescription rootType) {
-      if (rootType == null) {
-        return ImmutableSet.of();
-      }
-
-      final Set<TypeDescription> flatTypes = Sets.newHashSetWithExpectedSize(rootType.getMaximumId());
-      final Queue<TypeDescription> queue = Queues.newLinkedBlockingQueue();
-      queue.add(rootType);
-      while (!queue.isEmpty()) {
-        TypeDescription type = queue.remove();
-        flatTypes.add(type);
-        queue.addAll(Optional.ofNullable(type.getChildren()).orElse(ImmutableList.of()));
-      }
-      return flatTypes;
-    }
-
-    @Override
-    public TypeDescription record(Types.StructType iStruct, TypeDescription record,
-                                  List<String> names, List<TypeDescription> fields) {
-      return record;
-    }
-
-    @Override
-    public TypeDescription list(Types.ListType iList, TypeDescription array, TypeDescription element) {
-      columnsInContainers.addAll(flatten(element));
-      return array;
-    }
-
-    @Override
-    public TypeDescription map(Types.MapType iMap, TypeDescription map,
-                    TypeDescription key, TypeDescription value) {
-      columnsInContainers.addAll(flatten(key));
-      columnsInContainers.addAll(flatten(value));
-      return map;
-    }
-
-    @Override
-    public TypeDescription primitive(Type.PrimitiveType iPrimitive, TypeDescription primitive) {
-      return primitive;
+    switch (column.getCategory()) {
+      case LIST:
+      case MAP:
+        for (TypeDescription child : column.getChildren()) {
+          findColumnsInContainers(child, columnsInContainers, true);
+        }
+        return;
+      case STRUCT:
+        for (TypeDescription child : column.getChildren()) {
+          findColumnsInContainers(child, columnsInContainers, isInContainers);
+        }
+        return;
+      case UNION:
+        throw new UnsupportedOperationException("Cannot handle " + column);
+      default:
     }
   }
 }
