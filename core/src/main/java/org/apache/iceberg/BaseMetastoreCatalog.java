02ff06f3429b08787571eda9793de64ea147956e
Ryan Blue <blue@apache.org>
2020-06-24 13:10:56 -0700

    Minor updates to Hive table behavior.

--- core/src/main/java/org/apache/iceberg/BaseMetastoreCatalog.java
+++ core/src/main/java/org/apache/iceberg/BaseMetastoreCatalog.java
@@ -282,7 +282,7 @@ public abstract class BaseMetastoreCatalog implements Catalog {
         });
   }
 
-  private static String fullTableName(String catalogName, TableIdentifier identifier) {
+  protected static String fullTableName(String catalogName, TableIdentifier identifier) {
     StringBuilder sb = new StringBuilder();
 
     if (catalogName.contains("/") || catalogName.contains(":")) {
