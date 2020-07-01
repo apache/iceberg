396f23ba5dfa498892d6c1e712934893f03ce51b
Ryan Blue <blue@apache.org>
2020-07-01 16:02:13 -0700

    Make SparkCatalogTestBase abstract to avoid running it as a suite.

--- spark3/src/test/java/org/apache/iceberg/spark/SparkCatalogTestBase.java
+++ spark3/src/test/java/org/apache/iceberg/spark/SparkCatalogTestBase.java
@@ -35,7 +35,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 @RunWith(Parameterized.class)
-public class SparkCatalogTestBase extends SparkTestBase {
+public abstract class SparkCatalogTestBase extends SparkTestBase {
   private static File warehouse = null;
 
   @BeforeClass
