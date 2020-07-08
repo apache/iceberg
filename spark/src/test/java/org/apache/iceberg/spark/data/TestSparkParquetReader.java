cea839f58b8b0e3d73f415b2770913e1d0955f31
Gustavo Torres <gustavoatt@gmail.com>
2020-07-08 12:07:11 -0700

    Parquet int96 timestamp spark read tests

--- spark/src/test/java/org/apache/iceberg/spark/data/TestSparkParquetReader.java
+++ spark/src/test/java/org/apache/iceberg/spark/data/TestSparkParquetReader.java
@@ -21,21 +21,32 @@ package org.apache.iceberg.spark.data;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.time.Instant;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 import org.apache.avro.generic.GenericData;
 import org.apache.iceberg.Files;
 import org.apache.iceberg.Schema;
 import org.apache.iceberg.io.CloseableIterable;
 import org.apache.iceberg.io.FileAppender;
+import org.apache.iceberg.io.InputFile;
 import org.apache.iceberg.parquet.Parquet;
+import org.apache.iceberg.relocated.com.google.common.collect.ImmutableList;
+import org.apache.iceberg.relocated.com.google.common.collect.Lists;
 import org.apache.iceberg.types.TypeUtil;
 import org.apache.iceberg.types.Types;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.catalyst.InternalRow;
 import org.junit.Assert;
 import org.junit.Assume;
+import org.junit.Test;
 
 import static org.apache.iceberg.spark.data.TestHelpers.assertEqualsUnsafe;
+import static org.apache.iceberg.types.Types.NestedField.optional;
 
 public class TestSparkParquetReader extends AvroDataTest {
   @Override
@@ -66,5 +77,50 @@ public class TestSparkParquetReader extends AvroDataTest {
       }
       Assert.assertFalse("Should not have extra rows", rows.hasNext());
     }
+  }
+
+  protected List<InternalRow> rowsFromFile(InputFile inputFile, Schema schema) throws IOException {
+    try (CloseableIterable<InternalRow> reader =
+        Parquet.read(inputFile)
+            .project(schema)
+            .createReaderFunc(type -> SparkParquetReaders.buildReader(schema, type))
+            .build()) {
+      return Lists.newArrayList(reader);
+    }
+  }
+
+  @Test
+  public void testInt96TimestampProducedBySparkIsReadCorrectly() throws IOException {
+    final SparkSession spark =
+        SparkSession.builder()
+            .master("local[2]")
+            .config("spark.sql.parquet.int96AsTimestamp", "false")
+            .getOrCreate();
+
+    final String parquetPath = temp.getRoot().getAbsolutePath() + "/parquet_int96";
+    final java.sql.Timestamp ts = java.sql.Timestamp.valueOf("2014-01-01 23:00:01");
+    spark.createDataset(ImmutableList.of(ts), Encoders.TIMESTAMP()).write().parquet(parquetPath);
+    spark.stop();
+
+    // Get the single parquet file produced by spark.
+    List<Path> parquetOutputs =
+        java.nio.file.Files.find(
+            java.nio.file.Paths.get(parquetPath),
+            1,
+            (path, basicFileAttributes) -> path.toString().endsWith(".parquet"))
+        .collect(Collectors.toList());
+    Assert.assertEquals(1, parquetOutputs.size());
+
+    List<InternalRow> rows =
+        rowsFromFile(
+            Files.localInput(parquetOutputs.get(0).toFile()),
+            new Schema(optional(1, "timestamp", Types.TimestampType.withoutZone())));
+    Assert.assertEquals(1, rows.size());
+    Assert.assertEquals(1, rows.get(0).numFields());
+
+    // Spark represents Timestamps as epoch micros and are stored as longs.
+    Assert.assertEquals(
+        ts.toInstant(),
+        Instant.ofEpochMilli(TimeUnit.MICROSECONDS.toMillis(rows.get(0).getLong(0))));
   }
 }
