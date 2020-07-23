ebcfba9ad91e6826004b99341897d1f2ccf5247e
Junjie Chen <chenjunjiedada@gmail.com>
2020-07-24 01:14:02 +0800

    Update Flink Parquet reader and writer to use schema visitor

--- flink/src/test/java/org/apache/iceberg/flink/data/RandomData.java
+++ flink/src/test/java/org/apache/iceberg/flink/data/RandomData.java
@@ -19,15 +19,27 @@
 
 package org.apache.iceberg.flink.data;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Random;
+import java.util.Set;
 import java.util.function.Supplier;
+import org.apache.flink.table.data.DecimalData;
+import org.apache.flink.table.data.GenericArrayData;
+import org.apache.flink.table.data.GenericMapData;
+import org.apache.flink.table.data.GenericRowData;
+import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.data.StringData;
+import org.apache.flink.table.data.TimestampData;
 import org.apache.flink.types.Row;
 import org.apache.iceberg.Schema;
 import org.apache.iceberg.data.RandomGenericData;
 import org.apache.iceberg.relocated.com.google.common.collect.Lists;
+import org.apache.iceberg.relocated.com.google.common.collect.Sets;
 import org.apache.iceberg.types.Type;
 import org.apache.iceberg.types.TypeUtil;
 import org.apache.iceberg.types.Types;
@@ -64,7 +76,9 @@ public class RandomData {
               required(23, "koala", Types.IntegerType.get()),
               required(24, "couch rope", Types.IntegerType.get())
           ))),
-      optional(2, "slide", Types.StringType.get())
+      optional(2, "slide", Types.StringType.get()),
+      optional(25, "binary", Types.BinaryType.get()),
+      optional(26, "decimal", Types.DecimalType.of(10, 2))
   );
 
   private static Iterable<Row> generateData(Schema schema, int numRecords, Supplier<RandomRowGenerator> supplier) {
@@ -88,20 +102,151 @@ public class RandomData {
     };
   }
 
+  private static Iterable<RowData> generateRowData(Schema schema, int numRecords,
+                                                   Supplier<RandomRowDataGenerator> supplier) {
+    return () -> new Iterator<RowData>() {
+      private final RandomRowDataGenerator generator = supplier.get();
+      private int count = 0;
+
+      @Override
+      public boolean hasNext() {
+        return count < numRecords;
+      }
+
+      @Override
+      public RowData next() {
+        if (!hasNext()) {
+          throw new NoSuchElementException();
+        }
+        ++count;
+        return (RowData) TypeUtil.visit(schema, generator);
+      }
+    };
+  }
+
+  public static Iterable<RowData> generateRowData(Schema schema, int numRecords, long seed) {
+    return generateRowData(schema, numRecords, () -> new RandomRowDataGenerator(seed));
+  }
+
   public static Iterable<Row> generate(Schema schema, int numRecords, long seed) {
     return generateData(schema, numRecords, () -> new RandomRowGenerator(seed));
   }
 
-  public static Iterable<Row> generateFallbackData(Schema schema, int numRecords, long seed, long numDictRows) {
-    return generateData(schema, numRecords, () -> new FallbackGenerator(seed, numDictRows));
+  public static Iterable<RowData> generateFallbackData(Schema schema, int numRecords, long seed, long numDictRows) {
+    return generateRowData(schema, numRecords, () -> new FallbackGenerator(seed, numDictRows));
   }
 
-  public static Iterable<Row> generateDictionaryEncodableData(Schema schema, int numRecords, long seed) {
-    return generateData(schema, numRecords, () -> new DictionaryEncodedGenerator(seed));
+  public static Iterable<RowData> generateDictionaryEncodableData(Schema schema, int numRecords, long seed) {
+    return generateRowData(schema, numRecords, () -> new DictionaryEncodedGenerator(seed));
+  }
+
+  private static class RandomRowDataGenerator extends TypeUtil.CustomOrderSchemaVisitor<Object> {
+    protected final Random random;
+    private static final int MAX_ENTRIES = 20;
+
+    RandomRowDataGenerator(long seed) {
+      this.random = new Random(seed);
+    }
+
+    protected int getMaxEntries() {
+      return MAX_ENTRIES;
+    }
+
+    @Override
+    public RowData schema(Schema schema, Supplier<Object> structResult) {
+      return (RowData) structResult.get();
+    }
+
+    @Override
+    public RowData struct(Types.StructType struct, Iterable<Object> fieldResults) {
+      GenericRowData row = new GenericRowData(struct.fields().size());
+
+      List<Object> values = Lists.newArrayList(fieldResults);
+      for (int i = 0; i < values.size(); i += 1) {
+        row.setField(i, values.get(i));
+      }
+
+      return row;
+    }
+
+    @Override
+    public Object field(Types.NestedField field, Supplier<Object> fieldResult) {
+      // return null 5% of the time when the value is optional
+      if (field.isOptional() && random.nextInt(20) == 1) {
+        return null;
+      }
+      return fieldResult.get();
+    }
+
+    @Override
+    public Object list(Types.ListType list, Supplier<Object> elementResult) {
+      int numElements = random.nextInt(20);
+      Object[] arr = new Object[numElements];
+      GenericArrayData result = new GenericArrayData(arr);
+
+      for (int i = 0; i < numElements; i += 1) {
+        // return null 5% of the time when the value is optional
+        if (list.isElementOptional() && random.nextInt(20) == 1) {
+          arr[i] = null;
+        } else {
+          arr[i] = elementResult.get();
+        }
+      }
+
+      return result;
+    }
+
+    @Override
+    public Object map(Types.MapType map, Supplier<Object> keyResult, Supplier<Object> valueResult) {
+      int numEntries = random.nextInt(getMaxEntries());
+
+      Object[] keysArr = new Object[numEntries];
+      Map<Object, Object> javaMap = new HashMap<>();
+
+      Set<Object> keySet = Sets.newHashSet();
+      for (int i = 0; i < numEntries; i += 1) {
+        Object key = keyResult.get();
+        // ensure no collisions
+        while (keySet.contains(key)) {
+          key = keyResult.get();
+        }
+
+        keySet.add(key);
+        keysArr[i] = key;
+
+        if (map.isValueOptional() && random.nextInt(20) == 1) {
+          javaMap.put(keysArr[i], null);
+        } else {
+          javaMap.put(keysArr[i], valueResult.get());
+        }
+      }
+
+      return new GenericMapData(javaMap);
+    }
+
+    @Override
+    public Object primitive(Type.PrimitiveType primitive) {
+      Object obj = randomValue(primitive, random);
+      switch (primitive.typeId()) {
+        case STRING:
+          return StringData.fromString((String) obj);
+        case DECIMAL:
+          return DecimalData.fromBigDecimal((BigDecimal) obj,
+              ((BigDecimal) obj).precision(),
+              ((BigDecimal) obj).scale());
+        case TIMESTAMP:
+          return TimestampData.fromEpochMillis((Long) obj);
+        default:
+          return obj;
+      }
+    }
+
+    protected Object randomValue(Type.PrimitiveType primitive, Random rand) {
+      return RandomUtil.generatePrimitive(primitive, random);
+    }
   }
 
   private static class RandomRowGenerator extends RandomGenericData.RandomDataGenerator<Row> {
-
     RandomRowGenerator(long seed) {
       super(seed);
     }
@@ -124,7 +269,7 @@ public class RandomData {
     }
   }
 
-  private static class DictionaryEncodedGenerator extends RandomRowGenerator {
+  private static class DictionaryEncodedGenerator extends RandomRowDataGenerator {
     DictionaryEncodedGenerator(long seed) {
       super(seed);
     }
@@ -144,7 +289,7 @@ public class RandomData {
     }
   }
 
-  private static class FallbackGenerator extends RandomRowGenerator {
+  private static class FallbackGenerator extends RandomRowDataGenerator {
     private final long dictionaryEncodedRows;
     private long rowCount = 0;
 
--- flink/src/test/java/org/apache/iceberg/flink/data/TestFlinkParquetReaderWriter.java
+++ flink/src/test/java/org/apache/iceberg/flink/data/TestFlinkParquetReaderWriter.java
@@ -21,13 +21,19 @@ package org.apache.iceberg.flink.data;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
-import org.apache.flink.types.Row;
+import java.util.List;
+import org.apache.flink.table.data.ArrayData;
+import org.apache.flink.table.data.RowData;
 import org.apache.iceberg.Files;
 import org.apache.iceberg.Schema;
+import org.apache.iceberg.flink.FlinkSchemaUtil;
 import org.apache.iceberg.io.CloseableIterable;
 import org.apache.iceberg.io.FileAppender;
 import org.apache.iceberg.parquet.Parquet;
+import org.apache.iceberg.types.Type;
+import org.apache.iceberg.types.Types;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
@@ -41,34 +47,193 @@ public class TestFlinkParquetReaderWriter {
   @Rule
   public TemporaryFolder temp = new TemporaryFolder();
 
-  private void testCorrectness(Schema schema, int numRecords, Iterable<Row> iterable) throws IOException {
+  private void testCorrectness(Schema schema, int numRecords, Iterable<RowData> iterable) throws IOException {
     File testFile = temp.newFile();
     Assert.assertTrue("Delete should succeed", testFile.delete());
 
-    try (FileAppender<Row> writer = Parquet.write(Files.localOutput(testFile))
+    try (FileAppender<RowData> writer = Parquet.write(Files.localOutput(testFile))
         .schema(schema)
-        .createWriterFunc(FlinkParquetWriters::buildWriter)
+        .createWriterFunc(msgType -> FlinkParquetWriters.buildWriter(FlinkSchemaUtil.convert(schema), msgType))
         .build()) {
       writer.addAll(iterable);
     }
 
-    try (CloseableIterable<Row> reader = Parquet.read(Files.localInput(testFile))
+    try (CloseableIterable<RowData> reader = Parquet.read(Files.localInput(testFile))
         .project(schema)
         .createReaderFunc(type -> FlinkParquetReaders.buildReader(schema, type))
         .build()) {
-      Iterator<Row> expected = iterable.iterator();
-      Iterator<Row> rows = reader.iterator();
+      Iterator<RowData> expected = iterable.iterator();
+      Iterator<RowData> rows = reader.iterator();
       for (int i = 0; i < numRecords; i += 1) {
         Assert.assertTrue("Should have expected number of rows", rows.hasNext());
-        Assert.assertEquals(expected.next(), rows.next());
+        assertRowData(schema.asStruct(), expected.next(), rows.next());
       }
       Assert.assertFalse("Should not have extra rows", rows.hasNext());
     }
   }
 
+  private void assertRowData(Type type, RowData expected, RowData actual) {
+    List<Type> types = new ArrayList<>();
+    for (Types.NestedField field : type.asStructType().fields()) {
+      types.add(field.type());
+    }
+
+    for (int i = 0; i < types.size(); i += 1) {
+      if (expected.isNullAt(i)) {
+        Assert.assertEquals(expected.isNullAt(i), actual.isNullAt(i));
+        continue;
+      }
+      switch (types.get(i).typeId()) {
+        case BOOLEAN:
+          Assert.assertEquals("boolean value should be equal", expected.getBoolean(i), actual.getBoolean(i));
+          break;
+        case INTEGER:
+          Assert.assertEquals("int value should be equal", expected.getInt(i), actual.getInt(i));
+          break;
+        case LONG:
+          Assert.assertEquals("long value should be equal", expected.getLong(i), actual.getLong(i));
+          break;
+        case FLOAT:
+          Assert.assertEquals("float value should be equal", Float.valueOf(expected.getFloat(i)),
+              Float.valueOf(actual.getFloat(i)));
+          break;
+        case DOUBLE:
+          Assert.assertEquals("double should be equal", Double.valueOf(expected.getDouble(i)),
+              Double.valueOf(actual.getDouble(i)));
+          break;
+        case DATE:
+          Assert.assertEquals("date should be equal", expected.getInt(i), expected.getInt(i));
+          break;
+        case TIME:
+          Assert.assertEquals("time should be equal", expected.getInt(i), expected.getInt(i));
+          break;
+        case TIMESTAMP:
+          Assert.assertEquals("timestamp should be equal", expected.getTimestamp(i, 6),
+              actual.getTimestamp(i, 6));
+          break;
+        case UUID:
+        case FIXED:
+        case BINARY:
+          Assert.assertArrayEquals("binary should be equal", expected.getBinary(i), actual.getBinary(i));
+          break;
+        case DECIMAL:
+          Types.DecimalType decimal = (Types.DecimalType) types.get(i);
+          int precision = decimal.precision();
+          int scale = decimal.scale();
+          Assert.assertEquals("uuid should be equal",
+              expected.getDecimal(i, precision, scale),
+              actual.getDecimal(i, precision, scale));
+          break;
+        case LIST:
+          ArrayData arrayData1 = expected.getArray(i);
+          ArrayData arrayData2 = actual.getArray(i);
+          Assert.assertEquals("array length should be equal", arrayData1.size(), arrayData2.size());
+          for (int j = 0; j < arrayData1.size(); j += 1) {
+            assertArrayValues(types.get(i).asListType().elementType(), arrayData1, arrayData2);
+          }
+          break;
+        case MAP:
+          ArrayData keyArrayData1 = expected.getMap(i).keyArray();
+          ArrayData valueArrayData1 = expected.getMap(i).valueArray();
+          ArrayData keyArrayData2 = actual.getMap(i).keyArray();
+          ArrayData valueArrayData2 = actual.getMap(i).valueArray();
+          Type keyType = types.get(i).asMapType().keyType();
+          Type valueType = types.get(i).asMapType().valueType();
+
+          Assert.assertEquals("map size should be equal", expected.getMap(i).size(), actual.getMap(i).size());
+
+          for (int j = 0; j < keyArrayData1.size(); j += 1) {
+            assertArrayValues(keyType, keyArrayData1, keyArrayData2);
+            assertArrayValues(valueType, valueArrayData1, valueArrayData2);
+          }
+          break;
+        case STRUCT:
+          int numFields = types.get(i).asStructType().fields().size();
+          assertRowData(types.get(i).asStructType(), expected.getRow(i, numFields), actual.getRow(i, numFields));
+          break;
+      }
+    }
+  }
+
+  private void assertArrayValues(Type type, ArrayData expected, ArrayData actual) {
+    for (int i = 0; i < expected.size(); i += 1) {
+      if (expected.isNullAt(i)) {
+        Assert.assertEquals(expected.isNullAt(i), actual.isNullAt(i));
+        continue;
+      }
+      switch (type.typeId()) {
+        case BOOLEAN:
+          Assert.assertEquals("boolean value should be equal", expected.getBoolean(i), actual.getBoolean(i));
+          break;
+        case INTEGER:
+          Assert.assertEquals("int value should be equal", expected.getInt(i), actual.getInt(i));
+          break;
+        case LONG:
+          Assert.assertEquals("long value should be equal", expected.getLong(i), actual.getLong(i));
+          break;
+        case FLOAT:
+          Assert.assertEquals("float value should be equal", Float.valueOf(expected.getFloat(i)),
+              Float.valueOf(actual.getFloat(i)));
+          break;
+        case DOUBLE:
+          Assert.assertEquals("double should be equal", Double.valueOf(expected.getDouble(i)),
+              Double.valueOf(actual.getDouble(i)));
+          break;
+        case DATE:
+          Assert.assertEquals("date should be equal", expected.getInt(i), expected.getInt(i));
+          break;
+        case TIME:
+          Assert.assertEquals("time should be equal", expected.getInt(i), expected.getInt(i));
+          break;
+        case TIMESTAMP:
+          Assert.assertEquals("timestamp should be equal", expected.getTimestamp(i, 6),
+              actual.getTimestamp(i, 6));
+          break;
+        case UUID:
+        case FIXED:
+        case BINARY:
+          Assert.assertArrayEquals("binary should be equal", expected.getBinary(i), actual.getBinary(i));
+          break;
+        case DECIMAL:
+          Types.DecimalType decimal = (Types.DecimalType) type;
+          int precision = decimal.precision();
+          int scale = decimal.scale();
+          Assert.assertEquals("uuid should be equal",
+              expected.getDecimal(i, precision, scale),
+              actual.getDecimal(i, precision, scale));
+          break;
+        case LIST:
+          ArrayData arrayData1 = expected.getArray(i);
+          ArrayData arrayData2 = actual.getArray(i);
+          Assert.assertEquals("array length should be equal", arrayData1.size(), arrayData2.size());
+          for (int j = 0; j < arrayData1.size(); j += 1) {
+            assertArrayValues(type.asListType().elementType(), arrayData1, arrayData2);
+          }
+          break;
+        case MAP:
+          ArrayData keyArrayData1 = expected.getMap(i).keyArray();
+          ArrayData valueArrayData1 = expected.getMap(i).valueArray();
+          ArrayData keyArrayData2 = actual.getMap(i).keyArray();
+          ArrayData valueArrayData2 = actual.getMap(i).valueArray();
+
+          Assert.assertEquals("map size should be equal", expected.getMap(i).size(), actual.getMap(i).size());
+
+          for (int j = 0; j < keyArrayData1.size(); j += 1) {
+            assertArrayValues(type.asMapType().keyType(), keyArrayData1, keyArrayData2);
+            assertArrayValues(type.asMapType().valueType(), valueArrayData1, valueArrayData2);
+          }
+          break;
+        case STRUCT:
+          int numFields = type.asStructType().fields().size();
+          assertRowData(type.asStructType(), expected.getRow(i, numFields), actual.getRow(i, numFields));
+          break;
+      }
+    }
+  }
+
   @Test
   public void testNormalRowData() throws IOException {
-    testCorrectness(COMPLEX_SCHEMA, NUM_RECORDS, RandomData.generate(COMPLEX_SCHEMA, NUM_RECORDS, 19981));
+    testCorrectness(COMPLEX_SCHEMA, NUM_RECORDS, RandomData.generateRowData(COMPLEX_SCHEMA, NUM_RECORDS, 19981));
   }
 
   @Test
