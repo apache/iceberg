c0ac93a391a6eef4556b72857a18cb95366b2595
Chen, Junjie <chenjunjiedada@gmail.com>
2020-08-11 19:52:51 +0800

    Flink: use schema visitor for flink parquet writer

--- flink/src/test/java/org/apache/iceberg/flink/data/RandomData.java
+++ flink/src/test/java/org/apache/iceberg/flink/data/RandomData.java
@@ -19,14 +19,28 @@
 
 package org.apache.iceberg.flink.data;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.Random;
+import java.util.Set;
 import java.util.function.Supplier;
+import org.apache.flink.table.data.DecimalData;
+import org.apache.flink.table.data.GenericArrayData;
+import org.apache.flink.table.data.GenericMapData;
+import org.apache.flink.table.data.GenericRowData;
+import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.data.StringData;
+import org.apache.flink.table.data.TimestampData;
 import org.apache.flink.types.Row;
 import org.apache.iceberg.Schema;
 import org.apache.iceberg.data.RandomGenericData;
 import org.apache.iceberg.relocated.com.google.common.collect.Lists;
+import org.apache.iceberg.relocated.com.google.common.collect.Sets;
+import org.apache.iceberg.types.Type;
 import org.apache.iceberg.types.TypeUtil;
 import org.apache.iceberg.types.Types;
 
@@ -61,7 +75,11 @@ public class RandomData {
               required(23, "koala", Types.IntegerType.get()),
               required(24, "couch rope", Types.IntegerType.get())
           ))),
-      optional(2, "slide", Types.StringType.get())
+      optional(2, "slide", Types.StringType.get()),
+      optional(25, "binary", Types.BinaryType.get()),
+      optional(26, "decimal", Types.DecimalType.of(10, 2)),
+      optional(27, "time micro", Types.TimeType.get()),
+      optional(28, "fix len binary", Types.FixedType.ofLength(20))
   );
 
   private static Iterable<Row> generateData(Schema schema, int numRecords, Supplier<RandomRowGenerator> supplier) {
@@ -85,8 +103,150 @@ public class RandomData {
     };
   }
 
+  private static Iterable<RowData> generateRowData(Schema schema, int numRecords,
+                                                   Supplier<RandomRowDataGenerator> supplier) {
+    return () -> new Iterator<RowData>() {
+      private final RandomRowDataGenerator generator = supplier.get();
+      private int count = 0;
+
+      @Override
+      public boolean hasNext() {
+        return count < numRecords;
+      }
+
+      @Override
+      public RowData next() {
+        if (!hasNext()) {
+          throw new NoSuchElementException();
+        }
+        ++count;
+        return (RowData) TypeUtil.visit(schema, generator);
+      }
+    };
+  }
+
+  public static Iterable<RowData> generateRowData(Schema schema, int numRecords, long seed) {
+    return generateRowData(schema, numRecords, () -> new RandomRowDataGenerator(seed));
+  }
+
   public static Iterable<Row> generate(Schema schema, int numRecords, long seed) {
     return generateData(schema, numRecords, () -> new RandomRowGenerator(seed));
+  }
+
+  public static Iterable<RowData> generateFallbackRowData(Schema schema, int numRecords, long seed, long numDictRows) {
+    return generateRowData(schema, numRecords, () -> new FallbackRowDataGenerator(seed, numDictRows));
+  }
+
+  public static Iterable<RowData> generateDictionaryEncodableRowData(Schema schema, int numRecords, long seed) {
+    return generateRowData(schema, numRecords, () -> new DictionaryEncodedRowDataGenerator(seed));
+  }
+
+  private static class RandomRowDataGenerator extends TypeUtil.CustomOrderSchemaVisitor<Object> {
+    protected final Random random;
+    private static final int MAX_ENTRIES = 20;
+
+    RandomRowDataGenerator(long seed) {
+      this.random = new Random(seed);
+    }
+
+    protected int getMaxEntries() {
+      return MAX_ENTRIES;
+    }
+
+    @Override
+    public RowData schema(Schema schema, Supplier<Object> structResult) {
+      return (RowData) structResult.get();
+    }
+
+    @Override
+    public RowData struct(Types.StructType struct, Iterable<Object> fieldResults) {
+      GenericRowData row = new GenericRowData(struct.fields().size());
+
+      List<Object> values = Lists.newArrayList(fieldResults);
+      for (int i = 0; i < values.size(); i += 1) {
+        row.setField(i, values.get(i));
+      }
+
+      return row;
+    }
+
+    @Override
+    public Object field(Types.NestedField field, Supplier<Object> fieldResult) {
+      // return null 5% of the time when the value is optional
+      if (field.isOptional() && random.nextInt(20) == 1) {
+        return null;
+      }
+      return fieldResult.get();
+    }
+
+    @Override
+    public Object list(Types.ListType list, Supplier<Object> elementResult) {
+      int numElements = random.nextInt(20);
+      Object[] arr = new Object[numElements];
+      GenericArrayData result = new GenericArrayData(arr);
+
+      for (int i = 0; i < numElements; i += 1) {
+        // return null 5% of the time when the value is optional
+        if (list.isElementOptional() && random.nextInt(20) == 1) {
+          arr[i] = null;
+        } else {
+          arr[i] = elementResult.get();
+        }
+      }
+
+      return result;
+    }
+
+    @Override
+    public Object map(Types.MapType map, Supplier<Object> keyResult, Supplier<Object> valueResult) {
+      int numEntries = random.nextInt(getMaxEntries());
+
+      Object[] keysArr = new Object[numEntries];
+      Map<Object, Object> javaMap = new HashMap<>();
+
+      Set<Object> keySet = Sets.newHashSet();
+      for (int i = 0; i < numEntries; i += 1) {
+        Object key = keyResult.get();
+        // ensure no collisions
+        while (keySet.contains(key)) {
+          key = keyResult.get();
+        }
+
+        keySet.add(key);
+        keysArr[i] = key;
+
+        if (map.isValueOptional() && random.nextInt(20) == 1) {
+          javaMap.put(keysArr[i], null);
+        } else {
+          javaMap.put(keysArr[i], valueResult.get());
+        }
+      }
+
+      return new GenericMapData(javaMap);
+    }
+
+    @Override
+    public Object primitive(Type.PrimitiveType primitive) {
+      Object obj = randomValue(primitive, random);
+      switch (primitive.typeId()) {
+        case STRING:
+          return StringData.fromString((String) obj);
+        case DECIMAL:
+          return DecimalData.fromBigDecimal((BigDecimal) obj,
+              ((BigDecimal) obj).precision(),
+              ((BigDecimal) obj).scale());
+        case TIMESTAMP:
+          return TimestampData.fromEpochMillis((Long) obj);
+        case TIME:
+          return ((Long) ((Long) obj / 1000)).intValue();
+        default:
+          return obj;
+      }
+    }
+
+    protected Object randomValue(Type.PrimitiveType primitive, Random rand) {
+      return RandomUtil.generatePrimitive(primitive, random);
+    }
   }
 
   private static class RandomRowGenerator extends RandomGenericData.RandomDataGenerator<Row> {
@@ -109,6 +269,42 @@ public class RandomData {
       }
 
       return row;
+    }
+  }
+
+  private static class DictionaryEncodedRowDataGenerator extends RandomRowDataGenerator  {
+    DictionaryEncodedRowDataGenerator(long seed) {
+      super(seed);
+    }
+
+    @Override
+    protected int getMaxEntries() {
+      return 3;
+    }
+
+    @Override
+    protected Object randomValue(Type.PrimitiveType primitive, Random random) {
+      return RandomUtil.generateDictionaryEncodablePrimitive(primitive, random);
+    }
+  }
+
+  private static class FallbackRowDataGenerator extends RandomRowDataGenerator {
+    private final long dictionaryEncodedRows;
+    private long rowCount = 0;
+
+    FallbackRowDataGenerator(long seed, long numDictionaryEncoded) {
+      super(seed);
+      this.dictionaryEncodedRows = numDictionaryEncoded;
+    }
+
+    @Override
+    protected Object randomValue(Type.PrimitiveType primitive, Random rand) {
+      this.rowCount += 1;
+      if (rowCount > dictionaryEncodedRows) {
+        return RandomUtil.generatePrimitive(primitive, rand);
+      } else {
+        return RandomUtil.generateDictionaryEncodablePrimitive(primitive, rand);
+      }
     }
   }
 }
