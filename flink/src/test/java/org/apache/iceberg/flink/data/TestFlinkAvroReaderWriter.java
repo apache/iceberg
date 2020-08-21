2690619275701fdb686ff18627b29e99be17a0a0
huzheng <openinx@gmail.com>
2020-08-21 14:28:13 +0800

    Flink: Enhance the flink avro reader-writer unit tests by asserting between record and rowdata

--- flink/src/test/java/org/apache/iceberg/flink/data/TestFlinkAvroReaderWriter.java
+++ flink/src/test/java/org/apache/iceberg/flink/data/TestFlinkAvroReaderWriter.java
@@ -24,12 +24,14 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 import org.apache.flink.table.data.RowData;
+import org.apache.flink.table.types.logical.RowType;
 import org.apache.iceberg.Files;
 import org.apache.iceberg.Schema;
 import org.apache.iceberg.avro.Avro;
 import org.apache.iceberg.data.DataTest;
 import org.apache.iceberg.data.RandomGenericData;
 import org.apache.iceberg.data.Record;
+import org.apache.iceberg.data.avro.DataReader;
 import org.apache.iceberg.data.avro.DataWriter;
 import org.apache.iceberg.flink.FlinkSchemaUtil;
 import org.apache.iceberg.io.CloseableIterable;
@@ -43,55 +45,56 @@ public class TestFlinkAvroReaderWriter extends DataTest {
 
   @Override
   protected void writeAndValidate(Schema schema) throws IOException {
-    List<RowData> inputs = generateDataFromAvroFile(schema);
+    RowType flinkSchema = FlinkSchemaUtil.convert(schema);
+    List<Record> expectedRecords = RandomGenericData.generate(schema, NUM_RECORDS, 1991L);
+    List<RowData> expectedRows = Lists.newArrayList(RandomRowData.convert(schema, expectedRecords));
 
-    File testFile = temp.newFile();
-    Assert.assertTrue("Delete should succeed", testFile.delete());
+    File recordsFile = temp.newFile();
+    Assert.assertTrue("Delete should succeed", recordsFile.delete());
 
-    try (FileAppender<RowData> writer = Avro.write(Files.localOutput(testFile))
-        .schema(schema)
-        .createWriterFunc(ignore -> new FlinkAvroWriter(FlinkSchemaUtil.convert(schema)))
-        .build()) {
-      writer.addAll(inputs);
-    }
-
-    try (CloseableIterable<RowData> reader = Avro.read(Files.localInput(testFile))
-        .project(schema)
-        .createReaderFunc(FlinkAvroReader::new)
-        .build()) {
-      Iterator<RowData> expected = inputs.iterator();
-      Iterator<RowData> rows = reader.iterator();
-      for (int i = 0; i < NUM_RECORDS; i += 1) {
-        Assert.assertTrue("Should have expected number of rows", rows.hasNext());
-        Assert.assertEquals(expected.next(), rows.next());
-      }
-      Assert.assertFalse("Should not have extra rows", rows.hasNext());
-    }
-  }
-
-  private List<RowData> generateDataFromAvroFile(Schema schema) throws IOException {
-    List<Record> expected = RandomGenericData.generate(schema, NUM_RECORDS, 0L);
-
-    File testFile = temp.newFile();
-    Assert.assertTrue("Delete should succeed", testFile.delete());
-
-    try (FileAppender<Record> writer = Avro.write(Files.localOutput(testFile))
+    // Write the expected records into AVRO file, then read them into RowData and assert with the expected Record list.
+    try (FileAppender<Record> writer = Avro.write(Files.localOutput(recordsFile))
         .schema(schema)
         .createWriterFunc(DataWriter::create)
-        .named("test")
         .build()) {
-      for (Record rec : expected) {
-        writer.add(rec);
-      }
+      writer.addAll(expectedRecords);
     }
 
-    List<RowData> rows;
-    try (CloseableIterable<RowData> reader = Avro.read(Files.localInput(testFile))
+    try (CloseableIterable<RowData> reader = Avro.read(Files.localInput(recordsFile))
         .project(schema)
         .createReaderFunc(FlinkAvroReader::new)
         .build()) {
-      rows = Lists.newArrayList(reader);
+      Iterator<Record> expected = expectedRecords.iterator();
+      Iterator<RowData> rows = reader.iterator();
+      for (int i = 0; i < NUM_RECORDS; i++) {
+        Assert.assertTrue("Should have expected number of records", rows.hasNext());
+        TestHelpers.assertRowData(schema.asStruct(), flinkSchema, expected.next(), rows.next());
+      }
+      Assert.assertFalse("Should not have extra records", rows.hasNext());
     }
-    return rows;
+
+    File rowDataFile = temp.newFile();
+    Assert.assertTrue("Delete should succeed", rowDataFile.delete());
+
+    // Write the expected RowData into AVRO file, then read them into Record and assert with the expected RowData list.
+    try (FileAppender<RowData> writer = Avro.write(Files.localOutput(rowDataFile))
+        .schema(schema)
+        .createWriterFunc(ignore -> new FlinkAvroWriter(flinkSchema))
+        .build()) {
+      writer.addAll(expectedRows);
+    }
+
+    try (CloseableIterable<Record> reader = Avro.read(Files.localInput(rowDataFile))
+        .project(schema)
+        .createReaderFunc(DataReader::create)
+        .build()) {
+      Iterator<RowData> expected = expectedRows.iterator();
+      Iterator<Record> records = reader.iterator();
+      for (int i = 0; i < NUM_RECORDS; i += 1) {
+        Assert.assertTrue("Should have expected number of records", records.hasNext());
+        TestHelpers.assertRowData(schema.asStruct(), flinkSchema, records.next(), expected.next());
+      }
+      Assert.assertFalse("Should not have extra records", records.hasNext());
+    }
   }
 }
