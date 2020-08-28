2d1aed64d2681f41d27c7d945e009a72e51e05d2
JingsongLi <lzljs3620320@aliyun.com>
2020-08-28 17:15:55 +0800

    checkstyle

--- flink/src/main/java/org/apache/iceberg/flink/FlinkCatalog.java
+++ flink/src/main/java/org/apache/iceberg/flink/FlinkCatalog.java
@@ -26,7 +26,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.function.BiConsumer;
 import java.util.stream.Collectors;
 import org.apache.flink.table.api.TableSchema;
 import org.apache.flink.table.catalog.AbstractCatalog;
