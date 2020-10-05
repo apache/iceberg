89bfab5ffcccf26be09e95d6e311c58acf123aae
Anton Okolnychyi <aokolnychyi@apple.com>
2020-10-05 11:46:36 -0700

    Build: Ignore OverloadMethodsDeclarationOrder rule (#1550)

--- spark/src/test/java/org/apache/iceberg/spark/data/TestHelpers.java
+++ spark/src/test/java/org/apache/iceberg/spark/data/TestHelpers.java
@@ -66,7 +66,6 @@ import static org.apache.iceberg.spark.SparkSchemaUtil.convert;
 import static scala.collection.JavaConverters.mapAsJavaMapConverter;
 import static scala.collection.JavaConverters.seqAsJavaListConverter;
 
-@SuppressWarnings("checkstyle:OverloadMethodsDeclarationOrder")
 public class TestHelpers {
 
   private TestHelpers() {
