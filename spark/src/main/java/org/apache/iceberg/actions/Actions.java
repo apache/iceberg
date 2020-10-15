3f5fb2b76898a3fdc2e9bd6e4b96df0671e5180c
Russell Spitzer <russell.spitzer@gmail.com>
2020-10-15 12:06:04 -0500

    Seperate Action Implementations into Spark Specific Modules
    Uses reflection in the base Actions class to determine the actual
    implementation on the classloader to use. This allows both Spark2
    and Spark3 to use different implementations of Actions while not
    changing the location of the original Actions class.

--- spark/src/main/java/org/apache/iceberg/actions/Actions.java
+++ spark/src/main/java/org/apache/iceberg/actions/Actions.java
@@ -19,25 +19,57 @@
 
 package org.apache.iceberg.actions;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import org.apache.iceberg.Table;
 import org.apache.spark.sql.SparkSession;
 
 public class Actions {
+  private static final String IMPL_NAME = "SparkActions";
+
+  private static Class<Actions> implClass;
+  private static Constructor<Actions> implConstructor;
 
   private SparkSession spark;
   private Table table;
 
-  private Actions(SparkSession spark, Table table) {
+  protected Actions(SparkSession spark, Table table) {
     this.spark = spark;
     this.table = table;
   }
 
+  private static Constructor actionConstructor() {
+    if (implClass == null) {
+      String className = Actions.class.getPackage().getName() + "." + IMPL_NAME;
+      try {
+        implClass = (Class<Actions>) Class.forName(className);
+      } catch (ClassNotFoundException e) {
+        throw new IllegalArgumentException("Cannot use Actions if no Spark Module is on the classpath.", e);
+      }
+    }
+    if (implConstructor == null) {
+      try {
+        implConstructor = implClass.getDeclaredConstructor(SparkSession.class, Table.class);
+      } catch (NoSuchMethodException e) {
+        throw new IllegalArgumentException(
+            "Cannot use Actions if SparkAction implementation does not have a constructor of (SparkSession, Table).",
+            e);
+      }
+    }
+    return implConstructor;
+  }
+
   public static Actions forTable(SparkSession spark, Table table) {
-    return new Actions(spark, table);
+    try {
+      return (Actions) actionConstructor().newInstance(spark, table);
+    } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+      throw new IllegalStateException("Cannot create new Actions, this most likely means there is a classpath issue.",
+          e);
+    }
   }
 
   public static Actions forTable(Table table) {
-    return new Actions(SparkSession.active(), table);
+    return forTable(SparkSession.active(), table);
   }
 
   public RemoveOrphanFilesAction removeOrphanFiles() {
