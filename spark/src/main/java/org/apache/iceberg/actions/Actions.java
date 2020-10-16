cdd4e8bd30329b13d364c5eea9c64663e482c2c5
Russell Spitzer <russell.spitzer@gmail.com>
2020-10-16 12:37:07 -0500

    Switch to using Reflection Utils Class

--- spark/src/main/java/org/apache/iceberg/actions/Actions.java
+++ spark/src/main/java/org/apache/iceberg/actions/Actions.java
@@ -19,16 +19,30 @@
 
 package org.apache.iceberg.actions;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import org.apache.iceberg.Table;
+import org.apache.iceberg.common.DynConstructors;
 import org.apache.spark.sql.SparkSession;
 
 public class Actions {
-  private static final String IMPL_NAME = "SparkActions";
 
-  private static Class<Actions> implClass;
-  private static Constructor<Actions> implConstructor;
+  /*
+  We load the actual implementation of Actions via reflection to allow for differences
+  between the major Spark APIs while still defining the API in this class.
+  */
+  private static final String IMPL_NAME = "SparkActions";
+  private static DynConstructors.Ctor<Actions> implConstructor;
+
+  private static DynConstructors.Ctor<Actions> actionConstructor() {
+    if (implConstructor == null) {
+      String className = Actions.class.getPackage().getName() + "." + IMPL_NAME;
+      try {
+        implConstructor = DynConstructors.builder().impl(className, SparkSession.class, Table.class).buildChecked();
+      } catch (NoSuchMethodException e) {
+        throw new IllegalArgumentException("Cannot find appropriate Actions implementation on the classpath.", e);
+      }
+    }
+    return implConstructor;
+  }
 
   private SparkSession spark;
   private Table table;
@@ -38,34 +52,8 @@ public class Actions {
     this.table = table;
   }
 
-  private static Constructor actionConstructor() {
-    if (implClass == null) {
-      String className = Actions.class.getPackage().getName() + "." + IMPL_NAME;
-      try {
-        implClass = (Class<Actions>) Class.forName(className);
-      } catch (ClassNotFoundException e) {
-        throw new IllegalArgumentException("Cannot use Actions if no Spark Module is on the classpath.", e);
-      }
-    }
-    if (implConstructor == null) {
-      try {
-        implConstructor = implClass.getDeclaredConstructor(SparkSession.class, Table.class);
-      } catch (NoSuchMethodException e) {
-        throw new IllegalArgumentException(
-            "Cannot use Actions if SparkAction implementation does not have a constructor of (SparkSession, Table).",
-            e);
-      }
-    }
-    return implConstructor;
-  }
-
   public static Actions forTable(SparkSession spark, Table table) {
-    try {
-      return (Actions) actionConstructor().newInstance(spark, table);
-    } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
-      throw new IllegalStateException("Cannot create new Actions, this most likely means there is a classpath issue.",
-          e);
-    }
+    return actionConstructor().newInstance(spark, table);
   }
 
   public static Actions forTable(Table table) {
@@ -87,4 +75,13 @@ public class Actions {
   public ExpireSnapshotsAction expireSnapshots() {
     return new ExpireSnapshotsAction(spark, table);
   }
+
+  protected SparkSession spark() {
+    return spark;
+  }
+
+  protected Table table() {
+    return table;
+  }
+
 }
