d7acc1a024c7b50135cb71be3bbdecf26deff916
Russell Spitzer <russell.spitzer@gmail.com>
2020-10-19 10:21:25 -0500

    Package Private SparkActions

--- spark2/src/main/java/org/apache/iceberg/actions/SparkActions.java
+++ spark2/src/main/java/org/apache/iceberg/actions/SparkActions.java
@@ -22,7 +22,7 @@ package org.apache.iceberg.actions;
 import org.apache.iceberg.Table;
 import org.apache.spark.sql.SparkSession;
 
-public class SparkActions extends Actions {
+class SparkActions extends Actions {
   protected SparkActions(SparkSession spark, Table table) {
     super(spark, table);
   }
