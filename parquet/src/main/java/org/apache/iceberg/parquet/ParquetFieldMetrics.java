10220065611ca4486ff7aa7a38c3ccc10e8298e9
Yan Yan <yyanyyyy@gmail.com>
2020-11-02 18:03:34 -0800

    update some comments

--- parquet/src/main/java/org/apache/iceberg/parquet/ParquetFieldMetrics.java
+++ parquet/src/main/java/org/apache/iceberg/parquet/ParquetFieldMetrics.java
@@ -23,10 +23,18 @@ import java.nio.ByteBuffer;
 import org.apache.iceberg.FieldMetrics;
 
 /**
- * TODO comments
+ * Iceberg internally tracked field level metrics, used by Parquet writer only.
+ * Parquet keeps track of most metrics in its footer, and only NaN counter is actually tracked by writers. This
+ * wrapper ensures that metrics not being updated by Parquet writers will not be incorrectly used, by throwing
+ * exceptions when they are accessed.
  */
 public class ParquetFieldMetrics extends FieldMetrics {
 
+  /**
+   * Constructor for creating a Parquet-specific FieldMetrics.
+   * @param id field id being tracked by the writer
+   * @param nanValueCount number of NaN values, will only be non-0 for double or float field.
+   */
   public ParquetFieldMetrics(int id,
                              long nanValueCount) {
     super(id, 0L, 0L, nanValueCount, null, null);
@@ -34,21 +42,25 @@ public class ParquetFieldMetrics extends FieldMetrics {
 
   @Override
   public long valueCount() {
-    throw new IllegalStateException("TODO");
+    throw new IllegalStateException(
+        "Shouldn't access valueCount() within ParquetFieldMetrics, as this metric is tracked by Parquet footer. ");
   }
 
   @Override
   public long nullValueCount() {
-    throw new IllegalStateException("TODO");
+    throw new IllegalStateException(
+        "Shouldn't access nullValueCount() within ParquetFieldMetrics, as this metric is tracked by Parquet footer. ");
   }
 
   @Override
   public ByteBuffer lowerBound() {
-    throw new IllegalStateException("TODO");
+    throw new IllegalStateException(
+        "Shouldn't access lowerBound() within ParquetFieldMetrics, as this metric is tracked by Parquet footer. ");
   }
 
   @Override
   public ByteBuffer upperBound() {
-    throw new IllegalStateException("TODO");
+    throw new IllegalStateException(
+        "Shouldn't access upperBound() within ParquetFieldMetrics, as this metric is tracked by Parquet footer. ");
   }
 }
