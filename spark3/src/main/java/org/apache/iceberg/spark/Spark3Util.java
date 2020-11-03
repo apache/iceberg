cbf104c81a5909fa96f5a9690bd6ccd55e97ca8e
Ryan Blue <blue@apache.org>
2020-11-02 16:54:19 -0800

    Fix checkstyle.

--- spark3/src/main/java/org/apache/iceberg/spark/Spark3Util.java
+++ spark3/src/main/java/org/apache/iceberg/spark/Spark3Util.java
@@ -23,7 +23,6 @@ import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.apache.iceberg.PartitionSpec;
