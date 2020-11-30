a57adf74dd2057649295fcdcb77d1716015b9725
zhangjun <jun.zhang1@ly.com>
2020-11-30 10:04:13 +0800

    check table.getParameters is null or not

--- hive-metastore/src/main/java/org/apache/iceberg/hive/HiveCatalog.java
+++ hive-metastore/src/main/java/org/apache/iceberg/hive/HiveCatalog.java
@@ -118,10 +118,10 @@ public class HiveCatalog extends BaseMetastoreCatalog implements Closeable, Supp
     String database = namespace.level(0);
 
     try {
-      List<String> tables = clients.run(client -> client.getAllTables(database));
-      List<Table> tableObject = clients.run(client -> client.getTableObjectsByName(database, tables));
-      List<TableIdentifier> tableIdentifiers = tableObject.stream()
-          .filter(table -> BaseMetastoreTableOperations.ICEBERG_TABLE_TYPE_VALUE
+      List<String> tableNames = clients.run(client -> client.getAllTables(database));
+      List<Table> tableObjects = clients.run(client -> client.getTableObjectsByName(database, tableNames));
+      List<TableIdentifier> tableIdentifiers = tableObjects.stream()
+          .filter(table -> table.getParameters() == null ? false : BaseMetastoreTableOperations.ICEBERG_TABLE_TYPE_VALUE
               .equalsIgnoreCase(table.getParameters().get(BaseMetastoreTableOperations.TABLE_TYPE_PROP)))
           .map(table -> TableIdentifier.of(namespace, table.getTableName()))
           .collect(Collectors.toList());
