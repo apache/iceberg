cb1e011fef1091c4734718cb2481d9dc88baa81d
Marton Bod <mbod@cloudera.com>
2020-12-09 17:39:32 +0100

    fix checkstyle 2

--- hive3/src/test/java/org/apache/iceberg/mr/hive/serde/objectinspector/TestIcebergTimestampWithZoneObjectInspectorHive3.java
+++ hive3/src/test/java/org/apache/iceberg/mr/hive/serde/objectinspector/TestIcebergTimestampWithZoneObjectInspectorHive3.java
@@ -1,15 +1,20 @@
 /*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.apache.iceberg.mr.hive.serde.objectinspector;
@@ -28,37 +33,37 @@ import org.junit.Test;
 
 public class TestIcebergTimestampWithZoneObjectInspectorHive3 {
 
-    @Test
-    public void testIcebergTimestampLocalTZObjectInspector() {
-        TimestampLocalTZObjectInspector oi = IcebergTimestampWithZoneObjectInspectorHive3.get();
+  @Test
+  public void testIcebergTimestampLocalTZObjectInspector() {
+    TimestampLocalTZObjectInspector oi = IcebergTimestampWithZoneObjectInspectorHive3.get();
 
-        Assert.assertEquals(ObjectInspector.Category.PRIMITIVE, oi.getCategory());
-        Assert.assertEquals(PrimitiveObjectInspector.PrimitiveCategory.TIMESTAMPLOCALTZ, oi.getPrimitiveCategory());
+    Assert.assertEquals(ObjectInspector.Category.PRIMITIVE, oi.getCategory());
+    Assert.assertEquals(PrimitiveObjectInspector.PrimitiveCategory.TIMESTAMPLOCALTZ, oi.getPrimitiveCategory());
 
-        Assert.assertEquals(TypeInfoFactory.timestampLocalTZTypeInfo, oi.getTypeInfo());
-        Assert.assertEquals(TypeInfoFactory.timestampLocalTZTypeInfo.getTypeName(), oi.getTypeName());
+    Assert.assertEquals(TypeInfoFactory.timestampLocalTZTypeInfo, oi.getTypeInfo());
+    Assert.assertEquals(TypeInfoFactory.timestampLocalTZTypeInfo.getTypeName(), oi.getTypeName());
 
-        Assert.assertEquals(TimestampTZ.class, oi.getJavaPrimitiveClass());
-        Assert.assertEquals(TimestampLocalTZWritable.class, oi.getPrimitiveWritableClass());
+    Assert.assertEquals(TimestampTZ.class, oi.getJavaPrimitiveClass());
+    Assert.assertEquals(TimestampLocalTZWritable.class, oi.getPrimitiveWritableClass());
 
-        Assert.assertNull(oi.copyObject(null));
-        Assert.assertNull(oi.getPrimitiveJavaObject(null));
-        Assert.assertNull(oi.getPrimitiveWritableObject(null));
+    Assert.assertNull(oi.copyObject(null));
+    Assert.assertNull(oi.getPrimitiveJavaObject(null));
+    Assert.assertNull(oi.getPrimitiveWritableObject(null));
 
-        long epochSeconds = 1601471970L;
-        OffsetDateTime offsetDateTime = OffsetDateTime.of(
-            LocalDateTime.ofEpochSecond(epochSeconds, 0, ZoneOffset.UTC), ZoneOffset.ofHours(4));
-        TimestampTZ ts = new TimestampTZ(offsetDateTime.toZonedDateTime());
+    long epochSeconds = 1601471970L;
+    OffsetDateTime offsetDateTime = OffsetDateTime.of(
+        LocalDateTime.ofEpochSecond(epochSeconds, 0, ZoneOffset.UTC), ZoneOffset.ofHours(4));
+    TimestampTZ ts = new TimestampTZ(offsetDateTime.toZonedDateTime());
 
-        Assert.assertEquals(ts, oi.getPrimitiveJavaObject(offsetDateTime));
-        Assert.assertEquals(new TimestampLocalTZWritable(ts), oi.getPrimitiveWritableObject(offsetDateTime));
+    Assert.assertEquals(ts, oi.getPrimitiveJavaObject(offsetDateTime));
+    Assert.assertEquals(new TimestampLocalTZWritable(ts), oi.getPrimitiveWritableObject(offsetDateTime));
 
-        TimestampTZ copy = (TimestampTZ) oi.copyObject(ts);
+    TimestampTZ copy = (TimestampTZ) oi.copyObject(ts);
 
-        Assert.assertEquals(ts, copy);
-        Assert.assertNotSame(ts, copy);
+    Assert.assertEquals(ts, copy);
+    Assert.assertNotSame(ts, copy);
 
-        Assert.assertFalse(oi.preferWritable());
-    }
+    Assert.assertFalse(oi.preferWritable());
+  }
 
 }
