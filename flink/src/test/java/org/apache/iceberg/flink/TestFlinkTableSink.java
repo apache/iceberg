235a8e3066664cd6a56a1421c70ddb298cf3affb
huzheng <openinx@gmail.com>
2021-01-11 17:34:39 +0800

    Fix compile error.

--- flink/src/test/java/org/apache/iceberg/flink/TestFlinkTableSink.java
+++ flink/src/test/java/org/apache/iceberg/flink/TestFlinkTableSink.java
@@ -24,7 +24,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
 import org.apache.flink.streaming.api.TimeCharacteristic;
