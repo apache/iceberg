4c92fd02bfc0a03be9956ebb49bfcf044a1a524a
dixingxing <dixingxing@autohome.com.cn>
2021-01-13 12:44:17 +0800

    Fix test and codestyle

--- hive-metastore/src/test/java/org/apache/iceberg/hive/TestHiveConf.java
+++ hive-metastore/src/test/java/org/apache/iceberg/hive/TestHiveConf.java
@@ -31,42 +31,35 @@ import org.junit.Test;
  * For https://github.com/apache/iceberg/issues/2070
  */
 public class TestHiveConf {
-    private static final String HIVE_SITE_CONTENT = "<?xml version=\"1.0\"?>\n" +
-            "<?xml-stylesheet type=\"text/xsl\" href=\"configuration.xsl\"?>\n" +
-            "<configuration>\n" +
-            "  <property>\n" +
-            "    <name>hive.metastore.sasl.enabled</name>\n" +
-            "    <value>true</value>\n" +
-            "  </property>\n" +
-            "  <property>\n" +
-            "    <name>hive.metastore.uris</name>\n" +
-            "    <value>thrift://xxxx:1234</value>\n" +
-            "  </property>\n" +
-            "</configuration>\n";
+  private static final String HIVE_SITE_CONTENT = "<?xml version=\"1.0\"?>\n" +
+      "<?xml-stylesheet type=\"text/xsl\" href=\"configuration.xsl\"?>\n" +
+      "<configuration>\n" +
+      "  <property>\n" +
+      "    <name>hive.metastore.sasl.enabled</name>\n" +
+      "    <value>true</value>\n" +
+      "  </property>\n" +
+      "</configuration>\n";
 
-    @Test
-    public void test() {
-        HiveConf hiveConf = createHiveConf();
-        Assert.assertTrue(hiveConf.getBoolVar(HiveConf.ConfVars.METASTORE_USE_THRIFT_SASL));
-        Assert.assertEquals("thrift://xxxx:1234", hiveConf.getVar(HiveConf.ConfVars.METASTOREURIS));
+  @Test
+  public void test() {
+    HiveConf hiveConf = createHiveConf();
+    Assert.assertTrue(hiveConf.getBoolVar(HiveConf.ConfVars.METASTORE_USE_THRIFT_SASL));
 
-        // will override configurations from `hiveConf` with hive default values which default value is null or empty string
-        HiveConf hiveConfNew = new HiveConf(hiveConf, HiveConf.class);
-        Assert.assertFalse(hiveConfNew.getBoolVar(HiveConf.ConfVars.METASTORE_USE_THRIFT_SASL));
-        Assert.assertEquals("thrift://xxxx:1234", hiveConfNew.getVar(HiveConf.ConfVars.METASTOREURIS));
+    // will override configurations from `hiveConf` with hive default values which default value is null or empty string
+    HiveConf hiveConfNew = new HiveConf(hiveConf, HiveConf.class);
+    Assert.assertFalse(hiveConfNew.getBoolVar(HiveConf.ConfVars.METASTORE_USE_THRIFT_SASL));
 
-        hiveConfNew.addResource(hiveConf);
-        Assert.assertTrue(hiveConfNew.getBoolVar(HiveConf.ConfVars.METASTORE_USE_THRIFT_SASL));
-        Assert.assertEquals("thrift://xxxx:1234", hiveConfNew.getVar(HiveConf.ConfVars.METASTOREURIS));
+    hiveConfNew.addResource(hiveConf);
+    Assert.assertTrue(hiveConfNew.getBoolVar(HiveConf.ConfVars.METASTORE_USE_THRIFT_SASL));
+  }
+
+  private HiveConf createHiveConf() {
+    HiveConf hiveConf = new HiveConf();
+    try (InputStream inputStream = new ByteArrayInputStream(HIVE_SITE_CONTENT.getBytes(StandardCharsets.UTF_8))) {
+      hiveConf.addResource(inputStream, "for_test");
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
-
-    private HiveConf createHiveConf() {
-        HiveConf hiveConf = new HiveConf();
-        try (InputStream inputStream = new ByteArrayInputStream(HIVE_SITE_CONTENT.getBytes(StandardCharsets.UTF_8))) {
-            hiveConf.addResource(inputStream, "for_test");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return hiveConf;
-    }
+    return hiveConf;
+  }
 }
