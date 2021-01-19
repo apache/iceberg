8a26f28afbd116dcd29b7ccedb2fac31785bc56c
XuQianJin-Stars <forwardxu@apache.com>
2021-01-19 11:48:05 +0800

    StreamingOffset Of Structured streaming read for Iceberg

--- spark/src/main/java/org/apache/iceberg/spark/source/StreamingOffset.java
+++ spark/src/main/java/org/apache/iceberg/spark/source/StreamingOffset.java
@@ -30,59 +30,51 @@ import org.apache.iceberg.util.JsonUtil;
 import org.apache.spark.sql.sources.v2.reader.streaming.Offset;
 
 class StreamingOffset extends Offset {
-  static final StreamingOffset START_OFFSET = new StreamingOffset(-1L, -1, false, true);
+  static final StreamingOffset START_OFFSET = new StreamingOffset(-1L, -1, false);
 
   private static final int CURR_VERSION = 1;
   private static final String VERSION = "version";
   private static final String SNAPSHOT_ID = "snapshot_id";
-  private static final String INDEX = "index";
+  private static final String POSITION = "position";
   private static final String SCAN_ALL_FILES = "scan_all_files";
-  private static final String SNAPSHOT_FULLY_PROCESSED = "snapshot_fully_processed";
 
   private final long snapshotId;
-  private final int index;
+  private final long position;
   private final boolean scanAllFiles;
-  private final boolean snapshotFullyProcessed;
 
   /**
    * An implementation of Spark Structured Streaming Offset, to track the current processed files of
    * Iceberg table.
    *
    * @param snapshotId             The current processed snapshot id.
-   * @param index                  The index of last scanned file in snapshot.
-   * @param scanAllFiles           Denote whether to scan all files in a snapshot, currently we only
-   *                               scan all files in the starting snapshot.
-   * @param snapshotFullyProcessed Denote whether the current snapshot is fully processed, to avoid
-   *                               revisiting the processed snapshot.
+   * @param position               The position of last scanned file in snapshot.
+   * @param scanAllFiles           whether to scan all files in a snapshot; for example, to read
+   *                               all data when starting a stream.
    */
-  StreamingOffset(long snapshotId, int index, boolean scanAllFiles,
-      boolean snapshotFullyProcessed) {
+  StreamingOffset(long snapshotId, long position, boolean scanAllFiles) {
     this.snapshotId = snapshotId;
-    this.index = index;
+    this.position = position;
     this.scanAllFiles = scanAllFiles;
-    this.snapshotFullyProcessed = snapshotFullyProcessed;
   }
 
   static StreamingOffset fromJson(String json) {
-    Preconditions.checkNotNull(json, "The input JSON string representation of a StreamingOffset cannot be null");
+    Preconditions.checkNotNull(json, "Cannot parse StreamingOffset JSON: null");
 
     try {
       JsonNode node = JsonUtil.mapper().readValue(json, JsonNode.class);
       // The version of StreamingOffset. The offset was created with a version number
       // used to validate when deserializing from json string.
       int version = JsonUtil.getInt(VERSION, node);
-      if (version > CURR_VERSION) {
-        throw new IOException(String.format("This version of iceberg only supports version %s", CURR_VERSION));
-      }
+      Preconditions.checkArgument(version == CURR_VERSION,
+          "Cannot parse offset JSON: offset version %s is not supported", version);
 
       long snapshotId = JsonUtil.getLong(SNAPSHOT_ID, node);
-      int index = JsonUtil.getInt(INDEX, node);
+      int position = JsonUtil.getInt(POSITION, node);
       boolean shouldScanAllFiles = JsonUtil.getBool(SCAN_ALL_FILES, node);
-      boolean snapshotFullyProcessed = JsonUtil.getBool(SNAPSHOT_FULLY_PROCESSED, node);
 
-      return new StreamingOffset(snapshotId, index, shouldScanAllFiles, snapshotFullyProcessed);
+      return new StreamingOffset(snapshotId, position, shouldScanAllFiles);
     } catch (IOException e) {
-      throw new IllegalStateException(String.format("Failed to parse StreamingOffset from JSON string %s", json), e);
+      throw new IllegalArgumentException(String.format("Failed to parse StreamingOffset from JSON string %s", json), e);
     }
   }
 
@@ -94,9 +86,8 @@ class StreamingOffset extends Offset {
       generator.writeStartObject();
       generator.writeNumberField(VERSION, CURR_VERSION);
       generator.writeNumberField(SNAPSHOT_ID, snapshotId);
-      generator.writeNumberField(INDEX, index);
+      generator.writeNumberField(POSITION, position);
       generator.writeBooleanField(SCAN_ALL_FILES, scanAllFiles);
-      generator.writeBooleanField(SNAPSHOT_FULLY_PROCESSED, snapshotFullyProcessed);
       generator.writeEndObject();
       generator.flush();
 
@@ -111,16 +102,12 @@ class StreamingOffset extends Offset {
     return snapshotId;
   }
 
-  int index() {
-    return index;
+  long position() {
+    return position;
   }
 
   boolean shouldScanAllFiles() {
     return scanAllFiles;
-  }
-
-  boolean isSnapshotFullyProcessed() {
-    return snapshotFullyProcessed;
   }
 
   @Override
@@ -128,9 +115,8 @@ class StreamingOffset extends Offset {
     if (obj instanceof StreamingOffset) {
       StreamingOffset offset = (StreamingOffset) obj;
       return offset.snapshotId == snapshotId &&
-          offset.index == index &&
-          offset.scanAllFiles == scanAllFiles &&
-          offset.snapshotFullyProcessed == snapshotFullyProcessed;
+          offset.position == position &&
+          offset.scanAllFiles == scanAllFiles;
     } else {
       return false;
     }
@@ -138,12 +124,12 @@ class StreamingOffset extends Offset {
 
   @Override
   public int hashCode() {
-    return Objects.hashCode(snapshotId, index, scanAllFiles, snapshotFullyProcessed);
+    return Objects.hashCode(snapshotId, position, scanAllFiles);
   }
 
   @Override
   public String toString() {
-    return String.format("Streaming Offset[%d: index (%d) scan_all_files (%b) snapshot_fully_processed (%b)]",
-      snapshotId, index, scanAllFiles, snapshotFullyProcessed);
+    return String.format("Streaming Offset[%d: position (%d) scan_all_files (%b)]",
+        snapshotId, position, scanAllFiles);
   }
 }
--- spark/src/test/java/org/apache/iceberg/spark/source/TestStreamingOffset.java
+++ spark/src/test/java/org/apache/iceberg/spark/source/TestStreamingOffset.java
@@ -19,37 +19,111 @@
 
 package org.apache.iceberg.spark.source;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.iceberg.DataFile;
 import org.apache.iceberg.ManifestFile;
 import org.apache.iceberg.ManifestFiles;
 import org.apache.iceberg.ManifestReader;
+import org.apache.iceberg.PartitionSpec;
+import org.apache.iceberg.Schema;
 import org.apache.iceberg.Snapshot;
+import org.apache.iceberg.Table;
+import org.apache.iceberg.hadoop.HadoopTables;
+import org.apache.iceberg.io.FileIO;
 import org.apache.iceberg.relocated.com.google.common.collect.Lists;
+import org.apache.iceberg.types.Types;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.TemporaryFolder;
 
-public abstract class TestStreamingOffset extends TestStructuredStreamingRead {
+import static org.apache.iceberg.types.Types.NestedField.optional;
+
+public abstract class TestStreamingOffset {
+  private static final Configuration CONF = new Configuration();
+  private static final Schema SCHEMA = new Schema(
+      optional(1, "id", Types.IntegerType.get()),
+      optional(2, "data", Types.StringType.get())
+  );
+  protected static final int INIT_SCANNED_FILE_INDEX = -1;
+  protected static final int END_SCANNED_FILE_INDEX = 3;
+  protected static SparkSession spark = null;
+  protected static Path parent = null;
+  protected static File tableLocation = null;
+  protected static Table table = null;
+  protected static List<SimpleRecord> expected = null;
+  protected static final FileIO FILE_IO = new TestTables.LocalFileIO();
+
+  @Rule
+  public TemporaryFolder temp = new TemporaryFolder();
+  @Rule
+  public ExpectedException exceptionRule = ExpectedException.none();
+
+  @BeforeClass
+  public static void startSpark() throws IOException {
+    TestStreamingOffset.spark = SparkSession.builder()
+        .master("local[2]")
+        .config("spark.sql.shuffle.partitions", 4)
+        .getOrCreate();
+
+    parent = Files.createTempDirectory("test");
+    tableLocation = new File(parent.toFile(), "table");
+    tableLocation.mkdir();
+    HadoopTables tables = new HadoopTables(CONF);
+    PartitionSpec spec = PartitionSpec.builderFor(SCHEMA).identity("data").build();
+    table = tables.create(SCHEMA, spec, tableLocation.toString());
+
+    expected = Lists.newArrayList(new SimpleRecord(1, "1"),
+        new SimpleRecord(2, "2"),
+        new SimpleRecord(3, "3"),
+        new SimpleRecord(4, "4"));
+
+    // Write records one by one to generate 3 snapshots.
+    for (int i = 0; i < 3; i++) {
+      Dataset<Row> df = spark.createDataFrame(expected, SimpleRecord.class);
+      df.select("id", "data").write()
+          .format("iceberg")
+          .mode("append")
+          .save(tableLocation.toString());
+    }
+
+    table.refresh();
+  }
+
+  @AfterClass
+  public static void stopSpark() {
+    SparkSession currentSpark = TestStreamingOffset.spark;
+    TestStreamingOffset.spark = null;
+    currentSpark.stop();
+  }
 
   @Test
   public void testStreamingOffsetWithPosition() throws IOException {
     Snapshot currSnap = table.currentSnapshot();
     StreamingOffset startOffset =
-        new StreamingOffset(currSnap.snapshotId(), INIT_SCANNED_FILE_INDEX, false, true);
+        new StreamingOffset(currSnap.snapshotId(), INIT_SCANNED_FILE_INDEX, false);
     StreamingOffset endOffset = startOffset;
     ManifestFile manifest = currSnap.dataManifests().get(0);
     try (ManifestReader<DataFile> reader = ManifestFiles.read(manifest, FILE_IO)) {
-      long expectedPos = startOffset.index();
+      long expectedPos = startOffset.position();
       for (DataFile file : reader) {
         expectedPos += 1;
         Assert.assertEquals("Position should match", (Long) expectedPos, file.pos());
-        endOffset =
-            new StreamingOffset(currSnap.snapshotId(), Math.toIntExact(file.pos()), false, true);
+        endOffset = new StreamingOffset(currSnap.snapshotId(), Math.toIntExact(file.pos()), false);
       }
-      StreamingOffset expectedOffset =
-          new StreamingOffset(currSnap.snapshotId(), (int) expectedPos, false, true);
+      StreamingOffset expectedOffset = new StreamingOffset(currSnap.snapshotId(), (int) expectedPos, false);
       Assert.assertEquals(expectedOffset, endOffset);
     }
   }
@@ -59,8 +133,7 @@ public abstract class TestStreamingOffset extends TestStructuredStreamingRead {
     List<ManifestFile> manifests = table.currentSnapshot().dataManifests();
     List<StreamingOffset> expectedOffsets = Lists.newArrayList();
     for (ManifestFile manifest : manifests) {
-      expectedOffsets
-          .add(new StreamingOffset(manifest.snapshotId(), END_SCANNED_FILE_INDEX, true, true));
+      expectedOffsets.add(new StreamingOffset(manifest.snapshotId(), END_SCANNED_FILE_INDEX, true));
     }
     testStreamingOffsetWithScanFiles(expectedOffsets, true);
   }
@@ -68,9 +141,7 @@ public abstract class TestStreamingOffset extends TestStructuredStreamingRead {
   @Test
   public void testNoScanAllFiles() throws IOException {
     List<StreamingOffset> expectedOffsets = Lists
-        .newArrayList(
-            new StreamingOffset(table.currentSnapshot().snapshotId(), END_SCANNED_FILE_INDEX, false,
-                true));
+        .newArrayList(new StreamingOffset(table.currentSnapshot().snapshotId(), END_SCANNED_FILE_INDEX, false));
     testStreamingOffsetWithScanFiles(expectedOffsets, false);
   }
 
@@ -89,11 +160,9 @@ public abstract class TestStreamingOffset extends TestStructuredStreamingRead {
           expectedPos += 1;
           Assert.assertEquals("Position should match", (Long) expectedPos, file.pos());
           if (scanAllFiles) {
-            offset = new StreamingOffset(manifest.snapshotId(), Math.toIntExact(file.pos()),
-                scanAllFiles, true);
+            offset = new StreamingOffset(manifest.snapshotId(), file.pos(), scanAllFiles);
           } else {
-            offset = new StreamingOffset(currSnap.snapshotId(), Math.toIntExact(file.pos()),
-                scanAllFiles, true);
+            offset = new StreamingOffset(currSnap.snapshotId(), file.pos(), scanAllFiles);
           }
         }
         actualOffsets.add(offset);
