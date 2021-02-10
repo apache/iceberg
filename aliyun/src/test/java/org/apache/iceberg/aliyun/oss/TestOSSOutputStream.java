f0c658c744a3e2c207d110e16082b1f6c3fce75e
huzheng <openinx@gmail.com>
2021-02-10 10:54:24 +0800

    Improve the TestOSSOutputStream.

--- aliyun/src/test/java/org/apache/iceberg/aliyun/oss/TestOSSOutputStream.java
+++ aliyun/src/test/java/org/apache/iceberg/aliyun/oss/TestOSSOutputStream.java
@@ -19,6 +19,7 @@
 
 package org.apache.iceberg.aliyun.oss;
 
+import com.aliyun.oss.OSS;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -35,51 +36,108 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.mockito.AdditionalAnswers.delegatesTo;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 public class TestOSSOutputStream extends OSSTestBase {
   private static final Logger LOG = LoggerFactory.getLogger(TestOSSOutputStream.class);
 
+  private final OSS oss = oss().get();
+  private final OSS ossMock = mock(OSS.class, delegatesTo(oss));
+
   private final Random random = new Random(1);
   private final Path tmpDir = Files.createTempDirectory("oss-file-io-test-");
+
+  private AliyunProperties props = new AliyunProperties(ImmutableMap.of(
+      AliyunProperties.OSS_MULTIPART_SIZE, Integer.toString(5 * 1024 * 1024),
+      AliyunProperties.OSS_MULTIPART_THRESHOLD_SIZE, Integer.toString(8 * 1024 * 1024),
+      AliyunProperties.OSS_STAGING_DIRECTORY, tmpDir.toString()
+  ));
 
   public TestOSSOutputStream() throws IOException {
   }
 
   @Test
-  public void testWriteInt() throws IOException {
+  public void testWrite() throws IOException {
     OSSURI uri = randomURI();
-
-    AliyunProperties props = new AliyunProperties(ImmutableMap.of(
-        AliyunProperties.OSS_MULTIPART_SIZE, Integer.toString(100 * 1024),
-        AliyunProperties.OSS_MULTIPART_THRESHOLD_SIZE, Integer.toString(5 * 1024 * 1024),
-        AliyunProperties.OSS_STAGING_DIRECTORY, tmpDir.toString()
-    ));
 
     for (int i = 0; i < 2; i++) {
       boolean arrayWrite = i % 2 == 0;
       // Write small file that is less than multi part size.
-      writeAndVerify(uri, props, data256(), arrayWrite);
+      writeAndVerify(ossMock, uri, data256(), arrayWrite);
+      verify(ossMock, times(1)).putObject(any());
+      reset(ossMock);
 
-      // Write file which will be uploaded in 1 part.
-      writeAndVerify(uri, props, randomData(100 * 1024), arrayWrite);
-      writeAndVerify(uri, props, randomData(100 * 1024 + 1), arrayWrite);
-
-      // Write file which will be uploaded in 3 parts.
-      writeAndVerify(uri, props, randomData(300 * 1024), arrayWrite);
+      // Write file larger than part size but less than multipart threshold.
+      writeAndVerify(ossMock, uri, randomData(6 * 1024 * 1024), arrayWrite);
+      verify(ossMock, times(1)).putObject(any());
+      reset(ossMock);
 
       // Write file large enough to trigger multipart upload.
       // TODO the current local oss does not support multipart upload.
-      writeAndVerify(uri, props, randomData(10 * 1024 * 1024), arrayWrite);
+      writeAndVerify(ossMock, uri, randomData(10 * 1024 * 1024), arrayWrite);
+      verify(ossMock, times(1)).initiateMultipartUpload(any());
+      verify(ossMock, times(2)).uploadPart(any());
+      verify(ossMock, times(1)).completeMultipartUpload(any());
+      reset(ossMock);
 
       // Test uploading many parts
-      writeAndVerify(uri, props, randomData(30 * 1024 * 1024), arrayWrite);
+      writeAndVerify(ossMock, uri, randomData(22 * 1024 * 1024), arrayWrite);
+      verify(ossMock, times(1)).initiateMultipartUpload(any());
+      verify(ossMock, times(5)).uploadPart(any());
+      verify(ossMock, times(1)).completeMultipartUpload(any());
+      reset(ossMock);
     }
   }
 
-  private void writeAndVerify(OSSURI uri, AliyunProperties props, byte[] data, boolean arrayWrite) throws IOException {
+  @Test
+  public void testAbortAfterFailedPartUpload() {
+    doThrow(new RuntimeException()).when(ossMock).uploadPart(any());
+
+    boolean caughtError = false;
+    try (OSSOutputStream stream = new OSSOutputStream(ossMock, randomURI(), props)) {
+      stream.write(randomData(10 * 1024 * 1024));
+    } catch (Exception e) {
+      caughtError = true;
+      verify(ossMock, times(1)).initiateMultipartUpload(any());
+      verify(ossMock, atLeastOnce()).abortMultipartUpload(any());
+    }
+    Assert.assertTrue("Should have encountered the upload part failure.", caughtError);
+  }
+
+  @Test
+  public void testAbortMultipart() {
+    doThrow(new RuntimeException()).when(ossMock).completeMultipartUpload(any());
+
+    boolean caughtError = false;
+    try (OSSOutputStream stream = new OSSOutputStream(ossMock, randomURI(), props)) {
+      stream.write(randomData(10 * 1024 * 1024));
+    } catch (Exception e) {
+      caughtError = true;
+      verify(ossMock).abortMultipartUpload(any());
+    }
+    Assert.assertTrue("Should have encountered the failure", caughtError);
+  }
+
+  @Test
+  public void testMultipleClose() throws IOException {
+    OSSOutputStream stream = new OSSOutputStream(ossMock, randomURI(), props);
+    stream.close();
+    stream.close();
+  }
+
+  private void writeAndVerify(OSS mock, OSSURI uri, byte[] data, boolean arrayWrite)
+      throws IOException {
     LOG.info("Write and verify for arguments uri: {}, data length: {}, arrayWrite: {}", uri, data.length,
         arrayWrite);
 
-    try (OSSOutputStream out = new OSSOutputStream(oss().get(), uri, props)) {
+    try (OSSOutputStream out = new OSSOutputStream(mock, uri, props)) {
       if (arrayWrite) {
         out.write(data);
         Assert.assertEquals(data.length, out.getPos());
