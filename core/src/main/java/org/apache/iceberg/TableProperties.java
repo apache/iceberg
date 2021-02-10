6e6dddc914141f23a7a1cefa8dc7b60be72857f0
Szehon Ho <szehon.apache@gmail.com>
2021-02-10 19:57:04 +0100

    Core: Change default write.target-file-size-bytes to 512 MB (#2220)
    Co-authored-by: Szehon Ho <szehon_ho@apple.com>

--- core/src/main/java/org/apache/iceberg/TableProperties.java
+++ core/src/main/java/org/apache/iceberg/TableProperties.java
@@ -127,7 +127,7 @@ public class TableProperties {
   public static final String WRITE_AUDIT_PUBLISH_ENABLED_DEFAULT = "false";
 
   public static final String WRITE_TARGET_FILE_SIZE_BYTES = "write.target-file-size-bytes";
-  public static final long WRITE_TARGET_FILE_SIZE_BYTES_DEFAULT = Long.MAX_VALUE;
+  public static final long WRITE_TARGET_FILE_SIZE_BYTES_DEFAULT = 536870912; // 512 MB
 
   public static final String SPARK_WRITE_PARTITIONED_FANOUT_ENABLED = "write.spark.fanout.enabled";
   public static final boolean SPARK_WRITE_PARTITIONED_FANOUT_ENABLED_DEFAULT = false;
