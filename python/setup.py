4c500ba4e746dc6ba87563234ebefc6d7d105001
Ryan Murray <rymurr@dremio.com>
2021-02-25 10:31:02 +0100

    fix arrow issues, remove 3.6 and reduce pinning

--- python/setup.py
+++ python/setup.py
@@ -15,28 +15,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
-import sys
-
 from setuptools import setup
-
-DEPENDENCIES = ['botocore==1.20.14',
-                'boto3==1.17.14',
-                'fastavro==1.3.2',
-                'hmsclient==0.1.1',
-                'mmh3==3.0.0',
-                'pyparsing==2.4.7',
-                'python-dateutil==2.8.1',
-                'pytz==2021.1',
-                'requests==2.25.1',
-                'retrying==1.3.3',
-                'pyarrow==2.0.0'
-                ]
-
-# pandas doesn't get built for 3.6 anymore, we should consider deprecating 3.6
-if sys.version_info.minor == 6 and sys.version_info.major == 3:
-    DEPENDENCIES.append('pandas==1.1.5')
-else:
-    DEPENDENCIES.append('pandas==1.2.2')
 
 setup(
     name='iceberg',
@@ -46,12 +25,23 @@ setup(
     keywords='iceberg',
     url='https://github.com/apache/iceberg/blob/master/README.md',
     python_requires='>=3.6',
-    install_requires=DEPENDENCIES,
+    install_requires=['botocore',
+                      'boto3',
+                      'fastavro==1.3.2',
+                      'hmsclient==0.1.1',
+                      'mmh3',
+                      'pyparsing==2.4.7',
+                      'python-dateutil',
+                      'pytz',
+                      'requests',
+                      'retrying',
+                      'pandas',
+                      'pyarrow==3.0.0'
+                      ],
     setup_requires=['setupmeta'],
     license="Apache License 2.0",
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
