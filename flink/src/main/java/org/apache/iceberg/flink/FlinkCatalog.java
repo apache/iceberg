a7901992c252bb28d10686ef24c1de788c90d663
JunZhang <zhangjunemail@126.com>
2021-03-06 09:54:44 +0800

    Flink: Add FlinkCatalog.catalog accessor (#2259)
    Co-authored-by: zhangjun <jun.zhang1@ly.com>

--- flink/src/main/java/org/apache/iceberg/flink/FlinkCatalog.java
+++ flink/src/main/java/org/apache/iceberg/flink/FlinkCatalog.java
@@ -136,6 +136,10 @@ public class FlinkCatalog extends AbstractCatalog {
     }
   }
 
+  public Catalog catalog() {
+    return icebergCatalog;
+  }
+
   private Namespace toNamespace(String database) {
     String[] namespace = new String[baseNamespace.length + 1];
     System.arraycopy(baseNamespace, 0, namespace, 0, baseNamespace.length);
