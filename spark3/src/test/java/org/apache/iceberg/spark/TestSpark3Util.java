f0bd5e2b79d4df8dc2cf1b352860694dfdac400d
yufeigu <yufei@apache.org>
2021-04-06 18:33:58 -0700

    Spark: Add table sort-order to reserved table properties - fix the style issue

--- spark3/src/test/java/org/apache/iceberg/spark/TestSpark3Util.java
+++ spark3/src/test/java/org/apache/iceberg/spark/TestSpark3Util.java
@@ -19,62 +19,62 @@
 
 package org.apache.iceberg.spark;
 
-import org.junit.Assert;
-import org.junit.Test;
 import org.apache.iceberg.Schema;
 import org.apache.iceberg.SortOrder;
 import org.apache.iceberg.SortOrderParser;
 import org.apache.iceberg.types.Types;
+import org.junit.Assert;
+import org.junit.Test;
 
 import static org.apache.iceberg.NullOrder.NULLS_FIRST;
 import static org.apache.iceberg.NullOrder.NULLS_LAST;
 import static org.apache.iceberg.types.Types.NestedField.required;
 
 public class TestSpark3Util {
-    @Test
-    public void testDescribeSortOrder() {
-      Schema schema = new Schema(
-              required(1, "data", Types.StringType.get()),
-              required(2, "time", Types.TimestampType.withoutZone())
-      );
+  @Test
+  public void testDescribeSortOrder() {
+    Schema schema = new Schema(
+            required(1, "data", Types.StringType.get()),
+            required(2, "time", Types.TimestampType.withoutZone())
+    );
 
-      Assert.assertEquals("Sort order isn't correct.", "data DESC NULLS FIRST",
-          Spark3Util.describe(buildSortOrder("Identity", schema, 1)));
-      Assert.assertEquals("Sort order isn't correct.", "bucket(1, data) DESC NULLS FIRST",
-          Spark3Util.describe(buildSortOrder("bucket[1]", schema, 1)));
-      Assert.assertEquals("Sort order isn't correct.", "truncate(data, 3) DESC NULLS FIRST",
-          Spark3Util.describe(buildSortOrder("truncate[3]", schema, 1)));
-      Assert.assertEquals("Sort order isn't correct.", "years(time) DESC NULLS FIRST",
-          Spark3Util.describe(buildSortOrder("year", schema, 2)));
-      Assert.assertEquals("Sort order isn't correct.", "months(time) DESC NULLS FIRST",
-          Spark3Util.describe(buildSortOrder("month", schema, 2)));
-      Assert.assertEquals("Sort order isn't correct.", "days(time) DESC NULLS FIRST",
-          Spark3Util.describe(buildSortOrder("day", schema, 2)));
-      Assert.assertEquals("Sort order isn't correct.", "hours(time) DESC NULLS FIRST",
-          Spark3Util.describe(buildSortOrder("hour", schema, 2)));
-      Assert.assertEquals("Sort order isn't correct.", "unknown(data) DESC NULLS FIRST",
-          Spark3Util.describe(buildSortOrder("unknown", schema, 1)));
+    Assert.assertEquals("Sort order isn't correct.", "data DESC NULLS FIRST",
+        Spark3Util.describe(buildSortOrder("Identity", schema, 1)));
+    Assert.assertEquals("Sort order isn't correct.", "bucket(1, data) DESC NULLS FIRST",
+        Spark3Util.describe(buildSortOrder("bucket[1]", schema, 1)));
+    Assert.assertEquals("Sort order isn't correct.", "truncate(data, 3) DESC NULLS FIRST",
+        Spark3Util.describe(buildSortOrder("truncate[3]", schema, 1)));
+    Assert.assertEquals("Sort order isn't correct.", "years(time) DESC NULLS FIRST",
+        Spark3Util.describe(buildSortOrder("year", schema, 2)));
+    Assert.assertEquals("Sort order isn't correct.", "months(time) DESC NULLS FIRST",
+        Spark3Util.describe(buildSortOrder("month", schema, 2)));
+    Assert.assertEquals("Sort order isn't correct.", "days(time) DESC NULLS FIRST",
+        Spark3Util.describe(buildSortOrder("day", schema, 2)));
+    Assert.assertEquals("Sort order isn't correct.", "hours(time) DESC NULLS FIRST",
+        Spark3Util.describe(buildSortOrder("hour", schema, 2)));
+    Assert.assertEquals("Sort order isn't correct.", "unknown(data) DESC NULLS FIRST",
+        Spark3Util.describe(buildSortOrder("unknown", schema, 1)));
 
-      // multiple sort orders
-      SortOrder multiOrder = SortOrder.builderFor(schema)
-              .asc("time", NULLS_FIRST)
-              .asc("data", NULLS_LAST)
-              .build();
-      Assert.assertEquals("Sort order isn't correct.", "time ASC NULLS FIRST, data ASC NULLS LAST",
-              Spark3Util.describe(multiOrder));
-    }
+    // multiple sort orders
+    SortOrder multiOrder = SortOrder.builderFor(schema)
+            .asc("time", NULLS_FIRST)
+            .asc("data", NULLS_LAST)
+            .build();
+    Assert.assertEquals("Sort order isn't correct.", "time ASC NULLS FIRST, data ASC NULLS LAST",
+            Spark3Util.describe(multiOrder));
+  }
 
-    private SortOrder buildSortOrder(String transform, Schema schema, int sourceId) {
-      String jsonString = "{\n" +
-              "  \"order-id\" : 10,\n" +
-              "  \"fields\" : [ {\n" +
-              "    \"transform\" : \"" + transform + "\",\n" +
-              "    \"source-id\" : " + sourceId + ",\n" +
-              "    \"direction\" : \"desc\",\n" +
-              "    \"null-order\" : \"nulls-first\"\n" +
-              "  } ]\n" +
-              "}";
+  private SortOrder buildSortOrder(String transform, Schema schema, int sourceId) {
+    String jsonString = "{\n" +
+            "  \"order-id\" : 10,\n" +
+            "  \"fields\" : [ {\n" +
+            "    \"transform\" : \"" + transform + "\",\n" +
+            "    \"source-id\" : " + sourceId + ",\n" +
+            "    \"direction\" : \"desc\",\n" +
+            "    \"null-order\" : \"nulls-first\"\n" +
+            "  } ]\n" +
+            "}";
 
-      return SortOrderParser.fromJson(schema, jsonString);
-    }
+    return SortOrderParser.fromJson(schema, jsonString);
+  }
 }
