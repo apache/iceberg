17ffc401ac18e2283a240e3dfee38f85e45c247d
Holden Karau <hkarau@apple.com>
2021-05-18 10:39:54 -0700

    Restore collectors (removed from master branch)

--- core/src/main/java/org/apache/iceberg/SchemaUpdate.java
+++ core/src/main/java/org/apache/iceberg/SchemaUpdate.java
@@ -25,6 +25,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 import org.apache.iceberg.mapping.MappingUtil;
 import org.apache.iceberg.mapping.NameMapping;
 import org.apache.iceberg.mapping.NameMappingParser;
