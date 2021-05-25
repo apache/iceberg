1e4ed6d0d2ef68c3bb94ab63ccaa0d351bc31672
Gidon Gershinsky <ggershinsky@apple.com>
2021-05-25 15:57:51 +0300

    native encryption paramateres

---
+++ core/src/main/java/org/apache/iceberg/encryption/NativeFileDecryption.java
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+package org.apache.iceberg.encryption;
+
+import java.nio.ByteBuffer;
+import java.util.Map;
+
+/**
+ * The data keys and other parameters should be retrieved/unwrapped centrally (e.g., in a driver), by parsing the
+ * manifest key_metadata entry for a data file; and then sent to the worker that reads/decrypts this file in a native
+ * format.
+ * Key unwrapping requires authorization checks, and can involve interaction with a KMS. Therefore, unwrap only
+ * projected columns.
+ */
+public class NativeFileDecryption {
+  private ByteBuffer fileAadPrefix;
+  private Map<String, ByteBuffer> fileDataKeys;
+
+  private NativeFileDecryption(Map<String, ByteBuffer> fileDataKeys, ByteBuffer fileAadPrefix) {
+    this.fileDataKeys = fileDataKeys;
+    this.fileAadPrefix = fileAadPrefix;
+  }
+
+  /**
+   * Data encryption keys for a single file.
+   * NOTE: pass keys only for projected columns.
+   * @param dataKeys Map dekId -> dek.
+   *                 dekId is unique only within single file scope.
+   *                 dekIds are retrieved from manifest key_metadata field, along with the wrapped DEKs.
+   */
+  public static Builder create(Map<String, ByteBuffer> dataKeys) {
+    return new Builder(dataKeys);
+  }
+
+  public static class Builder {
+    private ByteBuffer fileAadPrefix;
+    private Map<String, ByteBuffer> fileDataKeys;
+
+    private Builder(Map<String, ByteBuffer> dataKeys) {
+      this.fileDataKeys = dataKeys;
+    }
+
+    public Builder aadPrefix(ByteBuffer aadPrefix) {
+      this.fileAadPrefix = aadPrefix;
+      return this;
+    }
+
+    public NativeFileDecryption build() {
+      return new NativeFileDecryption(fileDataKeys, fileAadPrefix);
+    }
+  }
+
+  public ByteBuffer aadPrefix() {
+    return fileAadPrefix;
+  }
+
+  public Map<String, ByteBuffer> fileDataKeys() {
+    return fileDataKeys;
+  }
+}
---
+++ core/src/main/java/org/apache/iceberg/encryption/NativeFileEncryption.java
@@ -0,0 +1,103 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+package org.apache.iceberg.encryption;
+
+import java.nio.ByteBuffer;
+import java.util.Map;
+
+/**
+ * The data keys and other parameters should be generated centrally (e.g., in a driver).
+ * Each object (/set of keys) must be created for one data file only, and sent to the worker that writes/encrypts
+ * this file in a native format.
+ * The central process, that generates the data keys, will wrap them (encrypt with master keys) and store in the
+ * manifest key_metadata entry for the data file. Key wrapping can involve interaction with a KMS.
+ */
+public class NativeFileEncryption {
+  private ByteBuffer fileAadPrefix;
+  private Map<String, ByteBuffer> fileDataKeys;
+  private String fileDekId;
+  private Map<String, String> columnDekIds;
+
+  private NativeFileEncryption(Map<String, ByteBuffer> fileDataKeys, String fileDekId,
+                              Map<String, String> columnDekIds, ByteBuffer fileAadPrefix) {
+    // TODO check
+    this.fileDataKeys = fileDataKeys;
+    this.fileDekId = fileDekId;
+    this.columnDekIds = columnDekIds;
+    this.fileAadPrefix = fileAadPrefix;
+  }
+
+  /**
+   * Data encryption keys for a single file.
+   * @param dataKeys Map dekId -> dek.
+   *                 dekId is unique only within single file scope, can be a simple counter.
+   *                 dekIds must be stored in manifest key_metadata field, along with the wrapped DEKs.
+   */
+  public static Builder create(Map<String, ByteBuffer> dataKeys) {
+    return new Builder(dataKeys);
+  }
+
+  public static class Builder {
+    private ByteBuffer fileAadPrefix;
+    private Map<String, ByteBuffer> fileDataKeys;
+    private String fileDekId;
+    private Map<String, String> columnDekIds;
+
+    private Builder(Map<String, ByteBuffer> dataKeys) {
+      this.fileDataKeys = dataKeys;
+    }
+
+    public Builder fileKeyId(String keyId) {
+      this.fileDekId = keyId;
+      return this;
+    }
+
+    public Builder columnKeyIds(Map<String, String> columnKeyIds) {
+      this.columnDekIds = columnKeyIds;
+      return this;
+    }
+
+    public Builder aadPrefix(ByteBuffer aadPrefix) {
+      this.fileAadPrefix = aadPrefix;
+      return this;
+    }
+
+    public NativeFileEncryption build() {
+      return new NativeFileEncryption(fileDataKeys, fileDekId, columnDekIds, fileAadPrefix);
+    }
+  }
+
+  public ByteBuffer aadPrefix() {
+    return fileAadPrefix;
+  }
+
+  public String fileDekId() {
+    return fileDekId;
+  }
+
+  public Map<String, ByteBuffer> fileDataKeys() {
+    return fileDataKeys;
+  }
+
+  public Map<String, String> columnDekIds() {
+    return columnDekIds;
+  }
+}
