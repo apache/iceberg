fc1f83aae5352df4d97b1bcc4aa17df90a43f7e2
chenliang <southernriver@users.noreply.github.com>
2021-06-03 11:22:39 +0800

    Update condition of  MicroBatch.open
    Param scanAllFiles Used to check whether all the data files should be processed, or only added files.Here we should replace  scanAllFiles to  !scanAllFiles.

--- core/src/main/java/org/apache/iceberg/MicroBatches.java
+++ core/src/main/java/org/apache/iceberg/MicroBatches.java
@@ -242,7 +242,7 @@ public class MicroBatches {
       ManifestGroup manifestGroup = new ManifestGroup(io, ImmutableList.of(manifestFile))
           .specsById(specsById)
           .caseSensitive(caseSensitive);
-      if (scanAllFiles) {
+      if (!scanAllFiles) {
         manifestGroup = manifestGroup
             .filterManifestEntries(entry ->
                 entry.snapshotId() == snapshot.snapshotId() && entry.status() == ManifestEntry.Status.ADDED)
