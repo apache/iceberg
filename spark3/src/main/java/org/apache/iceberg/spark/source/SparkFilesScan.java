557c6a2f930eec125c47179a1d92588c50a6b1e1
Russell_Spitzer <rspitzer@apple.com>
2021-06-23 13:12:51 -0500

    Remove unused import

--- spark3/src/main/java/org/apache/iceberg/spark/source/SparkFilesScan.java
+++ spark3/src/main/java/org/apache/iceberg/spark/source/SparkFilesScan.java
@@ -27,7 +27,6 @@ import org.apache.iceberg.FileScanTask;
 import org.apache.iceberg.Table;
 import org.apache.iceberg.exceptions.ValidationException;
 import org.apache.iceberg.io.CloseableIterable;
-import org.apache.iceberg.relocated.com.google.common.base.Preconditions;
 import org.apache.iceberg.relocated.com.google.common.collect.ImmutableList;
 import org.apache.iceberg.relocated.com.google.common.collect.Lists;
 import org.apache.iceberg.spark.FileScanTaskSetManager;
