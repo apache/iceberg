c92092f5260fbe295dab67b374d52c706c62271b
Karuppayya <karuppayya1990@gmail.com>
2021-07-01 17:25:48 -0700

    Spark: RemoveReachableFiles action should fail if GC is disabled (#2763)
    Co-authored-by: Karuppayya Rajendran <karuppayya.rajendran@apple.com>

--- spark/src/main/java/org/apache/iceberg/spark/actions/BaseRemoveReachableFilesSparkAction.java
+++ spark/src/main/java/org/apache/iceberg/spark/actions/BaseRemoveReachableFilesSparkAction.java
@@ -31,6 +31,7 @@ import org.apache.iceberg.TableMetadataParser;
 import org.apache.iceberg.actions.BaseRemoveFilesActionResult;
 import org.apache.iceberg.actions.RemoveReachableFiles;
 import org.apache.iceberg.exceptions.NotFoundException;
+import org.apache.iceberg.exceptions.ValidationException;
 import org.apache.iceberg.hadoop.HadoopFileIO;
 import org.apache.iceberg.io.FileIO;
 import org.apache.iceberg.relocated.com.google.common.base.Preconditions;
@@ -45,6 +46,9 @@ import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.functions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static org.apache.iceberg.TableProperties.GC_ENABLED;
+import static org.apache.iceberg.TableProperties.GC_ENABLED_DEFAULT;
 
 /**
  * An implementation of {@link RemoveReachableFiles} that uses metadata tables in Spark
@@ -65,7 +69,8 @@ public class BaseRemoveReachableFilesSparkAction
   // Creates an executor service that runs each task in the thread that invokes execute/submit.
   private static final ExecutorService DEFAULT_DELETE_EXECUTOR_SERVICE = null;
 
-  private final String metadataLocation;
+  private final TableMetadata tableMetadata;
+
   private final Consumer<String> defaultDelete = new Consumer<String>() {
     @Override
     public void accept(String file) {
@@ -79,7 +84,10 @@ public class BaseRemoveReachableFilesSparkAction
 
   public BaseRemoveReachableFilesSparkAction(SparkSession spark, String metadataLocation) {
     super(spark);
-    this.metadataLocation = metadataLocation;
+    this.tableMetadata = TableMetadataParser.read(io, metadataLocation);
+    ValidationException.check(
+        PropertyUtil.propertyAsBoolean(tableMetadata.properties(), GC_ENABLED, GC_ENABLED_DEFAULT),
+        "Cannot remove files: GC is disabled (deleting files may corrupt other tables)");
   }
 
   @Override
@@ -109,15 +117,14 @@ public class BaseRemoveReachableFilesSparkAction
   @Override
   public Result execute() {
     Preconditions.checkArgument(io != null, "File IO cannot be null");
-    String msg = String.format("Removing files reachable from %s", metadataLocation);
+    String msg = String.format("Removing files reachable from %s", tableMetadata.metadataFileLocation());
     JobGroupInfo info = newJobGroupInfo("REMOVE-FILES", msg);
     return withJobGroupInfo(info, this::doExecute);
   }
 
   private Result doExecute() {
     boolean streamResults = PropertyUtil.propertyAsBoolean(options(), STREAM_RESULTS, false);
-    TableMetadata metadata = TableMetadataParser.read(io, metadataLocation);
-    Dataset<Row> validFileDF = buildValidFileDF(metadata).distinct();
+    Dataset<Row> validFileDF = buildValidFileDF(tableMetadata).distinct();
     if (streamResults) {
       return deleteFiles(validFileDF.toLocalIterator());
     } else {
