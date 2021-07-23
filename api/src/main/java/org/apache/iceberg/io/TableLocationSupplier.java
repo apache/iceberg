e0454b0a253454d5cc4e2f2a9d6e46d884377715
Daniel Weeks <dweeks@apache.org>
2021-07-23 14:35:33 -0700

    Checkstyle and license

--- api/src/main/java/org/apache/iceberg/io/TableLocationSupplier.java
+++ api/src/main/java/org/apache/iceberg/io/TableLocationSupplier.java
@@ -1,16 +1,22 @@
 /*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
+
 
 package org.apache.iceberg.io;
 
@@ -26,11 +32,31 @@ import org.apache.iceberg.catalog.TableIdentifier;
  * contextual information from the table.
  */
 public interface TableLocationSupplier extends Supplier<String> {
-  default TableLocationSupplier uuid(String uuid) { return this; }
-  default TableLocationSupplier identifier(TableIdentifier identifier) { return this; }
-  default TableLocationSupplier schema(Schema schema) { return this; }
-  default TableLocationSupplier partitionSpec(PartitionSpec partitionSpec) { return this; }
-  default TableLocationSupplier location(String currentLocation) { return this; }
-  default TableLocationSupplier sortOrder(SortOrder sortOrder) { return this; }
-  default TableLocationSupplier properties(Map<String, String> properties) { return this; }
+  default TableLocationSupplier uuid(String uuid) {
+    return this;
+  }
+
+  default TableLocationSupplier identifier(TableIdentifier identifier) {
+    return this;
+  }
+
+  default TableLocationSupplier schema(Schema schema) {
+    return this;
+  }
+
+  default TableLocationSupplier partitionSpec(PartitionSpec partitionSpec) {
+    return this;
+  }
+
+  default TableLocationSupplier location(String currentLocation) {
+    return this;
+  }
+
+  default TableLocationSupplier sortOrder(SortOrder sortOrder) {
+    return this;
+  }
+
+  default TableLocationSupplier properties(Map<String, String> properties) {
+    return this;
+  }
 }
