1551b1d07b86ea1b30281fb7c3fb1a3b9a340e50
Jun He <jun-he@users.noreply.github.com>
2021-08-01 17:46:54 -0700

    update unit tests

--- python/tests/api/types/test_type_util.py
+++ python/tests/api/types/test_type_util.py
@@ -15,22 +15,35 @@
 # specific language governing permissions and limitations
 # under the License.
 
-import unittest
-
 from iceberg.api.types import (BooleanType, NestedField, StructType)
 from iceberg.api.types import type_util
 from iceberg.exceptions import ValidationException
+import pytest
 
 
-class TestConversions(unittest.TestCase):
+def test_invalid_schema_via_index_by_name():
+    bool_type1 = NestedField.required(1, "a", BooleanType.get())
+    bool_type2 = NestedField.required(2, "a", BooleanType.get())
 
-    def test_validate_schema_via_index_by_name(self):
-        nested_type = NestedField.required(
-            1, "a", StructType.of(
-                [NestedField.required(2, "b", StructType.of(
-                    [NestedField.required(3, "c", BooleanType.get())])),
-                 NestedField.required(4, "b.c", BooleanType.get())]))
+    with pytest.raises(ValidationException) as context:
+        type_util.index_by_name(StructType.of([bool_type1, bool_type2]))
+    assert str(context.value) == 'Invalid schema: multiple fields for name a: 1 and 2'
 
-        with self.assertRaises(ValidationException) as context:
-            type_util.index_by_name(StructType.of([nested_type]))
-        self.assertTrue('Invalid schema: multiple fields for name a.b.c: 3 and 4' in str(context.exception))
+
+def test_valid_schema_via_index_by_name():
+    bool_type1 = NestedField.required(1, "a", BooleanType.get())
+    bool_type2 = NestedField.required(2, "b", BooleanType.get())
+
+    assert {'a': 1, 'b': 2} == type_util.index_by_name(StructType.of([bool_type1, bool_type2]))
+
+
+def test_validate_schema_via_index_by_name_for_nested_type():
+    nested_type = NestedField.required(
+        1, "a", StructType.of(
+            [NestedField.required(2, "b", StructType.of(
+                [NestedField.required(3, "c", BooleanType.get())])),
+             NestedField.required(4, "b.c", BooleanType.get())]))
+
+    with pytest.raises(ValidationException) as context:
+        type_util.index_by_name(StructType.of([nested_type]))
+    assert str(context.value) == 'Invalid schema: multiple fields for name a.b.c: 3 and 4'
