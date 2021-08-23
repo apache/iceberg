0f975362cd1e3dd22800c6017fd5e9544662000e
Szehon Ho <szehon.apache@gmail.com>
2021-08-23 09:40:31 -0700

    Hive: Fix some message typos in HiveCatalog: Matastore => Metastore (#2950)

--- hive-metastore/src/main/java/org/apache/iceberg/hive/HiveCatalog.java
+++ hive-metastore/src/main/java/org/apache/iceberg/hive/HiveCatalog.java
@@ -236,7 +236,7 @@ public class HiveCatalog extends BaseMetastoreCatalog implements SupportsNamespa
         !namespace.isEmpty(),
         "Cannot create namespace with invalid name: %s", namespace);
     Preconditions.checkArgument(isValidateNamespace(namespace),
-        "Cannot support multi part namespace in Hive MetaStore: %s", namespace);
+        "Cannot support multi part namespace in Hive Metastore: %s", namespace);
 
     try {
       clients.run(client -> {
@@ -251,12 +251,12 @@ public class HiveCatalog extends BaseMetastoreCatalog implements SupportsNamespa
             namespace);
 
     } catch (TException e) {
-      throw new RuntimeException("Failed to create namespace " + namespace + " in Hive MataStore", e);
+      throw new RuntimeException("Failed to create namespace " + namespace + " in Hive Metastore", e);
 
     } catch (InterruptedException e) {
       Thread.currentThread().interrupt();
       throw new RuntimeException(
-          "Interrupted in call to createDatabase(name) " + namespace + " in Hive MataStore", e);
+          "Interrupted in call to createDatabase(name) " + namespace + " in Hive Metastore", e);
     }
   }
 
@@ -278,12 +278,12 @@ public class HiveCatalog extends BaseMetastoreCatalog implements SupportsNamespa
       return namespaces;
 
     } catch (TException e) {
-      throw new RuntimeException("Failed to list all namespace: " + namespace + " in Hive MataStore",  e);
+      throw new RuntimeException("Failed to list all namespace: " + namespace + " in Hive Metastore",  e);
 
     } catch (InterruptedException e) {
       Thread.currentThread().interrupt();
       throw new RuntimeException(
-          "Interrupted in call to getAllDatabases() " + namespace + " in Hive MataStore", e);
+          "Interrupted in call to getAllDatabases() " + namespace + " in Hive Metastore", e);
     }
   }
 
@@ -312,12 +312,12 @@ public class HiveCatalog extends BaseMetastoreCatalog implements SupportsNamespa
       return false;
 
     } catch (TException e) {
-      throw new RuntimeException("Failed to drop namespace " + namespace + " in Hive MataStore", e);
+      throw new RuntimeException("Failed to drop namespace " + namespace + " in Hive Metastore", e);
 
     } catch (InterruptedException e) {
       Thread.currentThread().interrupt();
       throw new RuntimeException(
-          "Interrupted in call to drop dropDatabase(name) " + namespace + " in Hive MataStore", e);
+          "Interrupted in call to drop dropDatabase(name) " + namespace + " in Hive Metastore", e);
     }
   }
 
@@ -363,11 +363,11 @@ public class HiveCatalog extends BaseMetastoreCatalog implements SupportsNamespa
 
     } catch (TException e) {
       throw new RuntimeException(
-          "Failed to list namespace under namespace: " + namespace + " in Hive MataStore", e);
+          "Failed to list namespace under namespace: " + namespace + " in Hive Metastore", e);
 
     } catch (InterruptedException e) {
       Thread.currentThread().interrupt();
-      throw new RuntimeException("Interrupted in call to getDatabase(name) " + namespace + " in Hive MataStore", e);
+      throw new RuntimeException("Interrupted in call to getDatabase(name) " + namespace + " in Hive Metastore", e);
     }
   }
 
@@ -387,12 +387,12 @@ public class HiveCatalog extends BaseMetastoreCatalog implements SupportsNamespa
       throw new NoSuchNamespaceException(e, "Namespace does not exist: %s", namespace);
 
     } catch (TException e) {
-      throw new RuntimeException("Failed to list namespace under namespace: " + namespace + " in Hive MataStore", e);
+      throw new RuntimeException("Failed to list namespace under namespace: " + namespace + " in Hive Metastore", e);
 
     } catch (InterruptedException e) {
       Thread.currentThread().interrupt();
       throw new RuntimeException(
-          "Interrupted in call to getDatabase(name) " + namespace + " in Hive MataStore", e);
+          "Interrupted in call to getDatabase(name) " + namespace + " in Hive Metastore", e);
     }
   }
 
