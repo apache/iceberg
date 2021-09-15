5d1aa357509c6f42067dbb0e21c76094ab4c8081
Szehon Ho <szehon.apache@gmail.com>
2021-09-14 17:43:53 -0700

    Core: Promote sorted column metrics to truncate(16) (#2240)

--- core/src/main/java/org/apache/iceberg/util/SortOrderUtil.java
+++ core/src/main/java/org/apache/iceberg/util/SortOrderUtil.java
@@ -20,6 +20,10 @@
 package org.apache.iceberg.util;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.apache.iceberg.PartitionField;
 import org.apache.iceberg.PartitionSpec;
 import org.apache.iceberg.Schema;
@@ -63,5 +67,18 @@ public class SortOrderUtil {
     SortOrderVisitor.visit(sortOrder, new CopySortOrderFields(builder));
 
     return builder.build();
+  }
+
+  public static Set<String> orderPreservingSortedColumns(SortOrder sortOrder) {
+    if (sortOrder == null) {
+      return Collections.emptySet();
+    } else {
+      return sortOrder.fields().stream()
+          .filter(f -> f.transform().preservesOrder())
+          .map(SortField::sourceId)
+          .map(sid -> sortOrder.schema().findColumnName(sid))
+          .filter(Objects::nonNull)
+          .collect(Collectors.toSet());
+    }
   }
 }
