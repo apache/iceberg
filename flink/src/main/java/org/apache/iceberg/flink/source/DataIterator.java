stevenzwu/incubator-iceberg

Iceberg source split and split reader

flink/src/main/java/org/apache/iceberg/flink/source/DataIterator.java
@@ -25,6 +25,7 @@ import java.nio.ByteBuffer;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.stream.Stream;
+import org.apache.flink.connector.file.src.util.CheckpointedPosition;
 import org.apache.iceberg.CombinedScanTask;
 import org.apache.iceberg.FileScanTask;
 import org.apache.iceberg.encryption.EncryptedFiles;
@@ -42,18 +43,19 @@ import org.apache.iceberg.relocated.com.google.common.collect.Maps;
  *
  * @param <T> is the Java class returned by this iterator whose objects contain one or more rows.
  */
-abstract class DataIterator<T> implements CloseableIterator<T> {
+public abstract class DataIterator<T> implements CloseableIterator<T> {
 
-  private Iterator<FileScanTask> tasks;
+  private final CombinedScanTask combinedTask;
   private final Map<String, InputFile> inputFiles;
 
+  private Iterator<FileScanTask> tasks;
   private CloseableIterator<T> currentIterator;
+  private Position position;
 
-  DataIterator(CombinedScanTask task, FileIO io, EncryptionManager encryption) {
-    this.tasks = task.files().iterator();
-
+  DataIterator(CombinedScanTask combinedTask, FileIO io, EncryptionManager encryption) {
+    this.combinedTask = combinedTask;
     Map<String, ByteBuffer> keyMetadata = Maps.newHashMap();
-    task.files().stream()
+    combinedTask.files().stream()
         .flatMap(fileScanTask -> Stream.concat(Stream.of(fileScanTask.file()), fileScanTask.deletes().stream()))
         .forEach(file -> keyMetadata.put(file.path().toString(), file.keyMetadata()));
     Stream<EncryptedInputFile> encrypted = keyMetadata.entrySet().stream()
@@ -62,11 +64,13 @@ abstract class DataIterator<T> implements CloseableIterator<T> {
     // decrypt with the batch call to avoid multiple RPCs to a key server, if possible
     Iterable<InputFile> decryptedFiles = encryption.decrypt(encrypted::iterator);
 
-    Map<String, InputFile> files = Maps.newHashMapWithExpectedSize(task.files().size());
+    Map<String, InputFile> files = Maps.newHashMapWithExpectedSize(combinedTask.files().size());
     decryptedFiles.forEach(decrypted -> files.putIfAbsent(decrypted.location(), decrypted));
     this.inputFiles = ImmutableMap.copyOf(files);
 
+    this.tasks = combinedTask.files().iterator();
     this.currentIterator = CloseableIterator.empty();
+    this.position = new Position();
   }
 
   InputFile getInputFile(FileScanTask task) {
@@ -79,6 +83,27 @@ abstract class DataIterator<T> implements CloseableIterator<T> {
     return inputFiles.get(location);
   }
 
+  public void seek(CheckpointedPosition checkpointedPosition)  {
+    // skip files
+    Preconditions.checkArgument(checkpointedPosition.getOffset() < combinedTask.files().size(),
+        "Checkpointed file offset is %d, while CombinedScanTask has %d files",
+            checkpointedPosition.getOffset(), combinedTask.files().size());
+    for (long i = 0L; i < checkpointedPosition.getOffset(); ++i) {
+      tasks.next();
+    }
+    updateCurrentIterator();
+    // skip records within the file
+    for (long i = 0; i < checkpointedPosition.getRecordsAfterOffset(); ++i) {
+      if (hasNext()) {
+        next();
+      } else {
+        throw new IllegalStateException("Not enough records to skip: " +
+            checkpointedPosition.getRecordsAfterOffset());
+      }
+    }
+    position = new Position(checkpointedPosition);
+  }
+
   @Override
   public boolean hasNext() {
     updateCurrentIterator();
@@ -88,7 +113,12 @@ abstract class DataIterator<T> implements CloseableIterator<T> {
   @Override
   public T next() {
     updateCurrentIterator();
+    position.advanceRecord();
     return currentIterator.next();
+  }
+
+  public boolean isCurrentIteratorDone() {
+    return !currentIterator.hasNext();
   }
 
   /**
@@ -100,6 +130,7 @@ abstract class DataIterator<T> implements CloseableIterator<T> {
       while (!currentIterator.hasNext() && tasks.hasNext()) {
         currentIterator.close();
         currentIterator = openTaskIterator(tasks.next());
+        position.advanceFile();
       }
     } catch (IOException e) {
       throw new UncheckedIOException(e);
@@ -113,5 +144,44 @@ abstract class DataIterator<T> implements CloseableIterator<T> {
     // close the current iterator
     currentIterator.close();
     tasks = null;
+  }
+
+  public Position position() {
+    return position;
+  }
+
+  public static class Position {
+
+    private long fileOffset;
+    private long recordOffset;
+
+    Position() {
+      // fileOffset starts at -1 because we started
+      // from an empty iterator that is not from the split files.
+      this.fileOffset = -1L;
+      this.recordOffset = 0L;
+    }
+
+    Position(CheckpointedPosition checkpointedPosition) {
+      this.fileOffset = checkpointedPosition.getOffset();
+      this.recordOffset = checkpointedPosition.getRecordsAfterOffset();
+    }
+
+    void advanceFile() {
+      this.fileOffset += 1;
+      this.recordOffset = 0L;
+    }
+
+    void advanceRecord() {
+      this.recordOffset += 1L;
+    }
+
+    public long fileOffset() {
+      return fileOffset;
+    }
+
+    public long recordOffset() {
+      return recordOffset;
+    }
   }
 }

flink/src/test/java/org/apache/iceberg/flink/source/reader/TestRecyclableArrayIterator.java
@@ -1,0 +1,85 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iceberg.flink.source.reader;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.apache.flink.connector.file.src.util.RecordAndPosition;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestRecyclableArrayIterator {
+
+  @Test
+  public void testEmptyConstruction() {
+    // dummy recycler
+    final RecyclableArrayIterator<Object> iter = new RecyclableArrayIterator<>(
+        ignored -> System.currentTimeMillis());
+    Assert.assertNull(iter.next());
+  }
+
+  @Test
+  public void testGetElements() {
+    final String[] elements = new String[]{"1", "2", "3", "4"};
+    final long initialOffset = 3;
+    final long initialSkipCount = 17;
+
+    // dummy recycler
+    final RecyclableArrayIterator<String> iter = new RecyclableArrayIterator<>(
+        ignored -> System.currentTimeMillis(), elements, elements.length, initialOffset, initialSkipCount);
+
+    for (int i = 0; i < elements.length; i++) {
+      final RecordAndPosition<String> recAndPos = iter.next();
+      Assert.assertEquals(elements[i], recAndPos.getRecord());
+      Assert.assertEquals(initialOffset, recAndPos.getOffset());
+      Assert.assertEquals(initialSkipCount + i + 1, recAndPos.getRecordSkipCount());
+    }
+  }
+
+  @Test
+  public void testExhausted() {
+    // dummy recycler
+    final RecyclableArrayIterator<String> iter = new RecyclableArrayIterator<>(
+        ignored -> System.currentTimeMillis(), new String[]{"1", "2"}, 2, 0L, 0L);
+
+    iter.next();
+    iter.next();
+
+    Assert.assertNull(iter.next());
+  }
+
+  @Test
+  public void testArraySubRange() {
+    // dummy recycler
+    final RecyclableArrayIterator<String> iter = new RecyclableArrayIterator<>(ignored -> System.currentTimeMillis(),
+        new String[]{"1", "2", "3"}, 2, 0L, 0L);
+
+    Assert.assertNotNull(iter.next());
+    Assert.assertNotNull(iter.next());
+    Assert.assertNull(iter.next());
+  }
+
+  @Test
+  public void testRecycler() {
+    final AtomicBoolean recycled = new AtomicBoolean();
+    final RecyclableArrayIterator<String> iter = new RecyclableArrayIterator<>(ignored -> recycled.set(true));
+    iter.releaseBatch();
+    Assert.assertTrue(recycled.get());
+  }
+}

flink/src/test/java/org/apache/iceberg/flink/source/split/TestIcebergSourceSplitSerializer.java
@@ -1,0 +1,100 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iceberg.flink.source.split;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import org.apache.flink.connector.file.src.util.CheckpointedPosition;
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class TestIcebergSourceSplitSerializer {
+
+  @ClassRule
+  public static final TemporaryFolder TEMPORARY_FOLDER = new TemporaryFolder();
+
+  private final IcebergSourceSplitSerializer serializer = IcebergSourceSplitSerializer.INSTANCE;
+
+  @Test
+  public void testLatestVersion() throws Exception {
+    serializeAndDeserialize(1, 1);
+    serializeAndDeserialize(10, 2);
+  }
+
+  private void serializeAndDeserialize(int splitCount, int filesPerSplit) throws Exception {
+    final List<IcebergSourceSplit> splits = SplitHelpers.createFileSplits(TEMPORARY_FOLDER, splitCount, filesPerSplit);
+    for (IcebergSourceSplit split : splits) {
+      final byte[] result = serializer.serialize(split);
+      final IcebergSourceSplit deserialized = serializer.deserialize(serializer.getVersion(), result);
+      Assert.assertEquals(split, deserialized);
+
+      final byte[] cachedResult = serializer.serialize(split);
+      Assert.assertSame(result, cachedResult);
+      final IcebergSourceSplit deserialized2 = serializer.deserialize(serializer.getVersion(), cachedResult);
+      Assert.assertEquals(split, deserialized2);
+    }
+  }
+
+  @Test
+  public void testV1() throws Exception {
+    serializeAndDeserializeV1(1, 1);
+    serializeAndDeserializeV1(10, 2);
+  }
+
+  private void serializeAndDeserializeV1(int splitCount, int filesPerSplit) throws Exception {
+    final List<IcebergSourceSplit> splits = SplitHelpers.createFileSplits(TEMPORARY_FOLDER, splitCount, filesPerSplit);
+    for (IcebergSourceSplit split : splits) {
+      final byte[] result = serializer.serializeV1(split);
+      final IcebergSourceSplit deserialized = serializer.deserializeV1(result);
+      Assert.assertEquals(split, deserialized);
+    }
+  }
+
+  @Test
+  public void testCheckpointedPosition() throws Exception {
+    final AtomicInteger index = new AtomicInteger();
+    final List<IcebergSourceSplit> splits = SplitHelpers.createFileSplits(TEMPORARY_FOLDER, 10, 2).stream()
+        .map(split -> {
+          final IcebergSourceSplit result;
+          if (index.get() % 2 == 0) {
+            result = new IcebergSourceSplit(split.task(), new CheckpointedPosition(index.get(), index.get()));
+          } else {
+            result = split;
+          }
+          index.incrementAndGet();
+          return result;
+        })
+        .collect(Collectors.toList());
+
+    for (IcebergSourceSplit split : splits) {
+      final byte[] result = serializer.serialize(split);
+      final IcebergSourceSplit deserialized = serializer.deserialize(serializer.getVersion(), result);
+      Assert.assertEquals(split, deserialized);
+
+      final byte[] cachedResult = serializer.serialize(split);
+      Assert.assertSame(result, cachedResult);
+      final IcebergSourceSplit deserialized2 = serializer.deserialize(serializer.getVersion(), cachedResult);
+      Assert.assertEquals(split, deserialized2);
+    }
+  }
+}

apache/iceberg

flink/src/main/java/org/apache/iceberg/flink/source/DataIterator.java
@@ -29,6 +29,7 @@ import org.apache.iceberg.encryption.EncryptionManager;
 import org.apache.iceberg.encryption.InputFilesDecryptor;
 import org.apache.iceberg.io.CloseableIterator;
 import org.apache.iceberg.io.FileIO;
+import org.apache.iceberg.relocated.com.google.common.base.Preconditions;
 
 /**
  * Flink data iterator that reads {@link CombinedScanTask} into a {@link CloseableIterator}
@@ -37,20 +38,46 @@ import org.apache.iceberg.io.FileIO;
  */
 @Internal
 public class DataIterator<T> implements CloseableIterator<T> {
-
   private final FileScanTaskReader<T> fileScanTaskReader;
-
   private final InputFilesDecryptor inputFilesDecryptor;
+  private final CombinedScanTask combinedTask;
+
   private Iterator<FileScanTask> tasks;
   private CloseableIterator<T> currentIterator;
+  private Position position;
 
   public DataIterator(FileScanTaskReader<T> fileScanTaskReader, CombinedScanTask task,
                       FileIO io, EncryptionManager encryption) {
     this.fileScanTaskReader = fileScanTaskReader;
-
     this.inputFilesDecryptor = new InputFilesDecryptor(task, io, encryption);
+    this.combinedTask = task;
+
     this.tasks = task.files().iterator();
     this.currentIterator = CloseableIterator.empty();
+    // fileOffset starts at -1 because we started
+    // from an empty iterator that is not from the split files.
+    this.position = new Position(-1L, 0L);
+  }
+
+  public void seek(Position startingPosition) {
+    // skip files
+    Preconditions.checkArgument(startingPosition.fileOffset() < combinedTask.files().size(),
+        "Checkpointed file offset is %d, while CombinedScanTask has %d files",
+        startingPosition.fileOffset(), combinedTask.files().size());
+    for (long i = 0L; i < startingPosition.fileOffset(); ++i) {
+      tasks.next();
+    }
+    updateCurrentIterator();
+    // skip records within the file
+    for (long i = 0; i < startingPosition.recordOffset(); ++i) {
+      if (hasNext()) {
+        next();
+      } else {
+        throw new IllegalStateException("Not enough records to skip: " +
+            startingPosition.recordOffset());
+      }
+    }
+    this.position.update(startingPosition.fileOffset(), startingPosition.recordOffset());
   }
 
   @Override
@@ -62,7 +89,12 @@ public class DataIterator<T> implements CloseableIterator<T> {
   @Override
   public T next() {
     updateCurrentIterator();
+    position.advanceRecord();
     return currentIterator.next();
+  }
+
+  public boolean isCurrentIteratorDone() {
+    return !currentIterator.hasNext();
   }
 
   /**
@@ -74,6 +106,7 @@ public class DataIterator<T> implements CloseableIterator<T> {
       while (!currentIterator.hasNext() && tasks.hasNext()) {
         currentIterator.close();
         currentIterator = openTaskIterator(tasks.next());
+        position.advanceFile();
       }
     } catch (IOException e) {
       throw new UncheckedIOException(e);
@@ -89,5 +122,9 @@ public class DataIterator<T> implements CloseableIterator<T> {
     // close the current iterator
     currentIterator.close();
     tasks = null;
+  }
+
+  public Position position() {
+    return position;
   }
 }

flink/src/test/java/org/apache/iceberg/flink/source/reader/TestRecyclableArrayIterator.java
@@ -1,0 +1,85 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iceberg.flink.source.reader;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import org.apache.flink.connector.file.src.util.RecordAndPosition;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestRecyclableArrayIterator {
+
+  @Test
+  public void testEmptyConstruction() {
+    // dummy recycler
+    final RecyclableArrayIterator<Object> iter = new RecyclableArrayIterator<>(
+        ignored -> System.currentTimeMillis());
+    Assert.assertNull(iter.next());
+  }
+
+  @Test
+  public void testGetElements() {
+    final String[] elements = new String[]{"1", "2", "3", "4"};
+    final long initialOffset = 3;
+    final long initialSkipCount = 17;
+
+    // dummy recycler
+    final RecyclableArrayIterator<String> iter = new RecyclableArrayIterator<>(
+        ignored -> System.currentTimeMillis(), elements, elements.length, initialOffset, initialSkipCount);
+
+    for (int i = 0; i < elements.length; i++) {
+      final RecordAndPosition<String> recAndPos = iter.next();
+      Assert.assertEquals(elements[i], recAndPos.getRecord());
+      Assert.assertEquals(initialOffset, recAndPos.getOffset());
+      Assert.assertEquals(initialSkipCount + i + 1, recAndPos.getRecordSkipCount());
+    }
+  }
+
+  @Test
+  public void testExhausted() {
+    // dummy recycler
+    final RecyclableArrayIterator<String> iter = new RecyclableArrayIterator<>(
+        ignored -> System.currentTimeMillis(), new String[]{"1", "2"}, 2, 0L, 0L);
+
+    iter.next();
+    iter.next();
+
+    Assert.assertNull(iter.next());
+  }
+
+  @Test
+  public void testArraySubRange() {
+    // dummy recycler
+    final RecyclableArrayIterator<String> iter = new RecyclableArrayIterator<>(ignored -> System.currentTimeMillis(),
+        new String[]{"1", "2", "3"}, 2, 0L, 0L);
+
+    Assert.assertNotNull(iter.next());
+    Assert.assertNotNull(iter.next());
+    Assert.assertNull(iter.next());
+  }
+
+  @Test
+  public void testRecycler() {
+    final AtomicBoolean recycled = new AtomicBoolean();
+    final RecyclableArrayIterator<String> iter = new RecyclableArrayIterator<>(ignored -> recycled.set(true));
+    iter.close();
+    Assert.assertTrue(recycled.get());
+  }
+}

flink/src/test/java/org/apache/iceberg/flink/source/split/TestIcebergSourceSplitSerializer.java
@@ -1,0 +1,100 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iceberg.flink.source.split;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import org.apache.iceberg.flink.source.Position;
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class TestIcebergSourceSplitSerializer {
+
+  @ClassRule
+  public static final TemporaryFolder TEMPORARY_FOLDER = new TemporaryFolder();
+
+  private final IcebergSourceSplitSerializer serializer = IcebergSourceSplitSerializer.INSTANCE;
+
+  @Test
+  public void testLatestVersion() throws Exception {
+    serializeAndDeserialize(1, 1);
+    serializeAndDeserialize(10, 2);
+  }
+
+  private void serializeAndDeserialize(int splitCount, int filesPerSplit) throws Exception {
+    final List<IcebergSourceSplit> splits = SplitHelpers.createFileSplits(TEMPORARY_FOLDER, splitCount, filesPerSplit);
+    for (IcebergSourceSplit split : splits) {
+      final byte[] result = serializer.serialize(split);
+      final IcebergSourceSplit deserialized = serializer.deserialize(serializer.getVersion(), result);
+      Assert.assertEquals(split, deserialized);
+
+      final byte[] cachedResult = serializer.serialize(split);
+      Assert.assertSame(result, cachedResult);
+      final IcebergSourceSplit deserialized2 = serializer.deserialize(serializer.getVersion(), cachedResult);
+      Assert.assertEquals(split, deserialized2);
+    }
+  }
+
+  @Test
+  public void testV1() throws Exception {
+    serializeAndDeserializeV1(1, 1);
+    serializeAndDeserializeV1(10, 2);
+  }
+
+  private void serializeAndDeserializeV1(int splitCount, int filesPerSplit) throws Exception {
+    final List<IcebergSourceSplit> splits = SplitHelpers.createFileSplits(TEMPORARY_FOLDER, splitCount, filesPerSplit);
+    for (IcebergSourceSplit split : splits) {
+      final byte[] result = serializer.serializeV1(split);
+      final IcebergSourceSplit deserialized = serializer.deserializeV1(result);
+      Assert.assertEquals(split, deserialized);
+    }
+  }
+
+  @Test
+  public void testCheckpointedPosition() throws Exception {
+    final AtomicInteger index = new AtomicInteger();
+    final List<IcebergSourceSplit> splits = SplitHelpers.createFileSplits(TEMPORARY_FOLDER, 10, 2).stream()
+        .map(split -> {
+          final IcebergSourceSplit result;
+          if (index.get() % 2 == 0) {
+            result = new IcebergSourceSplit(split.task(), new Position(index.get(), index.get()));
+          } else {
+            result = split;
+          }
+          index.incrementAndGet();
+          return result;
+        })
+        .collect(Collectors.toList());
+
+    for (IcebergSourceSplit split : splits) {
+      final byte[] result = serializer.serialize(split);
+      final IcebergSourceSplit deserialized = serializer.deserialize(serializer.getVersion(), result);
+      Assert.assertEquals(split, deserialized);
+
+      final byte[] cachedResult = serializer.serialize(split);
+      Assert.assertSame(result, cachedResult);
+      final IcebergSourceSplit deserialized2 = serializer.deserialize(serializer.getVersion(), cachedResult);
+      Assert.assertEquals(split, deserialized2);
+    }
+  }
+}