927c75dfe91e5fbccc13742c1d904a168b575120
Jiebao Xiao <xiaojiebao@xiaomi.com>
2021-11-22 15:02:06 +0800

    no need string format

--- spark/v3.2/spark/src/main/java/org/apache/iceberg/spark/actions/BaseRewriteDataFilesSparkAction.java
+++ spark/v3.2/spark/src/main/java/org/apache/iceberg/spark/actions/BaseRewriteDataFilesSparkAction.java
@@ -333,8 +333,8 @@ abstract class BaseRewriteDataFilesSparkAction
           "into smaller commits.", PARTIAL_PROGRESS_ENABLED, PARTIAL_PROGRESS_MAX_COMMITS);
       if (!ignoreNoSuccessfulCommitEnabled) {
         throw new RewriteDataFiles.NoSuccessfulCommitException(
-            String.format("No rewrite commits succeeded after %s times commits, table name: %s, total group count: %s.",
-                maxCommits, table().name(), ctx.totalGroupCount()));
+            "No rewrite commits succeeded after %s times commits, table name: %s, total group count: %s.",
+            maxCommits, table().name(), ctx.totalGroupCount());
       }
     }
 
