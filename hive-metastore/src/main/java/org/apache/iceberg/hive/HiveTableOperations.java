a8067a4773fd739a1c3823167bc246b1d1a4ba74
Kevin Liu <kevinliu@stripe.com>
2021-12-06 19:13:52 -0800

    refactor comment
    Co-authored-by: kevinliu <kevin.jq.liu@gmail.com>
    Co-authored-by: kevinliu <kevinliu@stripe.com>

--- hive-metastore/src/main/java/org/apache/iceberg/hive/HiveTableOperations.java
+++ hive-metastore/src/main/java/org/apache/iceberg/hive/HiveTableOperations.java
@@ -362,7 +362,7 @@ public class HiveTableOperations extends BaseMetastoreTableOperations {
     // remove any props from HMS that are no longer present in Iceberg table props
     obsoleteProps.forEach(parameters::remove);
 
-    // remove the DDL_TIME so it gets refreshed
+    // remove any props that are controlled by HMS so it can handle them
     parameters.remove(hive_metastoreConstants.DDL_TIME);
 
     parameters.put(TABLE_TYPE_PROP, ICEBERG_TABLE_TYPE_VALUE.toUpperCase(Locale.ENGLISH));
