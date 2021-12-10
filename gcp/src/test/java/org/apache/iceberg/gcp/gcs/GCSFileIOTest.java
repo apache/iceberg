7fa38388a714a16ad3dd428ef9d1d33620e6d2f9
Daniel Weeks <dweeks@apache.org>
2021-12-10 10:32:13 -0800

    Checkstyle

--- gcp/src/test/java/org/apache/iceberg/gcp/gcs/GCSFileIOTest.java
+++ gcp/src/test/java/org/apache/iceberg/gcp/gcs/GCSFileIOTest.java
@@ -1,15 +1,20 @@
 /*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.apache.iceberg.gcp.gcs;
@@ -41,7 +46,7 @@ public class GCSFileIOTest {
   GCSFileIO io;
 
   @Before
-  public void setup() {
+  public void before() {
     io = new GCSFileIO(() -> storage, new GCPProperties());
   }
 
@@ -88,5 +93,4 @@ public class GCSFileIOTest {
     assertThat(StreamSupport.stream(storage.list(TEST_BUCKET).iterateAll().spliterator(), false).count())
         .isZero();
   }
-
 }
