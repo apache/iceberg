ff3aa325ed642812463f3dc5cbf295fc1224da89
CodingCat <zhunansjtu@gmail.com>
2021-12-22 08:54:46 -0800

    remove unused import

--- core/src/main/java/org/apache/iceberg/hadoop/HadoopTables.java
+++ core/src/main/java/org/apache/iceberg/hadoop/HadoopTables.java
@@ -21,7 +21,6 @@ package org.apache.iceberg.hadoop;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
