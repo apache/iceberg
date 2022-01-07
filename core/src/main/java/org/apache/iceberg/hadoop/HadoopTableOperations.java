005c366344874b5eaedfc6a262793f930fb05551
smallx <e9999e@163.com>
2022-01-08 01:47:29 +0800

    fix code review

--- core/src/main/java/org/apache/iceberg/hadoop/HadoopTableOperations.java
+++ core/src/main/java/org/apache/iceberg/hadoop/HadoopTableOperations.java
@@ -41,7 +41,6 @@ import org.apache.iceberg.TableProperties;
 import org.apache.iceberg.encryption.EncryptionManager;
 import org.apache.iceberg.exceptions.CommitFailedException;
 import org.apache.iceberg.exceptions.RuntimeIOException;
-import org.apache.iceberg.exceptions.TableUUIDMismatchException;
 import org.apache.iceberg.exceptions.ValidationException;
 import org.apache.iceberg.io.FileIO;
 import org.apache.iceberg.io.LocationProvider;
@@ -424,9 +423,9 @@ public class HadoopTableOperations implements TableOperations {
 
   private static TableMetadata checkUUID(TableMetadata currentMetadata, TableMetadata newMetadata) {
     String newUUID = newMetadata.uuid();
-    if (currentMetadata != null && currentMetadata.uuid() != null && newUUID != null &&
-        !newUUID.equals(currentMetadata.uuid())) {
-      throw new TableUUIDMismatchException(currentMetadata.uuid(), newUUID);
+    if (currentMetadata != null && currentMetadata.uuid() != null && newUUID != null) {
+      Preconditions.checkState(newUUID.equals(currentMetadata.uuid()),
+          "Table UUID does not match: current=%s != refreshed=%s", currentMetadata.uuid(), newUUID);
     }
     return newMetadata;
   }
--- spark/v3.0/spark/src/main/java/org/apache/iceberg/spark/SparkCatalog.java
+++ spark/v3.0/spark/src/main/java/org/apache/iceberg/spark/SparkCatalog.java
@@ -39,7 +39,6 @@ import org.apache.iceberg.catalog.Namespace;
 import org.apache.iceberg.catalog.SupportsNamespaces;
 import org.apache.iceberg.catalog.TableIdentifier;
 import org.apache.iceberg.exceptions.AlreadyExistsException;
-import org.apache.iceberg.exceptions.TableUUIDMismatchException;
 import org.apache.iceberg.hadoop.HadoopCatalog;
 import org.apache.iceberg.hadoop.HadoopTables;
 import org.apache.iceberg.relocated.com.google.common.base.Preconditions;
@@ -270,20 +269,12 @@ public class SparkCatalog extends BaseCatalog {
   @Override
   public void invalidateTable(Identifier ident) {
     try {
-      load(ident).first().refresh();
+      if (cacheEnabled && !isPathIdentifier(ident)) {
+        ((CachingCatalog) icebergCatalog).invalidate(buildIdentifier(ident));
+      }
+      load(ident);
     } catch (org.apache.iceberg.exceptions.NoSuchTableException ignored) {
       // ignore if the table doesn't exist, it is not cached
-    } catch (TableUUIDMismatchException e) {
-      try {
-        if (cacheEnabled && !isPathIdentifier(ident)) {
-          ((CachingCatalog) icebergCatalog).invalidate(buildIdentifier(ident));
-          load(ident);
-        } else {
-          throw e;
-        }
-      } catch (org.apache.iceberg.exceptions.NoSuchTableException ignored) {
-        // ignore if the table doesn't exist, it is not cached
-      }
     }
   }
 
