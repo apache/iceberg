f28ce8113474889c7d1ec40a1bb94a5ca6d82c8c
Robert Stupp <snazy@snazy.de>
2022-01-11 13:55:31 +0100

    Allow using a custom NessieClientBuilder implementation (#3877)
    Nessie defaults to use the HttpClientBuilder, but certain use cases
    require a custom client builder implementation. This change allows
    this by having a new configuration option.

---
+++ nessie/src/test/java/org/apache/iceberg/nessie/TestCustomNessieClient.java
@@ -0,0 +1,123 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iceberg.nessie;
+
+import java.net.URI;
+import java.util.function.Function;
+import org.apache.iceberg.CatalogProperties;
+import org.apache.iceberg.relocated.com.google.common.collect.ImmutableMap;
+import org.junit.jupiter.api.Test;
+import org.projectnessie.client.NessieClientBuilder;
+import org.projectnessie.client.api.NessieApi;
+import org.projectnessie.client.auth.NessieAuthentication;
+import org.projectnessie.client.http.HttpClientBuilder;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class TestCustomNessieClient extends BaseTestIceberg {
+
+  public TestCustomNessieClient() {
+    super("main");
+  }
+
+  @Test
+  public void testNoCustomClient() {
+    NessieCatalog catalog = new NessieCatalog();
+    catalog.initialize("nessie",
+        ImmutableMap.of(CatalogProperties.WAREHOUSE_LOCATION, temp.toUri().toString(),
+            CatalogProperties.URI, uri));
+  }
+
+  @Test
+  public void testUnnecessaryDefaultCustomClient() {
+    NessieCatalog catalog = new NessieCatalog();
+    catalog.initialize("nessie",
+        ImmutableMap.of(CatalogProperties.WAREHOUSE_LOCATION, temp.toUri().toString(),
+            CatalogProperties.URI, uri,
+            NessieUtil.CONFIG_CLIENT_BUILDER_IMPL, HttpClientBuilder.class.getName()));
+  }
+
+  @Test
+  public void testNonExistentCustomClient() {
+    String nonExistingClass = "non.existent.ClientBuilderImpl";
+    assertThatThrownBy(() -> {
+      NessieCatalog catalog = new NessieCatalog();
+      catalog.initialize("nessie",
+          ImmutableMap.of(CatalogProperties.WAREHOUSE_LOCATION, temp.toUri().toString(),
+              CatalogProperties.URI, uri,
+              NessieUtil.CONFIG_CLIENT_BUILDER_IMPL, nonExistingClass));
+    })
+        .isInstanceOf(RuntimeException.class)
+        .hasMessageContaining(nonExistingClass);
+  }
+
+  @Test
+  public void testCustomClient() {
+    assertThatThrownBy(() -> {
+      NessieCatalog catalog = new NessieCatalog();
+      catalog.initialize("nessie",
+          ImmutableMap.of(CatalogProperties.WAREHOUSE_LOCATION, temp.toUri().toString(),
+              CatalogProperties.URI, uri,
+              NessieUtil.CONFIG_CLIENT_BUILDER_IMPL, DummyClientBuilderImpl.class.getName()));
+    })
+        .isInstanceOf(RuntimeException.class)
+        .hasMessage("BUILD CALLED");
+  }
+
+  @SuppressWarnings("rawtypes")
+  public static final class DummyClientBuilderImpl implements NessieClientBuilder {
+
+    @SuppressWarnings("unused")
+    public static DummyClientBuilderImpl builder() {
+      return new DummyClientBuilderImpl();
+    }
+
+    @Override
+    public NessieClientBuilder fromSystemProperties() {
+      return this;
+    }
+
+    @Override
+    public NessieClientBuilder withAuthentication(
+        NessieAuthentication authentication) {
+      return this;
+    }
+
+    @Override
+    public NessieClientBuilder withUri(URI uri) {
+      return this;
+    }
+
+    @Override
+    public NessieClientBuilder withAuthenticationFromConfig(Function configuration) {
+      return this;
+    }
+
+    @Override
+    public NessieClientBuilder fromConfig(Function configuration) {
+      return this;
+    }
+
+    @Override
+    public NessieApi build(Class apiContract) {
+      throw new RuntimeException("BUILD CALLED");
+    }
+  }
+}
