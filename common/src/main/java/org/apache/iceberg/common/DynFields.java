e8d726315c3809cd62f9aea5350363c29268add0
yangminghua <abc549825@163.com>
2022-01-26 13:30:51 +0800

    fix: ImplicitPublicBuilderConstructor

--- common/src/main/java/org/apache/iceberg/common/DynFields.java
+++ common/src/main/java/org/apache/iceberg/common/DynFields.java
@@ -201,6 +201,10 @@ public class DynFields {
     private final Set<String> candidates = Sets.newHashSet();
     private boolean defaultAlwaysNull = false;
 
+    private Builder() {
+
+    }
+
     /**
      * Set the {@link ClassLoader} used to lookup classes by name.
      * <p>
