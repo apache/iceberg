6b1d85fc282d99f68c86da457791e830f6109018
Steven Wu <stevenz3wu@gmail.com>
2022-01-26 19:15:36 -0800

    rename SplitRecords to ArrayBatchRecords, as previous impl uses iterator to represent an array batch of records. reviewers can easily confuse it with the DataIterator

---
+++ flink/v1.14/flink/src/main/java/org/apache/iceberg/flink/source/reader/ArrayBatchRecords.java
@@ -0,0 +1,155 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iceberg.flink.source.reader;
+
+import java.util.Collections;
+import java.util.Set;
+import javax.annotation.Nullable;
+import org.apache.flink.annotation.Internal;
+import org.apache.flink.annotation.VisibleForTesting;
+import org.apache.flink.connector.base.source.reader.RecordsWithSplitIds;
+import org.apache.flink.connector.file.src.util.Pool;
+import org.apache.flink.table.data.RowData;
+import org.apache.iceberg.flink.source.DataIterator;
+
+/**
+ * {@link RecordsWithSplitIds} is used to pass a batch of records from fetcher to source reader.
+ * Batching is to improve the efficiency for records handover.
+ *
+ * {@link RecordsWithSplitIds} interface can encapsulate batches from multiple splits.
+ * This is the case for Kafka source where fetchers can retrieve records from multiple
+ * Kafka partitions at the same time.
+ *
+ * For file-based sources like Iceberg, readers always read one split/file at a time.
+ * Hence, we will only have a batch of records for one split here.
+ *
+ * This class uses array to store a batch of records from the same file (with the same fileOffset).
+ */
+@Internal
+public class ArrayBatchRecords<T> implements RecordsWithSplitIds<RecordAndPosition<T>> {
+  @Nullable
+  private String splitId;
+  @Nullable
+  private final Pool.Recycler<T[]> recycler;
+  @Nullable
+  private final T[] records;
+  private final int numberOfRecords;
+  private final Set<String> finishedSplits;
+  private final RecordAndPosition<T> recordAndPosition;
+
+  // point to current read position within the records array
+  private int position;
+
+  private ArrayBatchRecords(
+      String splitId, Pool.Recycler<T[]> recycler, T[] records, int numberOfRecords,
+      int fileOffset, long startingRecordOffset, Set<String> finishedSplits) {
+    this.splitId = splitId;
+    this.recycler = recycler;
+    this.records = records;
+    this.numberOfRecords = numberOfRecords;
+    this.finishedSplits = finishedSplits;
+    this.recordAndPosition = new RecordAndPosition<>();
+
+    recordAndPosition.set(null, fileOffset, startingRecordOffset);
+    this.position = 0;
+  }
+
+  @Nullable
+  @Override
+  public String nextSplit() {
+    String nextSplit = this.splitId;
+    // set the splitId to null to indicate no more splits
+    // this class only contains record for one split
+    this.splitId = null;
+    return nextSplit;
+  }
+
+  @Nullable
+  @Override
+  public RecordAndPosition<T> nextRecordFromSplit() {
+    if (position < numberOfRecords) {
+      recordAndPosition.record(records[position++]);
+      return recordAndPosition;
+    } else {
+      return null;
+    }
+  }
+
+  /**
+   * This method is called when all records from this batch has been emitted.
+   * When can
+   */
+  @Override
+  public void recycle() {
+    if (recycler != null) {
+      recycler.recycle(records);
+    }
+  }
+
+  @Override
+  public Set<String> finishedSplits() {
+    return finishedSplits;
+  }
+
+  @VisibleForTesting
+  T[] records() {
+    return records;
+  }
+
+  @VisibleForTesting
+  int numberOfRecords() {
+    return numberOfRecords;
+  }
+
+  /**
+   * Create a ArrayBatchRecords backed up an array with records from the same file
+   *
+   * @param splitId Iceberg source only read from one split a time.
+   *                We never have multiple records from multiple splits.
+   * @param recycler Because {@link DataIterator} with {@link RowData} returns an iterator of reused RowData object,
+   *                 we need to clone RowData eagerly when constructing a batch fo records.
+   *                 Object pool is used to recycle the RowData array, which can be expensive to create.
+   *                 This recycler is called to return the array object back to pool after read is exhausted.
+   *                 If the {@link DataIterator} returns an iterator of non-reused objects (like Avro GenericRecord),
+   *                 we don't need to clone objects. It is cheap to just create the batch array without cloning.
+   *                 Hence, we don't use object pool and recycler can be set to null.
+   * @param records an array (maybe reused) holding a batch of records
+   * @param numberOfRecords actual number of records in the array
+   * @param fileOffset fileOffset for all records in this batch
+   * @param startingRecordOffset starting recordOffset
+   * @param <T> record type
+   */
+  public static <T> ArrayBatchRecords<T> forRecords(
+      String splitId, Pool.Recycler<T[]> recycler, T[] records, int numberOfRecords,
+      int fileOffset, long startingRecordOffset) {
+    return new ArrayBatchRecords<>(splitId, recycler, records, numberOfRecords,
+        fileOffset, startingRecordOffset, Collections.emptySet());
+  }
+
+  /**
+   * Create ab ArrayBatchRecords with only finished split id
+   *
+   * @param splitId for the split that is just exhausted
+   */
+  public static <T> ArrayBatchRecords<T> finishedSplit(String splitId) {
+    return new ArrayBatchRecords<>(null, null, null,
+        0, 0, 0, Collections.singleton(splitId));
+  }
+}
