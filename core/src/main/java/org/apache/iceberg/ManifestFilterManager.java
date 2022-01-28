3a2fe72088ca596c2a8e1afd8d598b5b32d72515
lipeidian <lipeidian@qq.com>
2022-01-28 18:21:31 +0800

    fix checkstyle

--- core/src/main/java/org/apache/iceberg/ManifestFilterManager.java
+++ core/src/main/java/org/apache/iceberg/ManifestFilterManager.java
@@ -39,7 +39,6 @@ import org.apache.iceberg.relocated.com.google.common.collect.Iterables;
 import org.apache.iceberg.relocated.com.google.common.collect.Lists;
 import org.apache.iceberg.relocated.com.google.common.collect.Maps;
 import org.apache.iceberg.relocated.com.google.common.collect.Sets;
-import org.apache.iceberg.types.Types;
 import org.apache.iceberg.util.CharSequenceSet;
 import org.apache.iceberg.util.CharSequenceWrapper;
 import org.apache.iceberg.util.ManifestFileUtil;
