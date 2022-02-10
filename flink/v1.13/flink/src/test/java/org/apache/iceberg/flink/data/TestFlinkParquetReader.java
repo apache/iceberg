068163bd90b9590dc28defd1402ec3b48db0b462
King <wangdongyang@deepexi.com>
2022-02-10 17:01:06 +0800

    :art: checkstyle fix

--- flink/v1.13/flink/src/test/java/org/apache/iceberg/flink/data/TestFlinkParquetReader.java
+++ flink/v1.13/flink/src/test/java/org/apache/iceberg/flink/data/TestFlinkParquetReader.java
@@ -21,7 +21,6 @@ package org.apache.iceberg.flink.data;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
