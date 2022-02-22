922df4ba4977301bf267cf2e9d15af73c8f0be5f
吴文池 <wuwenchi@deepexi.com>
2022-02-22 11:32:12 +0800

    Core: modify variable name

--- core/src/main/java/org/apache/iceberg/actions/BaseRewriteDataFilesAction.java
+++ core/src/main/java/org/apache/iceberg/actions/BaseRewriteDataFilesAction.java
@@ -59,7 +59,7 @@ public abstract class BaseRewriteDataFilesAction<ThisT>
     extends BaseSnapshotUpdateAction<ThisT, RewriteDataFilesActionResult> {
 
   private static final Logger LOG = LoggerFactory.getLogger(BaseRewriteDataFilesAction.class);
-  private static final int PARQUET_MAGIC_BYTES = 4;
+  private static final int PARQUET_MAGIC_LEN = 4;
 
   private final Table table;
   private final FileIO fileIO;
@@ -304,7 +304,7 @@ public abstract class BaseRewriteDataFilesAction<ThisT>
       FileScanTask fileScanTask = task.files().iterator().next();
       if (fileScanTask.file().format() == FileFormat.PARQUET) {
         // in parquet format, there is an initial offset of 4 bytes
-        return fileScanTask.file().fileSizeInBytes() != (fileScanTask.length() + PARQUET_MAGIC_BYTES);
+        return fileScanTask.file().fileSizeInBytes() != (fileScanTask.length() + PARQUET_MAGIC_LEN);
       }
       return fileScanTask.file().fileSizeInBytes() != fileScanTask.length();
     } else {
