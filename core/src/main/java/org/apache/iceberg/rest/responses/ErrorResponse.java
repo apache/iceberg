80756e1214bca822586693c7c9d4ffb038d5d96e
Kyle Bendickson <kjbendickson@gmail.com>
2022-02-22 15:39:28 -0800

    CORE - Initial basic API for the REST HTTP client

---
+++ core/src/main/java/org/apache/iceberg/rest/responses/ErrorResponse.java
@@ -0,0 +1,104 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iceberg.rest.responses;
+
+import org.apache.iceberg.relocated.com.google.common.base.MoreObjects;
+import org.apache.iceberg.relocated.com.google.common.base.Preconditions;
+
+/**
+ * Standard response body for all API errors
+ */
+public class ErrorResponse {
+
+  private String message;
+  private String type;
+  private Integer code;
+
+  public ErrorResponse() {
+    // Needed for Jackson deserialization
+  }
+
+  private ErrorResponse(String message, String type, int code) {
+    this.message = message;
+    this.type = type;
+    this.code = code;
+    validate();
+  }
+
+  ErrorResponse validate() {
+    Preconditions.checkArgument(code != null, "Invalid response, missing field: code");
+    return this;
+  }
+
+  public String message() {
+    return message;
+  }
+
+  public String type() {
+    return type;
+  }
+
+  public Integer code() {
+    return code;
+  }
+
+
+  @Override
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+        .add("message", message)
+        .add("type", type)
+        .add("code", code)
+        .toString();
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  public static class Builder {
+    private String message;
+    private String type;
+    private Integer code;
+
+    private Builder() {
+    }
+
+    public Builder withMessage(String errorMessage) {
+      this.message = errorMessage;
+      return this;
+    }
+
+    public Builder withType(String errorType) {
+      this.type = errorType;
+      return this;
+    }
+
+    public Builder responseCode(Integer responseCode) {
+      this.code = responseCode;
+      return this;
+    }
+
+    public ErrorResponse build() {
+      Preconditions.checkArgument(code != null, "Invalid response, missing field: code");
+      return new ErrorResponse(message, type, code);
+    }
+  }
+}
---
+++ core/src/test/java/org/apache/iceberg/rest/responses/TestErrorResponseParser.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iceberg.rest.responses;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestErrorResponseParser {
+
+  @Test
+  public void testErrorResponseToJson() {
+    String message = "The given namespace does not exist";
+    String type = "NoSuchNamespaceException";
+    Integer code = 404;
+    String json = String.format("{\"message\":\"%s\",\"type\":\"%s\",\"code\":%d}", message, type, code);
+    ErrorResponse response = ErrorResponse.builder().withMessage(message).withType(type).responseCode(code).build();
+    Assert.assertEquals("Should be able to serialize an error response as json",
+        ErrorResponseParser.toJson(response), json);
+  }
+
+  @Test
+  public void testErrorResponseFromJson() {
+    String message = "The given namespace does not exist";
+    String type = "NoSuchNamespaceException";
+    Integer code = 404;
+    String json = String.format("{\"message\":\"%s\",\"type\":\"%s\",\"code\":%d}", message, type, code);
+
+    ErrorResponse expected = ErrorResponse.builder().withMessage(message).withType(type).responseCode(code).build();
+    assertEquals(expected, ErrorResponseParser.fromJson(json));
+  }
+
+  public void assertEquals(ErrorResponse expected, ErrorResponse actual) {
+    Assert.assertEquals("Message should be equal", expected.message(), actual.message());
+    Assert.assertEquals("Typ should be equal", expected.type(), actual.type());
+    Assert.assertEquals("Response code should be equal", expected.code(), actual.code());
+  }
+}
