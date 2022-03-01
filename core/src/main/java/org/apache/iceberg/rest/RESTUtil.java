e17f0573ab843e10a87fdaee69c5dc65bf327912
Kyle Bendickson <kjbendickson@gmail.com>
2022-02-28 17:24:42 -0800

    Core - Add in RESTUtil class for encoding namespace as URL variable

---
+++ core/src/main/java/org/apache/iceberg/rest/RESTUtil.java
@@ -0,0 +1,93 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iceberg.rest;
+
+import java.io.UncheckedIOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import org.apache.iceberg.catalog.Namespace;
+import org.apache.iceberg.relocated.com.google.common.base.Joiner;
+import org.apache.iceberg.relocated.com.google.common.base.Splitter;
+import org.apache.iceberg.relocated.com.google.common.collect.Maps;
+import org.apache.iceberg.relocated.com.google.common.collect.Streams;
+
+class RESTUtil {
+  private static final Joiner NULL_JOINER = Joiner.on('\u0000');
+  private static final Splitter NULL_SPLITTER = Splitter.on('\u0000');
+
+  private RESTUtil() {
+  }
+
+  public static String stripTrailingSlash(String path) {
+    if (path == null) {
+      return null;
+    }
+
+    return path.endsWith("/") ? path.substring(0, path.length() - 1) : path;
+  }
+
+  public static Map<String, String> filterAndRemovePrefix(Map<String, String> properties, String prefix) {
+    Map<String, String> result = Maps.newHashMap();
+    properties.forEach((key, value) -> {
+      if (key.startsWith(prefix)) {
+        result.put(key.substring(prefix.length()), value);
+      }
+    });
+
+    return result;
+  }
+
+  public static String asURLVariable(Namespace ns) {
+    String[] levels = ns.levels();
+    String[] encodedLevels = new String[levels.length];
+
+    for (int i = 0; i < levels.length; i++) {
+      try {
+        encodedLevels[i] = URLEncoder.encode(levels[i], StandardCharsets.UTF_8.name());
+      } catch (UnsupportedEncodingException e) {
+        throw new UncheckedIOException(
+            String.format("Failed to URL encode namespace as UTF-8 encoding is not supported: %s", ns), e);
+      }
+    }
+
+    return NULL_JOINER.join(encodedLevels);
+  }
+
+  public static Namespace fromURLVariable(String encodedNs) {
+    Iterable<String> encodedLevels = NULL_SPLITTER.split(encodedNs);
+    String[] decodedLevels =
+        Streams
+            .stream(encodedLevels)
+            .map(encodedLevel -> {
+              try {
+                return URLDecoder.decode(encodedLevel, StandardCharsets.UTF_8.name());
+              } catch (UnsupportedEncodingException e) {
+                throw new UncheckedIOException(
+                    String.format("Failed to decode namespace as UTF-8 encoding is not supported: %s", encodedNs), e);
+              }
+            })
+            .toArray(String[]::new);
+    return Namespace.of(decodedLevels);
+  }
+
+}
---
+++ core/src/test/java/org/apache/iceberg/rest/TestRESTUtil.java
@@ -0,0 +1,100 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iceberg.rest;
+
+import java.util.List;
+import java.util.Map;
+import org.apache.iceberg.catalog.Namespace;
+import org.apache.iceberg.relocated.com.google.common.collect.ImmutableList;
+import org.apache.iceberg.relocated.com.google.common.collect.ImmutableMap;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class TestRESTUtil {
+
+  static class LevelsAndURLEncodedString {
+    public String[] levels;
+    public String parameterEncodedForUrlUsage;
+
+    LevelsAndURLEncodedString(String[] levels, String parameterEncodedForUrlUsage) {
+      this.levels = levels;
+      this.parameterEncodedForUrlUsage = parameterEncodedForUrlUsage;
+    }
+  }
+
+  @Test
+  public void testFilterAndRemovePrefix() {
+    Map<String, String> input = ImmutableMap.of(
+        "warehouse", "/tmp/warehouse",
+        "rest.prefix", "/ws/ralphs_catalog",
+        "rest.token", "YnVybiBhZnRlciByZWFkaW5nIC0gYWxzbyBoYW5rIGFuZCByYXVsIDQgZXZlcgo=",
+        "rest.rest.uri", "https://localhost:1080/");
+
+    Map<String, String> expected = ImmutableMap.of(
+        "prefix", "/ws/ralphs_catalog",
+        "token", "YnVybiBhZnRlciByZWFkaW5nIC0gYWxzbyBoYW5rIGFuZCByYXVsIDQgZXZlcgo=",
+        "rest.uri", "https://localhost:1080/"
+    );
+
+    Map<String, String> actual = RESTUtil.filterAndRemovePrefix(input, "rest.");
+
+    Assertions.assertThat(actual).isEqualTo(expected);
+  }
+
+  @Test
+  public void testStripTrailingSlash() {
+    Map<String, String> testCaseAndExpectedAnswer = ImmutableMap.of(
+        "https://foo/", "https://foo",
+        "https://foo////", "https://foo///"
+    );
+
+    testCaseAndExpectedAnswer.forEach((input, expected) ->
+      Assertions.assertThat(RESTUtil.stripTrailingSlash(input)).isEqualTo(expected)
+    );
+  }
+
+  @Test
+  public void testAsURLVariable() {
+    List<LevelsAndURLEncodedString> testCases = ImmutableList.of(
+        new LevelsAndURLEncodedString(new String[] {"dogs"}, "dogs"),
+        new LevelsAndURLEncodedString(new String[] {"dogs.named.hank"}, "dogs.named.hank"),
+        new LevelsAndURLEncodedString(new String[] {"dogs/named/hank"}, "dogs%2Fnamed%2Fhank"),
+        new LevelsAndURLEncodedString(
+            new String[] {"dogs", "named", "hank"},
+            "dogs\u0000named\u0000hank"),
+        new LevelsAndURLEncodedString(
+            new String[] {"dogs.and.cats", "named", "hank.or.james-westfall"},
+            "dogs.and.cats\u0000named\u0000hank.or.james-westfall"));
+
+    for (LevelsAndURLEncodedString testCase : testCases) {
+      String[] levels = testCase.levels;
+      String nsEncodedForURLVariable = testCase.parameterEncodedForUrlUsage;
+      Namespace namespace = Namespace.of(levels);
+
+      // To be placed into a URL path as query parameter or path variable
+      Assertions.assertThat(RESTUtil.asURLVariable(namespace))
+          .isEqualTo(nsEncodedForURLVariable);
+
+      // Decoded (after pulling as String) from URL
+      Namespace asNamespace = RESTUtil.fromURLVariable(nsEncodedForURLVariable);
+      Assertions.assertThat(asNamespace).isEqualTo(namespace);
+    }
+  }
+}
