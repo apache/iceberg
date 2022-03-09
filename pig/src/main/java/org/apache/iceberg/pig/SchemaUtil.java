fdf09a8f7bfae1cafd06fc7ac50e93deede93dcf
felixYyu <felix2003@live.cn>
2022-03-09 15:03:23 +0800

    fix switch case checkstyle bug

--- pig/src/main/java/org/apache/iceberg/pig/SchemaUtil.java
+++ pig/src/main/java/org/apache/iceberg/pig/SchemaUtil.java
@@ -77,10 +77,10 @@ public class SchemaUtil {
       case LONG:      return DataType.LONG;
       case FLOAT:     return DataType.FLOAT;
       case DOUBLE:    return DataType.DOUBLE;
-      case TIMESTAMP: return DataType.CHARARRAY;
-      case DATE:      return DataType.CHARARRAY;
+      case TIMESTAMP:
+      case DATE:
       case STRING:    return DataType.CHARARRAY;
-      case FIXED:     return DataType.BYTEARRAY;
+      case FIXED:
       case BINARY:    return DataType.BYTEARRAY;
       case DECIMAL:   return DataType.BIGDECIMAL;
       case STRUCT:    return DataType.TUPLE;
