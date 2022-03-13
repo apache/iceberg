972859f792140e373dc023186f7e7e55a6ff2f3e
Kyle Bendickson <kjbendickson@gmail.com>
2022-03-13 09:25:59 -0700

    Flink: Remove deprecated calls to enable blink planner (#4315)

--- flink/v1.14/flink/src/test/java/org/apache/iceberg/flink/source/ChangeLogTableTestBase.java
+++ flink/v1.14/flink/src/test/java/org/apache/iceberg/flink/source/ChangeLogTableTestBase.java
@@ -52,7 +52,6 @@ public class ChangeLogTableTestBase extends FlinkTestBase {
         if (tEnv == null) {
           EnvironmentSettings settings = EnvironmentSettings
               .newInstance()
-              .useBlinkPlanner()
               .inStreamingMode()
               .build();
 
