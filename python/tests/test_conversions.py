3e03e3e6186ec88c8d50fcc3aed3a3578aeee898
samredai <43911210+samredai@users.noreply.github.com>
2022-03-14 18:39:35 -0700

    Add more docs and tests to test_conversions.py

--- python/tests/test_conversions.py
+++ python/tests/test_conversions.py
@@ -14,7 +14,63 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+"""This test module tests PrimitiveType based conversions of values to/from bytes
 
+Notes:
+    Boolean:
+        - Stored as 0x00 for False and non-zero byte for True
+    Integer:
+        - Stored as 4 bytes in little-endian order
+        - 84202 is 0...01|01001000|11101010 in binary
+    Long:
+        - Stored as 8 bytes in little-endian order
+        - 200L is 0...0|11001000 in binary
+        - 11001000 -> 200 (-56), 00000000 -> 0, ... , 00000000 -> 0
+    Double:
+        - Stored as 8 bytes in little-endian order
+        - floating point numbers are represented as sign * 2ˆexponent * mantissa
+        - 6.0 is 1 * 2ˆ4 * 1.5 and encoded as 01000000|00011000|0...0
+        - 00000000 -> 0, ... , 00011000 -> 24, 01000000 -> 64
+    Date:
+        - Stored as days from 1970-01-01 in a 4-byte little-endian int
+        - 1000 is 0...0|00000011|11101000 in binary
+        - 11101000 -> 232 (-24), 00000011 -> 3, ... , 00000000 -> 0
+    Time:
+        - Stored as microseconds from midnight in an 8-byte little-endian long
+        - 10000L is 0...0|00100111|00010000 in binary
+        - 00010000 -> 16, 00100111 -> 39, ... , 00000000 -> 0
+    Timestamp:
+        - Stored as microseconds from 1970-01-01 00:00:00.000000 in an 8-byte little-endian long
+        - 400000L is 0...110|00011010|10000000 in binary
+        - 10000000 -> 128 (-128), 00011010 -> 26, 00000110 -> 6, ... , 00000000 -> 0
+    String:
+        - Stored as UTF-8 bytes (without length)
+        - 'A' -> 65, 'B' -> 66, 'C' -> 67
+    UUID:
+        - Stored as 16-byte big-endian values
+        - f79c3e09-677c-4bbd-a479-3f349cb785e7 is encoded as F7 9C 3E 09 67 7C 4B BD A4 79 3F 34 9C B7 85 E7
+        - 0xF7 -> 11110111 -> 247 (-9), 0x9C -> 10011100 -> 156 (-100), 0x3E -> 00111110 -> 62,
+        - 0x09 -> 00001001 -> 9, 0x67 -> 01100111 -> 103, 0x7C -> 01111100 -> 124,
+        - 0x4B -> 01001011 -> 75, 0xBD -> 10111101 -> 189 (-67), 0xA4 -> 10100100 -> 164 (-92),
+        - 0x79 -> 01111001 -> 121, 0x3F -> 00111111 -> 63, 0x34 -> 00110100 -> 52,
+        - 0x9C -> 10011100 -> 156 (-100), 0xB7 -> 10110111 -> 183 (-73), 0x85 -> 10000101 -> 133 (-123),
+        - 0xE7 -> 11100111 -> 231 (-25)
+    Fixed:
+        - Stored directly
+        - 'a' -> 97, 'b' -> 98
+    Binary:
+        - Stored directly
+        - 'Z' -> 90
+    Decimal:
+        - Stored as unscaled values in the form of two's-complement big-endian binary using the minimum number of bytes for the values
+        - 345 is 0...1|01011001 in binary
+        - 00000001 -> 1, 01011001 -> 89
+    Float:
+        - Stored as 4 bytes in little-endian order
+        - floating point numbers are represented as sign * 2ˆexponent * mantissa
+        - -4.5F is -1 * 2ˆ2 * 1.125 and encoded as 11000000|10010000|0...0 in binary
+        - 00000000 -> 0, 00000000 -> 0, 10010000 -> 144 (-112), 11000000 -> 192 (-64),
+"""
 import uuid
 from decimal import Decimal
 
@@ -73,13 +129,25 @@ def test_from_partition_value_to_py(primitive_type, partition_value_as_str, expe
         (IntegerType(), b"\xd2\x04\x00\x00", 1234),
         (LongType(), b"\xd2\x04\x00\x00\x00\x00\x00\x00", 1234),
         (DoubleType(), b"\x8d\x97\x6e\x12\x83\xc0\xf3\x3f", 1.2345),
+        (DateType(), bytes([232, 3, 0, 0]), 1000),
         (DateType(), b"\xd2\x04\x00\x00", 1234),
+        (TimeType(), bytes([16, 39, 0, 0, 0, 0, 0, 0]), 10000),
         (TimeType(), b"\x00\xe8vH\x17\x00\x00\x00", 100000000000),
+        (TimestamptzType(), bytes([128, 26, 6, 0, 0, 0, 0, 0]), 400000),
         (TimestamptzType(), b"\x00\xe8vH\x17\x00\x00\x00", 100000000000),
+        (TimestampType(), bytes([128, 26, 6, 0, 0, 0, 0, 0]), 400000),
         (TimestampType(), b"\x00\xe8vH\x17\x00\x00\x00", 100000000000),
+        (StringType(), bytes([65, 66, 67]), "ABC"),
         (StringType(), b"foo", "foo"),
+        (
+            UUIDType(),
+            bytes([247, 156, 62, 9, 103, 124, 75, 189, 164, 121, 63, 52, 156, 183, 133, 231]),
+            uuid.UUID("f79c3e09-677c-4bbd-a479-3f349cb785e7"),
+        ),
         (UUIDType(), b"\xf7\x9c>\tg|K\xbd\xa4y?4\x9c\xb7\x85\xe7", uuid.UUID("f79c3e09-677c-4bbd-a479-3f349cb785e7")),
+        (FixedType(3), bytes([97, 98]), bytes("ab", "utf8")),
         (FixedType(3), b"foo", b"foo"),
+        (BinaryType(), bytearray("Z", "utf8"), bytes([90])),
         (BinaryType(), b"foo", b"foo"),
         (DecimalType(5, 2), b"\x30\x39", Decimal("123.45")),
         (DecimalType(7, 4), b"\x00\x12\xd6\x87", Decimal("123.4567")),
@@ -112,20 +180,45 @@ def test_from_bytes_approximately(primitive_type, b, approximate_result, approxi
     [
         (BooleanType(), b"\x00", False),
         (BooleanType(), b"\x01", True),
+        (IntegerType(), bytes([234, 72, 1, 0]), 84202),
         (IntegerType(), b"\xd2\x04\x00\x00", 1234),
+        (LongType(), bytes([200, 0, 0, 0, 0, 0, 0, 0]), 200),
         (LongType(), b"\xd2\x04\x00\x00\x00\x00\x00\x00", 1234),
+        (DoubleType(), bytes([0, 0, 0, 0, 0, 0, 24, 64]), 6.0),
+        (DoubleType(), b"\x8d\x97n\x12\x83\xc0\xf3?", 1.2345),
+        (DateType(), bytes([232, 3, 0, 0]), 1000),
         (DateType(), b"\xd2\x04\x00\x00", 1234),
+        (TimeType(), bytes([16, 39, 0, 0, 0, 0, 0, 0]), 10000),
         (TimeType(), b"\x00\xe8vH\x17\x00\x00\x00", 100000000000),
+        (TimestamptzType(), bytes([128, 26, 6, 0, 0, 0, 0, 0]), 400000),
         (TimestamptzType(), b"\x00\xe8vH\x17\x00\x00\x00", 100000000000),
         (TimestampType(), b"\x00\xe8vH\x17\x00\x00\x00", 100000000000),
+        (StringType(), bytes([65, 66, 67]), "ABC"),
         (StringType(), b"foo", "foo"),
+        (
+            UUIDType(),
+            bytes([247, 156, 62, 9, 103, 124, 75, 189, 164, 121, 63, 52, 156, 183, 133, 231]),
+            uuid.UUID("f79c3e09-677c-4bbd-a479-3f349cb785e7"),
+        ),
         (UUIDType(), b"\xf7\x9c>\tg|K\xbd\xa4y?4\x9c\xb7\x85\xe7", uuid.UUID("f79c3e09-677c-4bbd-a479-3f349cb785e7")),
+        (FixedType(3), bytes([97, 98]), bytes("ab", "utf8")),
         (FixedType(3), b"foo", b"foo"),
+        (BinaryType(), bytearray("Z", "utf8"), bytes([90])),
         (BinaryType(), b"foo", b"foo"),
         (DecimalType(5, 2), b"\x30\x39", Decimal("123.45")),
         (DecimalType(3, 2), bytes([1, 89]), Decimal("3.45")),
+        # decimal on 3-bytes to test that we use the minimum number of bytes and not a power of 2
+        # 1234567 is 00010010|11010110|10000111 in binary
+        # 00010010 -> 18, 11010110 -> 214, 10000111 -> 135
         (DecimalType(7, 4), bytes([18, 214, 135]), Decimal("123.4567")),
+        # negative decimal to test two's complement
+        # -1234567 is 11101101|00101001|01111001 in binary
+        # 11101101 -> 237, 00101001 -> 41, 01111001 -> 121
         (DecimalType(7, 4), bytes([237, 41, 121]), Decimal("-123.4567")),
+        # test empty byte in decimal
+        # 11 is 00001011 in binary
+        # 00001011 -> 11
+        (DecimalType(10, 3), bytes([11]), Decimal("0.011")),
     ],
 )
 def test_round_trip_conversion(primitive_type, b, result):
@@ -220,8 +313,8 @@ def test_round_trip_conversion_large_decimals(primitive_type, b, result):
 @pytest.mark.parametrize(
     "primitive_type, b, result",
     [
+        (FloatType(), bytes([0, 0, 144, 192]), -4.5),
         (FloatType(), b"\x19\x04\x9e?", 1.2345),
-        (DoubleType(), b"\x8d\x97n\x12\x83\xc0\xf3?", 1.2345),
     ],
 )
 def test_round_trip_conversion_approximation(primitive_type, b, result):
