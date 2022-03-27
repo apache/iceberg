8b288f97a426423f313561d7364a876f966d9495
Kyle Bendickson <kjbendickson@gmail.com>
2022-03-27 14:03:20 -0700

    Core: Add HTTPClient implementation of RESTClient (#4245)

--- core/src/test/java/org/apache/iceberg/rest/HttpMethod.java
+++ core/src/test/java/org/apache/iceberg/rest/HttpMethod.java
@@ -19,17 +19,29 @@
 
 package org.apache.iceberg.rest;
 
-import java.io.Closeable;
-import java.util.function.Consumer;
-import org.apache.iceberg.rest.responses.ErrorResponse;
+public enum HttpMethod {
+  POST("POST", true, true),
+  GET("GET", false, true),
+  DELETE("DELETE", false, true),
+  HEAD("HEAD", false, false);
 
-/**
- * Interface for a basic HTTP Client for interfacing with the REST catalog.
- */
-public interface RESTClient extends Closeable {
-  <T> T delete(String path, Class<T> responseType, Consumer<ErrorResponse> errorHandler);
-  <T> T post(String path, Object body, Class<T> responseType, Consumer<ErrorResponse> errorHandler);
-  <T> T get(String path, Class<T> responseType, Consumer<ErrorResponse> errorHandler);
-  void  head(String path, Consumer<ErrorResponse> errorHandler);
+  HttpMethod(String method, boolean usesRequestBody, boolean usesResponseBody) {
+    this.usesResponseBody = usesResponseBody;
+    this.usesRequestBody = usesRequestBody;
+  }
+
+  // Represents whether we presently use a request / response body with this type or not,
+  // not necessarily if a body is allowed in the request or response for this HTTP verb.
+  //
+  // These are used to build valid test cases with `mock-server`.
+  private final boolean usesRequestBody;
+  private final boolean usesResponseBody;
+
+  public boolean usesResponseBody() {
+    return usesResponseBody;
+  }
+
+  public boolean usesRequestBody() {
+    return usesRequestBody;
+  }
 }
-
