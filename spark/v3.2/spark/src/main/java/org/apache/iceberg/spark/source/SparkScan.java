c501dc7d6d756832fcac2b1032700edd33f1add2
Prashant Singh <psinghvk@amazon.com>
2022-03-31 11:31:04 +0530

    Improve stats estimation for spark scan

--- spark/v3.2/spark/src/main/java/org/apache/iceberg/spark/source/SparkScan.java
+++ spark/v3.2/spark/src/main/java/org/apache/iceberg/spark/source/SparkScan.java
@@ -145,10 +145,12 @@ abstract class SparkScan implements Scan, SupportsReportStatistics {
     }
 
     long numRows = 0L;
+    long defaultRowSizeInBytes = readSchema().defaultSize();
 
     for (CombinedScanTask task : tasks()) {
       for (FileScanTask file : task.files()) {
-        numRows += file.file().recordCount();
+        // TODO: if possible, take deletes also into consideration.
+        numRows += Math.min(file.length() / defaultRowSizeInBytes, file.file().recordCount());
       }
     }
 
