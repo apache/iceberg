7badb8d86694dbd7788c66476cb95075a8576b66
Steve Zhang <hongyue_zhang@apple.com>
2022-05-06 15:43:34 -0700

    Python: Add PartitionSpec

--- python/src/iceberg/table/partitioning.py
+++ python/src/iceberg/table/partitioning.py
@@ -14,6 +14,9 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+from typing import Dict, List, Tuple
+
+from iceberg.schema import Schema
 from iceberg.transforms import Transform
 
 
@@ -64,3 +67,100 @@ class PartitionField:
 
     def __repr__(self):
         return f"PartitionField(field_id={self.field_id}, name={self.name}, transform={repr(self.transform)}, source_id={self.source_id})"
+
+    def __hash__(self):
+        return hash((self.source_id, self.field_id, self.name, self.transform))
+
+
+class PartitionSpec:
+    """
+    PartitionSpec capture the transformation from table data to partition values
+
+    Attributes:
+        schema(Schema): the schema of data table
+        spec_id(int): any change to PartitionSpec will produce a new specId
+        fields(List[PartitionField): list of partition fields to produce partition values
+        last_assigned_field_id(int): auto-increment partition field id starting from PARTITION_DATA_ID_START
+    """
+
+    PARTITION_DATA_ID_START: int = 1000
+
+    def __init__(self, schema: Schema, spec_id: int, fields: Tuple[PartitionField], last_assigned_field_id: int):
+        self._schema = schema
+        self._spec_id = spec_id
+        self._fields = fields
+        self._last_assigned_field_id = last_assigned_field_id
+        # derived
+        self.fields_by_source_id: Dict[int, List[PartitionField]] = {}
+
+    @property
+    def schema(self) -> Schema:
+        return self._schema
+
+    @property
+    def spec_id(self) -> int:
+        return self._spec_id
+
+    @property
+    def fields(self) -> Tuple[PartitionField]:
+        return self._fields
+
+    @property
+    def last_assigned_field_id(self) -> int:
+        return self._last_assigned_field_id
+
+    def __eq__(self, other):
+        return self.spec_id == other.spec_id and self.fields == other.fields
+
+    def __str__(self):
+        if self.is_unpartitioned():
+            return "[]"
+        else:
+            delimiter = "\n  "
+            partition_fields_in_str = (str(partition_field) for partition_field in self.fields)
+            head = f"[{delimiter}"
+            tail = f"\n]"
+            return f"{head}{delimiter.join(partition_fields_in_str)}{tail}"
+
+    def __repr__(self):
+        return f"PartitionSpec: {str(self)}"
+
+    def __hash__(self):
+        return hash((self.spec_id, self.fields))
+
+    def is_unpartitioned(self) -> bool:
+        return len(self.fields) < 1
+
+    def get_fields_by_source_id(self, filed_id: int) -> List[PartitionField]:
+        if not self.fields_by_source_id:
+            for partition_field in self.fields:
+                source_column = self.schema.find_column_name(partition_field.source_id)
+                if not source_column:
+                    raise ValueError(f"Cannot find source column: {partition_field.source_id}")
+                existing = self.fields_by_source_id.get(partition_field.source_id, [])
+                existing.append(partition_field)
+                self.fields_by_source_id[partition_field.source_id] = existing
+        return self.fields_by_source_id.get(filed_id)  # type: ignore
+
+    def compatible_with(self, other) -> bool:
+        """
+        Returns true if this partition spec is equivalent to the other, with partition field_id ignored.
+        That is, if both specs have the same number of fields, field order, field name, source column ids, and transforms.
+        """
+        if self.__eq__(other):
+            return True
+
+        if len(self.fields) != len(other.fields):
+            return False
+
+        for index in range(len(self.fields)):
+            this_field = self.fields[index]
+            that_field = other.fields[index]
+            if (
+                this_field.source_id != that_field.source_id
+                or this_field.transform != that_field.transform
+                or this_field.name != that_field.name
+            ):
+                return False
+
+        return True
--- python/tests/table/test_partitioning.py
+++ python/tests/table/test_partitioning.py
@@ -15,7 +15,8 @@
 # specific language governing permissions and limitations
 # under the License.
 
-from iceberg.table.partitioning import PartitionField
+from iceberg.schema import Schema
+from iceberg.table.partitioning import PartitionField, PartitionSpec
 from iceberg.transforms import bucket
 from iceberg.types import IntegerType
 
@@ -34,3 +35,30 @@ def test_partition_field_init():
         repr(partition_field)
         == "PartitionField(field_id=1000, name=id, transform=transforms.bucket(source_type=IntegerType(), num_buckets=100), source_id=3)"
     )
+    assert hash(partition_field) == hash(partition_field)
+
+
+def test_partition_spec_init(table_schema_simple: Schema):
+    bucket_transform = bucket(IntegerType(), 4)
+    id_field1 = PartitionField(3, 1001, bucket_transform, "id")
+    partition_spec1 = PartitionSpec(table_schema_simple, 0, (id_field1,), 1001)
+
+    assert partition_spec1.spec_id == 0
+    assert partition_spec1.schema == table_schema_simple
+    assert partition_spec1 == partition_spec1
+    assert str(partition_spec1) == f"[\n  {str(id_field1)}\n]"
+    assert not partition_spec1.is_unpartitioned()
+    # only differ by PartitionField field_id
+    id_field2 = PartitionField(3, 1002, bucket_transform, "id")
+    partition_spec2 = PartitionSpec(table_schema_simple, 0, (id_field2,), 1001)
+    assert hash(partition_spec1) != hash(partition_spec2)
+    assert partition_spec1.compatible_with(partition_spec2)
+    assert partition_spec1.get_fields_by_source_id(3) == [id_field1]
+
+
+def test_unpartitioned(table_schema_simple: Schema):
+    unpartitioned = PartitionSpec(table_schema_simple, 1, (), 1000)
+
+    assert not unpartitioned.fields
+    assert unpartitioned.is_unpartitioned()
+    assert str(unpartitioned) == "[]"
