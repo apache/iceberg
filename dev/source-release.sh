220a0448ac7461552daac051f64a7b6c688100dc
Russell_Spitzer <rspitzer@apple.com>
2022-05-30 11:30:08 -0500

    Dev: Use keyId when passed in release script
    Previously keyId would only be used when not passed.

--- dev/source-release.sh
+++ dev/source-release.sh
@@ -112,7 +112,7 @@ tarball=$tag.tar.gz
 git archive $release_hash --worktree-attributes --prefix $tag/ -o $projectdir/$tarball
 
 echo "Signing the tarball..."
-[[ -z "$keyid" ]] && keyopt="-u $keyid"
+[[ -n "$keyid" ]] && keyopt="-u $keyid"
 gpg $keyopt --armor --output ${projectdir}/${tarball}.asc --detach-sig ${projectdir}/$tarball
 shasum -a 512 $tarball > ${projectdir}/${tarball}.sha512
 
