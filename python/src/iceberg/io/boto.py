8d595865591e3f6fd70f2209e59d7060d0fe9715
samredai <43911210+samredai@users.noreply.github.com>
2022-05-30 16:04:04 -0700

    Fix small issues and incorporate PR feedback

--- python/src/iceberg/io/boto.py
+++ python/src/iceberg/io/boto.py
@@ -16,23 +16,23 @@
 # under the License.
 """FileIO implementation for reading and writing table files that uses boto3"""
 
-from typing import Literal, Optional, Union
+from typing import Optional, Union
 from urllib.parse import urlparse
 
 from boto3 import Session
-from boto3.resources.factory import s3
 from botocore.exceptions import ClientError
 
 from iceberg.io.base import FileIO, InputFile, InputStream, OutputFile, OutputStream
 
-class BotoInputStream:
+
+class BotoInputStream(InputStream):
     """A seekable wrapper for reading an S3 Object that abides by the InputStream protocol
-        
+
     Args:
         s3_object(boto3.resources.factory.s3.Object): An s3 object
     """
 
-    def __init__(self, s3_object: s3.Object):
+    def __init__(self, s3_object):
         self._s3_object = s3_object
         self._position = 0
 
@@ -53,7 +53,9 @@ class BotoInputStream:
         else:
             position_new = self._position + size
 
-            if position_new >= self.size:  # If more bytes are requested than exists, just read the entire file from the current position
+            if (
+                position_new >= self._s3_object.content_length
+            ):  # If more bytes are requested than exists, just read the entire file from the current position
                 return self.read(size=-1)
 
             range_header = f"bytes={self._position}-{position_new -1}"
@@ -61,13 +63,17 @@ class BotoInputStream:
 
         return self._s3_object.get(Range=range_header)["Body"].read()
 
-    def seek(self, offset: int, whence: Literal[0, 1, 2] = 0) -> int:
-        position_new = offset if whence == 0 else self._position + offset if whence == 1 else self._s3_object.content_length + offset if whence == 2 else None
-
-        if not position_new:
+    def seek(self, offset: int, whence: int = 0) -> int:
+        if whence not in (0, 1, 2):
             raise ValueError(f"Cannot seek to position {offset}, invalid whence: {whence}")
 
-        self._position = position_new
+        if whence == 0:
+            self._position = offset
+        elif whence == 1:
+            self._position = self._position + offset
+        elif whence == 2:
+            self._position = self._s3_object.content_length - offset
+
         return self._position
 
     def tell(self) -> int:
@@ -79,19 +85,20 @@ class BotoInputStream:
     def close(self) -> None:
         pass
 
-class BotoOutputStream:
+
+class BotoOutputStream(OutputStream):
     """A wrapper for writing an S3 Object that abides by the OutputStream protocol
-        
-        Args:
-        s3_object(boto3.resources.factory.s3.Object): An s3 object
+
+    Args:
+    s3_object(boto3.resources.factory.s3.Object): An s3 object
     """
 
-    def __init__(self, s3_object: s3.Object):
+    def __init__(self, s3_object):
         self._s3_object = s3_object
 
     def write(self, b: bytes) -> None:
         """Write to the S3 Object
-        
+
         Args:
             b(bytes): The bytes to write to the S3 Object
         """
@@ -106,10 +113,10 @@ class BotoOutputStream:
 
     def close(self) -> None:
         """Closes the stream
-        
+
         Since this is a wrapper for requests to S3, there is no concept of closing, therefore this method does nothing
         """
-        pass
+
 
 class BotoInputFile(InputFile):
     """An input file implementation for the BotoFileIO
@@ -119,47 +126,46 @@ class BotoInputFile(InputFile):
 
     Attributes:
         location(str): An S3 URI
-
-    Examples:
-        >>> from iceberg.io.boto import BotoInputFile
-        >>> input_file = BotoInputFile("s3://foo/bar.txt")
-        >>> file_content = input_file.open().read()  # Read the contents of the BotoInputFile instance
     """
 
-    def __init__(self, location: str, session: Session):
+    def __init__(self, location: str, session: Session, **client_kwargs):
         parsed_location = urlparse(location)  # Create a ParseResult from the uri
 
-        if not parsed_location.scheme.startswith('s3'):
+        if not parsed_location.scheme.startswith("s3"):
             raise ValueError(f"Cannot create BotoInputFile, scheme not supported: {parsed_location.scheme}")
 
         self._bucket = parsed_location.netloc
         self._path = parsed_location.path.strip("/")
         self._session = session
+        self._client_kwargs = client_kwargs
         super().__init__(location=location)
 
     def __len__(self) -> int:
         """Returns the total length of the file, in bytes"""
-        file_info = self._file_info()
-        return file_info.size
+        return self._session.resource("s3", **self._client_kwargs).Bucket(self._bucket).Object(self._path).content_length
 
     def exists(self) -> bool:
         """Checks whether the location exists"""
         try:
-            self._session.resource('s3').Bucket(self._bucket).Object(self._path).load()  # raises botocore.exceptions.ClientError with a 404 if it does not exist
+            self._session.resource("s3", **self._client_kwargs).Bucket(self._bucket).Object(
+                self._path
+            ).load()  # raises botocore.exceptions.ClientError with a 404 if it does not exist
             return True
         except ClientError as e:
-            if e.response['Error']['Code'] == "404":
+            if e.response["Error"]["Code"] == "404":
                 return False
             else:
                 raise
 
-    def open(self) -> InputStream:
+    def open(self) -> BotoInputStream:
         """Create a BotoInputStream for reading the contents of the file
 
         Returns:
             BotoInputStream: A BotoInputStream instance for the file located at `self.location`
         """
-        return BotoInputStream(s3_object=self._session.resource('s3').Bucket(self._bucket).Object(self._path))
+        return BotoInputStream(
+            s3_object=self._session.resource("s3", **self._client_kwargs).Bucket(self._bucket).Object(self._path)
+        )
 
 
 class BotoOutputFile(OutputFile):
@@ -170,36 +176,33 @@ class BotoOutputFile(OutputFile):
 
     Attributes:
         location(str): An S3 URI
-
-    Examples:
-        >>> from iceberg.io.boto import BotoOutputFile
-        >>> output_file = BotoOutputFile("s3://baz/qux.txt")
-        >>> output_file.create().write(b'foobytes')  # Write bytes to the BotoOutputFile instance
     """
 
-    def __init__(self, location: str, session: Session):
+    def __init__(self, location: str, session: Session, **client_kwargs):
         parsed_location = urlparse(location)  # Create a ParseResult from the uri
 
-        if not parsed_location.scheme.startswith('s3'):
+        if not parsed_location.scheme.startswith("s3"):
             raise ValueError(f"Cannot create BotoOutputFile, scheme not supported: {parsed_location.scheme}")
 
         self._bucket = parsed_location.netloc
         self._path = parsed_location.path.strip("/")
         self._session = session
+        self._client_kwargs = client_kwargs
         super().__init__(location=location)
 
     def __len__(self) -> int:
         """Returns the total length of the file, in bytes"""
-        file_info = self._file_info()
-        return file_info.size
+        return self._session.resource("s3", **self._client_kwargs).Bucket(self._bucket).Object(self._path).content_length
 
     def exists(self) -> bool:
         """Checks whether the location exists"""
         try:
-            self._session.resource('s3').Bucket(self._bucket).Object(self._path).load()  # raises botocore.exceptions.ClientError with a 404 if it does not exist
+            self._session.resource("s3", **self._client_kwargs).Bucket(self._bucket).Object(
+                self._path
+            ).load()  # raises botocore.exceptions.ClientError with a 404 if it does not exist
             return True
         except ClientError as e:
-            if e.response['Error']['Code'] == "404":
+            if e.response["Error"]["Code"] == "404":
                 return False
             else:
                 raise
@@ -222,32 +225,21 @@ class BotoOutputFile(OutputFile):
             process after the existence check yet before the output stream is created. In such a case, the default
             behavior will truncate the contents of the existing file when opening the output stream.
         """
-        return BotoOutputStream(s3_object=self._session.resource('s3').Bucket(self._bucket).Object(self._path))
+        return BotoOutputStream(
+            s3_object=self._session.resource("s3", **self._client_kwargs).Bucket(self._bucket).Object(self._path)
+        )
 
     def to_input_file(self) -> BotoInputFile:
         """Returns a new BotoInputFile for the location"""
-        return BotoInputFile(location=self.location)
+        return BotoInputFile(location=self.location, session=self._session, **self._client_kwargs)
 
 
 class BotoFileIO(FileIO):
-    """A FileIO implementation that uses boto3
-    
-    Example1:
-        >>> from iceberg.io.boto import BotoFileIO
-        >>> file_io = BotoFileIO()
-        >>> input_file = file_io.input_file("s3://foo/bar.txt")
-        >>> input_stream = input_file.open()
-        >>> data = input_stream.read()
-    
-    Example2:
-        >>> from iceberg.io.boto import BotoFileIO
-        >>> file_io = BotoFileIO()
-        >>> output_file = file_io.output_file("s3://foo/bar.txt")
-        >>> output_stream = output_file.create()
-        >>> output_stream.write(b'foo')
-    """
-    def __init__(self, session: Optional[Session] = None):
+    """A FileIO implementation that uses boto3"""
+
+    def __init__(self, session: Optional[Session] = None, **client_kwargs):
         self._session = session or Session()
+        self._client_kwargs = client_kwargs
 
     def new_input(self, location: str) -> BotoInputFile:
         """Get a BotoInputFile instance to read bytes from the file at the given location
@@ -258,7 +250,7 @@ class BotoFileIO(FileIO):
         Returns:
             BotoInputFile: A BotoInputFile instance for the given location
         """
-        return BotoInputFile(location, session=self._session)
+        return BotoInputFile(location, session=self._session, **self._client_kwargs)
 
     def new_output(self, location: str) -> BotoOutputFile:
         """Get a BotoOutputFile instance to write bytes to the file at the given location
@@ -269,7 +261,7 @@ class BotoFileIO(FileIO):
         Returns:
             BotoOutputFile: A BotoOutputFile instance for the given location
         """
-        return BotoOutputFile(location, session=self._session)
+        return BotoOutputFile(location, session=self._session, **self._client_kwargs)
 
     def delete(self, location: Union[str, InputFile, OutputFile]) -> None:
         """Delete the file at the given location
@@ -284,11 +276,15 @@ class BotoFileIO(FileIO):
             PermissionError: If the file at the provided location cannot be accessed due to a permission error such as
                 an AWS error code 15
         """
-        parsed_location = urlparse(location)  # Create a ParseResult from the uri
+        # Create a ParseResult from the uri
+        if isinstance(location, (InputFile, OutputFile)):
+            parsed_location = urlparse(location.location)  # Use InputFile or OutputFile location
+        else:
+            parsed_location = urlparse(location)
 
-        if not parsed_location.scheme.startswith('s3'):
+        if not parsed_location.scheme.startswith("s3"):
             raise ValueError(f"Cannot delete location, scheme not supported: {parsed_location.scheme}")
 
         bucket = parsed_location.netloc
         path = parsed_location.path.strip("/")
-        self._session.resource('s3').Bucket(bucket).Object(path).delete()
+        self._session.resource("s3", **self._client_kwargs).Bucket(bucket).Object(path).delete()
