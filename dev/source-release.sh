fae977a9f0a79266a04647b0df2ab540cf0dcff4
Russell Spitzer <rspitzer@apple.com>
2022-06-01 13:48:42 -0500

    Dev: Fix Source Release Script (#4932)

--- dev/source-release.sh
+++ dev/source-release.sh
@@ -36,7 +36,7 @@ usage () {
 }
 
 # Default repository remote name
-remote="origin"
+remote="apache"
 
 while getopts "v:r:k:g:d" opt; do
   case "${opt}" in
@@ -112,9 +112,9 @@ tarball=$tag.tar.gz
 git archive $release_hash --worktree-attributes --prefix $tag/ -o $projectdir/$tarball
 
 echo "Signing the tarball..."
-[[ -z "$keyid" ]] && keyopt="-u $keyid"
-gpg --detach-sig $keyopt --armor --output ${projectdir}/${tarball}.asc ${projectdir}/$tarball
-shasum -a 512 ${projectdir}/$tarball > ${projectdir}/${tarball}.sha512
+[[ -n "$keyid" ]] && keyopt="-u $keyid"
+gpg $keyopt --armor --output ${projectdir}/${tarball}.asc --detach-sig ${projectdir}/$tarball
+shasum -a 512 $tarball > ${projectdir}/${tarball}.sha512
 
 
 echo "Checking out Iceberg RC subversion repo..."
