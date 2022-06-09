3e61362c2b6e80030594b2a22395d174541bf5a5
Kyle Bendickson <kjbendickson@gmail.com>
2022-06-09 14:11:59 -0700

    Backport changed-partition-count to zero to earlier Spark versions

--- spark/v2.4/spark/src/test/java/org/apache/iceberg/spark/source/TestIcebergSourceTablesBase.java
+++ spark/v2.4/spark/src/test/java/org/apache/iceberg/spark/source/TestIcebergSourceTablesBase.java
@@ -797,7 +797,7 @@ public abstract class TestIcebergSourceTablesBase extends SparkTestBase {
             .set("summary", ImmutableMap.of(
                 "added-records", "1",
                 "added-data-files", "1",
-                "changed-partition-count", "1",
+                "changed-partition-count", "0",
                 "total-data-files", "1",
                 "total-records", "1"
             ))
@@ -810,7 +810,7 @@ public abstract class TestIcebergSourceTablesBase extends SparkTestBase {
             .set("summary", ImmutableMap.of(
                 "deleted-records", "1",
                 "deleted-data-files", "1",
-                "changed-partition-count", "1",
+                "changed-partition-count", "0",
                 "total-records", "0",
                 "total-data-files", "0"
             ))
@@ -863,7 +863,7 @@ public abstract class TestIcebergSourceTablesBase extends SparkTestBase {
             .set("summary", ImmutableMap.of(
                 "added-records", "1",
                 "added-data-files", "1",
-                "changed-partition-count", "1",
+                "changed-partition-count", "0",
                 "total-data-files", "1",
                 "total-records", "1"
             ))
@@ -874,7 +874,7 @@ public abstract class TestIcebergSourceTablesBase extends SparkTestBase {
             .set("summary", ImmutableMap.of(
                 "deleted-records", "1",
                 "deleted-data-files", "1",
-                "changed-partition-count", "1",
+                "changed-partition-count", "0",
                 "total-records", "0",
                 "total-data-files", "0"
             ))
