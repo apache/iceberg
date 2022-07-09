17b66d04944f3ba774db1db0bab40992925c4fcc
Christopher Lambert <xn137@gmx.de>
2022-07-09 10:10:24 +0200

    Build: Upgrade test dependencies to latest version
    - upgrade junit5 to 5.8.2
    - upgrade assertj to 3.23.1
    - upgrade mockserver to 5.13.2
    - upgrade mockito to 4.6.1
    - org.mockito.Matchers class was removed

--- core/src/test/java/org/apache/iceberg/hadoop/TestHadoopCommits.java
+++ core/src/test/java/org/apache/iceberg/hadoop/TestHadoopCommits.java
@@ -58,7 +58,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
