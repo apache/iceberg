b5ef1d1a375250ba454159c78fb87bcce0357829
huaxingao <huaxin_gao@apple.com>
2022-07-11 18:39:08 -0700

    log warning when filter conversion/bind expression fail

--- spark/v3.3/spark/src/main/java/org/apache/iceberg/spark/source/SparkScanBuilder.java
+++ spark/v3.3/spark/src/main/java/org/apache/iceberg/spark/source/SparkScanBuilder.java
@@ -52,10 +52,13 @@ import org.apache.spark.sql.sources.Filter;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 import org.apache.spark.sql.util.CaseInsensitiveStringMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SparkScanBuilder implements ScanBuilder, SupportsPushDownFilters, SupportsPushDownRequiredColumns,
     SupportsReportStatistics {
 
+  private static final Logger LOG = LoggerFactory.getLogger(SparkScanBuilder.class);
   private static final Filter[] NO_FILTERS = new Filter[0];
 
   private final SparkSession spark;
@@ -105,6 +108,7 @@ public class SparkScanBuilder implements ScanBuilder, SupportsPushDownFilters, S
         expr = SparkFilters.convert(filter);
       } catch (IllegalArgumentException e) {
         // converting to Iceberg Expression failed, so this expression cannot be pushed down
+        LOG.warn("converting to Iceberg Expression failed, so this expression cannot be pushed down.", e);
       }
 
       if (expr != null) {
@@ -114,6 +118,7 @@ public class SparkScanBuilder implements ScanBuilder, SupportsPushDownFilters, S
           pushed.add(filter);
         } catch (ValidationException e) {
           // binding to the table schema failed, so this expression cannot be pushed down
+          LOG.warn("binding to the table schema failed, so this expression cannot be pushed down.", e);
         }
       }
     }
