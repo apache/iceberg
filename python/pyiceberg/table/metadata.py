cc1ed2cf4d87e4709d64a6ad383726b7dd1ed794
Fokko Driesprong <fokko@apache.org>
2022-07-13 18:22:11 +0200

    Revert unrelated change

--- python/pyiceberg/table/metadata.py
+++ python/pyiceberg/table/metadata.py
@@ -353,8 +353,8 @@ class TableMetadata:
         format_version = data["format-version"]
 
         if format_version == 1:
-            return TableMetadataV1.parse_obj(data)
+            return TableMetadataV1(**data)
         elif format_version == 2:
-            return TableMetadataV2.parse_obj(data)
+            return TableMetadataV2(**data)
         else:
             raise ValidationError(f"Unknown format version: {format_version}")
