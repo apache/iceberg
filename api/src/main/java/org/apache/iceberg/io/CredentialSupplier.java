68de4032e3aec3c28efa2ace4a6b16656a6113c9
aokolnychyi <aokolnychyi@apple.com>
2022-08-19 09:03:38 -0700

    Exclude CredentialSupplier

--- api/src/main/java/org/apache/iceberg/io/CredentialSupplier.java
+++ api/src/main/java/org/apache/iceberg/io/CredentialSupplier.java
@@ -26,7 +26,7 @@ package org.apache.iceberg.io;
  * the configured credential as a string, and use the credential for file access via other IO
  * libraries.
  */
-public interface CredentialSupplier extends FileIO {
+public interface CredentialSupplier {
   /** Returns the credential string */
   String getCredential();
 }
