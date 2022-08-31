dcd6ae9c806d641bae97d964501d3100f11586e4
xzw <xuzhiwen@deepexi.com>
2022-08-31 15:47:51 +0800

    The changes are as follows: 1. Modify the endInput comment 2. Disable the Boolean notation and set write to null

--- flink/v1.14/flink/src/main/java/org/apache/iceberg/flink/sink/IcebergStreamWriter.java
+++ flink/v1.14/flink/src/main/java/org/apache/iceberg/flink/sink/IcebergStreamWriter.java
@@ -86,9 +86,9 @@ class IcebergStreamWriter<T> extends AbstractStreamOperator<WriteResult>
     // For bounded stream, it may don't enable the checkpoint mechanism so we'd better to emit the
     // remaining completed files to downstream before closing the writer so that we won't miss any
     // of them.
+
     // Note that if the task is not closed after calling endInput, checkpoint may be triggered again
     // causing files to be sent repeatedly, so we need to set write to null after the file is sent.
-
     flush();
   }
 
