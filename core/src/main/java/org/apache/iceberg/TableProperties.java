b51c6e626c02899b8af498c8fc2c49179003bf6a
Gabor Kaszab <gaborkaszab@cloudera.com>
2022-09-14 15:11:45 +0200

    Core,Hive-metastore: Table owner can be incorrect in HMS
    When HiveCatalog is used for creating a table the table owner property
    is taken from a system property of the process running Iceberg. However,
    in some clients (e.g. Apache Impala) it's not the owner of the
    process who runs the create table operation and as a result not the
    desired owner gets written to HMS.
    For instance, in Impala's case the owner of Catalogd (the process that
    makes the Iceberg API calls) was set as the table owner and not the
    user who ran the CREATE TABLE statement.
    
    I changed Hive Catalog to accept an owner via a table property from the
    clients.
    Note, other Catalog implementations aren't affected because the issue
    is related to HMS only.

--- core/src/main/java/org/apache/iceberg/TableProperties.java
+++ core/src/main/java/org/apache/iceberg/TableProperties.java
@@ -352,4 +352,6 @@ public class TableProperties {
 
   public static final String UPSERT_ENABLED = "write.upsert.enabled";
   public static final boolean UPSERT_ENABLED_DEFAULT = false;
+
+  public static final String HMS_TABLE_OWNER = "hms_table_owner";
 }
