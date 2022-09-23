2dc301829c028a89568ab60c574dea6daef0620d
JonasJ-ap <rushanj@andrew.cmu.edu>
2022-09-23 17:23:36 -0400

    fix format issue

--- core/src/main/java/org/apache/iceberg/util/PropertyUtil.java
+++ core/src/main/java/org/apache/iceberg/util/PropertyUtil.java
@@ -66,7 +66,8 @@ public class PropertyUtil {
     return defaultValue;
   }
 
-  public static Long propertyAsLong(Map<String, String> properties, String property, Long defaultValue) {
+  public static Long propertyAsLong(
+      Map<String, String> properties, String property, Long defaultValue) {
     String value = properties.get(property);
     if (value != null) {
       return Long.parseLong(value);
