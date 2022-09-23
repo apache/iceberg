f44f8bc4ebff981ebbbeab2d95665e4da4d1b1db
Liang-Chi Hsieh <viirya@gmail.com>
2022-09-23 11:37:30 -0700

    Add hive.in.test config

--- hive-metastore/src/test/java/org/apache/iceberg/hive/TestHiveMetastore.java
+++ hive-metastore/src/test/java/org/apache/iceberg/hive/TestHiveMetastore.java
@@ -267,6 +267,8 @@ public class TestHiveMetastore {
     conf.set(HiveConf.ConfVars.METASTORE_TRY_DIRECT_SQL.varname, "false");
     conf.set(HiveConf.ConfVars.METASTORE_DISALLOW_INCOMPATIBLE_COL_TYPE_CHANGES.varname, "false");
     conf.set("iceberg.hive.client-pool-size", "2");
+    // Setting this to avoid thrift exception during running Iceberg tests outside Iceberg.
+    conf.set(HiveConf.ConfVars.HIVE_IN_TEST.varname, "false");
   }
 
   private static void setupMetastoreDB(String dbURL) throws SQLException, IOException {
