0d6f816e656f901b462a988202c84a0f40120a8f
JonasJ-ap <rushanj@andrew.cmu.edu>
2022-09-25 22:00:02 -0400

    first draft based on previous PR

--- python/pyiceberg/catalog/glue.py
+++ python/pyiceberg/catalog/glue.py
@@ -18,6 +18,8 @@ from __future__ import annotations
 
 import boto3
 
+from typing import Union, Optional, List, Set, Dict
+
 from pyiceberg.catalog import Catalog, PropertiesUpdateSummary
 from pyiceberg.schema import Schema
 from pyiceberg.table import Table
@@ -32,6 +34,10 @@ class GlueCatalog(Catalog):
         self.client = boto3.client("glue")
         super().__init__(name, properties)
 
+    def _check_response(self, response: Dict[str, Dict[str, str]]):
+        if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+            raise ValueError(f"Got unexpected status code {response['HttpStatusCode']}")
+
     def create_table(
         self,
         identifier: str | Identifier,
@@ -44,36 +50,121 @@ class GlueCatalog(Catalog):
         pass
 
     def load_table(self, identifier: str | Identifier) -> Table:
-        pass
+        database_name, table_name = self.identifier_to_tuple(identifier)
+
+        response = self.client.get_table(DatabseName=database_name, Name=table_name)
+
+        self._check_response(response)
+        # TODO: metadata stuff
+        return None
 
     def drop_table(self, identifier: str | Identifier) -> None:
-        pass
+        database_name, table_name = self.identifier_to_tuple(identifier)
+        self.client.delete_table(
+            DatabaseName=database_name,
+            Name=table_name
+        )
+        return None
 
     def purge_table(self, identifier: str | Identifier) -> None:
-        pass
+        self.drop_table(identifier)
 
     def rename_table(self, from_identifier: str | Identifier, to_identifier: str | Identifier) -> Table:
-        pass
+        raise NotImplementedError("AWS Glue does not support renaming of tables")
 
     def create_namespace(self, namespace: str | Identifier, properties: Properties = EMPTY_DICT) -> None:
-        pass
+        identifier = self.identifier_to_tuple(namespace)
+        database_name, = identifier
+
+        response = self.client.create_database(
+            DatabaseInput={
+                'Name': database_name,
+                #                'Description': 'string',
+                #                'LocationUri': 'string',
+                'Parameters': properties
+            }
+        )
+        self._check_response(response)
 
     def drop_namespace(self, namespace: str | Identifier) -> None:
-        pass
+        identifier = self.identifier_to_tuple(namespace)
+        database_name, = identifier
+
+        response = self.client.delete_database(
+            Name=database_name
+        )
+
+        self._check_response(response)
 
     def list_tables(self, namespace: str | Identifier) -> list[Identifier]:
-        pass
+        identifier = self.identifier_to_tuple(namespace)
+        database_name, = identifier
+
+        catalog_id = self._get_database(database_name)['CatalogId']
+
+        tables = self.client.get_tables(
+            CatalogId=catalog_id,
+            DatabaseName=database_name
+        )['TableList']
+
+        return [
+            identifier + self.identifier_to_tuple(table['Name']) for table in tables
+        ]
 
     def list_namespaces(self, namespace: str | Identifier = ()) -> list[Identifier]:
-        pass
+        databases = self.client.get_databases()['DatabaseList']
+        return [
+            self.identifier_to_tuple(database['Name']) for database in databases
+        ]
 
     def load_namespace_properties(self, namespace: str | Identifier) -> Properties:
-        pass
+        identifier = self.identifier_to_tuple(namespace)
+        database_name, = identifier
+        return self._get_database(database_name)['Parameters']
 
     def update_namespace_properties(
         self, namespace: str | Identifier, removals: set[str] | None = None, updates: Properties = EMPTY_DICT
     ) -> PropertiesUpdateSummary:
-        pass
+        identifier = self.identifier_to_tuple(namespace)
+        database_name, = identifier
+        database = self._get_database(database_name)
+        parameters = database['Parameters']
 
+        removed: Set[str] = set()
+        updated: Set[str] = set()
+
+        if removals:
+            for key in removals:
+                if key in parameters:
+                    parameters[key] = None
+                    removed.add(key)
+        if updates:
+            for key, value in updates.items():
+                parameters[key] = value
+                updated.add(key)
+
+        expected_to_change = (removals or set()).difference(removed)
+
+        response = self.client.update_database(
+            CatalogId=database['CatalogId'],
+            Name=database['Name'],
+            DatabaseInput={
+                'Name': database['Name'],
+                # 'Description': database.get('Description', ''),
+                # 'LocationUri': database.get('LocationUri', ''),
+                'Parameters': parameters
+            }
+        )
+
+        self._check_response(response)
+
+        return PropertiesUpdateSummary(
+            removed=list(removed or []), updated=list(updates.keys() if updates else []),
+            missing=list(expected_to_change)
+        )
+
+    def _get_database(self, database_name: str):
+        databases = self.client.get_databases()['DatabaseList']
+        return next(database for database in databases if database['Name'] == database_name)
 
 
