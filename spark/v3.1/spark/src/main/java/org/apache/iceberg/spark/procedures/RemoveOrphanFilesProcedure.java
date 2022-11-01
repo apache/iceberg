d3d47f3bd8470eecf6e47ca9c2ea2b2308b6da61
ajantha-bhat <ajanthabhat@gmail.com>
2022-11-01 22:52:15 +0530

    Spark: Optimize Preconditions.checkArgument in procedures

--- spark/v3.1/spark/src/main/java/org/apache/iceberg/spark/procedures/RemoveOrphanFilesProcedure.java
+++ spark/v3.1/spark/src/main/java/org/apache/iceberg/spark/procedures/RemoveOrphanFilesProcedure.java
@@ -96,7 +96,8 @@ public class RemoveOrphanFilesProcedure extends BaseProcedure {
 
     Preconditions.checkArgument(
         maxConcurrentDeletes == null || maxConcurrentDeletes > 0,
-        "max_concurrent_deletes should have value > 0,  value: " + maxConcurrentDeletes);
+        "max_concurrent_deletes should have value > 0, value: %s",
+        maxConcurrentDeletes);
 
     return withIcebergTable(
         tableIdent,
