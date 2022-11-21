92486a87ead878a8a9f63e7bd48348daa4e35b32
hameizi <1249369293@qq.com>
2022-11-21 17:33:52 +0800

    fix code style

--- core/src/main/java/org/apache/iceberg/DeleteFileIndex.java
+++ core/src/main/java/org/apache/iceberg/DeleteFileIndex.java
@@ -271,7 +271,8 @@ class DeleteFileIndex {
     T deleteUpper = Conversions.fromByteBuffer(type, deleteUpperBuf);
 
     return !((comparator.compare(deleteLower, dataLower) <= 0
-        && comparator.compare(deleteUpper, dataLower) <= 0) || ((comparator.compare(deleteLower, dataUpper) >= 0
+            && comparator.compare(deleteUpper, dataLower) <= 0)
+        || ((comparator.compare(deleteLower, dataUpper) >= 0
             && comparator.compare(deleteUpper, dataUpper) >= 0)));
   }
 
