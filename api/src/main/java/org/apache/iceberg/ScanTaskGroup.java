96f9b3de33c34d835f58c0ee3f96bc8efc378e2d
aokolnychyi <aokolnychyi@apple.com>
2022-11-28 14:16:07 -0800

    API, Core: Add groupingKey to ScanTaskGroup

--- api/src/main/java/org/apache/iceberg/ScanTaskGroup.java
+++ api/src/main/java/org/apache/iceberg/ScanTaskGroup.java
@@ -26,6 +26,24 @@ import java.util.Collection;
  * @param <T> the type of scan tasks
  */
 public interface ScanTaskGroup<T extends ScanTask> extends ScanTask {
+  /**
+   * Returns a grouping key for this task group.
+   *
+   * <p>Tasks may produce records that belong to a specific group of data (e.g. partition). It may
+   * be beneficial to preserve that grouping while combining tasks into groups if query engines can
+   * leverage that information to avoid shuffles. Grouping keys indicate how data is split between
+   * different task groups. The grouping key type is determined at planning time and is identical
+   * across all task groups produced by a scan.
+   *
+   * <p>All records produced by tasks in this group are guaranteed to have the reported grouping
+   * key. Implementations should return an empty struct if the data grouping is random or unknown.
+   *
+   * @return a grouping key for this task group
+   */
+  default StructLike groupingKey() {
+    return EmptyStructLike.get();
+  }
+
   /** Returns scan tasks in this group. */
   Collection<T> tasks();
 
