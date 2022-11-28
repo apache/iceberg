6b8f7e0e31a81029b478e7757aba749f5ed27f42
Haizhou Zhao <zhaohaizhou940527@gmail.com>
2022-11-28 06:51:57 -0800

    Hive: Set the database owner on namespace creation (#6045)

--- core/src/main/java/org/apache/iceberg/TableProperties.java
+++ core/src/main/java/org/apache/iceberg/TableProperties.java
@@ -359,6 +359,4 @@ public class TableProperties {
 
   public static final String UPSERT_ENABLED = "write.upsert.enabled";
   public static final boolean UPSERT_ENABLED_DEFAULT = false;
-
-  public static final String HMS_TABLE_OWNER = "hive.metastore.table.owner";
 }
