c8ab919976631da4818dec5f6d333eda3ca7c3ff
Fokko Driesprong <fokko@apache.org>
2022-12-02 16:39:49 +0100

    Python: Fix PyArrow Type conversion
    It turns out that we need to cast the literal to the right type:
    
    ```
    ArrowNotImplementedError: Function 'greater_equal' has no kernel matching input types (timestamp[us, tz=+00:00], int64)
    ```
    
    After casting the scalar, it works perfect. One awkward thing to note that
    we don't have to do this for the `isin` predicate. There this is done
    for us. Please check the Jupyter notebook.

--- python/pyiceberg/io/pyarrow.py
+++ python/pyiceberg/io/pyarrow.py
@@ -64,6 +64,7 @@ from pyiceberg.types import (
     DoubleType,
     FixedType,
     FloatType,
+    IcebergType,
     IntegerType,
     ListType,
     LongType,
@@ -387,6 +388,12 @@ def _(_: BinaryType) -> pa.DataType:
     return pa.binary()
 
 
+def _convert_scalar(value: Any, iceberg_type: IcebergType) -> pa.scalar:
+    if not isinstance(iceberg_type, PrimitiveType):
+        raise ValueError(f"Expected primitive type, got: {iceberg_type}")
+    return pa.scalar(value).cast(_iceberg_to_pyarrow_type(iceberg_type))
+
+
 class _ConvertToArrowExpression(BoundBooleanExpressionVisitor[pc.Expression]):
     def visit_in(self, term: BoundTerm[pc.Expression], literals: Set[Any]) -> pc.Expression:
         return pc.field(term.ref().field.name).isin(literals)
@@ -409,22 +416,22 @@ class _ConvertToArrowExpression(BoundBooleanExpressionVisitor[pc.Expression]):
         return pc.field(term.ref().field.name).is_valid()
 
     def visit_equal(self, term: BoundTerm[Any], literal: Literal[Any]) -> pc.Expression:
-        return pc.field(term.ref().field.name) == literal.value
+        return pc.field(term.ref().field.name) == _convert_scalar(literal.value, term.ref().field.field_type)
 
     def visit_not_equal(self, term: BoundTerm[Any], literal: Literal[Any]) -> pc.Expression:
-        return pc.field(term.ref().field.name) != literal.value
+        return pc.field(term.ref().field.name) != _convert_scalar(literal.value, term.ref().field.field_type)
 
     def visit_greater_than_or_equal(self, term: BoundTerm[Any], literal: Literal[Any]) -> pc.Expression:
-        return pc.field(term.ref().field.name) >= literal.value
+        return pc.field(term.ref().field.name) >= _convert_scalar(literal.value, term.ref().field.field_type)
 
     def visit_greater_than(self, term: BoundTerm[Any], literal: Literal[Any]) -> pc.Expression:
-        return pc.field(term.ref().field.name) > literal.value
+        return pc.field(term.ref().field.name) > _convert_scalar(literal.value, term.ref().field.field_type)
 
     def visit_less_than(self, term: BoundTerm[Any], literal: Literal[Any]) -> pc.Expression:
-        return pc.field(term.ref().field.name) < literal.value
+        return pc.field(term.ref().field.name) < _convert_scalar(literal.value, term.ref().field.field_type)
 
     def visit_less_than_or_equal(self, term: BoundTerm[Any], literal: Literal[Any]) -> pc.Expression:
-        return pc.field(term.ref().field.name) <= literal.value
+        return pc.field(term.ref().field.name) <= _convert_scalar(literal.value, term.ref().field.field_type)
 
     def visit_true(self) -> pc.Expression:
         return pc.scalar(True)
