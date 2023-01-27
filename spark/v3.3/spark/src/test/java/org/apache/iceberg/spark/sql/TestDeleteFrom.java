bf55dcf85960487c38308322ab8d9bd15c6cd623
Russell_Spitzer <rspitzer@apple.com>
2023-01-27 11:07:42 -0600

    Remove Spark Test

--- spark/v3.3/spark/src/test/java/org/apache/iceberg/spark/sql/TestDeleteFrom.java
+++ spark/v3.3/spark/src/test/java/org/apache/iceberg/spark/sql/TestDeleteFrom.java
@@ -21,24 +21,18 @@ package org.apache.iceberg.spark.sql;
 import java.util.List;
 import java.util.Map;
 import org.apache.iceberg.AssertHelpers;
-import org.apache.iceberg.PartitionData;
 import org.apache.iceberg.Table;
 import org.apache.iceberg.relocated.com.google.common.collect.ImmutableList;
 import org.apache.iceberg.relocated.com.google.common.collect.Iterables;
 import org.apache.iceberg.relocated.com.google.common.collect.Lists;
-import org.apache.iceberg.spark.Spark3Util;
 import org.apache.iceberg.spark.SparkCatalogTestBase;
 import org.apache.iceberg.spark.source.SimpleRecord;
-import org.apache.iceberg.util.StructLikeWrapper;
 import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.catalyst.analysis.NoSuchTableException;
-import org.apache.spark.sql.catalyst.parser.ParseException;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
-import scala.Tuple2;
 
 public class TestDeleteFrom extends SparkCatalogTestBase {
   public TestDeleteFrom(String catalogName, String implementation, Map<String, String> config) {
@@ -172,47 +166,5 @@ public class TestDeleteFrom extends SparkCatalogTestBase {
         "Should have expected rows",
         ImmutableList.of(),
         sql("SELECT * FROM %s ORDER BY id", tableName));
-  }
-
-  @Test
-  public void testDeleteWithCommonPartitionHashCode() throws NoSuchTableException, ParseException {
-    sql(
-        "CREATE TABLE %s (log_dateint int, request_dateint int) USING iceberg "
-            + "PARTITIONED BY (log_dateint, request_dateint)",
-        tableName);
-
-    Table table = Spark3Util.loadIcebergTable(spark, tableName);
-    PartitionData partitionOne = new PartitionData(table.spec().partitionType());
-    partitionOne.set(0, 20220730);
-    partitionOne.set(1, 20220721);
-    PartitionData partitionTwo = new PartitionData(table.spec().partitionType());
-    partitionTwo.set(0, 20220728);
-    partitionTwo.set(1, 20220803);
-
-    Assert.assertEquals(
-        StructLikeWrapper.forType(table.spec().partitionType()).set(partitionOne).hashCode(),
-        StructLikeWrapper.forType(table.spec().partitionType()).set(partitionTwo).hashCode());
-
-    Dataset<Row> df =
-        spark
-            .createDataset(
-                ImmutableList.of(
-                    Tuple2.apply((int) partitionOne.get(0), (int) partitionOne.get(1)),
-                    Tuple2.apply((int) partitionTwo.get(0), (int) partitionTwo.get(1))),
-                Encoders.tuple(Encoders.INT(), Encoders.INT()))
-            .toDF("log_dateint", "request_dateint");
-
-    df.writeTo(tableName).append();
-
-    Assert.assertEquals(
-        1, sql("SELECT * FROM %s WHERE request_dateint == 20220803", tableName).size());
-    Assert.assertEquals(
-        1, sql("SELECT * FROM %s WHERE request_dateint == 20220721", tableName).size());
-
-    sql("DELETE FROM %s WHERE request_dateint == 20220803", tableName);
-    Assert.assertEquals(
-        0, sql("SELECT * FROM %s WHERE request_dateint == 20220803", tableName).size());
-    Assert.assertEquals(
-        1, sql("SELECT * FROM %s WHERE request_dateint == 20220721", tableName).size());
   }
 }
