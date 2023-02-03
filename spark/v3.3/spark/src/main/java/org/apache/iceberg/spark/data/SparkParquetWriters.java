65d9c640a183b2979340ccdd91a1ced1a5dea77e
slfan1989 <louj1988@@>
2023-02-03 19:02:04 +0800

    fix spark3.3 parquet deprecated method.

--- spark/v3.3/spark/src/main/java/org/apache/iceberg/spark/data/SparkParquetWriters.java
+++ spark/v3.3/spark/src/main/java/org/apache/iceberg/spark/data/SparkParquetWriters.java
@@ -34,8 +34,8 @@ import org.apache.iceberg.types.TypeUtil;
 import org.apache.iceberg.util.DecimalUtil;
 import org.apache.parquet.column.ColumnDescriptor;
 import org.apache.parquet.io.api.Binary;
-import org.apache.parquet.schema.DecimalMetadata;
 import org.apache.parquet.schema.GroupType;
+import org.apache.parquet.schema.LogicalTypeAnnotation;
 import org.apache.parquet.schema.MessageType;
 import org.apache.parquet.schema.PrimitiveType;
 import org.apache.parquet.schema.Type;
@@ -151,7 +151,9 @@ public class SparkParquetWriters {
           case TIMESTAMP_MICROS:
             return ParquetValueWriters.longs(desc);
           case DECIMAL:
-            DecimalMetadata decimal = primitive.getDecimalMetadata();
+            LogicalTypeAnnotation typeAnnotation = primitive.getLogicalTypeAnnotation();
+            LogicalTypeAnnotation.DecimalLogicalTypeAnnotation decimal =
+                (LogicalTypeAnnotation.DecimalLogicalTypeAnnotation) typeAnnotation;
             switch (primitive.getPrimitiveTypeName()) {
               case INT32:
                 return decimalAsInteger(desc, decimal.getPrecision(), decimal.getScale());
