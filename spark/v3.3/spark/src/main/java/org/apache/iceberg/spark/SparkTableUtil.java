2c9848482663c3aff3472264bbb223fde376144d
Abid Mohammed <abid_mohammed@apple.com>
2023-02-12 22:53:32 -0800

    spotless apply

--- spark/v3.3/spark/src/main/java/org/apache/iceberg/spark/SparkTableUtil.java
+++ spark/v3.3/spark/src/main/java/org/apache/iceberg/spark/SparkTableUtil.java
@@ -373,7 +373,9 @@ public class SparkTableUtil {
       FileIO io = new HadoopFileIO(conf.get());
       TaskContext ctx = TaskContext.get();
       String suffix =
-          String.format("stage-%d-task-%d-manifest-%s", ctx.stageId(), ctx.taskAttemptId(), UUID.randomUUID());
+          String.format(
+              "stage-%d-task-%d-manifest-%s",
+              ctx.stageId(), ctx.taskAttemptId(), UUID.randomUUID());
       Path location = new Path(basePath, suffix);
       String outputPath = FileFormat.AVRO.addExtension(location.toString());
       OutputFile outputFile = io.newOutputFile(outputPath);
