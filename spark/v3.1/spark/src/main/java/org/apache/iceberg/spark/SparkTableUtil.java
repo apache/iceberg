18cb72c903af6a28e38e6ca0dc25a29fa6340c19
Abid Mohammed <abid_mohammed@apple.com>
2023-02-15 11:17:06 -0800

    remove unintended commit

--- spark/v3.1/spark/src/main/java/org/apache/iceberg/spark/SparkTableUtil.java
+++ spark/v3.1/spark/src/main/java/org/apache/iceberg/spark/SparkTableUtil.java
@@ -27,7 +27,6 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 import java.util.stream.Collectors;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
