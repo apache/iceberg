3bc5e4e7538d494c5d6f7152504bb18cbeaae5df
pritampan <pritampan@pinterest.com>
2023-02-16 07:14:20 +0000

    corrected lint failure

--- python/pyiceberg/expressions/literals.py
+++ python/pyiceberg/expressions/literals.py
@@ -565,8 +565,8 @@ class StringLiteral(Literal[str]):
     @to.register(BooleanType)
     def _(self, type_var: BooleanType) -> Literal[bool]:
         value_upper = self.value.upper()
-        if value_upper in ['TRUE', 'FALSE']:
-            return BooleanLiteral(value_upper == 'TRUE')
+        if value_upper in ["TRUE", "FALSE"]:
+            return BooleanLiteral(value_upper == "TRUE")
         else:
             raise ValueError(f"Could not convert {self.value} into a {type_var}")
 
