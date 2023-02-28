d1679dfac55d9cc12d8f869a85109054a41ebd31
Amogh Jahagirdar <jahamogh@amazon.com>
2023-02-28 15:43:44 -0800

    Flink: Backport PR #6660 to Flink 1.14 and 1.15 (#6949)

--- flink/v1.14/flink/src/test/java/org/apache/iceberg/flink/sink/TestFlinkIcebergSinkV2.java
+++ flink/v1.14/flink/src/test/java/org/apache/iceberg/flink/sink/TestFlinkIcebergSinkV2.java
@@ -18,38 +18,25 @@
  */
 package org.apache.iceberg.flink.sink;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-import org.apache.flink.api.common.typeinfo.TypeInformation;
-import org.apache.flink.api.java.functions.KeySelector;
-import org.apache.flink.api.java.typeutils.RowTypeInfo;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.test.util.MiniClusterWithClientResource;
 import org.apache.flink.types.Row;
-import org.apache.flink.types.RowKind;
 import org.apache.iceberg.AssertHelpers;
 import org.apache.iceberg.FileFormat;
 import org.apache.iceberg.PartitionSpec;
-import org.apache.iceberg.Snapshot;
-import org.apache.iceberg.Table;
+import org.apache.iceberg.SnapshotRef;
 import org.apache.iceberg.TableProperties;
-import org.apache.iceberg.data.IcebergGenerics;
-import org.apache.iceberg.data.Record;
 import org.apache.iceberg.flink.HadoopCatalogResource;
 import org.apache.iceberg.flink.MiniClusterResource;
 import org.apache.iceberg.flink.SimpleDataUtil;
-import org.apache.iceberg.flink.TableLoader;
 import org.apache.iceberg.flink.TestFixtures;
 import org.apache.iceberg.flink.source.BoundedTestSource;
-import org.apache.iceberg.io.CloseableIterable;
 import org.apache.iceberg.relocated.com.google.common.collect.ImmutableList;
 import org.apache.iceberg.relocated.com.google.common.collect.ImmutableMap;
 import org.apache.iceberg.relocated.com.google.common.collect.Lists;
 import org.apache.iceberg.relocated.com.google.common.collect.Sets;
 import org.apache.iceberg.types.Types;
-import org.apache.iceberg.util.StructLikeSet;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -60,7 +47,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 @RunWith(Parameterized.class)
-public class TestFlinkIcebergSinkV2 {
+public class TestFlinkIcebergSinkV2 extends TestFlinkIcebergSinkV2Base {
 
   @ClassRule
   public static final MiniClusterWithClientResource MINI_CLUSTER_RESOURCE =
@@ -71,29 +58,6 @@ public class TestFlinkIcebergSinkV2 {
   @Rule
   public final HadoopCatalogResource catalogResource =
       new HadoopCatalogResource(TEMPORARY_FOLDER, TestFixtures.DATABASE, TestFixtures.TABLE);
-
-  private static final int FORMAT_V2 = 2;
-  private static final TypeInformation<Row> ROW_TYPE_INFO =
-      new RowTypeInfo(SimpleDataUtil.FLINK_SCHEMA.getFieldTypes());
-
-  private static final Map<String, RowKind> ROW_KIND_MAP =
-      ImmutableMap.of(
-          "+I", RowKind.INSERT,
-          "-D", RowKind.DELETE,
-          "-U", RowKind.UPDATE_BEFORE,
-          "+U", RowKind.UPDATE_AFTER);
-
-  private static final int ROW_ID_POS = 0;
-  private static final int ROW_DATA_POS = 1;
-
-  private final FileFormat format;
-  private final int parallelism;
-  private final boolean partitioned;
-  private final String writeDistributionMode;
-
-  private Table table;
-  private StreamExecutionEnvironment env;
-  private TableLoader tableLoader;
 
   @Parameterized.Parameters(
       name = "FileFormat = {0}, Parallelism = {1}, Partitioned={2}, WriteDistributionMode ={3}")
@@ -155,67 +119,6 @@ public class TestFlinkIcebergSinkV2 {
     tableLoader = catalogResource.tableLoader();
   }
 
-  private List<Snapshot> findValidSnapshots() {
-    List<Snapshot> validSnapshots = Lists.newArrayList();
-    for (Snapshot snapshot : table.snapshots()) {
-      if (snapshot.allManifests(table.io()).stream()
-          .anyMatch(m -> snapshot.snapshotId() == m.snapshotId())) {
-        validSnapshots.add(snapshot);
-      }
-    }
-    return validSnapshots;
-  }
-
-  private void testChangeLogs(
-      List<String> equalityFieldColumns,
-      KeySelector<Row, Object> keySelector,
-      boolean insertAsUpsert,
-      List<List<Row>> elementsPerCheckpoint,
-      List<List<Record>> expectedRecordsPerCheckpoint)
-      throws Exception {
-    DataStream<Row> dataStream =
-        env.addSource(new BoundedTestSource<>(elementsPerCheckpoint), ROW_TYPE_INFO);
-
-    FlinkSink.forRow(dataStream, SimpleDataUtil.FLINK_SCHEMA)
-        .tableLoader(tableLoader)
-        .tableSchema(SimpleDataUtil.FLINK_SCHEMA)
-        .writeParallelism(parallelism)
-        .equalityFieldColumns(equalityFieldColumns)
-        .upsert(insertAsUpsert)
-        .append();
-
-    // Execute the program.
-    env.execute("Test Iceberg Change-Log DataStream.");
-
-    table.refresh();
-    List<Snapshot> snapshots = findValidSnapshots();
-    int expectedSnapshotNum = expectedRecordsPerCheckpoint.size();
-    Assert.assertEquals(
-        "Should have the expected snapshot number", expectedSnapshotNum, snapshots.size());
-
-    for (int i = 0; i < expectedSnapshotNum; i++) {
-      long snapshotId = snapshots.get(i).snapshotId();
-      List<Record> expectedRecords = expectedRecordsPerCheckpoint.get(i);
-      Assert.assertEquals(
-          "Should have the expected records for the checkpoint#" + i,
-          expectedRowSet(expectedRecords.toArray(new Record[0])),
-          actualRowSet(snapshotId, "*"));
-    }
-  }
-
-  private Row row(String rowKind, int id, String data) {
-    RowKind kind = ROW_KIND_MAP.get(rowKind);
-    if (kind == null) {
-      throw new IllegalArgumentException("Unknown row kind: " + rowKind);
-    }
-
-    return Row.ofKind(kind, id, data);
-  }
-
-  private Record record(int id, String data) {
-    return SimpleDataUtil.createRecord(id, data);
-  }
-
   @Test
   public void testCheckAndGetEqualityFieldIds() {
     table
@@ -249,79 +152,12 @@ public class TestFlinkIcebergSinkV2 {
 
   @Test
   public void testChangeLogOnIdKey() throws Exception {
-    List<List<Row>> elementsPerCheckpoint =
-        ImmutableList.of(
-            ImmutableList.of(
-                row("+I", 1, "aaa"),
-                row("-D", 1, "aaa"),
-                row("+I", 1, "bbb"),
-                row("+I", 2, "aaa"),
-                row("-D", 2, "aaa"),
-                row("+I", 2, "bbb")),
-            ImmutableList.of(
-                row("-U", 2, "bbb"), row("+U", 2, "ccc"), row("-D", 2, "ccc"), row("+I", 2, "ddd")),
-            ImmutableList.of(
-                row("-D", 1, "bbb"),
-                row("+I", 1, "ccc"),
-                row("-D", 1, "ccc"),
-                row("+I", 1, "ddd")));
-
-    List<List<Record>> expectedRecords =
-        ImmutableList.of(
-            ImmutableList.of(record(1, "bbb"), record(2, "bbb")),
-            ImmutableList.of(record(1, "bbb"), record(2, "ddd")),
-            ImmutableList.of(record(1, "ddd"), record(2, "ddd")));
-
-    if (partitioned && writeDistributionMode.equals(TableProperties.WRITE_DISTRIBUTION_MODE_HASH)) {
-      AssertHelpers.assertThrows(
-          "Should be error because equality field columns don't include all partition keys",
-          IllegalStateException.class,
-          "should be included in equality fields",
-          () -> {
-            testChangeLogs(
-                ImmutableList.of("id"),
-                row -> row.getField(ROW_ID_POS),
-                false,
-                elementsPerCheckpoint,
-                expectedRecords);
-            return null;
-          });
-    } else {
-      testChangeLogs(
-          ImmutableList.of("id"),
-          row -> row.getField(ROW_ID_POS),
-          false,
-          elementsPerCheckpoint,
-          expectedRecords);
-    }
+    testChangeLogOnIdKey(SnapshotRef.MAIN_BRANCH);
   }
 
   @Test
   public void testChangeLogOnDataKey() throws Exception {
-    List<List<Row>> elementsPerCheckpoint =
-        ImmutableList.of(
-            ImmutableList.of(
-                row("+I", 1, "aaa"),
-                row("-D", 1, "aaa"),
-                row("+I", 2, "bbb"),
-                row("+I", 1, "bbb"),
-                row("+I", 2, "aaa")),
-            ImmutableList.of(row("-U", 2, "aaa"), row("+U", 1, "ccc"), row("+I", 1, "aaa")),
-            ImmutableList.of(row("-D", 1, "bbb"), row("+I", 2, "aaa"), row("+I", 2, "ccc")));
-
-    List<List<Record>> expectedRecords =
-        ImmutableList.of(
-            ImmutableList.of(record(1, "bbb"), record(2, "aaa")),
-            ImmutableList.of(record(1, "aaa"), record(1, "bbb"), record(1, "ccc")),
-            ImmutableList.of(
-                record(1, "aaa"), record(1, "ccc"), record(2, "aaa"), record(2, "ccc")));
-
-    testChangeLogs(
-        ImmutableList.of("data"),
-        row -> row.getField(ROW_DATA_POS),
-        false,
-        elementsPerCheckpoint,
-        expectedRecords);
+    testChangeLogOnDataKey(SnapshotRef.MAIN_BRANCH);
   }
 
   @Test
@@ -344,59 +180,12 @@ public class TestFlinkIcebergSinkV2 {
 
   @Test
   public void testChangeLogOnIdDataKey() throws Exception {
-    List<List<Row>> elementsPerCheckpoint =
-        ImmutableList.of(
-            ImmutableList.of(
-                row("+I", 1, "aaa"),
-                row("-D", 1, "aaa"),
-                row("+I", 2, "bbb"),
-                row("+I", 1, "bbb"),
-                row("+I", 2, "aaa")),
-            ImmutableList.of(row("-U", 2, "aaa"), row("+U", 1, "ccc"), row("+I", 1, "aaa")),
-            ImmutableList.of(row("-D", 1, "bbb"), row("+I", 2, "aaa")));
-
-    List<List<Record>> expectedRecords =
-        ImmutableList.of(
-            ImmutableList.of(record(1, "bbb"), record(2, "aaa"), record(2, "bbb")),
-            ImmutableList.of(
-                record(1, "aaa"), record(1, "bbb"), record(1, "ccc"), record(2, "bbb")),
-            ImmutableList.of(
-                record(1, "aaa"), record(1, "ccc"), record(2, "aaa"), record(2, "bbb")));
-
-    testChangeLogs(
-        ImmutableList.of("data", "id"),
-        row -> Row.of(row.getField(ROW_ID_POS), row.getField(ROW_DATA_POS)),
-        false,
-        elementsPerCheckpoint,
-        expectedRecords);
+    testChangeLogOnIdDataKey(SnapshotRef.MAIN_BRANCH);
   }
 
   @Test
   public void testChangeLogOnSameKey() throws Exception {
-    List<List<Row>> elementsPerCheckpoint =
-        ImmutableList.of(
-            // Checkpoint #1
-            ImmutableList.of(row("+I", 1, "aaa"), row("-D", 1, "aaa"), row("+I", 1, "aaa")),
-            // Checkpoint #2
-            ImmutableList.of(row("-U", 1, "aaa"), row("+U", 1, "aaa")),
-            // Checkpoint #3
-            ImmutableList.of(row("-D", 1, "aaa"), row("+I", 1, "aaa")),
-            // Checkpoint #4
-            ImmutableList.of(row("-U", 1, "aaa"), row("+U", 1, "aaa"), row("+I", 1, "aaa")));
-
-    List<List<Record>> expectedRecords =
-        ImmutableList.of(
-            ImmutableList.of(record(1, "aaa")),
-            ImmutableList.of(record(1, "aaa")),
-            ImmutableList.of(record(1, "aaa")),
-            ImmutableList.of(record(1, "aaa"), record(1, "aaa")));
-
-    testChangeLogs(
-        ImmutableList.of("id", "data"),
-        row -> Row.of(row.getField(ROW_ID_POS), row.getField(ROW_DATA_POS)),
-        false,
-        elementsPerCheckpoint,
-        expectedRecords);
+    testChangeLogOnSameKey(SnapshotRef.MAIN_BRANCH);
   }
 
   @Test
@@ -426,97 +215,16 @@ public class TestFlinkIcebergSinkV2 {
 
   @Test
   public void testUpsertOnIdKey() throws Exception {
-    List<List<Row>> elementsPerCheckpoint =
-        ImmutableList.of(
-            ImmutableList.of(row("+I", 1, "aaa"), row("+U", 1, "bbb")),
-            ImmutableList.of(row("+I", 1, "ccc")),
-            ImmutableList.of(row("+U", 1, "ddd"), row("+I", 1, "eee")));
-
-    List<List<Record>> expectedRecords =
-        ImmutableList.of(
-            ImmutableList.of(record(1, "bbb")),
-            ImmutableList.of(record(1, "ccc")),
-            ImmutableList.of(record(1, "eee")));
-
-    if (!partitioned) {
-      testChangeLogs(
-          ImmutableList.of("id"),
-          row -> row.getField(ROW_ID_POS),
-          true,
-          elementsPerCheckpoint,
-          expectedRecords);
-    } else {
-      AssertHelpers.assertThrows(
-          "Should be error because equality field columns don't include all partition keys",
-          IllegalStateException.class,
-          "should be included in equality fields",
-          () -> {
-            testChangeLogs(
-                ImmutableList.of("id"),
-                row -> row.getField(ROW_ID_POS),
-                true,
-                elementsPerCheckpoint,
-                expectedRecords);
-            return null;
-          });
-    }
+    testUpsertOnIdKey(SnapshotRef.MAIN_BRANCH);
   }
 
   @Test
   public void testUpsertOnDataKey() throws Exception {
-    List<List<Row>> elementsPerCheckpoint =
-        ImmutableList.of(
-            ImmutableList.of(row("+I", 1, "aaa"), row("+I", 2, "aaa"), row("+I", 3, "bbb")),
-            ImmutableList.of(row("+U", 4, "aaa"), row("-U", 3, "bbb"), row("+U", 5, "bbb")),
-            ImmutableList.of(row("+I", 6, "aaa"), row("+U", 7, "bbb")));
-
-    List<List<Record>> expectedRecords =
-        ImmutableList.of(
-            ImmutableList.of(record(2, "aaa"), record(3, "bbb")),
-            ImmutableList.of(record(4, "aaa"), record(5, "bbb")),
-            ImmutableList.of(record(6, "aaa"), record(7, "bbb")));
-
-    testChangeLogs(
-        ImmutableList.of("data"),
-        row -> row.getField(ROW_DATA_POS),
-        true,
-        elementsPerCheckpoint,
-        expectedRecords);
+    testUpsertOnDataKey(SnapshotRef.MAIN_BRANCH);
   }
 
   @Test
   public void testUpsertOnIdDataKey() throws Exception {
-    List<List<Row>> elementsPerCheckpoint =
-        ImmutableList.of(
-            ImmutableList.of(row("+I", 1, "aaa"), row("+U", 1, "aaa"), row("+I", 2, "bbb")),
-            ImmutableList.of(row("+I", 1, "aaa"), row("-D", 2, "bbb"), row("+I", 2, "ccc")),
-            ImmutableList.of(row("+U", 1, "bbb"), row("-U", 1, "ccc"), row("-D", 1, "aaa")));
-
-    List<List<Record>> expectedRecords =
-        ImmutableList.of(
-            ImmutableList.of(record(1, "aaa"), record(2, "bbb")),
-            ImmutableList.of(record(1, "aaa"), record(2, "ccc")),
-            ImmutableList.of(record(1, "bbb"), record(2, "ccc")));
-
-    testChangeLogs(
-        ImmutableList.of("id", "data"),
-        row -> Row.of(row.getField(ROW_ID_POS), row.getField(ROW_DATA_POS)),
-        true,
-        elementsPerCheckpoint,
-        expectedRecords);
-  }
-
-  private StructLikeSet expectedRowSet(Record... records) {
-    return SimpleDataUtil.expectedRowSet(table, records);
-  }
-
-  private StructLikeSet actualRowSet(long snapshotId, String... columns) throws IOException {
-    table.refresh();
-    StructLikeSet set = StructLikeSet.create(table.schema().asStruct());
-    try (CloseableIterable<Record> reader =
-        IcebergGenerics.read(table).useSnapshot(snapshotId).select(columns).build()) {
-      reader.forEach(set::add);
-    }
-    return set;
+    testUpsertOnIdDataKey(SnapshotRef.MAIN_BRANCH);
   }
 }
