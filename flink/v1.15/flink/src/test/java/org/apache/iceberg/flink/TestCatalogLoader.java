fd5b685d6b544dc184ff5fd726d1a650645183ee
Fokko Driesprong <fokko@apache.org>
2023-03-09 08:52:24 +0100

    Flink: Add REST catalog shorthand

--- flink/v1.15/flink/src/test/java/org/apache/iceberg/flink/TestCatalogLoader.java
+++ flink/v1.15/flink/src/test/java/org/apache/iceberg/flink/TestCatalogLoader.java
@@ -18,19 +18,22 @@
  */
 package org.apache.iceberg.flink;
 
+import static org.apache.iceberg.CatalogProperties.URI;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.Map;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.iceberg.CatalogProperties;
 import org.apache.iceberg.Schema;
 import org.apache.iceberg.Table;
 import org.apache.iceberg.catalog.TableIdentifier;
 import org.apache.iceberg.hadoop.HadoopFileIO;
-import org.apache.iceberg.hadoop.HadoopTables;
 import org.apache.iceberg.io.FileIO;
 import org.apache.iceberg.relocated.com.google.common.collect.Maps;
 import org.apache.iceberg.types.Types;
@@ -40,8 +43,8 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-/** Test for {@link TableLoader}. */
-public class TestCatalogTableLoader extends FlinkTestBase {
+/** Test for {@link CatalogLoader}. */
+public class TestCatalogLoader extends FlinkTestBase {
 
   private static File warehouse = null;
   private static final TableIdentifier IDENTIFIER = TableIdentifier.of("default", "my_table");
@@ -65,30 +68,30 @@ public class TestCatalogTableLoader extends FlinkTestBase {
   }
 
   @Test
-  public void testHadoopTableLoader() throws IOException, ClassNotFoundException {
-    String location = "file:" + warehouse + "/my_table";
-    new HadoopTables(hiveConf).create(SCHEMA, location);
-    validateTableLoader(TableLoader.fromHadoopTable(location, hiveConf));
+  public void testHadoopCatalogLoader() throws IOException, ClassNotFoundException {
+    Map<String, String> properties = Maps.newHashMap();
+    properties.put(CatalogProperties.WAREHOUSE_LOCATION, "file:" + warehouse);
+    CatalogLoader loader = CatalogLoader.hadoop("my_catalog", hiveConf, properties);
+    validateCatalogLoader(loader);
   }
 
   @Test
-  public void testHiveCatalogTableLoader() throws IOException, ClassNotFoundException {
+  public void testHiveCatalogLoader() throws IOException, ClassNotFoundException {
     CatalogLoader loader = CatalogLoader.hive("my_catalog", hiveConf, Maps.newHashMap());
-    javaSerdes(loader).loadCatalog().createTable(IDENTIFIER, SCHEMA);
-
-    CatalogLoader catalogLoader = CatalogLoader.hive("my_catalog", hiveConf, Maps.newHashMap());
-    validateTableLoader(TableLoader.fromCatalog(catalogLoader, IDENTIFIER));
+    validateCatalogLoader(loader);
   }
 
-  private static void validateTableLoader(TableLoader loader)
+  @Test
+  public void testRESTCatalogLoader() {
+    Map<String, String> properties = Maps.newHashMap();
+    properties.put(URI, "http://localhost/");
+    CatalogLoader.rest("my_catalog", hiveConf, Maps.newHashMap());
+  }
+
+  private static void validateCatalogLoader(CatalogLoader loader)
       throws IOException, ClassNotFoundException {
-    TableLoader copied = javaSerdes(loader);
-    copied.open();
-    try {
-      validateHadoopConf(copied.loadTable());
-    } finally {
-      copied.close();
-    }
+    Table table = javaSerAndDeSer(loader).loadCatalog().createTable(IDENTIFIER, SCHEMA);
+    validateHadoopConf(table);
   }
 
   private static void validateHadoopConf(Table table) {
@@ -101,7 +104,7 @@ public class TestCatalogTableLoader extends FlinkTestBase {
   }
 
   @SuppressWarnings("unchecked")
-  private static <T> T javaSerdes(T object) throws IOException, ClassNotFoundException {
+  private static <T> T javaSerAndDeSer(T object) throws IOException, ClassNotFoundException {
     ByteArrayOutputStream bytes = new ByteArrayOutputStream();
     try (ObjectOutputStream out = new ObjectOutputStream(bytes)) {
       out.writeObject(object);
