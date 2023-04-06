53353ffa1e030c4f4384e5ed2b2b8d6f428d9901
LuigiCerone <luigicerone96@gmail.com>
2023-04-06 08:37:00 +0200

    Add mkdocs theme

--- python/mkdocs/gen_doc_stubs.py
+++ python/mkdocs/gen_doc_stubs.py
@@ -18,16 +18,39 @@
 from pathlib import Path
 
 import mkdocs_gen_files  # type: ignore
+import griffe  # type: ignore
+
+nav = mkdocs_gen_files.Nav()
 
 root = Path(__file__).parent.parent
 src_root = root.joinpath("pyiceberg")
 
-for path in src_root.glob("**/*.py"):
-    if "__init__" in str(path):  # skip __init__ files
+data = griffe.load(src_root)
+for path in sorted(src_root.glob("**/*.py")):
+    module_path = path.relative_to(root).with_suffix("")
+    doc_path = path.relative_to(root).with_suffix(".md")
+    full_doc_path = Path("reference", doc_path)
+
+    parts = tuple(module_path.parts)
+
+    if parts[-1] == "__init__":
+        init = True
+        parts = parts[:-1]
+        doc_path = doc_path.with_name("index.md")
+        full_doc_path = full_doc_path.with_name("index.md")
+    elif parts[-1].startswith("_"):
         continue
 
-    doc_path = Path("references", path.relative_to(root)).with_suffix(".md")
+    if module_path.parts[1:] in data.members and not data[module_path.parts[1:]].has_docstrings:
+        continue
 
-    with mkdocs_gen_files.open(doc_path, "w") as f:
-        ident = ".".join(path.relative_to(root).with_suffix("").parts)
-        print(f"::: {ident}", file=f)
+    nav[parts] = doc_path.as_posix()
+
+    with mkdocs_gen_files.open(full_doc_path, "w") as fd:
+        ident = ".".join(parts)
+        fd.write(f"::: {ident}")
+
+    mkdocs_gen_files.set_edit_path(full_doc_path, Path("../") / path)
+
+with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
+    nav_file.writelines(nav.build_literate_nav())
