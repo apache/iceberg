601c24c6df98ae7ef694b2853e2a77f4344b5d77
Gustavo Torres <gustavoatt@gmail.com>
2023-04-12 14:14:41 -0400

    Remove unnecessary comment

--- spark/v3.2/spark/src/main/java/org/apache/iceberg/spark/SparkWriteOptions.java
+++ spark/v3.2/spark/src/main/java/org/apache/iceberg/spark/SparkWriteOptions.java
@@ -57,7 +57,6 @@ public class SparkWriteOptions {
   public static final String HANDLE_TIMESTAMP_WITHOUT_TIMEZONE =
       "handle-timestamp-without-timezone";
 
-  // Output partition spec ID where writes should go.
   public static final String OUTPUT_SPEC_ID = "output-spec-id";
 
   public static final String OVERWRITE_MODE = "overwrite-mode";
