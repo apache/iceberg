1980e66ffed9c28fb92cdf2a516aaf35ed1109e5
Fokko Driesprong <fokko@apache.org>
2023-06-06 18:48:25 +0200

    Python: Fix 🐛 with ∅ partitions
    Resolves #7736

--- python/pyiceberg/expressions/visitors.py
+++ python/pyiceberg/expressions/visitors.py
@@ -494,16 +494,20 @@ class _ExpressionEvaluator(BoundBooleanExpressionVisitor[bool]):
         return term.eval(self.struct) != literal.value
 
     def visit_greater_than_or_equal(self, term: BoundTerm[L], literal: Literal[L]) -> bool:
-        return term.eval(self.struct) >= literal.value
+        value = term.eval(self.struct)
+        return value is not None and value >= literal.value
 
     def visit_greater_than(self, term: BoundTerm[L], literal: Literal[L]) -> bool:
-        return term.eval(self.struct) > literal.value
+        value = term.eval(self.struct)
+        return value is not None and value > literal.value
 
     def visit_less_than(self, term: BoundTerm[L], literal: Literal[L]) -> bool:
-        return term.eval(self.struct) < literal.value
+        value = term.eval(self.struct)
+        return value is not None and value < literal.value
 
     def visit_less_than_or_equal(self, term: BoundTerm[L], literal: Literal[L]) -> bool:
-        return term.eval(self.struct) <= literal.value
+        value = term.eval(self.struct)
+        return value is not None and value <= literal.value
 
     def visit_starts_with(self, term: BoundTerm[L], literal: Literal[L]) -> bool:
         eval_res = term.eval(self.struct)
