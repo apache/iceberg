5a268e7569d8ecaef03adf5d77ae0dd40fce7839
Xianyang Liu <liu-xianyang@hotmail.com>
2023-06-30 03:20:59 +0800

    Spark 3.1: Fixes bucket on binary column (#7719)
    * fixes bucket binary
    
    * add testDefaultSortOnBinaryBucketedColumn
    
    * address comments
    
    ---------
    
    Co-authored-by: xianyangliu <xianyangliu@tencent.com>

--- spark/v3.1/spark/src/main/scala/org/apache/spark/sql/catalyst/expressions/TransformExpressions.scala
+++ spark/v3.1/spark/src/main/scala/org/apache/spark/sql/catalyst/expressions/TransformExpressions.scala
@@ -96,6 +96,9 @@ case class IcebergBucketTransform(numBuckets: Int, child: Expression) extends Ic
       // TODO: pass bytes without the copy out of the InternalRow
       val t = Transforms.bucket[ByteBuffer](Types.BinaryType.get(), numBuckets)
       s: Any => t(ByteBuffer.wrap(s.asInstanceOf[UTF8String].getBytes)).toInt
+    case _: BinaryType =>
+      val t = Transforms.bucket[Any](numBuckets).bind(icebergInputType)
+      b: Any => t(ByteBuffer.wrap(b.asInstanceOf[Array[Byte]])).toInt
     case _ =>
       val t = Transforms.bucket[Any](icebergInputType, numBuckets)
       a: Any => t(a).toInt
