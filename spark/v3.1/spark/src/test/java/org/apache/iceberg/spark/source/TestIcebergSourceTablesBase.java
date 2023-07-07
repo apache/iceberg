88b85041f5daf18ad57fcb9b7a312b79bb48ea71
hsiang-c <huangc@apple.com>
2023-07-07 14:37:35 +0800

    Revert "Switch to array impl"
    This reverts commit e3dbd94a7ff0b7c3ba67ad8997b2655b6c8bfab5.

--- spark/v3.1/spark/src/test/java/org/apache/iceberg/spark/source/TestIcebergSourceTablesBase.java
+++ spark/v3.1/spark/src/test/java/org/apache/iceberg/spark/source/TestIcebergSourceTablesBase.java
@@ -25,6 +25,7 @@ import static org.apache.iceberg.types.Types.NestedField.required;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.StringJoiner;
@@ -1487,7 +1488,7 @@ public abstract class TestIcebergSourceTablesBase extends SparkTestBase {
 
     List<DataFile> dataFiles = dataFiles(table);
     Assert.assertEquals("Table should have 3 data files", 3, dataFiles.size());
-    assertDataFilePartitions(dataFiles, new int[] {1, 2, 2});
+    assertDataFilePartitions(dataFiles, Arrays.asList(1, 2, 2));
 
     List<GenericData.Record> expected = Lists.newArrayList();
     expected.add(
@@ -2063,11 +2064,12 @@ public abstract class TestIcebergSourceTablesBase extends SparkTestBase {
     return Lists.newArrayList(CloseableIterable.transform(tasks, FileScanTask::file));
   }
 
-  private void assertDataFilePartitions(List<DataFile> dataFiles, int[] expectedPartitionIds) {
+  private void assertDataFilePartitions(
+      List<DataFile> dataFiles, List<Integer> expectedPartitionIds) {
     for (int i = 0; i < dataFiles.size(); ++i) {
       Assert.assertEquals(
-          "Data file should have partition of id " + expectedPartitionIds[i],
-          expectedPartitionIds[i],
+          "Data file should have partition of id " + expectedPartitionIds.get(i),
+          expectedPartitionIds.get(i).intValue(),
           dataFiles.get(i).partition().get(0, Integer.class).intValue());
     }
   }
