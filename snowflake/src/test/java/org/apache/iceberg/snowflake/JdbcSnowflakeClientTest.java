14123d57c1a65e8121b886b5d75e310d8bce91be
skytin1004 <skytin1004@sms7950>
2023-07-10 18:58:35 +0900

    Switch Junit5 in Core,aws,deltalake,snowflake

--- snowflake/src/test/java/org/apache/iceberg/snowflake/JdbcSnowflakeClientTest.java
+++ snowflake/src/test/java/org/apache/iceberg/snowflake/JdbcSnowflakeClientTest.java
@@ -23,7 +23,7 @@ import static org.apache.iceberg.snowflake.JdbcSnowflakeClient.SCHEMA_NOT_FOUND_
 import static org.apache.iceberg.snowflake.JdbcSnowflakeClient.TABLE_NOT_FOUND_ERROR_CODES;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -38,14 +38,14 @@ import org.apache.iceberg.jdbc.JdbcClientPool;
 import org.apache.iceberg.jdbc.UncheckedInterruptedException;
 import org.apache.iceberg.jdbc.UncheckedSQLException;
 import org.assertj.core.api.Assertions;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class JdbcSnowflakeClientTest {
   @Mock private Connection mockConnection;
   @Mock private JdbcClientPool mockClientPool;
@@ -54,15 +54,17 @@ public class JdbcSnowflakeClientTest {
 
   private JdbcSnowflakeClient snowflakeClient;
 
-  @Before
+  @BeforeEach
   public void before() throws SQLException, InterruptedException {
     snowflakeClient = new JdbcSnowflakeClient(mockClientPool);
     snowflakeClient.setQueryHarness(mockQueryHarness);
-
-    doAnswer(invocation -> ((ClientPool.Action) invocation.getArguments()[0]).run(mockConnection))
+    lenient()
+        .doAnswer(
+            invocation -> ((ClientPool.Action) invocation.getArguments()[0]).run(mockConnection))
         .when(mockClientPool)
         .run(any(ClientPool.Action.class));
-    doAnswer(
+    lenient()
+        .doAnswer(
             invocation ->
                 ((JdbcSnowflakeClient.ResultSetParser) invocation.getArguments()[2])
                     .parse(mockResultSet))
