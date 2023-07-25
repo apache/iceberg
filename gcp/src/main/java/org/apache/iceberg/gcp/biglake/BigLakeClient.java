23c1dc2c3e58c6a7e936dbccb42702120c279d66
coufon <zhoufang@google.com>
2023-07-25 02:07:52 +0000

    fix review  comments

--- gcp/src/main/java/org/apache/iceberg/gcp/biglake/BigLakeClient.java
+++ gcp/src/main/java/org/apache/iceberg/gcp/biglake/BigLakeClient.java
@@ -42,7 +42,6 @@ import com.google.cloud.bigquery.biglake.v1.Table;
 import com.google.cloud.bigquery.biglake.v1.TableName;
 import com.google.cloud.bigquery.biglake.v1.UpdateDatabaseRequest;
 import com.google.cloud.bigquery.biglake.v1.UpdateTableRequest;
-import com.google.protobuf.Empty;
 import com.google.protobuf.FieldMask;
 import java.io.Closeable;
 import java.io.IOException;
@@ -78,88 +77,80 @@ final class BigLakeClient implements Closeable {
 
   public Catalog createCatalog(CatalogName name, Catalog catalog) {
     return convertException(
-        () ->
-            stub.createCatalog(
+        () -> {
+          try {
+            return stub.createCatalog(
                 CreateCatalogRequest.newBuilder()
                     .setParent(LocationName.of(name.getProject(), name.getLocation()).toString())
                     .setCatalogId(name.getCatalog())
                     .setCatalog(catalog)
-                    .build()),
+                    .build());
+          } catch (com.google.api.gax.rpc.AlreadyExistsException e) {
+            throw new AlreadyExistsException(e, "Catalog already exists: %s", name.getCatalog());
+          }
+        },
         name.getCatalog());
   }
 
   public Catalog catalog(CatalogName name) {
-    return convertException(
-        () -> {
-          try {
-            return stub.getCatalog(GetCatalogRequest.newBuilder().setName(name.toString()).build());
-          } catch (PermissionDeniedException e) {
-            throw new NoSuchNamespaceException(
-                e, "Namespace does not exist: %s (or permission denied)", name.getCatalog());
-          }
-        },
-        name.getCatalog());
+    try {
+      return stub.getCatalog(GetCatalogRequest.newBuilder().setName(name.toString()).build());
+    } catch (PermissionDeniedException e) {
+      throw new NoSuchNamespaceException(
+          e, "Catalog does not exist: %s (or permission denied)", name.getCatalog());
+    }
   }
 
   public void deleteCatalog(CatalogName name) {
-    convertException(
-        () -> {
-          try {
-            stub.deleteCatalog(DeleteCatalogRequest.newBuilder().setName(name.toString()).build());
-            return Empty.getDefaultInstance();
-          } catch (PermissionDeniedException e) {
-            throw new NoSuchNamespaceException(
-                e, "Namespace does not exist: %s (or permission denied)", name.getCatalog());
-          }
-        },
-        name.getCatalog());
+    try {
+      stub.deleteCatalog(DeleteCatalogRequest.newBuilder().setName(name.toString()).build());
+    } catch (PermissionDeniedException e) {
+      throw new NoSuchNamespaceException(
+          e, "Catalog does not exist: %s (or permission denied)", name.getCatalog());
+    }
   }
 
   public Database createDatabase(DatabaseName name, Database db) {
     return convertException(
-        () ->
-            stub.createDatabase(
+        () -> {
+          try {
+            return stub.createDatabase(
                 CreateDatabaseRequest.newBuilder()
                     .setParent(
                         CatalogName.of(name.getProject(), name.getLocation(), name.getCatalog())
                             .toString())
                     .setDatabaseId(name.getDatabase())
                     .setDatabase(db)
-                    .build()),
+                    .build());
+          } catch (com.google.api.gax.rpc.AlreadyExistsException e) {
+            throw new AlreadyExistsException(e, "Namespace already exists: %s", name.getDatabase());
+          }
+        },
         name.getDatabase());
   }
 
   public Database database(DatabaseName name) {
-    return convertException(
-        () -> {
-          try {
-            return stub.getDatabase(
-                GetDatabaseRequest.newBuilder().setName(name.toString()).build());
-          } catch (PermissionDeniedException e) {
-            throw new NoSuchNamespaceException(
-                e, "Namespace does not exist: %s (or permission denied)", name.getDatabase());
-          }
-        },
-        name.getDatabase());
+    try {
+      return stub.getDatabase(GetDatabaseRequest.newBuilder().setName(name.toString()).build());
+    } catch (PermissionDeniedException e) {
+      throw new NoSuchNamespaceException(
+          e, "Namespace does not exist: %s (or permission denied)", name.getDatabase());
+    }
   }
 
   public Database updateDatabaseParameters(DatabaseName name, Map<String, String> parameters) {
     Database.Builder builder = Database.newBuilder().setName(name.toString());
     builder.getHiveOptionsBuilder().putAllParameters(parameters);
-    return convertException(
-        () -> {
-          try {
-            return stub.updateDatabase(
-                UpdateDatabaseRequest.newBuilder()
-                    .setDatabase(builder)
-                    .setUpdateMask(FieldMask.newBuilder().addPaths("hive_options.parameters"))
-                    .build());
-          } catch (PermissionDeniedException e) {
-            throw new NoSuchNamespaceException(
-                e, "Namespace does not exist: %s (or permission denied)", name.getDatabase());
-          }
-        },
-        name.getDatabase());
+    try {
+      return stub.updateDatabase(
+          UpdateDatabaseRequest.newBuilder()
+              .setDatabase(builder)
+              .setUpdateMask(FieldMask.newBuilder().addPaths("hive_options.parameters"))
+              .build());
+    } catch (PermissionDeniedException e) {
+      throw new NoSuchNamespaceException(
+          e, "Namespace does not exist: %s (or permission denied)", name.getDatabase());
+    }
   }
 
   public Iterable<Database> listDatabases(CatalogName name) {
@@ -171,18 +162,12 @@ final class BigLakeClient implements Closeable {
   }
 
   public void deleteDatabase(DatabaseName name) {
-    convertException(
-        () -> {
-          try {
-            stub.deleteDatabase(
-                DeleteDatabaseRequest.newBuilder().setName(name.toString()).build());
-            return Empty.getDefaultInstance();
-          } catch (PermissionDeniedException e) {
-            throw new NoSuchNamespaceException(
-                e, "Namespace does not exist: %s (or permission denied)", name.getDatabase());
-          }
-        },
-        name.getDatabase());
+    try {
+      stub.deleteDatabase(DeleteDatabaseRequest.newBuilder().setName(name.toString()).build());
+    } catch (PermissionDeniedException e) {
+      throw new NoSuchNamespaceException(
+          e, "Namespace does not exist: %s (or permission denied)", name.getDatabase());
+    }
   }
 
   public Table createTable(TableName name, Table table) {
@@ -206,68 +191,51 @@ final class BigLakeClient implements Closeable {
     if (name.getTable().isEmpty()) {
       throw new NoSuchTableException("BigLake API does not allow tables with empty ID");
     }
-    return convertException(
-        () -> {
-          try {
-            return stub.getTable(GetTableRequest.newBuilder().setName(name.toString()).build());
-          } catch (PermissionDeniedException e) {
-            throw new NoSuchTableException(
-                e, "Table does not exist: %s (or permission denied)", name.getTable());
-          }
-        },
-        name.getTable());
+    try {
+      return stub.getTable(GetTableRequest.newBuilder().setName(name.toString()).build());
+    } catch (PermissionDeniedException e) {
+      throw new NoSuchTableException(
+          e, "Table does not exist: %s (or permission denied)", name.getTable());
+    }
   }
 
   public Table updateTableParameters(TableName name, Map<String, String> parameters, String etag) {
     Table.Builder builder = Table.newBuilder().setName(name.toString()).setEtag(etag);
     builder.getHiveOptionsBuilder().putAllParameters(parameters);
-    return convertException(
-        () -> {
-          try {
-            return stub.updateTable(
-                UpdateTableRequest.newBuilder()
-                    .setTable(builder)
-                    .setUpdateMask(FieldMask.newBuilder().addPaths("hive_options.parameters"))
-                    .build());
-          } catch (PermissionDeniedException e) {
-            throw new NoSuchTableException(
-                e, "Table does not exist: %s (or permission denied)", name.getTable());
-          }
-        },
-        name.getTable());
+    try {
+      return stub.updateTable(
+          UpdateTableRequest.newBuilder()
+              .setTable(builder)
+              .setUpdateMask(FieldMask.newBuilder().addPaths("hive_options.parameters"))
+              .build());
+    } catch (PermissionDeniedException e) {
+      throw new NoSuchTableException(
+          e, "Table does not exist: %s (or permission denied)", name.getTable());
+    }
   }
 
   public Table renameTable(TableName name, TableName newName) {
-    return convertException(
-        () -> {
-          try {
-            return stub.renameTable(
-                RenameTableRequest.newBuilder()
-                    .setName(name.toString())
-                    .setNewName(newName.toString())
-                    .build());
-          } catch (PermissionDeniedException e) {
-            throw new NoSuchTableException(
-                e, "Table does not exist: %s (or permission denied)", name.getTable());
-          } catch (com.google.api.gax.rpc.AlreadyExistsException e) {
-            throw new AlreadyExistsException(e, "Table already exists: %s", newName.getTable());
-          }
-        },
-        name.getTable());
+    try {
+      return stub.renameTable(
+          RenameTableRequest.newBuilder()
+              .setName(name.toString())
+              .setNewName(newName.toString())
+              .build());
+    } catch (PermissionDeniedException e) {
+      throw new NoSuchTableException(
+          e, "Table does not exist: %s (or permission denied)", name.getTable());
+    } catch (com.google.api.gax.rpc.AlreadyExistsException e) {
+      throw new AlreadyExistsException(e, "Table already exists: %s", newName.getTable());
+    }
   }
 
   public Table deleteTable(TableName name) {
-    return convertException(
-        () -> {
-          try {
-            return stub.deleteTable(
-                DeleteTableRequest.newBuilder().setName(name.toString()).build());
-          } catch (PermissionDeniedException e) {
-            throw new NoSuchTableException(
-                e, "Table does not exist: %s (or permission denied)", name.getTable());
-          }
-        },
-        name.getTable());
+    try {
+      return stub.deleteTable(DeleteTableRequest.newBuilder().setName(name.toString()).build());
+    } catch (PermissionDeniedException e) {
+      throw new NoSuchTableException(
+          e, "Table does not exist: %s (or permission denied)", name.getTable());
+    }
   }
 
   public Iterable<Table> listTables(DatabaseName name) {
@@ -289,9 +257,6 @@ final class BigLakeClient implements Closeable {
       return result.get();
     } catch (PermissionDeniedException e) {
       throw new NotAuthorizedException(e, "Permission denied");
-    } catch (com.google.api.gax.rpc.AlreadyExistsException e) {
-      // "Table already exists" error should be caught earlier.
-      throw new AlreadyExistsException(e, "Namespace already exists: %s", resourceId);
     }
   }
 
