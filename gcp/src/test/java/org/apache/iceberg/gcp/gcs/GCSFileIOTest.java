3f949497f946aa0eb2391c3f0e31ed4d0f9e58a6
Bryan Keller <bryanck@gmail.com>
2023-08-04 23:41:53 -0700

    GCP: Add prefix and bulk operations to GCSFileIO (#8168)

--- gcp/src/test/java/org/apache/iceberg/gcp/gcs/GCSFileIOTest.java
+++ gcp/src/test/java/org/apache/iceberg/gcp/gcs/GCSFileIOTest.java
@@ -20,7 +20,11 @@ package org.apache.iceberg.gcp.gcs;
 
 import static java.lang.String.format;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.spy;
 
+import com.google.cloud.storage.BlobId;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.contrib.nio.testing.LocalStorageHelper;
@@ -28,6 +32,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
+import java.util.List;
 import java.util.Random;
 import java.util.stream.StreamSupport;
 import org.apache.iceberg.TestHelpers;
@@ -36,7 +41,9 @@ import org.apache.iceberg.io.FileIO;
 import org.apache.iceberg.io.IOUtil;
 import org.apache.iceberg.io.InputFile;
 import org.apache.iceberg.io.OutputFile;
+import org.apache.iceberg.relocated.com.google.common.collect.ImmutableList;
 import org.apache.iceberg.relocated.com.google.common.collect.ImmutableMap;
+import org.apache.iceberg.relocated.com.google.common.collect.Lists;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -44,11 +51,25 @@ public class GCSFileIOTest {
   private static final String TEST_BUCKET = "TEST_BUCKET";
   private final Random random = new Random(1);
 
-  private final Storage storage = LocalStorageHelper.getOptions().getService();
+  private final Storage storage = spy(LocalStorageHelper.getOptions().getService());
   private GCSFileIO io;
 
   @BeforeEach
   public void before() {
+    // LocalStorageHelper doesn't support batch operations, so mock that here
+    doAnswer(
+            invoke -> {
+              Iterable<BlobId> iter = invoke.getArgument(0);
+              List<Boolean> answer = Lists.newArrayList();
+              iter.forEach(
+                  blobId -> {
+                    answer.add(storage.delete(blobId));
+                  });
+              return answer;
+            })
+        .when(storage)
+        .delete(any(Iterable.class));
+
     io = new GCSFileIO(() -> storage, new GCPProperties());
   }
 
@@ -91,13 +112,77 @@ public class GCSFileIOTest {
                 .count())
         .isEqualTo(1);
 
-    io.deleteFile(format("gs://%s/%s", TEST_BUCKET, path));
+    io.deleteFile(gsUri(path));
 
     // The bucket should now be empty
     assertThat(
             StreamSupport.stream(storage.list(TEST_BUCKET).iterateAll().spliterator(), false)
                 .count())
         .isZero();
+  }
+
+  private String gsUri(String path) {
+    return format("gs://%s/%s", TEST_BUCKET, path);
+  }
+
+  @Test
+  public void testListPrefix() {
+    String prefix = "list/path/";
+    String path1 = prefix + "data1.dat";
+    storage.create(BlobInfo.newBuilder(TEST_BUCKET, path1).build());
+    String path2 = prefix + "data2.dat";
+    storage.create(BlobInfo.newBuilder(TEST_BUCKET, path2).build());
+    String path3 = "list/skip/data3.dat";
+    storage.create(BlobInfo.newBuilder(TEST_BUCKET, path3).build());
+
+    assertThat(StreamSupport.stream(io.listPrefix(gsUri("list/")).spliterator(), false).count())
+        .isEqualTo(3);
+
+    assertThat(StreamSupport.stream(io.listPrefix(gsUri(prefix)).spliterator(), false).count())
+        .isEqualTo(2);
+
+    assertThat(StreamSupport.stream(io.listPrefix(gsUri(path1)).spliterator(), false).count())
+        .isEqualTo(1);
+  }
+
+  @Test
+  public void testDeleteFiles() {
+    String prefix = "del/path/";
+    String path1 = prefix + "data1.dat";
+    storage.create(BlobInfo.newBuilder(TEST_BUCKET, path1).build());
+    String path2 = prefix + "data2.dat";
+    storage.create(BlobInfo.newBuilder(TEST_BUCKET, path2).build());
+    String path3 = "del/skip/data3.dat";
+    storage.create(BlobInfo.newBuilder(TEST_BUCKET, path3).build());
+
+    assertThat(StreamSupport.stream(io.listPrefix(gsUri("del/")).spliterator(), false).count())
+        .isEqualTo(3);
+
+    Iterable<String> deletes =
+        () -> ImmutableList.of(gsUri(path1), gsUri(path3)).stream().iterator();
+    io.deleteFiles(deletes);
+
+    assertThat(StreamSupport.stream(io.listPrefix(gsUri("del/")).spliterator(), false).count())
+        .isEqualTo(1);
+  }
+
+  @Test
+  public void testDeletePrefix() {
+    String prefix = "del/path/";
+    String path1 = prefix + "data1.dat";
+    storage.create(BlobInfo.newBuilder(TEST_BUCKET, path1).build());
+    String path2 = prefix + "data2.dat";
+    storage.create(BlobInfo.newBuilder(TEST_BUCKET, path2).build());
+    String path3 = "del/skip/data3.dat";
+    storage.create(BlobInfo.newBuilder(TEST_BUCKET, path3).build());
+
+    assertThat(StreamSupport.stream(io.listPrefix(gsUri("del/")).spliterator(), false).count())
+        .isEqualTo(3);
+
+    io.deletePrefix(gsUri(prefix));
+
+    assertThat(StreamSupport.stream(io.listPrefix(gsUri("del/")).spliterator(), false).count())
+        .isEqualTo(1);
   }
 
   @Test
