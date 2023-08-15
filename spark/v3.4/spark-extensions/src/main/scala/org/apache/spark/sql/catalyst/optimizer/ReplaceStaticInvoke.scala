ae0d9db3de569278234d90db5b1a6e7cef28107c
xianyangliu <xianyangliu@tencent.com>
2023-08-16 00:16:18 +0800

    address comments

--- spark/v3.4/spark-extensions/src/main/scala/org/apache/spark/sql/catalyst/optimizer/ReplaceStaticInvoke.scala
+++ spark/v3.4/spark-extensions/src/main/scala/org/apache/spark/sql/catalyst/optimizer/ReplaceStaticInvoke.scala
@@ -25,24 +25,23 @@ import org.apache.spark.sql.catalyst.SQLConfHelper
 import org.apache.spark.sql.catalyst.expressions.ApplyFunctionExpression
 import org.apache.spark.sql.catalyst.expressions.Expression
 import org.apache.spark.sql.catalyst.expressions.objects.StaticInvoke
-import org.apache.spark.sql.catalyst.planning.RewrittenRowLevelCommand
-import org.apache.spark.sql.catalyst.planning.ScanOperation
 import org.apache.spark.sql.catalyst.plans.logical.Filter
 import org.apache.spark.sql.catalyst.plans.logical.Join
 import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
-import org.apache.spark.sql.catalyst.plans.logical.NoStatsUnaryNode
+import org.apache.spark.sql.catalyst.plans.logical.MergeIntoIcebergTable
 import org.apache.spark.sql.catalyst.plans.logical.ReplaceData
 import org.apache.spark.sql.catalyst.plans.logical.ReplaceIcebergData
 import org.apache.spark.sql.catalyst.plans.logical.RowLevelCommand
+import org.apache.spark.sql.catalyst.plans.logical.RowLevelWrite
 import org.apache.spark.sql.catalyst.plans.logical.WriteDelta
 import org.apache.spark.sql.catalyst.plans.logical.WriteIcebergDelta
 import org.apache.spark.sql.catalyst.rules.Rule
 import org.apache.spark.sql.connector.catalog.Table
 import org.apache.spark.sql.connector.catalog.functions.ScalarFunction
+import org.apache.spark.sql.connector.write.RowLevelOperation
 import org.apache.spark.sql.connector.write.RowLevelOperationTable
 import org.apache.spark.sql.execution.datasources.v2.DataSourceV2Relation
 import org.apache.spark.sql.execution.datasources.v2.DataSourceV2ScanRelation
-import org.apache.spark.sql.execution.datasources.v2.UnplannedGroupBasedMergeOperation
 import org.apache.spark.sql.types.StructField
 import org.apache.spark.sql.types.StructType
 import scala.annotation.tailrec
@@ -63,43 +62,63 @@ object ReplaceStaticInvoke extends Rule[LogicalPlan] with SQLConfHelper {
   }
 
   private def rewrite(plan: LogicalPlan): LogicalPlan = plan transform {
-    case rowLevelCommand: RowLevelCommand if rowLevelCommand.rewritePlan.nonEmpty =>
-      // Align with RowLevelCommandScanRelationPushDown
-      rowLevelCommand match {
-        case RewrittenRowLevelCommand(_, _: DataSourceV2Relation, rewritePlan: WriteIcebergDelta) =>
-          rowLevelCommand.withNewRewritePlan(rewrite(rewritePlan))
+    case command: RowLevelCommand if command.rewritePlan.nonEmpty && command.condition.nonEmpty =>
+      command.rewritePlan.get match {
+        // Some special cases need to handle for copy-on-write because the follows:
+        // For group-based MERGE operations, it pushes down the join condition
+        // For others, it pushes down the filter from the command condition instead of the filter in the rewrite plan
+        case rd: ReplaceIcebergData =>
+          val condition = command.condition.get
+          val newCondition = replaceStaticInvoke(condition)
 
-        case UnplannedGroupBasedMergeOperation(_, rd: ReplaceIcebergData,
-                                               Join(_, _, _, Some(joinCond), _), relation: DataSourceV2Relation) =>
-          val newQuery = rd.query transform {
-            case j @ Join(
-            NoStatsUnaryNode(
-            ScanOperation(_, pushDownFilters, pushUpFilters, r: DataSourceV2Relation)), _, _, _, _)
-              if j.condition.nonEmpty && pushUpFilters.isEmpty && pushDownFilters.isEmpty &&
-                r.table.eq(relation.table) =>
-              val newCondition = replaceStaticInvoke(j.condition.get)
-              j.copy(condition = Some(newCondition))
-          }
-
-          rowLevelCommand.withNewRewritePlan(rd.copy(query = newQuery))
-
-        case RewrittenRowLevelCommand(_, _: DataSourceV2Relation, _) =>
-          if (rowLevelCommand.condition.nonEmpty) {
-            rowLevelCommand.withNewCondition(replaceStaticInvoke(rowLevelCommand.condition.get))
+          if (condition fastEquals newCondition) {
+            // There is not any StaticInvoke needs to replace
+            command
           } else {
-            rowLevelCommand
+            command match {
+              case _: MergeIntoIcebergTable =>
+                // Trying to rewrite the join condition in group-based MERGE operations
+                val newQuery = rd.query.transform {
+                  case join: Join if join.condition.nonEmpty =>
+                    join.copy(condition = Some(replaceStaticInvoke(join.condition.get)))
+                }
+
+                command.withNewCondition(newCondition).withNewRewritePlan(rd.copy(query = newQuery))
+
+              case _ =>
+                command.withNewCondition(newCondition)
+            }
           }
+
+        // The query have filters that can be pushed down directly
+        case wd: WriteIcebergDelta =>
+          val newQuery = rewriteFilterCondition(wd.query, false)
+          command.withNewRewritePlan(wd.copy(query = newQuery))
       }
 
-    case replaceData: ReplaceData if icebergRelation(replaceData.table) =>
+    // For copy-on-write mode delete
+    // Just needs to rewrite the condition because both metadata delete and filter push down are based on condition
+    case replaceData: ReplaceData if icebergRelation(replaceData.table, false) && isDeleteCommand(replaceData) =>
       val newCondition = replaceStaticInvoke(replaceData.condition)
       replaceData.copy(condition = newCondition)
 
-    case writeDelta: WriteDelta if icebergRelation(writeDelta.table) =>
+    // For merge-on-read mode delete
+    // Rewrite the condition because metadata delete is based on the condition
+    // Rewrite the query because it has filters which will be pushed down
+    case writeDelta: WriteDelta if icebergRelation(writeDelta.table, false) && isDeleteCommand(writeDelta) =>
       val newCondition = replaceStaticInvoke(writeDelta.condition)
-      writeDelta.copy(condition = newCondition)
+      val newQuery = rewriteFilterCondition(writeDelta.query, false)
+      writeDelta.copy(condition = newCondition, query = newQuery)
 
-    case filter @ Filter(condition, child) if icebergRelation(child) =>
+    // Row level command has already been handled. Exclude it because this could rewrite row-level command.
+    // For example, rewrite copy-on-write command which will only push down condition of the command
+    case other => rewriteFilterCondition(other, true)
+  }
+
+  private def rewriteFilterCondition(
+      plan: LogicalPlan,
+      excludeRowLevelOperationTable: Boolean): LogicalPlan = plan transform {
+    case filter @ Filter(condition, child) if icebergRelation(child, excludeRowLevelOperationTable) =>
       val newCondition = replaceStaticInvoke(condition)
       if (newCondition fastEquals condition) {
         filter
@@ -112,7 +131,11 @@ object ReplaceStaticInvoke extends Rule[LogicalPlan] with SQLConfHelper {
     conf.getConfString(
       SparkSQLProperties.SYSTEM_FUNC_PUSH_DOWN_ENABLED,
       SparkSQLProperties.SYSTEM_FUNC_PUSH_DOWN_ENABLED_DEFAULT.toString
-      ).toBoolean
+    ).toBoolean
+  }
+
+  private def isDeleteCommand(rowLevelWrite: RowLevelWrite): Boolean = {
+    rowLevelWrite.operation.command() == RowLevelOperation.Command.DELETE
   }
 
   private def replaceStaticInvoke(expression: Expression): Expression = expression.transform {
@@ -120,6 +143,11 @@ object ReplaceStaticInvoke extends Rule[LogicalPlan] with SQLConfHelper {
       invoke.functionName match {
         case "invoke" =>
           // Adaptive from `resolveV2Function` in org.apache.spark.sql.catalyst.analysis.ResolveFunctions
+          if (invoke.arguments.forall(_.foldable)) {
+            // The invoke should be folded into constant
+            return invoke
+          }
+
           val unbound = SparkFunctions.loadFunctionByClass(invoke.staticObject)
           if (unbound == null) {
             return invoke
@@ -149,12 +177,12 @@ object ReplaceStaticInvoke extends Rule[LogicalPlan] with SQLConfHelper {
   }
 
   @tailrec
-  private def icebergRelation(plan: LogicalPlan): Boolean = {
+  private def icebergRelation(plan: LogicalPlan, excludeRowLevelOperationTable: Boolean): Boolean = {
 
     @tailrec
     def isIcebergTable(table: Table): Boolean = table match {
       case _: SparkTable => true
-      case r: RowLevelOperationTable => isIcebergTable(r.table)
+      case r: RowLevelOperationTable if !excludeRowLevelOperationTable => isIcebergTable(r.table)
       case _ => false
     }
 
@@ -162,7 +190,7 @@ object ReplaceStaticInvoke extends Rule[LogicalPlan] with SQLConfHelper {
       case relation: DataSourceV2Relation =>
         isIcebergTable(relation.table)
       case scanRelation: DataSourceV2ScanRelation =>
-        icebergRelation(scanRelation.relation)
+        icebergRelation(scanRelation.relation, excludeRowLevelOperationTable)
       case _ => false
     }
   }
