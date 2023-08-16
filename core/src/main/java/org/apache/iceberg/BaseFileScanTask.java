1caf1fe6c111b87a3d8f13bc7a164111de1c8ad5
aokolnychyi <aokolnychyi@apple.com>
2023-08-16 08:27:22 -0700

    Core: Optimize offset splits handling

--- core/src/main/java/org/apache/iceberg/BaseFileScanTask.java
+++ core/src/main/java/org/apache/iceberg/BaseFileScanTask.java
@@ -101,6 +101,11 @@ public class BaseFileScanTask extends BaseContentScanTask<FileScanTask, DataFile
     }
 
     @Override
+    public long estimatedRowsCount() {
+      return BaseContentScanTask.estimatedRowsCount(len, fileScanTask.file());
+    }
+
+    @Override
     public Expression residual() {
       return fileScanTask.residual();
     }
