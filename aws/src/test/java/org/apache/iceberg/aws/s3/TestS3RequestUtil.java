71eb740235971e1382af35b8cabb2b9f496c1fe1
Akira Ajisaka <akiraaj@amazon.com>
2023-08-22 19:20:02 +0900

    Applied ./gradlew :iceberg-aws:spotlessApply

--- aws/src/test/java/org/apache/iceberg/aws/s3/TestS3RequestUtil.java
+++ aws/src/test/java/org/apache/iceberg/aws/s3/TestS3RequestUtil.java
@@ -94,12 +94,12 @@ public class TestS3RequestUtil {
     s3FileIOProperties.setSseType(S3FileIOProperties.DSSE_TYPE_KMS);
     s3FileIOProperties.setSseKey("key");
     S3RequestUtil.configureEncryption(
-            s3FileIOProperties,
-            this::setServerSideEncryption,
-            this::setKmsKeyId,
-            this::setCustomAlgorithm,
-            this::setCustomKey,
-            this::setCustomMd5);
+        s3FileIOProperties,
+        this::setServerSideEncryption,
+        this::setKmsKeyId,
+        this::setCustomAlgorithm,
+        this::setCustomKey,
+        this::setCustomMd5);
     Assertions.assertThat(serverSideEncryption).isEqualTo(ServerSideEncryption.AWS_KMS_DSSE);
     Assertions.assertThat(kmsKeyId).isEqualTo("key");
     Assertions.assertThat(customAlgorithm).isNull();
