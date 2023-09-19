53508d7f9f38f88f40d6b6f96b3afe208b9517eb
CodingCat <zhunansjtu@gmail.com>
2023-09-19 09:01:42 -0700

    fix merge errors

--- core/src/main/java/org/apache/iceberg/SchemaUpdate.java
+++ core/src/main/java/org/apache/iceberg/SchemaUpdate.java
@@ -750,6 +750,7 @@ class SchemaUpdate implements UpdateSchema {
     return newFields;
   }
 
+  @SuppressWarnings({"checkstyle:IllegalType", "JdkObsolete"})
   private static List<Types.NestedField> moveFields(
       List<Types.NestedField> fields, Collection<Move> moves) {
     LinkedList<Types.NestedField> reordered = Lists.newLinkedList(fields);
