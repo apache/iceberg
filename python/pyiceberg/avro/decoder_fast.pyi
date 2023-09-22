8714505be63f8d8aed45c1aa99b175689b3a7d73
Mehul Batra <mehulbatra1997@gmail.com>
2023-09-22 20:40:03 +0530

    resolve conflict

--- python/pyiceberg/avro/decoder_fast.pyi
+++ python/pyiceberg/avro/decoder_fast.pyi
@@ -15,7 +15,9 @@
 # specific language governing permissions and limitations
 # under the License.
 
-class CythonBinaryDecoder:
+from pyiceberg.avro.decoder import BinaryDecoder
+
+class CythonBinaryDecoder(BinaryDecoder):
     def __init__(self, input_contents: bytes) -> None:
         pass
     def tell(self) -> int:
