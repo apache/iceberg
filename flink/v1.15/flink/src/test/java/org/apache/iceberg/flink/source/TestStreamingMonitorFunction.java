953ccb8e0e20631a9b03e5fa6bd7b9178817c722
shreyanshR7 <134536320+shreyanshR7@users.noreply.github.com>
2023-10-06 11:48:37 +0530

    Update TestStreamingMonitorFunction.java

--- flink/v1.15/flink/src/test/java/org/apache/iceberg/flink/source/TestStreamingMonitorFunction.java
+++ flink/v1.15/flink/src/test/java/org/apache/iceberg/flink/source/TestStreamingMonitorFunction.java
@@ -112,10 +112,9 @@ public class TestStreamingMonitorFunction extends TableTestBase {
       TestSourceContext sourceContext = new TestSourceContext(latch);
       runSourceFunctionInTask(sourceContext, function);
 
-      Awaitility.await()
-        .atMost(WAIT_TIME_MILLIS, TimeUnit.MILLISECONDS)
-        .pollInterval(100, TimeUnit.MILLISECONDS)
-        .until(() -> latch.getCount() == 0);
+      Assert.assertTrue(
+          "Should have expected elements.", latch.await(WAIT_TIME_MILLIS, TimeUnit.MILLISECONDS));
+      Awaitility.await().atLeast(1, TimeUnit.SECONDS).until(() -> true);
 
       // Stop the stream task.
       function.close();
