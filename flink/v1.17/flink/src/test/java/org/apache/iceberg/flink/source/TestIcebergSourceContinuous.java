67c5978981d74d65a2fb5684081a07e2becdc94d
shreyanshR7 <134536320+shreyanshR7@users.noreply.github.com>
2023-10-06 12:01:40 +0530

    Update TestIcebergSourceContinuous.java

--- flink/v1.17/flink/src/test/java/org/apache/iceberg/flink/source/TestIcebergSourceContinuous.java
+++ flink/v1.17/flink/src/test/java/org/apache/iceberg/flink/source/TestIcebergSourceContinuous.java
@@ -326,7 +326,7 @@ public class TestIcebergSourceContinuous {
     long snapshot0Timestamp = tableResource.table().currentSnapshot().timestampMillis();
 
     // sleep for 2 ms to make sure snapshot1 has a higher timestamp value
-    Awaitility.await().pollDelay(2, TimeUnit.MILLISECONDS).until(() -> System.currentTimeMillis()-snapshot0Timestamp>2);
+     waitUntil1ms();
 
     // snapshot1
     List<Record> batch1 =
@@ -400,6 +400,14 @@ public class TestIcebergSourceContinuous {
       }
     }
     return results;
+  }
+  public static long waitUntil1ms() {
+    long previous = System.currentTimeMillis();
+    long current = System.currentTimeMillis(); 
+    while (current <= previous) { 
+      current = System.currentTimeMillis(); 
+    } 
+    return current; 
   }
 
   public static List<JobID> getRunningJobs(ClusterClient<?> client) throws Exception {
