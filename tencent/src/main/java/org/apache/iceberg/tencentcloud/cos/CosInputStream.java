3a1cd39eddb4d7a4f287f14307f263278db17a0e
bollu <bollu@tencent.com>
2023-11-22 16:36:08 +0800

    Support tencent COS fileIO

---
+++ tencent/src/main/java/org/apache/iceberg/tencentcloud/cos/CosInputStream.java
@@ -0,0 +1,170 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.iceberg.tencentcloud.cos;
+
+import com.qcloud.cos.COS;
+import com.qcloud.cos.model.GetObjectRequest;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import org.apache.iceberg.io.FileIOMetricsContext;
+import org.apache.iceberg.io.SeekableInputStream;
+import org.apache.iceberg.metrics.Counter;
+import org.apache.iceberg.metrics.MetricsContext;
+import org.apache.iceberg.metrics.MetricsContext.Unit;
+import org.apache.iceberg.relocated.com.google.common.base.Joiner;
+import org.apache.iceberg.relocated.com.google.common.base.Preconditions;
+import org.apache.iceberg.relocated.com.google.common.io.ByteStreams;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class CosInputStream extends SeekableInputStream {
+  private static final Logger LOG = LoggerFactory.getLogger(CosInputStream.class);
+  private static final int SKIP_SIZE = 1024 * 1024;
+
+  private final StackTraceElement[] createStack;
+  private final COS client;
+  private final CosURI uri;
+
+  private InputStream stream = null;
+  private long pos = 0;
+  private long next = 0;
+  private boolean closed = false;
+
+  private final Counter readBytes;
+  private final Counter readOperations;
+
+  CosInputStream(COS client, CosURI uri) {
+    this(client, uri, MetricsContext.nullMetrics());
+  }
+
+  CosInputStream(COS client, CosURI uri, MetricsContext metrics) {
+    this.client = client;
+    this.uri = uri;
+    this.createStack = Thread.currentThread().getStackTrace();
+
+    this.readBytes = metrics.counter(FileIOMetricsContext.READ_BYTES, Unit.BYTES);
+    this.readOperations = metrics.counter(FileIOMetricsContext.READ_OPERATIONS);
+  }
+
+  @Override
+  public long getPos() {
+    return next;
+  }
+
+  @Override
+  public void seek(long newPos) {
+    Preconditions.checkState(!closed, "Cannot seek: already closed");
+    Preconditions.checkArgument(newPos >= 0, "Position is negative: %s", newPos);
+
+    // this allows a seek beyond the end of the stream but the next read will fail
+    next = newPos;
+  }
+
+  @Override
+  public int read() throws IOException {
+    Preconditions.checkState(!closed, "Cannot read: already closed");
+    positionStream();
+
+    pos += 1;
+    next += 1;
+    readBytes.increment();
+    readOperations.increment();
+
+    return stream.read();
+  }
+
+  @Override
+  public int read(byte[] b, int off, int len) throws IOException {
+    Preconditions.checkState(!closed, "Cannot read: already closed");
+    positionStream();
+
+    int bytesRead = stream.read(b, off, len);
+    pos += bytesRead;
+    next += bytesRead;
+    readBytes.increment(bytesRead);
+    readOperations.increment();
+
+    return bytesRead;
+  }
+
+  @Override
+  public void close() throws IOException {
+    if (closed) {
+      return;
+    }
+
+    super.close();
+    closeStream();
+    closed = true;
+  }
+
+  private void positionStream() throws IOException {
+    if ((stream != null) && (next == pos)) {
+      // already at specified position.
+      return;
+    }
+
+    if ((stream != null) && (next > pos)) {
+      // seeking forwards
+      long skip = next - pos;
+      if (skip <= Math.max(stream.available(), SKIP_SIZE)) {
+        // already buffered or seek is small enough
+        LOG.debug("Read-through seek for {} from {} to offset {}", uri, pos, next);
+        try {
+          ByteStreams.skipFully(stream, skip);
+          pos = next;
+          return;
+        } catch (IOException ignored) {
+          // will retry by re-opening the stream.
+        }
+      }
+    }
+
+    // close the stream and open at desired position.
+    LOG.debug("Seek with new stream for {} to offset {}", uri, next);
+    pos = next;
+    openStream();
+  }
+
+  private void openStream() throws IOException {
+    closeStream();
+
+    GetObjectRequest request = new GetObjectRequest(uri.bucket(), uri.key()).withRange(pos, -1);
+    stream = client.getObject(request).getObjectContent();
+  }
+
+  private void closeStream() throws IOException {
+    if (stream != null) {
+      stream.close();
+      stream = null;
+    }
+  }
+
+  @SuppressWarnings("checkstyle:NoFinalizer")
+  @Override
+  protected void finalize() throws Throwable {
+    super.finalize();
+    if (!closed) {
+      close(); // releasing resources is more important than printing the warning
+      String trace = Joiner.on("\n\t").join(Arrays.copyOfRange(createStack, 1, createStack.length));
+      LOG.warn("Unclosed input stream created by: \n\t{}", trace);
+    }
+  }
+}
---
+++ tencent/src/main/java/org/apache/iceberg/tencentcloud/cos/CosOutputStream.java
@@ -0,0 +1,177 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.iceberg.tencentcloud.cos;
+
+import com.qcloud.cos.COS;
+import com.qcloud.cos.model.ObjectMetadata;
+import com.qcloud.cos.model.PutObjectRequest;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UncheckedIOException;
+import java.util.Arrays;
+import org.apache.iceberg.exceptions.NotFoundException;
+import org.apache.iceberg.io.FileIOMetricsContext;
+import org.apache.iceberg.io.PositionOutputStream;
+import org.apache.iceberg.metrics.Counter;
+import org.apache.iceberg.metrics.MetricsContext;
+import org.apache.iceberg.metrics.MetricsContext.Unit;
+import org.apache.iceberg.relocated.com.google.common.base.Joiner;
+import org.apache.iceberg.relocated.com.google.common.base.Preconditions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CosOutputStream extends PositionOutputStream {
+  private static final Logger LOG = LoggerFactory.getLogger(CosOutputStream.class);
+  private final StackTraceElement[] createStack;
+
+  private final COS client;
+  private final CosURI uri;
+
+  private final File currentStagingFile;
+  private final OutputStream stream;
+  private long pos = 0;
+  private boolean closed = false;
+
+  private final Counter writeBytes;
+  private final Counter writeOperations;
+
+  CosOutputStream(COS client, CosURI uri, CosProperties cosProperties, MetricsContext metrics) {
+    this.client = client;
+    this.uri = uri;
+    this.createStack = Thread.currentThread().getStackTrace();
+    this.currentStagingFile = newStagingFile(cosProperties.cosTmpDir());
+    this.stream = newStream(currentStagingFile);
+    this.writeBytes = metrics.counter(FileIOMetricsContext.WRITE_BYTES, Unit.BYTES);
+    this.writeOperations = metrics.counter(FileIOMetricsContext.WRITE_OPERATIONS);
+  }
+
+  private File newStagingFile(String cosStagingDirectory) {
+    try {
+      File stagingFile =
+          File.createTempFile("iceberg_cos_", ".cache", new File(cosStagingDirectory));
+      stagingFile.deleteOnExit();
+      return stagingFile;
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
+    }
+  }
+
+  private OutputStream newStream(File stageFile) {
+    try {
+      return new BufferedOutputStream(new FileOutputStream(stageFile));
+    } catch (FileNotFoundException e) {
+      throw new NotFoundException(e, "Failed to create file: %s", stageFile);
+    }
+  }
+
+  private static InputStream uncheckedInputStream(File file) {
+    try {
+      return new FileInputStream(file);
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
+    }
+  }
+
+  @Override
+  public long getPos() {
+    return pos;
+  }
+
+  @Override
+  public void flush() throws IOException {
+    Preconditions.checkState(!closed, "Already closed.");
+    stream.flush();
+  }
+
+  @Override
+  public void write(int b) throws IOException {
+    Preconditions.checkState(!closed, "Already closed.");
+    stream.write(b);
+    pos += 1;
+    writeBytes.increment();
+    writeOperations.increment();
+  }
+
+  @Override
+  public void write(byte[] b, int off, int len) throws IOException {
+    Preconditions.checkState(!closed, "Already closed.");
+    stream.write(b, off, len);
+    pos += len;
+    writeBytes.increment(len);
+    writeOperations.increment();
+  }
+
+  @Override
+  public void close() throws IOException {
+    if (closed) {
+      return;
+    }
+
+    super.close();
+    closed = true;
+
+    try {
+      stream.close();
+      completeUploads();
+    } finally {
+      cleanUpStagingFiles();
+    }
+  }
+
+  private void completeUploads() {
+    long contentLength = currentStagingFile.length();
+    if (contentLength == 0) {
+      LOG.debug("Skipping empty upload to Cos");
+      return;
+    }
+
+    LOG.debug("Uploading {} staged bytes to Cos", contentLength);
+    InputStream contentStream = uncheckedInputStream(currentStagingFile);
+
+    ObjectMetadata metadata = new ObjectMetadata();
+    metadata.setContentLength(contentLength);
+
+    PutObjectRequest request =
+        new PutObjectRequest(uri.bucket(), uri.key(), contentStream, metadata);
+    client.putObject(request);
+  }
+
+  private void cleanUpStagingFiles() {
+    if (!currentStagingFile.delete()) {
+      LOG.warn("Failed to delete staging file: {}", currentStagingFile);
+    }
+  }
+
+  @SuppressWarnings("checkstyle:NoFinalizer")
+  @Override
+  protected void finalize() throws Throwable {
+    super.finalize();
+    if (!closed) {
+      close(); // releasing resources is more important than printing the warning.
+      String trace = Joiner.on("\n\t").join(Arrays.copyOfRange(createStack, 1, createStack.length));
+      LOG.warn("Unclosed output stream created by:\n\t{}", trace);
+    }
+  }
+}
