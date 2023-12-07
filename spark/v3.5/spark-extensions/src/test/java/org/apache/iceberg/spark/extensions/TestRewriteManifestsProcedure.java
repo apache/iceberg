0ca9fabd3390b6c8bdefe661164ff1183e1da5c6
Pucheng Yang <pyang@pinterest.com>
2023-12-07 09:24:11 -0800

    spotless

--- spark/v3.5/spark-extensions/src/test/java/org/apache/iceberg/spark/extensions/TestRewriteManifestsProcedure.java
+++ spark/v3.5/spark-extensions/src/test/java/org/apache/iceberg/spark/extensions/TestRewriteManifestsProcedure.java
@@ -335,25 +335,29 @@ public class TestRewriteManifestsProcedure extends SparkExtensionsTestBase {
   @Test
   public void testWriteManifestWithSpecId() {
     sql(
-            "CREATE TABLE %s (id int, dt string, hr string) USING iceberg PARTITIONED BY (dt)", tableName);
+        "CREATE TABLE %s (id int, dt string, hr string) USING iceberg PARTITIONED BY (dt)",
+        tableName);
 
     sql("INSERT INTO %s VALUES (1, '2024-01-01', '00')", tableName);
     sql("INSERT INTO %s VALUES (2, '2024-01-01', '00')", tableName);
     assertEquals(
-            "Should have 2 manifests",
-            ImmutableList.of(row(2L)),
-            sql("SELECT count(*) FROM %s.manifests", tableName));
+        "Should have 2 manifests",
+        ImmutableList.of(row(2L)),
+        sql("SELECT count(*) FROM %s.manifests", tableName));
 
     sql("ALTER TABLE %s ADD PARTITION FIELD hr", tableName);
     assertEquals(
-            "Should still have 2 manifests",
-            ImmutableList.of(row(2L)),
-            sql("SELECT count(*) FROM %s.manifests", tableName));
+        "Should still have 2 manifests",
+        ImmutableList.of(row(2L)),
+        sql("SELECT count(*) FROM %s.manifests", tableName));
 
     List<Object[]> output = sql("CALL %s.system.rewrite_manifests('%s')", catalogName, tableIdent);
     assertEquals("Procedure output must match", ImmutableList.of(row(0, 0)), output);
 
-    output = sql("CALL %s.system.rewrite_manifests(table => '%s', spec_id => 0)", catalogName, tableIdent);
+    output =
+        sql(
+            "CALL %s.system.rewrite_manifests(table => '%s', spec_id => 0)",
+            catalogName, tableIdent);
     assertEquals("Procedure output must match", ImmutableList.of(row(2, 1)), output);
   }
 }
