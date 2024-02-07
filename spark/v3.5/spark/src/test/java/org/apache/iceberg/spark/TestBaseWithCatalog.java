a7cd6afa084dda4d81c23c5e7d8a67225ca5644c
Eduard Tudenhoefner <etudenhoefner@gmail.com>
2024-02-07 10:35:35 +0100

    Spark: Avoid NPE when catalog config doesn't have "type" set (#9676)
    We do the same check in `SparkTestBaseWithCatalog` to avoid a potential NPE when a catalog configuration doesn't have the `type` set

--- spark/v3.5/spark/src/test/java/org/apache/iceberg/spark/TestBaseWithCatalog.java
+++ spark/v3.5/spark/src/test/java/org/apache/iceberg/spark/TestBaseWithCatalog.java
@@ -101,7 +101,7 @@ public abstract class TestBaseWithCatalog extends TestBase {
     catalogConfig.forEach(
         (key, value) -> spark.conf().set("spark.sql.catalog." + catalogName + "." + key, value));
 
-    if (catalogConfig.get("type").equalsIgnoreCase("hadoop")) {
+    if ("hadoop".equalsIgnoreCase(catalogConfig.get("type"))) {
       spark.conf().set("spark.sql.catalog." + catalogName + ".warehouse", "file:" + warehouse);
     }
 
