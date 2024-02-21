eed29cc6f8cbc1a518c3fa738e9321f9407cefb6
Rahil Chertara <rchertar@amazon.com>
2024-02-21 10:10:03 -0800

    address jack and nastra comments

--- open-api/rest-catalog-open-api.py
+++ open-api/rest-catalog-open-api.py
@@ -78,7 +78,7 @@ class Namespace(BaseModel):
 class PageToken(BaseModel):
     __root__: str = Field(
         ...,
-        description='An opaque token which allows clients to make use of pagination for a list API (e.g. ListTables) by signaling to the service that they would prefer the response to be paginated.\nClients will initiate the request by sending an empty pageToken e.g. GET /tables?pageToken or /tables?pageToken= For servers that support pagination, they will recognize pageToken and return a next pageToken in response if there are more results available. After initial request, it is expected that the next pageToken from the last response be used in the subsequent request. For servers that do not support pagination, they will ignore the pageToken and return all results.',
+        description='An opaque token which allows clients to make use of pagination for a list API (e.g. ListTables). Clients will initiate the first paginated request by sending an empty pageToken e.g. GET /tables?pageToken or GET /tables?pageToken= signaling to the service that the response should be paginated.\nFor servers that support pagination, they will recognize `pageToken` and return a next-page-token in response if there are more results available. After the initial request, it is expected that the next-page-token from the last response, is used in the subsequent request. For servers that do not support pagination, they will ignore the `pageToken` and return all results.',
     )
 
 
