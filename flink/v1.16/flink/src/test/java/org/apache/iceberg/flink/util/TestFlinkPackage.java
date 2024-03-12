08e941dde171b7c5abfd5c4e71540b28c352fce1
Ajantha Bhat <ajanthabhat@gmail.com>
2024-03-12 12:30:22 +0530

    Flink: Bump minor versions (#9875)

--- flink/v1.16/flink/src/test/java/org/apache/iceberg/flink/util/TestFlinkPackage.java
+++ flink/v1.16/flink/src/test/java/org/apache/iceberg/flink/util/TestFlinkPackage.java
@@ -28,7 +28,7 @@ public class TestFlinkPackage {
   /** This unit test would need to be adjusted as new Flink version is supported. */
   @Test
   public void testVersion() {
-    Assert.assertEquals("1.16.2", FlinkPackage.version());
+    Assert.assertEquals("1.16.3", FlinkPackage.version());
   }
 
   @Test
