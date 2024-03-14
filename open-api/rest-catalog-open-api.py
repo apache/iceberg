2a0e672d2469e4d455c019d9e2112b23b4016e92
Rahil Chertara <rchertar@amazon.com>
2024-03-13 21:26:54 -0700

    make next-page-token allow null

--- open-api/rest-catalog-open-api.py
+++ open-api/rest-catalog-open-api.py
@@ -78,8 +78,8 @@ class Namespace(BaseModel):
 
 
 class PageToken(BaseModel):
-    __root__: str = Field(
-        ...,
+    __root__: Optional[str] = Field(
+        None,
         description='An opaque token that allows clients to make use of pagination for list APIs (e.g. ListTables). Clients may initiate the first paginated request by sending an empty query parameter `pageToken` to the server.\nServers that support pagination should identify the `pageToken` parameter and return a `next-page-token` in the response if there are more results available.  After the initial request, the value of `next-page-token` from each response must be used as the `pageToken` parameter value for the next request. The server must return `null` value for the `next-page-token` in the last response.\nServers that support pagination must return all results in a single response with the value of `next-page-token` set to `null` if the query parameter `pageToken` is not set in the request.\nServers that do not support pagination should ignore the `pageToken` parameter and return all results in a single response. The `next-page-token` must be omitted from the response.\nClients must interpret either `null` or missing response value of `next-page-token` as the end of the listing results.',
     )
 
