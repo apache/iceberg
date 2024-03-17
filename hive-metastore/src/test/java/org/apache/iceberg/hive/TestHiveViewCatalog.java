92629a9bafadcf012e9b9d3ea8ae575667816960
nk1506 <nk1506@gmail.com>
2024-03-17 11:24:20 +0530

    Hive: Add test cases for commitLocks with view concurrent commits and exceptions. Add tests with Hive-View and Hive-Iceberg View. failedCommitStatusCheckSupplier for table and view with metadataLocation and versionId respectively.

--- hive-metastore/src/test/java/org/apache/iceberg/hive/TestHiveViewCatalog.java
+++ hive-metastore/src/test/java/org/apache/iceberg/hive/TestHiveViewCatalog.java
@@ -18,15 +18,28 @@
  */
 package org.apache.iceberg.hive;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import org.apache.hadoop.hive.metastore.TableType;
+import org.apache.hadoop.hive.metastore.api.SerDeInfo;
+import org.apache.hadoop.hive.metastore.api.StorageDescriptor;
+import org.apache.hadoop.hive.serde.serdeConstants;
 import org.apache.iceberg.CatalogProperties;
 import org.apache.iceberg.CatalogUtil;
 import org.apache.iceberg.catalog.Catalog;
+import org.apache.iceberg.catalog.Namespace;
+import org.apache.iceberg.catalog.TableIdentifier;
 import org.apache.iceberg.relocated.com.google.common.collect.ImmutableMap;
+import org.apache.iceberg.relocated.com.google.common.collect.Lists;
+import org.apache.iceberg.relocated.com.google.common.collect.Maps;
 import org.apache.iceberg.view.ViewCatalogTests;
 import org.apache.thrift.TException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 public class TestHiveViewCatalog extends ViewCatalogTests<HiveCatalog> {
@@ -68,5 +81,85 @@ public class TestHiveViewCatalog extends ViewCatalogTests<HiveCatalog> {
   @Override
   protected boolean requiresNamespaceCreate() {
     return true;
+  }
+
+  @Test
+  public void testListView() throws TException {
+    String dbName = "hivedb";
+    Namespace ns = Namespace.of(dbName);
+    TableIdentifier identifier = TableIdentifier.of(ns, "tbl");
+
+    if (requiresNamespaceCreate()) {
+      catalog.createNamespace(identifier.namespace());
+    }
+
+    assertThat(catalog.viewExists(identifier)).isFalse();
+    assertThat(catalog.listViews(ns)).isEmpty();
+
+    String hiveTableName = "test_hive_view";
+    // create a hive table
+    org.apache.hadoop.hive.metastore.api.Table hiveTable =
+        createHiveView(hiveTableName, dbName, tempDir.toUri().toString());
+    HIVE_METASTORE_EXTENSION.metastoreClient().createTable(hiveTable);
+
+    catalog.setListAllTables(true);
+    List<TableIdentifier> tableIdents1 = catalog.listTables(ns);
+    assertThat(tableIdents1).as("should have one table with type VIRTUAL_VIEW.").hasSize(1);
+
+    List<TableIdentifier> tableIdents2 = catalog.listViews(ns);
+    assertThat(tableIdents2).as("should have zero iceberg view.").hasSize(0);
+
+    catalog
+        .buildView(identifier)
+        .withSchema(SCHEMA)
+        .withDefaultNamespace(ns)
+        .withQuery("hive", "select * from ns.tbl")
+        .create();
+    assertThat(catalog.viewExists(identifier)).isTrue();
+
+    List<TableIdentifier> tableIdents3 = catalog.listViews(ns);
+    assertThat(tableIdents3).as("should have one iceberg view .").hasSize(1);
+  }
+
+  private org.apache.hadoop.hive.metastore.api.Table createHiveView(
+      String hiveViewName, String dbName, String location) {
+    Map<String, String> parameters = Maps.newHashMap();
+    parameters.put(
+        serdeConstants.SERIALIZATION_CLASS, "org.apache.hadoop.hive.serde2.thrift.test.IntString");
+    parameters.put(
+        serdeConstants.SERIALIZATION_FORMAT, "org.apache.thrift.protocol.TBinaryProtocol");
+
+    SerDeInfo serDeInfo =
+        new SerDeInfo(null, "org.apache.hadoop.hive.serde2.thrift.ThriftDeserializer", parameters);
+
+    // StorageDescriptor has an empty list of fields - SerDe will report them.
+    StorageDescriptor sd =
+        new StorageDescriptor(
+            Lists.newArrayList(),
+            location,
+            "org.apache.hadoop.mapred.TextInputFormat",
+            "org.apache.hadoop.mapred.TextOutputFormat",
+            false,
+            -1,
+            serDeInfo,
+            Lists.newArrayList(),
+            Lists.newArrayList(),
+            Maps.newHashMap());
+
+    org.apache.hadoop.hive.metastore.api.Table hiveTable =
+        new org.apache.hadoop.hive.metastore.api.Table(
+            hiveViewName,
+            dbName,
+            "test_owner",
+            0,
+            0,
+            0,
+            sd,
+            Lists.newArrayList(),
+            Maps.newHashMap(),
+            "viewOriginalText",
+            "viewExpandedText",
+            TableType.VIRTUAL_VIEW.name());
+    return hiveTable;
   }
 }
