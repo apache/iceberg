f9f55f84884c7ea24b20e0ab1c30873c24de1d52
nk1506 <nk1506@gmail.com>
2024-04-03 16:56:33 +0530

    Hive, JDBC: Add null check before matching the error message

--- hive-metastore/src/main/java/org/apache/iceberg/hive/HiveClientPool.java
+++ hive-metastore/src/main/java/org/apache/iceberg/hive/HiveClientPool.java
@@ -59,29 +59,19 @@ public class HiveClientPool extends ClientPoolImpl<IMetaStoreClient, TException>
   @Override
   protected IMetaStoreClient newClient() {
     try {
-      try {
-        return GET_CLIENT.invoke(
-            hiveConf, (HiveMetaHookLoader) tbl -> null, HiveMetaStoreClient.class.getName());
-      } catch (RuntimeException e) {
-        // any MetaException would be wrapped into RuntimeException during reflection, so let's
-        // double-check type here
-        if (e.getCause() instanceof MetaException) {
-          throw (MetaException) e.getCause();
-        }
-        throw e;
-      }
-    } catch (MetaException e) {
-      throw new RuntimeMetaException(e, "Failed to connect to Hive Metastore");
-    } catch (Throwable t) {
-      if (t.getMessage().contains("Another instance of Derby may have already booted")) {
+      return GET_CLIENT.invoke(
+          hiveConf, (HiveMetaHookLoader) tbl -> null, HiveMetaStoreClient.class.getName());
+    } catch (RuntimeException e) {
+      if (e.getMessage() != null
+          && e.getMessage().contains("Another instance of Derby may have already booted")) {
         throw new RuntimeMetaException(
-            t,
+            e,
             "Failed to start an embedded metastore because embedded "
                 + "Derby supports only one client at a time. To fix this, use a metastore that supports "
                 + "multiple clients.");
       }
 
-      throw new RuntimeMetaException(t, "Failed to connect to Hive Metastore");
+      throw new RuntimeMetaException(e, "Failed to connect to Hive Metastore");
     }
   }
 
