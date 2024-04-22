1c335679be9b527192422440ebba90f7a4b801b6
hsiang-c <huangc@apple.com>
2024-04-22 21:01:42 +0800

    Fix style

--- data/src/main/java/org/apache/iceberg/data/DeleteFilter.java
+++ data/src/main/java/org/apache/iceberg/data/DeleteFilter.java
@@ -24,7 +24,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.function.Predicate;
 import org.apache.iceberg.Accessor;
-import org.apache.iceberg.ContentFile;
 import org.apache.iceberg.DeleteFile;
 import org.apache.iceberg.MetadataColumns;
 import org.apache.iceberg.Schema;
