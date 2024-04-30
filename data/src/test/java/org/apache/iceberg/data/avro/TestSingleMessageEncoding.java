db7e95dd3b181ce196b8409fe15031f2445b316a
Igor Adulyan <53629576+igoradulian@users.noreply.github.com>
2024-04-30 09:08:31 -0700

    Update data/src/test/java/org/apache/iceberg/data/avro/TestSingleMessageEncoding.java
    Co-authored-by: Eduard Tudenhoefner <etudenhoefner@gmail.com>

--- data/src/test/java/org/apache/iceberg/data/avro/TestSingleMessageEncoding.java
+++ data/src/test/java/org/apache/iceberg/data/avro/TestSingleMessageEncoding.java
@@ -86,7 +86,7 @@ public class TestSingleMessageEncoding {
 
     Record copy = decoder.decode(encoder.encode(V2_RECORDS.get(0)));
 
-    Assertions.assertThat(copy != V2_RECORDS.get(0))
+    Assertions.assertThat(copy).as(...).isNotSameAs(V2_RECORDS.get(0)))
         .as("Copy should not be the same object")
         .isTrue();
     Assertions.assertThat(V2_RECORDS.get(0))
