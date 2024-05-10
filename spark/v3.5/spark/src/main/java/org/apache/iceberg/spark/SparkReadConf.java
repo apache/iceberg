06dae9a4ed11d8b2a397666b81297fc1cbd78547
Huaxin Gao <huaxin.gao@apple.com>
2024-05-10 16:22:57 -0700

    Add EnumConfParser to SparkConfParser

--- spark/v3.5/spark/src/main/java/org/apache/iceberg/spark/SparkReadConf.java
+++ spark/v3.5/spark/src/main/java/org/apache/iceberg/spark/SparkReadConf.java
@@ -300,14 +300,12 @@ public class SparkReadConf {
       return LOCAL;
     }
 
-    String modeName =
-        confParser
-            .stringConf()
-            .sessionConf(SparkSQLProperties.DATA_PLANNING_MODE)
-            .tableProperty(TableProperties.DATA_PLANNING_MODE)
-            .defaultValue(TableProperties.PLANNING_MODE_DEFAULT)
-            .parse();
-    return PlanningMode.fromName(modeName);
+    return confParser
+        .enumConf(PlanningMode::fromName)
+        .sessionConf(SparkSQLProperties.DATA_PLANNING_MODE)
+        .tableProperty(TableProperties.DATA_PLANNING_MODE)
+        .defaultValue(PlanningMode.fromName(TableProperties.PLANNING_MODE_DEFAULT))
+        .parse();
   }
 
   public PlanningMode deletePlanningMode() {
