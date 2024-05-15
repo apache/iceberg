6fc04a391ac7529291fc3852f2bc3ae425aafc0e
Igor Adulyan <53629576+igoradulian@users.noreply.github.com>
2024-05-15 10:40:04 -0700

    Update data/src/test/java/org/apache/iceberg/data/TestLocalScan.java
    Co-authored-by: Eduard Tudenhoefner <etudenhoefner@gmail.com>

--- data/src/test/java/org/apache/iceberg/data/TestLocalScan.java
+++ data/src/test/java/org/apache/iceberg/data/TestLocalScan.java
@@ -274,7 +274,7 @@ public class TestLocalScan {
     Assertions.assertThat(records).as("Should produce correct number of records")
         .hasSameSizeAs(expected);
     Assertions.assertThat(records).as("Random record set should match")
-        .hasSameSizeAs(expected);
+        .isEqualTo(expected);
   }
 
   @TestTemplate
