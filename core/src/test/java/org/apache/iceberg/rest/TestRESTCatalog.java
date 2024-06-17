5f3809c5159694618e0619cffd563077f1b56c12
Piotr Findeisen <piotr.findeisen@gmail.com>
2024-06-17 14:59:32 +0200

    Core, Flink, Spark: Import the right assertThatThrownBy method from AssertJ (#10512)

--- core/src/test/java/org/apache/iceberg/rest/TestRESTCatalog.java
+++ core/src/test/java/org/apache/iceberg/rest/TestRESTCatalog.java
@@ -20,7 +20,7 @@ package org.apache.iceberg.rest;
 
 import static org.apache.iceberg.types.Types.NestedField.required;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.atLeastOnce;
