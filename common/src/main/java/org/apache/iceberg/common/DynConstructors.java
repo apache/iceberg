b120904d464194d051c7e5a0064b9e37a4bf869b
Piotr Findeisen <piotr.findeisen@gmail.com>
2024-06-28 11:32:05 +0200

    Common: DynConstructors cleanup (#10542)
    The `DynConstructors.Builder.hiddenImpl(Class<?>...)` is a varargs
    method, but cannot be used as such, because `hiddenImpl(Class<T>,
    Class<?>...)` exists and would be used instead. Since there are no
    usages, deprecate it instead of fixing.

--- common/src/main/java/org/apache/iceberg/common/DynConstructors.java
+++ common/src/main/java/org/apache/iceberg/common/DynConstructors.java
@@ -43,6 +43,8 @@ public class DynConstructors {
       this.constructed = constructed;
     }
 
+    /** @deprecated since 1.6.0, will be removed in 1.7.0 */
+    @Deprecated
     public Class<? extends C> getConstructedClass() {
       return constructed;
     }
@@ -57,9 +59,9 @@ public class DynConstructors {
       } catch (InstantiationException | IllegalAccessException e) {
         throw e;
       } catch (InvocationTargetException e) {
-        Throwables.propagateIfInstanceOf(e.getCause(), Exception.class);
-        Throwables.propagateIfInstanceOf(e.getCause(), RuntimeException.class);
-        throw Throwables.propagate(e.getCause());
+        Throwables.throwIfInstanceOf(e.getCause(), Exception.class);
+        Throwables.throwIfInstanceOf(e.getCause(), RuntimeException.class);
+        throw new RuntimeException(e.getCause());
       }
     }
 
@@ -67,8 +69,8 @@ public class DynConstructors {
       try {
         return newInstanceChecked(args);
       } catch (Exception e) {
-        Throwables.propagateIfInstanceOf(e, RuntimeException.class);
-        throw Throwables.propagate(e);
+        Throwables.throwIfInstanceOf(e, RuntimeException.class);
+        throw new RuntimeException(e.getCause());
       }
     }
 
@@ -115,8 +117,8 @@ public class DynConstructors {
   public static class Builder {
     private final Class<?> baseClass;
     private ClassLoader loader = Thread.currentThread().getContextClassLoader();
-    private Ctor ctor = null;
-    private Map<String, Throwable> problems = Maps.newHashMap();
+    private Ctor<?> ctor = null;
+    private final Map<String, Throwable> problems = Maps.newHashMap();
 
     public Builder(Class<?> baseClass) {
       this.baseClass = baseClass;
@@ -162,7 +164,7 @@ public class DynConstructors {
       }
 
       try {
-        ctor = new Ctor<T>(targetClass.getConstructor(types), targetClass);
+        ctor = new Ctor<>(targetClass.getConstructor(types), targetClass);
       } catch (NoSuchMethodException e) {
         // not the right implementation
         problems.put(methodName(targetClass, types), e);
@@ -170,12 +172,16 @@ public class DynConstructors {
       return this;
     }
 
+    /**
+     * @deprecated since 1.6.0, will be removed in 1.7.0; This varargs method conflicts with {@link
+     *     #hiddenImpl(Class, Class...)}. Use {@link #builder(Class)} instead.
+     */
+    @Deprecated
     public Builder hiddenImpl(Class<?>... types) {
       hiddenImpl(baseClass, types);
       return this;
     }
 
-    @SuppressWarnings("unchecked")
     public Builder hiddenImpl(String className, Class<?>... types) {
       // don't do any work if an implementation has been found
       if (ctor != null) {
@@ -183,7 +189,7 @@ public class DynConstructors {
       }
 
       try {
-        Class targetClass = Class.forName(className, true, loader);
+        Class<?> targetClass = Class.forName(className, true, loader);
         hiddenImpl(targetClass, types);
       } catch (NoClassDefFoundError | ClassNotFoundException e) {
         // cannot load this implementation
@@ -201,7 +207,7 @@ public class DynConstructors {
       try {
         Constructor<T> hidden = targetClass.getDeclaredConstructor(types);
         AccessController.doPrivileged(new MakeAccessible(hidden));
-        ctor = new Ctor<T>(hidden, targetClass);
+        ctor = new Ctor<>(hidden, targetClass);
       } catch (SecurityException e) {
         // unusable
         problems.put(methodName(targetClass, types), e);
@@ -215,7 +221,7 @@ public class DynConstructors {
     @SuppressWarnings("unchecked")
     public <C> Ctor<C> buildChecked() throws NoSuchMethodException {
       if (ctor != null) {
-        return ctor;
+        return (Ctor<C>) ctor;
       }
       throw buildCheckedException(baseClass, problems);
     }
@@ -223,14 +229,14 @@ public class DynConstructors {
     @SuppressWarnings("unchecked")
     public <C> Ctor<C> build() {
       if (ctor != null) {
-        return ctor;
+        return (Ctor<C>) ctor;
       }
       throw buildRuntimeException(baseClass, problems);
     }
   }
 
   private static class MakeAccessible implements PrivilegedAction<Void> {
-    private Constructor<?> hidden;
+    private final Constructor<?> hidden;
 
     MakeAccessible(Constructor<?> hidden) {
       this.hidden = hidden;
