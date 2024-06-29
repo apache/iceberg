bb312af83d81b6064c77f7463ef6888d6a9435ba
Tai Le Manh <49281946+tlm365@users.noreply.github.com>
2024-06-29 21:22:01 +0700

    Fix incorrect double-checked-locking around TestStreamScanSql#tEnv (#10605)
    Signed-off-by: Tai Le Manh <manhtai.lmt@gmail.com>

--- flink/v1.18/flink/src/test/java/org/apache/iceberg/flink/source/TestStreamScanSql.java
+++ flink/v1.18/flink/src/test/java/org/apache/iceberg/flink/source/TestStreamScanSql.java
@@ -59,25 +59,27 @@ public class TestStreamScanSql extends CatalogTestBase {
 
   @Override
   protected TableEnvironment getTableEnv() {
-    if (tEnv == null) {
-      synchronized (this) {
-        if (tEnv == null) {
-          EnvironmentSettings.Builder settingsBuilder =
-              EnvironmentSettings.newInstance().inStreamingMode();
+    TableEnvironment tableEnv = tEnv;
+    if (tableEnv != null) {
+      return tableEnv;
+    }
+    synchronized (this) {
+      if (tEnv == null) {
+        EnvironmentSettings.Builder settingsBuilder =
+            EnvironmentSettings.newInstance().inStreamingMode();
 
-          StreamExecutionEnvironment env =
-              StreamExecutionEnvironment.getExecutionEnvironment(
-                  MiniClusterResource.DISABLE_CLASSLOADER_CHECK_CONFIG);
-          env.enableCheckpointing(400);
+        StreamExecutionEnvironment env =
+            StreamExecutionEnvironment.getExecutionEnvironment(
+                MiniClusterResource.DISABLE_CLASSLOADER_CHECK_CONFIG);
+        env.enableCheckpointing(400);
 
-          StreamTableEnvironment streamTableEnv =
-              StreamTableEnvironment.create(env, settingsBuilder.build());
-          streamTableEnv
-              .getConfig()
-              .getConfiguration()
-              .set(TableConfigOptions.TABLE_DYNAMIC_TABLE_OPTIONS_ENABLED, true);
-          tEnv = streamTableEnv;
-        }
+        StreamTableEnvironment streamTableEnv =
+            StreamTableEnvironment.create(env, settingsBuilder.build());
+        streamTableEnv
+            .getConfig()
+            .getConfiguration()
+            .set(TableConfigOptions.TABLE_DYNAMIC_TABLE_OPTIONS_ENABLED, true);
+        tEnv = streamTableEnv;
       }
     }
     return tEnv;
