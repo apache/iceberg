229d8f6fcd109e6c8943ea7cbb41dab746c6d0ed
Piotr Findeisen <piotr.findeisen@gmail.com>
2024-07-18 02:35:21 +0200

    Common: Update the version in deprecation messages (#10715)

--- common/src/main/java/org/apache/iceberg/common/DynFields.java
+++ common/src/main/java/org/apache/iceberg/common/DynFields.java
@@ -394,7 +394,7 @@ public class DynFields {
      * @return a {@link StaticField} with a valid implementation
      * @throws IllegalStateException if the method is not static
      * @throws NoSuchFieldException if no implementation was found
-     * @deprecated since 1.7.0, will be removed in 2.0.0
+     * @deprecated since 1.6.0, will be removed in 1.7.0
      */
     @Deprecated
     public <T> StaticField<T> buildStaticChecked() throws NoSuchFieldException {
