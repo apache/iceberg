5afeee5223643b0cfbd520e23f589a9d071d2090
Rodrigo Meneses <rmenesespinillos@apple.com>
2024-08-14 14:26:47 -0700

    Introduces the new IcebergSink based on the new V2 Flink Sink Abstraction Co-authored-by: Liwei Li <hililiwei@gmail.com> Co-authored-by: Kyle Bendickson <kjbendickson@gmail.com> Co-authored-by: Peter Vary <peter.vary.apache@gmail.com>

--- flink/v1.19/flink/src/main/java/org/apache/iceberg/flink/sink/committer/IcebergCommittable.java
+++ flink/v1.19/flink/src/main/java/org/apache/iceberg/flink/sink/committer/IcebergCommittable.java
@@ -73,8 +73,14 @@ public class IcebergCommittable implements Serializable {
 
   @Override
   public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
+    if (this == o) {
+      return true;
+    }
+
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
     IcebergCommittable that = (IcebergCommittable) o;
     return checkpointId == that.checkpointId
         && Arrays.equals(manifest, that.manifest)
