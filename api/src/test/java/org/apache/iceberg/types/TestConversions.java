4d7720281f936e751b69e4c724225c76958f766d
Ryan Blue <blue@apache.org>
2024-08-25 16:04:17 -0700

    Add a comment to clarify conversion test.

--- api/src/test/java/org/apache/iceberg/types/TestConversions.java
+++ api/src/test/java/org/apache/iceberg/types/TestConversions.java
@@ -104,10 +104,12 @@ public class TestConversions {
         .isEqualTo(new byte[] {-128, 26, 6, 0, 0, 0, 0, 0});
     assertThat(Literal.of(400000L).to(TimestampType.withZone()).toByteBuffer().array())
         .isEqualTo(new byte[] {-128, 26, 6, 0, 0, 0, 0, 0});
+    // values passed to assertConversion and Literal.of differ because Literal.of(...) assumes
+    // the value is in micros, which gets converted when to(TimestampNanoType) is called
     assertConversion(
-        400000L, TimestampNanoType.withoutZone(), new byte[] {-128, 26, 6, 0, 0, 0, 0, 0});
+        400000000L, TimestampNanoType.withoutZone(), new byte[] {0, -124, -41, 23, 0, 0, 0, 0});
     assertConversion(
-        400000L, TimestampNanoType.withZone(), new byte[] {-128, 26, 6, 0, 0, 0, 0, 0});
+        400000000L, TimestampNanoType.withZone(), new byte[] {0, -124, -41, 23, 0, 0, 0, 0});
     assertThat(Literal.of(400000L).to(TimestampNanoType.withoutZone()).toByteBuffer().array())
         .isEqualTo(new byte[] {0, -124, -41, 23, 0, 0, 0, 0});
     assertThat(Literal.of(400000L).to(TimestampNanoType.withZone()).toByteBuffer().array())
