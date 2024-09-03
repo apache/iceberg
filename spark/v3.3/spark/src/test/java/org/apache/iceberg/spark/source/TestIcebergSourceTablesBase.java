51507e38a7b3bc926bcbb9283fd8f37db723f1bf
hsiang-c <huangc@apple.com>
2024-09-03 12:37:27 +0800

    (Spark 3.3 unit test) Count data/delete manifests from all snapshots

--- spark/v3.3/spark/src/test/java/org/apache/iceberg/spark/source/TestIcebergSourceTablesBase.java
+++ spark/v3.3/spark/src/test/java/org/apache/iceberg/spark/source/TestIcebergSourceTablesBase.java
@@ -362,9 +362,13 @@ public abstract class TestIcebergSourceTablesBase extends SparkTestBase {
         .format("iceberg")
         .mode("append")
         .save(loadLocation(tableIdentifier));
+    long firstSnapshotId = table.currentSnapshot().snapshotId();
+    long firstSnapshotTimestamp = table.currentSnapshot().timestampMillis();
 
     // delete the first file to test that not only live files are listed
     table.newDelete().deleteFromRowFilter(Expressions.equal("id", 1)).commit();
+    long secondSnapshotId = table.currentSnapshot().snapshotId();
+    long secondSnapshotTimestamp = table.currentSnapshot().timestampMillis();
 
     // add a second file
     df2.select("id", "data")
@@ -375,6 +379,8 @@ public abstract class TestIcebergSourceTablesBase extends SparkTestBase {
 
     // ensure table data isn't stale
     table.refresh();
+    long thirdSnapshotId = table.currentSnapshot().snapshotId();
+    long thirdSnapshotTimestamp = table.currentSnapshot().timestampMillis();
 
     Dataset<Row> entriesTableDs =
         spark
@@ -393,12 +399,21 @@ public abstract class TestIcebergSourceTablesBase extends SparkTestBase {
         // each row must inherit snapshot_id and sequence_number
         rows.forEach(
             row -> {
-              if (row.get("snapshot_id").equals(table.currentSnapshot().snapshotId())) {
+              if (row.get("snapshot_id").equals(thirdSnapshotId)) {
                 row.put(2, 3L); // data sequence number
                 row.put(3, 3L); // file sequence number
+                row.put(5, thirdSnapshotId);
+                row.put(6, thirdSnapshotTimestamp);
+              } else if (row.get("status").equals(2)) {
+                row.put(2, 1L); // data sequence number
+                row.put(3, 1L); // file sequence number
+                row.put(5, secondSnapshotId);
+                row.put(6, secondSnapshotTimestamp);
               } else {
                 row.put(2, 1L); // data sequence number
                 row.put(3, 1L); // file sequence number
+                row.put(5, firstSnapshotId);
+                row.put(6, firstSnapshotTimestamp);
               }
               GenericData.Record file = (GenericData.Record) row.get("data_file");
               TestHelpers.asMetadataRecord(file);
@@ -1204,12 +1219,12 @@ public abstract class TestIcebergSourceTablesBase extends SparkTestBase {
 
     table.newDelete().deleteFromRowFilter(Expressions.alwaysTrue()).commit();
 
-    Stream<Pair<Long, ManifestFile>> snapshotIdToManifests =
+    Stream<Pair<Snapshot, ManifestFile>> snapshotIdToManifests =
         StreamSupport.stream(table.snapshots().spliterator(), false)
             .flatMap(
                 snapshot ->
                     snapshot.allManifests(table.io()).stream()
-                        .map(manifest -> Pair.of(snapshot.snapshotId(), manifest)));
+                        .map(manifest -> Pair.of(snapshot, manifest)));
 
     List<Row> actual =
         spark
@@ -2042,7 +2057,7 @@ public abstract class TestIcebergSourceTablesBase extends SparkTestBase {
     Dataset<Row> df =
         spark.createDataFrame(Lists.newArrayList(new SimpleRecord(1, "a")), SimpleRecord.class);
 
-    List<Pair<Long, ManifestFile>> snapshotIdToManifests = Lists.newArrayList();
+    List<Pair<Snapshot, ManifestFile>> snapshotToManifests = Lists.newArrayList();
 
     df.select("id", "data")
         .write()
@@ -2052,9 +2067,9 @@ public abstract class TestIcebergSourceTablesBase extends SparkTestBase {
 
     table.refresh();
     Snapshot snapshot1 = table.currentSnapshot();
-    snapshotIdToManifests.addAll(
+    snapshotToManifests.addAll(
         snapshot1.allManifests(table.io()).stream()
-            .map(manifest -> Pair.of(snapshot1.snapshotId(), manifest))
+            .map(manifest -> Pair.of(snapshot1, manifest))
             .collect(Collectors.toList()));
 
     df.select("id", "data")
@@ -2066,9 +2081,9 @@ public abstract class TestIcebergSourceTablesBase extends SparkTestBase {
     table.refresh();
     Snapshot snapshot2 = table.currentSnapshot();
     Assert.assertEquals("Should have two manifests", 2, snapshot2.allManifests(table.io()).size());
-    snapshotIdToManifests.addAll(
+    snapshotToManifests.addAll(
         snapshot2.allManifests(table.io()).stream()
-            .map(manifest -> Pair.of(snapshot2.snapshotId(), manifest))
+            .map(manifest -> Pair.of(snapshot2, manifest))
             .collect(Collectors.toList()));
 
     // Add manifests that will not be selected
@@ -2099,7 +2114,7 @@ public abstract class TestIcebergSourceTablesBase extends SparkTestBase {
     table.refresh();
 
     List<GenericData.Record> expected =
-        snapshotIdToManifests.stream()
+        snapshotToManifests.stream()
             .map(
                 snapshotManifest ->
                     manifestRecord(
@@ -2186,7 +2201,7 @@ public abstract class TestIcebergSourceTablesBase extends SparkTestBase {
   }
 
   private GenericData.Record manifestRecord(
-      Table manifestTable, Long referenceSnapshotId, ManifestFile manifest) {
+      Table manifestTable, Snapshot referenceSnapshot, ManifestFile manifest) {
     GenericRecordBuilder builder =
         new GenericRecordBuilder(AvroSchemaUtil.convert(manifestTable.schema(), "manifests"));
     GenericRecordBuilder summaryBuilder =
@@ -2227,7 +2242,8 @@ public abstract class TestIcebergSourceTablesBase extends SparkTestBase {
                         .set("lower_bound", "1")
                         .set("upper_bound", "1")
                         .build()))
-        .set("reference_snapshot_id", referenceSnapshotId)
+        .set("reference_snapshot_id", referenceSnapshot.snapshotId())
+        .set("reference_snapshot_timestamp_millis", referenceSnapshot.timestampMillis())
         .build();
   }
 
