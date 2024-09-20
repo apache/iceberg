1a44d7dd6053831d1ea37bb61711ad248270d284
Wing Yew Poon <wypoon@cloudera.com>
2024-09-20 15:31:59 -0700

    Remove unused code for streaming position deletes

--- core/src/main/java/org/apache/iceberg/deletes/Deletes.java
+++ core/src/main/java/org/apache/iceberg/deletes/Deletes.java
@@ -29,10 +29,7 @@ import org.apache.iceberg.Accessor;
 import org.apache.iceberg.MetadataColumns;
 import org.apache.iceberg.Schema;
 import org.apache.iceberg.StructLike;
-import org.apache.iceberg.io.CloseableGroup;
 import org.apache.iceberg.io.CloseableIterable;
-import org.apache.iceberg.io.CloseableIterator;
-import org.apache.iceberg.io.FilterIterator;
 import org.apache.iceberg.relocated.com.google.common.collect.ImmutableList;
 import org.apache.iceberg.relocated.com.google.common.collect.Iterables;
 import org.apache.iceberg.relocated.com.google.common.collect.Lists;
@@ -44,12 +41,8 @@ import org.apache.iceberg.util.ParallelIterable;
 import org.apache.iceberg.util.SortedMerge;
 import org.apache.iceberg.util.StructLikeSet;
 import org.apache.iceberg.util.ThreadPools;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Deletes {
-
-  private static final Logger LOG = LoggerFactory.getLogger(Deletes.class);
 
   private static final Schema POSITION_DELETE_SCHEMA =
       new Schema(MetadataColumns.DELETE_FILE_PATH, MetadataColumns.DELETE_FILE_POS);
@@ -192,6 +185,10 @@ public class Deletes {
     }
   }
 
+  /**
+   * @deprecated since 1.7.0, will be removed in future.
+   */
+  @Deprecated
   public static <T> CloseableIterable<T> streamingFilter(
       CloseableIterable<T> rows,
       Function<T, Long> rowToPosition,
@@ -199,20 +196,32 @@ public class Deletes {
     return streamingFilter(rows, rowToPosition, posDeletes, new DeleteCounter());
   }
 
+  /**
+   * @deprecated since 1.7.0, will be removed in future.
+   */
+  @Deprecated
   public static <T> CloseableIterable<T> streamingFilter(
       CloseableIterable<T> rows,
       Function<T, Long> rowToPosition,
       CloseableIterable<Long> posDeletes,
       DeleteCounter counter) {
-    return new PositionStreamDeleteFilter<>(rows, rowToPosition, posDeletes, counter);
+    PositionDeleteIndex positionIndex = toPositionIndex(posDeletes);
+    Predicate<T> isDeleted = row -> positionIndex.isDeleted(rowToPosition.apply(row));
+    return filterDeleted(rows, isDeleted, counter);
   }
 
+  /**
+   * @deprecated since 1.7.0, will be removed in future.
+   */
+  @Deprecated
   public static <T> CloseableIterable<T> streamingMarker(
       CloseableIterable<T> rows,
       Function<T, Long> rowToPosition,
       CloseableIterable<Long> posDeletes,
-      Consumer<T> markDeleted) {
-    return new PositionStreamDeleteMarker<>(rows, rowToPosition, posDeletes, markDeleted);
+      Consumer<T> markRowDeleted) {
+    PositionDeleteIndex positionIndex = toPositionIndex(posDeletes);
+    Predicate<T> isDeleted = row -> positionIndex.isDeleted(rowToPosition.apply(row));
+    return markDeleted(rows, isDeleted, markRowDeleted);
   }
 
   public static CloseableIterable<Long> deletePositions(
@@ -245,148 +254,6 @@ public class Deletes {
     @Override
     protected boolean shouldKeep(T row) {
       return !deletes.contains(extractEqStruct.apply(row));
-    }
-  }
-
-  private abstract static class PositionStreamDeleteIterable<T> extends CloseableGroup
-      implements CloseableIterable<T> {
-    private final CloseableIterable<T> rows;
-    private final CloseableIterator<Long> deletePosIterator;
-    private final Function<T, Long> rowToPosition;
-    private long nextDeletePos;
-
-    PositionStreamDeleteIterable(
-        CloseableIterable<T> rows,
-        Function<T, Long> rowToPosition,
-        CloseableIterable<Long> deletePositions) {
-      this.rows = rows;
-      this.rowToPosition = rowToPosition;
-      this.deletePosIterator = deletePositions.iterator();
-    }
-
-    @Override
-    public CloseableIterator<T> iterator() {
-      CloseableIterator<T> iter;
-      if (deletePosIterator.hasNext()) {
-        nextDeletePos = deletePosIterator.next();
-        iter = applyDelete(rows.iterator(), deletePosIterator);
-      } else {
-        iter = rows.iterator();
-      }
-
-      addCloseable(iter);
-      addCloseable(deletePosIterator);
-
-      return iter;
-    }
-
-    boolean isDeleted(T row) {
-      long currentPos = rowToPosition.apply(row);
-      if (currentPos < nextDeletePos) {
-        return false;
-      }
-
-      // consume delete positions until the next is past the current position
-      boolean isDeleted = currentPos == nextDeletePos;
-      while (deletePosIterator.hasNext() && nextDeletePos <= currentPos) {
-        this.nextDeletePos = deletePosIterator.next();
-        if (!isDeleted && currentPos == nextDeletePos) {
-          // if any delete position matches the current position
-          isDeleted = true;
-        }
-      }
-
-      return isDeleted;
-    }
-
-    protected abstract CloseableIterator<T> applyDelete(
-        CloseableIterator<T> items, CloseableIterator<Long> deletePositions);
-  }
-
-  private static class PositionStreamDeleteFilter<T> extends PositionStreamDeleteIterable<T> {
-    private final DeleteCounter counter;
-
-    PositionStreamDeleteFilter(
-        CloseableIterable<T> rows,
-        Function<T, Long> rowToPosition,
-        CloseableIterable<Long> deletePositions,
-        DeleteCounter counter) {
-      super(rows, rowToPosition, deletePositions);
-      this.counter = counter;
-    }
-
-    @Override
-    protected CloseableIterator<T> applyDelete(
-        CloseableIterator<T> items, CloseableIterator<Long> deletePositions) {
-      return new FilterIterator<T>(items) {
-        @Override
-        protected boolean shouldKeep(T item) {
-          boolean deleted = isDeleted(item);
-          if (deleted) {
-            counter.increment();
-          }
-
-          return !deleted;
-        }
-
-        @Override
-        public void close() {
-          try {
-            deletePositions.close();
-          } catch (IOException e) {
-            LOG.warn("Error closing delete file", e);
-          }
-          super.close();
-        }
-      };
-    }
-  }
-
-  private static class PositionStreamDeleteMarker<T> extends PositionStreamDeleteIterable<T> {
-    private final Consumer<T> markDeleted;
-
-    PositionStreamDeleteMarker(
-        CloseableIterable<T> rows,
-        Function<T, Long> rowToPosition,
-        CloseableIterable<Long> deletePositions,
-        Consumer<T> markDeleted) {
-      super(rows, rowToPosition, deletePositions);
-      this.markDeleted = markDeleted;
-    }
-
-    @Override
-    protected CloseableIterator<T> applyDelete(
-        CloseableIterator<T> items, CloseableIterator<Long> deletePositions) {
-
-      return new CloseableIterator<T>() {
-        @Override
-        public void close() {
-          try {
-            deletePositions.close();
-          } catch (IOException e) {
-            LOG.warn("Error closing delete file", e);
-          }
-          try {
-            items.close();
-          } catch (IOException e) {
-            LOG.warn("Error closing data file", e);
-          }
-        }
-
-        @Override
-        public boolean hasNext() {
-          return items.hasNext();
-        }
-
-        @Override
-        public T next() {
-          T row = items.next();
-          if (isDeleted(row)) {
-            markDeleted.accept(row);
-          }
-          return row;
-        }
-      };
     }
   }
 
