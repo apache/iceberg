d879b140dafd4628823ebbd518bc7f6d9eabd772
Eric Maynard <eric.maynard@snowflake.com>
2024-09-23 17:37:33 -0700

    linter

--- core/src/main/java/org/apache/iceberg/rest/RESTTableOperations.java
+++ core/src/main/java/org/apache/iceberg/rest/RESTTableOperations.java
@@ -20,7 +20,6 @@ package org.apache.iceberg.rest;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
