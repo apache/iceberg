d3837e9c40b635da6bd3192350d940e78e6335c8
liu yang <lurnagaodahua@gmail.com>
2024-09-24 17:50:04 +0800

    Update SpecSchema method scope to public

--- core/src/main/java/org/apache/iceberg/TableMetadata.java
+++ core/src/main/java/org/apache/iceberg/TableMetadata.java
@@ -719,7 +719,7 @@ public class TableMetadata implements Serializable {
     return new Builder(this).upgradeFormatVersion(newFormatVersion).build();
   }
 
-  private static PartitionSpec updateSpecSchema(Schema schema, PartitionSpec partitionSpec) {
+  public static PartitionSpec updateSpecSchema(Schema schema, PartitionSpec partitionSpec) {
     PartitionSpec.Builder specBuilder =
         PartitionSpec.builderFor(schema).withSpecId(partitionSpec.specId());
 
