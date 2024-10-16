11a8a78b951d6f83fbeff703ac4e1a4b7d3f3597
Marc Cenac <547446+mrcnc@users.noreply.github.com>
2024-10-16 10:39:50 -0500

    Core, Azure: Support wasb[s] paths in ADLSFileIO (#11294)

--- core/src/main/java/org/apache/iceberg/io/ResolvingFileIO.java
+++ core/src/main/java/org/apache/iceberg/io/ResolvingFileIO.java
@@ -62,7 +62,9 @@ public class ResolvingFileIO implements HadoopConfigurable, DelegateFileIO {
           "s3n", S3_FILE_IO_IMPL,
           "gs", GCS_FILE_IO_IMPL,
           "abfs", ADLS_FILE_IO_IMPL,
-          "abfss", ADLS_FILE_IO_IMPL);
+          "abfss", ADLS_FILE_IO_IMPL,
+          "wasb", ADLS_FILE_IO_IMPL,
+          "wasbs", ADLS_FILE_IO_IMPL);
 
   private final Map<String, DelegateFileIO> ioInstances = Maps.newConcurrentMap();
   private final AtomicBoolean isClosed = new AtomicBoolean(false);
