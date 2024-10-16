32b1ab6ea833f4067b92f7943cbfc0057faba4c7
Piotr Findeisen <piotr.findeisen@gmail.com>
2024-10-16 08:46:16 +0200

    Core: Fix version number in deprecation note for invalidateAll (#11325)

--- core/src/main/java/org/apache/iceberg/io/ContentCache.java
+++ core/src/main/java/org/apache/iceberg/io/ContentCache.java
@@ -140,7 +140,7 @@ public class ContentCache {
   }
 
   /**
-   * @deprecated since 1.6.0, will be removed in 1.7.0; This method does only best-effort
+   * @deprecated since 1.7.0, will be removed in 2.0.0; This method does only best-effort
    *     invalidation and is susceptible to a race condition. If the caller changed the state that
    *     could be cached (perhaps files on the storage) and calls this method, there is no guarantee
    *     that the cache will not contain stale entries some time after this method returns.
