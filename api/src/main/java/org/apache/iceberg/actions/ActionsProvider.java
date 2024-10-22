a8ec43d975d8d3bbacb6880b487208b00cb7361f
Hongyue/Steve Zhang <steveiszhy@gmail.com>
2024-10-22 15:28:57 -0700

    Core, Spark 3.5: Remove dangling deletes as part of RewriteDataFilesAction (#9724)

--- api/src/main/java/org/apache/iceberg/actions/ActionsProvider.java
+++ api/src/main/java/org/apache/iceberg/actions/ActionsProvider.java
@@ -82,4 +82,10 @@ public interface ActionsProvider {
     throw new UnsupportedOperationException(
         this.getClass().getName() + " does not implement rewriteTablePath");
   }
+
+  /** Instantiates an action to remove dangling delete files from current snapshot. */
+  default RemoveDanglingDeleteFiles removeDanglingDeleteFiles(Table table) {
+    throw new UnsupportedOperationException(
+        this.getClass().getName() + " does not implement removeDanglingDeleteFiles");
+  }
 }
