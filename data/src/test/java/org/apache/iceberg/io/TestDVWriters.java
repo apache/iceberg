89b310799da8123c07470e5625f226f89f1c0259
Anton Okolnychyi <aokolnychyi@apache.org>
2024-11-06 09:33:31 +0100

    Feedback

--- data/src/test/java/org/apache/iceberg/io/TestDVWriters.java
+++ data/src/test/java/org/apache/iceberg/io/TestDVWriters.java
@@ -20,9 +20,7 @@ package org.apache.iceberg.io;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -65,10 +63,6 @@ public abstract class TestDVWriters<T> extends WriterTestBase<T> {
   @Override
   @BeforeEach
   public void setupTable() throws Exception {
-    this.tableDir = Files.createTempDirectory(temp, "junit").toFile();
-    assertThat(tableDir.delete()).isTrue(); // created during table creation
-
-    this.metadataDir = new File(tableDir, "metadata");
     this.table = create(SCHEMA, PartitionSpec.unpartitioned());
     this.fileFactory = OutputFileFactory.builderFor(table, 1, 1).format(FileFormat.PUFFIN).build();
   }
