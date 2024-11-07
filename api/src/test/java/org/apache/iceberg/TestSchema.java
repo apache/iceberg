ce560b68e36f710cd8825bfd7d2917be91eb0c8d
Russell Spitzer <russell.spitzer@GMAIL.COM>
2024-11-07 09:47:50 -0600

    API: Removes Explicit Parameterization of Schema Tests (#11444)

--- api/src/test/java/org/apache/iceberg/TestSchema.java
+++ api/src/test/java/org/apache/iceberg/TestSchema.java
@@ -18,32 +18,27 @@
  */
 package org.apache.iceberg;
 
+import static org.apache.iceberg.Schema.DEFAULT_VALUES_MIN_FORMAT_VERSION;
+import static org.apache.iceberg.Schema.MIN_FORMAT_VERSIONS;
+import static org.apache.iceberg.TestHelpers.MAX_FORMAT_VERSION;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import org.apache.iceberg.relocated.com.google.common.collect.ImmutableList;
+import org.apache.iceberg.types.Type;
 import org.apache.iceberg.types.Types;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.FieldSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class TestSchema {
-  private static final Schema TS_NANO_CASES =
-      new Schema(
-          Types.NestedField.required(1, "id", Types.LongType.get()),
-          Types.NestedField.optional(2, "ts", Types.TimestampNanoType.withZone()),
-          Types.NestedField.optional(
-              3, "arr", Types.ListType.ofRequired(4, Types.TimestampNanoType.withoutZone())),
-          Types.NestedField.required(
-              5,
-              "struct",
-              Types.StructType.of(
-                  Types.NestedField.optional(6, "inner_ts", Types.TimestampNanoType.withZone()),
-                  Types.NestedField.required(7, "data", Types.StringType.get()))),
-          Types.NestedField.optional(
-              8,
-              "struct_arr",
-              Types.StructType.of(
-                  Types.NestedField.optional(9, "ts", Types.TimestampNanoType.withoutZone()))));
+
+  private static final List<Type> TEST_TYPES =
+      ImmutableList.of(Types.TimestampNanoType.withoutZone(), Types.TimestampNanoType.withZone());
 
   private static final Schema INITIAL_DEFAULT_SCHEMA =
       new Schema(
@@ -64,27 +59,77 @@ public class TestSchema {
               .withWriteDefault("--")
               .build());
 
+  private Schema generateTypeSchema(Type type) {
+    return new Schema(
+        Types.NestedField.required(1, "id", Types.LongType.get()),
+        Types.NestedField.optional(2, "top", type),
+        Types.NestedField.optional(3, "arr", Types.ListType.ofRequired(4, type)),
+        Types.NestedField.required(
+            5,
+            "struct",
+            Types.StructType.of(
+                Types.NestedField.optional(6, "inner_op", type),
+                Types.NestedField.required(7, "inner_req", type),
+                Types.NestedField.optional(
+                    8,
+                    "struct_arr",
+                    Types.StructType.of(Types.NestedField.optional(9, "deep", type))))));
+  }
+
+  private static Stream<Arguments> unsupportedTypes() {
+    return TEST_TYPES.stream()
+        .flatMap(
+            type ->
+                IntStream.range(1, MIN_FORMAT_VERSIONS.get(type.typeId()))
+                    .mapToObj(unsupportedVersion -> Arguments.of(type, unsupportedVersion)));
+  }
+
   @ParameterizedTest
-  @ValueSource(ints = {1, 2})
-  public void testUnsupportedTimestampNano(int formatVersion) {
-    assertThatThrownBy(() -> Schema.checkCompatibility(TS_NANO_CASES, formatVersion))
+  @MethodSource("unsupportedTypes")
+  public void testUnsupportedTypes(Type type, int unsupportedVersion) {
+    assertThatThrownBy(
+            () -> Schema.checkCompatibility(generateTypeSchema(type), unsupportedVersion))
         .isInstanceOf(IllegalStateException.class)
         .hasMessage(
             "Invalid schema for v%s:\n"
-                + "- Invalid type for ts: timestamptz_ns is not supported until v3\n"
-                + "- Invalid type for arr.element: timestamp_ns is not supported until v3\n"
-                + "- Invalid type for struct.inner_ts: timestamptz_ns is not supported until v3\n"
-                + "- Invalid type for struct_arr.ts: timestamp_ns is not supported until v3",
-            formatVersion);
+                + "- Invalid type for top: %s is not supported until v%s\n"
+                + "- Invalid type for arr.element: %s is not supported until v%s\n"
+                + "- Invalid type for struct.inner_op: %s is not supported until v%s\n"
+                + "- Invalid type for struct.inner_req: %s is not supported until v%s\n"
+                + "- Invalid type for struct.struct_arr.deep: %s is not supported until v%s",
+            unsupportedVersion,
+            type,
+            MIN_FORMAT_VERSIONS.get(type.typeId()),
+            type,
+            MIN_FORMAT_VERSIONS.get(type.typeId()),
+            type,
+            MIN_FORMAT_VERSIONS.get(type.typeId()),
+            type,
+            MIN_FORMAT_VERSIONS.get(type.typeId()),
+            type,
+            MIN_FORMAT_VERSIONS.get(type.typeId()));
   }
 
-  @Test
-  public void testSupportedTimestampNano() {
-    assertThatCode(() -> Schema.checkCompatibility(TS_NANO_CASES, 3)).doesNotThrowAnyException();
+  private static Stream<Arguments> supportedTypes() {
+    return TEST_TYPES.stream()
+        .flatMap(
+            type ->
+                IntStream.rangeClosed(MIN_FORMAT_VERSIONS.get(type.typeId()), MAX_FORMAT_VERSION)
+                    .mapToObj(supportedVersion -> Arguments.of(type, supportedVersion)));
   }
 
   @ParameterizedTest
-  @ValueSource(ints = {1, 2})
+  @MethodSource("supportedTypes")
+  public void testTypeSupported(Type type, int supportedVersion) {
+    assertThatCode(() -> Schema.checkCompatibility(generateTypeSchema(type), supportedVersion))
+        .doesNotThrowAnyException();
+  }
+
+  private static int[] unsupportedInitialDefault =
+      IntStream.range(1, DEFAULT_VALUES_MIN_FORMAT_VERSION).toArray();
+
+  @ParameterizedTest
+  @FieldSource("unsupportedInitialDefault")
   public void testUnsupportedInitialDefault(int formatVersion) {
     assertThatThrownBy(() -> Schema.checkCompatibility(INITIAL_DEFAULT_SCHEMA, formatVersion))
         .isInstanceOf(IllegalStateException.class)
@@ -95,14 +140,18 @@ public class TestSchema {
             formatVersion);
   }
 
-  @Test
-  public void testSupportedInitialDefault() {
-    assertThatCode(() -> Schema.checkCompatibility(INITIAL_DEFAULT_SCHEMA, 3))
+  private static int[] supportedInitialDefault =
+      IntStream.rangeClosed(DEFAULT_VALUES_MIN_FORMAT_VERSION, MAX_FORMAT_VERSION).toArray();
+
+  @ParameterizedTest
+  @FieldSource("supportedInitialDefault")
+  public void testSupportedInitialDefault(int formatVersion) {
+    assertThatCode(() -> Schema.checkCompatibility(INITIAL_DEFAULT_SCHEMA, formatVersion))
         .doesNotThrowAnyException();
   }
 
   @ParameterizedTest
-  @ValueSource(ints = {1, 2, 3})
+  @FieldSource("org.apache.iceberg.TestHelpers#ALL_VERSIONS")
   public void testSupportedWriteDefault(int formatVersion) {
     // only the initial default is a forward-incompatible change
     assertThatCode(() -> Schema.checkCompatibility(WRITE_DEFAULT_SCHEMA, formatVersion))
