d5f60878a8c1765d10aee4f207e03c947ae8d589
Fokko <fokko@apache.org>
2024-11-08 21:45:12 +0100

    Revert "Revert "Build: Bump parquet from 1.13.1 to 1.14.3 (#11264)" (#11462)"
    This reverts commit 7cc16fa94d7cd4e19397e9b4fba62185e0fa5eac.

--- flink/v1.18/flink/src/test/java/org/apache/iceberg/flink/source/TestMetadataTableReadableMetrics.java
+++ flink/v1.18/flink/src/test/java/org/apache/iceberg/flink/source/TestMetadataTableReadableMetrics.java
@@ -217,27 +217,27 @@ public class TestMetadataTableReadableMetrics extends CatalogTestBase {
 
     Row binaryCol =
         Row.of(
-            52L,
+            55L,
             4L,
             2L,
             null,
             Base64.getDecoder().decode("1111"),
             Base64.getDecoder().decode("2222"));
-    Row booleanCol = Row.of(32L, 4L, 0L, null, false, true);
-    Row decimalCol = Row.of(85L, 4L, 1L, null, new BigDecimal("1.00"), new BigDecimal("2.00"));
-    Row doubleCol = Row.of(85L, 4L, 0L, 1L, 1.0D, 2.0D);
+    Row booleanCol = Row.of(36L, 4L, 0L, null, false, true);
+    Row decimalCol = Row.of(91L, 4L, 1L, null, new BigDecimal("1.00"), new BigDecimal("2.00"));
+    Row doubleCol = Row.of(91L, 4L, 0L, 1L, 1.0D, 2.0D);
     Row fixedCol =
         Row.of(
-            44L,
+            47L,
             4L,
             2L,
             null,
             Base64.getDecoder().decode("1111"),
             Base64.getDecoder().decode("2222"));
-    Row floatCol = Row.of(71L, 4L, 0L, 2L, 0f, 0f);
-    Row intCol = Row.of(71L, 4L, 0L, null, 1, 2);
-    Row longCol = Row.of(79L, 4L, 0L, null, 1L, 2L);
-    Row stringCol = Row.of(79L, 4L, 0L, null, "1", "2");
+    Row floatCol = Row.of(77L, 4L, 0L, 2L, 0f, 0f);
+    Row intCol = Row.of(77L, 4L, 0L, null, 1, 2);
+    Row longCol = Row.of(85L, 4L, 0L, null, 1L, 2L);
+    Row stringCol = Row.of(85L, 4L, 0L, null, "1", "2");
 
     List<Row> expected =
         Lists.newArrayList(
@@ -289,8 +289,8 @@ public class TestMetadataTableReadableMetrics extends CatalogTestBase {
   public void testNestedValues() throws Exception {
     createNestedTable();
 
-    Row leafDoubleCol = Row.of(46L, 3L, 1L, 1L, 0.0D, 0.0D);
-    Row leafLongCol = Row.of(54L, 3L, 1L, null, 0L, 1L);
+    Row leafDoubleCol = Row.of(50L, 3L, 1L, 1L, 0.0D, 0.0D);
+    Row leafLongCol = Row.of(57L, 3L, 1L, null, 0L, 1L);
     Row metrics = Row.of(Row.of(leafDoubleCol, leafLongCol));
 
     TestHelpers.assertRows(
