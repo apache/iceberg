355a280fdc0ac00607bcaa7b0b24a41ed23d1b41
ZhendongBai <johnnyprimary@163.com>
2024-11-18 15:58:51 +0800

    [BugFix] Fixed the problem of mistakenly deleting table snapshot metadata file in the event of unexpected errors, like OutOfMemoryError, during the checkCommitStatus method execution. While the org.apache.iceberg.hive.HiveViewOperations.checkCurrentMetadataLocation method performs a refresh operation to download and update the current table metadata, this action consumes memory. To address this, we now set the commit status to UNKNOWN before checking the commit status, thus preventing inadvertent deletion of snapshot metadata files.

--- hive-metastore/src/main/java/org/apache/iceberg/hive/HiveTableOperations.java
+++ hive-metastore/src/main/java/org/apache/iceberg/hive/HiveTableOperations.java
@@ -292,6 +292,7 @@ public class HiveTableOperations extends BaseMetastoreTableOperations
             database,
             tableName,
             e);
+        commitStatus = BaseMetastoreOperations.CommitStatus.UNKNOWN;
         commitStatus =
             BaseMetastoreOperations.CommitStatus.valueOf(
                 checkCommitStatus(newMetadataLocation, metadata).name());
