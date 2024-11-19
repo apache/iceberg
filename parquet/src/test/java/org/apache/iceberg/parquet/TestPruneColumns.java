f8fb7dbc92d47e3c39cc6268d04d4237ba7ba629
zhuangsongtao <1270728683@qq.com>
2024-11-19 17:03:51 +0800

    fix: filter field

--- parquet/src/test/java/org/apache/iceberg/parquet/TestPruneColumns.java
+++ parquet/src/test/java/org/apache/iceberg/parquet/TestPruneColumns.java
@@ -20,6 +20,8 @@ package org.apache.iceberg.parquet;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import org.apache.iceberg.Schema;
 import org.apache.iceberg.types.Types.DoubleType;
 import org.apache.iceberg.types.Types.ListType;
@@ -35,6 +37,126 @@ import org.apache.parquet.schema.Types;
 import org.junit.jupiter.api.Test;
 
 public class TestPruneColumns {
+
+  private static final Schema MULTI_LEVEL_ICEBERG_SCHEMA =
+      new Schema(
+          ImmutableList.of(
+              NestedField.of(1, false, "a1", org.apache.iceberg.types.Types.BinaryType.get()),
+              NestedField.of(2, false, "a2", org.apache.iceberg.types.Types.BinaryType.get()),
+              NestedField.of(
+                  3,
+                  false,
+                  "a3",
+                  StructType.of(
+                      NestedField.of(
+                          4, false, "b1", org.apache.iceberg.types.Types.BinaryType.get()),
+                      NestedField.of(
+                          5, false, "b2", org.apache.iceberg.types.Types.BinaryType.get()))),
+              NestedField.of(
+                  6,
+                  false,
+                  "a4",
+                  StructType.of(
+                      NestedField.of(
+                          7,
+                          false,
+                          "b1",
+                          StructType.of(
+                              NestedField.of(
+                                  8, false, "c1", org.apache.iceberg.types.Types.BinaryType.get()),
+                              NestedField.of(
+                                  9,
+                                  false,
+                                  "c2",
+                                  org.apache.iceberg.types.Types.BinaryType.get()))),
+                      NestedField.of(
+                          10,
+                          false,
+                          "b2",
+                          StructType.of(
+                              NestedField.of(
+                                  11, false, "c1", org.apache.iceberg.types.Types.BinaryType.get()),
+                              NestedField.of(
+                                  12,
+                                  false,
+                                  "c2",
+                                  org.apache.iceberg.types.Types.BinaryType.get()))))),
+              NestedField.of(
+                  13,
+                  false,
+                  "a5",
+                  StructType.of(
+                      NestedField.of(
+                          14,
+                          false,
+                          "b1",
+                          StructType.of(
+                              NestedField.of(
+                                  15,
+                                  false,
+                                  "c1",
+                                  StructType.of(
+                                      NestedField.of(
+                                          16,
+                                          false,
+                                          "d1",
+                                          org.apache.iceberg.types.Types.BinaryType.get()),
+                                      NestedField.of(
+                                          17,
+                                          false,
+                                          "d2",
+                                          org.apache.iceberg.types.Types.BinaryType.get()))),
+                              NestedField.of(
+                                  18,
+                                  false,
+                                  "c2",
+                                  StructType.of(
+                                      NestedField.of(
+                                          19,
+                                          false,
+                                          "d1",
+                                          org.apache.iceberg.types.Types.BinaryType.get()),
+                                      NestedField.of(
+                                          20,
+                                          false,
+                                          "d2",
+                                          org.apache.iceberg.types.Types.BinaryType.get()))))),
+                      NestedField.of(
+                          21,
+                          false,
+                          "b2",
+                          StructType.of(
+                              NestedField.of(
+                                  22,
+                                  false,
+                                  "c1",
+                                  StructType.of(
+                                      NestedField.of(
+                                          23,
+                                          false,
+                                          "d1",
+                                          org.apache.iceberg.types.Types.BinaryType.get()),
+                                      NestedField.of(
+                                          24,
+                                          false,
+                                          "d2",
+                                          org.apache.iceberg.types.Types.BinaryType.get()))),
+                              NestedField.of(
+                                  25,
+                                  false,
+                                  "c2",
+                                  StructType.of(
+                                      NestedField.of(
+                                          26,
+                                          false,
+                                          "d1",
+                                          org.apache.iceberg.types.Types.BinaryType.get()),
+                                      NestedField.of(
+                                          27,
+                                          false,
+                                          "d2",
+                                          org.apache.iceberg.types.Types.BinaryType.get())))))))));
+
   @Test
   public void testMapKeyValueName() {
     MessageType fileSchema =
@@ -268,6 +390,154 @@ public class TestPruneColumns {
             .named("table");
 
     MessageType actual = ParquetSchemaUtil.pruneColumns(fileSchema, projection);
+    assertThat(actual).as("Pruned schema should be matched").isEqualTo(expected);
+  }
+
+  @Test
+  public void testTwoLevelOneChildPruneColumns() {
+    MessageType expected =
+        Types.buildMessage()
+            .addField(
+                Types.buildGroup(Type.Repetition.REQUIRED)
+                    .addField(
+                        Types.buildGroup(Type.Repetition.REQUIRED)
+                            .addField(
+                                Types.primitive(PrimitiveTypeName.BINARY, Type.Repetition.REQUIRED)
+                                    .id(8)
+                                    .named("c1"))
+                            .id(7)
+                            .named("b1"))
+                    .id(6)
+                    .named("a4"))
+            .named("schema");
+    Type actual =
+        TypeWithSchemaVisitor.visit(
+            MULTI_LEVEL_ICEBERG_SCHEMA.asStruct(),
+            ParquetSchemaUtil.convert(MULTI_LEVEL_ICEBERG_SCHEMA, "schema"),
+            new PruneColumns(ImmutableSet.of(6, 7, 8)));
+    assertThat(actual).as("Pruned schema should be matched").isEqualTo(expected);
+  }
+
+  @Test
+  public void testThreeLevelFullChildPruneColumns() {
+    MessageType expected =
+        Types.buildMessage()
+            .addField(
+                Types.buildGroup(Type.Repetition.REQUIRED)
+                    .addField(
+                        Types.buildGroup(Type.Repetition.REQUIRED)
+                            .addField(
+                                Types.primitive(PrimitiveTypeName.BINARY, Type.Repetition.REQUIRED)
+                                    .id(8)
+                                    .named("c1"))
+                            .addField(
+                                Types.primitive(PrimitiveTypeName.BINARY, Type.Repetition.REQUIRED)
+                                    .id(9)
+                                    .named("c2"))
+                            .id(7)
+                            .named("b1"))
+                    .id(6)
+                    .named("a4"))
+            .named("schema");
+    Type actual =
+        TypeWithSchemaVisitor.visit(
+            MULTI_LEVEL_ICEBERG_SCHEMA.asStruct(),
+            ParquetSchemaUtil.convert(MULTI_LEVEL_ICEBERG_SCHEMA, "schema"),
+            new PruneColumns(ImmutableSet.of(6, 7, 8, 9)));
+    assertThat(actual).as("Pruned schema should be matched").isEqualTo(expected);
+  }
+
+  @Test
+  public void testThreeLevelEmptyChildPruneColumns() {
+    MessageType expected =
+        Types.buildMessage()
+            .addField(
+                Types.buildGroup(Type.Repetition.REQUIRED)
+                    .addField(
+                        Types.buildGroup(Type.Repetition.REQUIRED)
+                            .addField(
+                                Types.primitive(PrimitiveTypeName.BINARY, Type.Repetition.REQUIRED)
+                                    .id(8)
+                                    .named("c1"))
+                            .addField(
+                                Types.primitive(PrimitiveTypeName.BINARY, Type.Repetition.REQUIRED)
+                                    .id(9)
+                                    .named("c2"))
+                            .id(7)
+                            .named("b1"))
+                    .id(6)
+                    .named("a4"))
+            .named("schema");
+    Type actual =
+        TypeWithSchemaVisitor.visit(
+            MULTI_LEVEL_ICEBERG_SCHEMA.asStruct(),
+            ParquetSchemaUtil.convert(MULTI_LEVEL_ICEBERG_SCHEMA, "schema"),
+            new PruneColumns(ImmutableSet.of(6, 7)));
+    assertThat(actual).as("Pruned schema should be matched").isEqualTo(expected);
+  }
+
+  @Test
+  public void testFourLevelFullChildPruneColumns() {
+    MessageType expected =
+        Types.buildMessage()
+            .addField(
+                Types.buildGroup(Type.Repetition.REQUIRED)
+                    .addField(
+                        Types.buildGroup(Type.Repetition.REQUIRED)
+                            .addField(
+                                Types.buildGroup(Type.Repetition.REQUIRED)
+                                    .addField(
+                                        Types.primitive(
+                                                PrimitiveTypeName.BINARY, Type.Repetition.REQUIRED)
+                                            .id(16)
+                                            .named("d1"))
+                                    .addField(
+                                        Types.primitive(
+                                                PrimitiveTypeName.BINARY, Type.Repetition.REQUIRED)
+                                            .id(17)
+                                            .named("d2"))
+                                    .id(15)
+                                    .named("c1"))
+                            .id(14)
+                            .named("b1"))
+                    .id(13)
+                    .named("a5"))
+            .named("schema");
+    Type actual =
+        TypeWithSchemaVisitor.visit(
+            MULTI_LEVEL_ICEBERG_SCHEMA.asStruct(),
+            ParquetSchemaUtil.convert(MULTI_LEVEL_ICEBERG_SCHEMA, "schema"),
+            new PruneColumns(ImmutableSet.of(13, 14, 15)));
+    assertThat(actual).as("Pruned schema should be matched").isEqualTo(expected);
+  }
+
+  @Test
+  public void testFourLevelOneChildPruneColumns() {
+    MessageType expected =
+        Types.buildMessage()
+            .addField(
+                Types.buildGroup(Type.Repetition.REQUIRED)
+                    .addField(
+                        Types.buildGroup(Type.Repetition.REQUIRED)
+                            .addField(
+                                Types.buildGroup(Type.Repetition.REQUIRED)
+                                    .addField(
+                                        Types.primitive(
+                                                PrimitiveTypeName.BINARY, Type.Repetition.REQUIRED)
+                                            .id(17)
+                                            .named("d2"))
+                                    .id(15)
+                                    .named("c1"))
+                            .id(14)
+                            .named("b1"))
+                    .id(13)
+                    .named("a5"))
+            .named("schema");
+    Type actual =
+        TypeWithSchemaVisitor.visit(
+            MULTI_LEVEL_ICEBERG_SCHEMA.asStruct(),
+            ParquetSchemaUtil.convert(MULTI_LEVEL_ICEBERG_SCHEMA, "schema"),
+            new PruneColumns(ImmutableSet.of(13, 14, 15, 17)));
     assertThat(actual).as("Pruned schema should be matched").isEqualTo(expected);
   }
 }
