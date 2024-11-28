57be178c7424549f1aa3a307aaf25017ff95c562
Fokko Driesprong <fokko@apache.org>
2024-11-28 23:28:34 +0100

    Core,API: Set `503: added_snapshot_id` as required (#11626)
    * Core,API: Set `503: added_snapshot_id` as required
    
    `503: added_snapshot_id` field should be written as a required field,
    but currently it is written as optional.
    
    As the reference implementation should produce metadata that is
    as close to the spec as possible.
    
    For reading, this isn't a problem with the current Java implementation
    as it will still read optional fields as required, but only thrown an
    error when it encounters a `null` value.
    
    * Improve constructor instead

--- core/src/test/java/org/apache/iceberg/TestTableMetadata.java
+++ core/src/test/java/org/apache/iceberg/TestTableMetadata.java
@@ -1663,7 +1663,8 @@ public class TestTableMetadata {
     try (ManifestListWriter writer =
         ManifestLists.write(1, Files.localOutput(manifestList), snapshotId, parentSnapshotId, 0)) {
       writer.addAll(
-          ImmutableList.of(new GenericManifestFile(localInput(manifestFile), SPEC_5.specId())));
+          ImmutableList.of(
+              new GenericManifestFile(localInput(manifestFile), SPEC_5.specId(), snapshotId)));
     }
 
     return localInput(manifestList).location();
