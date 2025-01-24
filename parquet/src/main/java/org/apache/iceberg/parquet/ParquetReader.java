65f28f1a21bce33d6e1e4f023d50b95738a2692d
Zizon Qiu <zzdtsv@gmail.com>
2025-01-25 00:40:49 +0800

    Revert "Fix Reader leak by removing useless copy"
    This reverts commit 6b655c07df353ab3ce33311c61b97d49141b0d89.

--- parquet/src/main/java/org/apache/iceberg/parquet/ParquetReader.java
+++ parquet/src/main/java/org/apache/iceberg/parquet/ParquetReader.java
@@ -80,7 +80,7 @@ public class ParquetReader<T> extends CloseableGroup implements CloseableIterabl
               reuseContainers,
               caseSensitive,
               null);
-      this.conf = readConf;
+      this.conf = readConf.copy();
       return readConf;
     }
     return conf;
