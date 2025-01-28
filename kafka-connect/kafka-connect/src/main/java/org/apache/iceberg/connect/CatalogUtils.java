c976a987caec013981f4f1bfd18b5b057f3ff1a3
Dawnpool <ye1219@gmail.com>
2025-01-28 16:15:39 +0900

    Add kerberos authentication option

--- kafka-connect/kafka-connect/src/main/java/org/apache/iceberg/connect/CatalogUtils.java
+++ kafka-connect/kafka-connect/src/main/java/org/apache/iceberg/connect/CatalogUtils.java
@@ -30,7 +30,9 @@ import org.apache.iceberg.common.DynClasses;
 import org.apache.iceberg.common.DynConstructors;
 import org.apache.iceberg.common.DynMethods;
 import org.apache.iceberg.common.DynMethods.BoundMethod;
+import org.apache.iceberg.common.DynMethods.StaticMethod;
 import org.apache.iceberg.relocated.com.google.common.collect.ImmutableList;
+import org.apache.kafka.common.config.ConfigException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -41,8 +43,15 @@ class CatalogUtils {
       ImmutableList.of("core-site.xml", "hdfs-site.xml", "hive-site.xml");
 
   static Catalog loadCatalog(IcebergSinkConfig config) {
-    return CatalogUtil.buildIcebergCatalog(
-        config.catalogName(), config.catalogProps(), loadHadoopConfig(config));
+    Object hadoopConf = loadHadoopConfig(config);
+    if (config.kerberosAuthentication()) {
+      configureKerberosAuthentication(
+          hadoopConf,
+          config.connectHdfsPrincipal(),
+          config.connectHdfsKeytab(),
+          config.kerberosTicketRenewPeriodMs());
+    }
+    return CatalogUtil.buildIcebergCatalog(config.catalogName(), config.catalogProps(), hadoopConf);
   }
 
   // use reflection here to avoid requiring Hadoop as a dependency
@@ -92,6 +101,72 @@ class CatalogUtils {
           "Hadoop found on classpath but could not create config, proceeding without config", e);
     }
     return null;
+  }
+
+  private static void configureKerberosAuthentication(
+      Object hadoopConf, String principal, String keytabPath, long renewPeriod) {
+    if (principal == null || keytabPath == null) {
+      throw new ConfigException(
+          "Hadoop is using Kerberos for authentication, you need to provide both a connect "
+              + "principal and the path to the keytab of the principal.");
+    }
+
+    Class<?> configurationClass =
+        DynClasses.builder().impl("org.apache.hadoop.conf.Configuration").build();
+    Class<?> userGroupInformationClass =
+        DynClasses.builder().impl("org.apache.hadoop.security.UserGroupInformation").build();
+    if (configurationClass == null) {
+      throw new RuntimeException("Hadoop not found on classpath, not creating Hadoop config");
+    }
+    if (userGroupInformationClass == null) {
+      throw new RuntimeException(
+          "Hadoop security not found on classpath, unable to configure Kerberos authentication");
+    }
+
+    try {
+      StaticMethod setConfigurationMethod =
+          DynMethods.builder("setConfiguration")
+              .impl(userGroupInformationClass, configurationClass)
+              .buildStatic();
+      StaticMethod loginUserFromKeytabMethod =
+          DynMethods.builder("loginUserFromKeytab")
+              .impl(userGroupInformationClass, String.class, String.class)
+              .buildStatic();
+      StaticMethod getLoginUserMethod =
+          DynMethods.builder("getLoginUser").impl(userGroupInformationClass).buildStatic();
+
+      setConfigurationMethod.invoke(hadoopConf);
+      loginUserFromKeytabMethod.invoke(principal, keytabPath);
+      final Object ugi = getLoginUserMethod.invoke();
+      BoundMethod getUserNameMethod =
+          DynMethods.builder("getUserName").impl(userGroupInformationClass).build(ugi);
+      String userName = getUserNameMethod.invoke();
+      LOG.info("login as: {}", userName);
+
+      Thread ticketRenewThread =
+          new Thread(() -> renewKerberosTicket(userGroupInformationClass, ugi, renewPeriod));
+      ticketRenewThread.setDaemon(true);
+      LOG.info("Starting the Kerberos ticket renew with period {} ms.", renewPeriod);
+      ticketRenewThread.start();
+    } catch (Exception e) {
+      throw new RuntimeException("Could not authenticate with Kerberos: " + e.getMessage());
+    }
+  }
+
+  private static void renewKerberosTicket(Class<?> ugiClass, Object ugi, long renewPeriod) {
+    BoundMethod reloginFromKeytabMethod =
+        DynMethods.builder("reloginFromKeytab").impl(ugiClass).build(ugi);
+    BoundMethod getUserNameMethod = DynMethods.builder("getUserName").impl(ugiClass).build(ugi);
+    String userName = getUserNameMethod.invoke();
+    while (true) {
+      try {
+        Thread.sleep(renewPeriod);
+        LOG.info("Attempting to re-login from keytab for user {}", userName);
+        reloginFromKeytabMethod.invoke();
+      } catch (Exception e) {
+        LOG.error("Error renewing the ticket", e);
+      }
+    }
   }
 
   private CatalogUtils() {}
