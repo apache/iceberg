d2a26d6e5f6ac08b4ce57e6a400da585f4ccf63f
Peter Vary <peter.vary.apache@gmail.com>
2025-02-18 09:57:10 +0100

    Proposed API interfaces - squashed for rebase

--- parquet/src/main/java/org/apache/iceberg/parquet/Parquet.java
+++ parquet/src/main/java/org/apache/iceberg/parquet/Parquet.java
@@ -302,8 +302,7 @@ public class Parquet {
     }
 
     // supposed to always be a private method used strictly by data and delete write builders
-    private WriteBuilder createContextFunc(
-        Function<Map<String, String>, Context> newCreateContextFunc) {
+    WriteBuilder createContextFunc(Function<Map<String, String>, Context> newCreateContextFunc) {
       this.createContextFunc = newCreateContextFunc;
       return this;
     }
@@ -498,7 +497,7 @@ public class Parquet {
       }
     }
 
-    private static class Context {
+    static class Context {
       private final int rowGroupSize;
       private final int pageSize;
       private final int pageRowLimit;
