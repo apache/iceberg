41113e7dcd9c531505c170d3189d6334c9818aeb
Fokko <fokko@apache.org>
2025-02-19 22:02:24 +0100

    Add V1 as well

--- core/src/test/java/org/apache/iceberg/rest/responses/TestLoadTableResponseParser.java
+++ core/src/test/java/org/apache/iceberg/rest/responses/TestLoadTableResponseParser.java
@@ -35,7 +35,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class TestLoadTableResponseParser {
-  private static final int FORMAT_V2 = 2;
 
   @Test
   public void nullAndEmptyCheck() {
@@ -60,9 +59,87 @@ public class TestLoadTableResponseParser {
         .hasMessage("Cannot parse missing field: metadata");
   }
 
+  @Test
+  public void roundTripSerdeV1() {
+    String uuid = "386b9f01-002b-4d8c-b77f-42c3fd3b7c9b";
+    TableMetadata metadata =
+        TableMetadata.buildFromEmpty(1)
+            .assignUUID(uuid)
+            .setLocation("location")
+            .setCurrentSchema(
+                new Schema(Types.NestedField.required(1, "x", Types.LongType.get())), 1)
+            .addPartitionSpec(PartitionSpec.unpartitioned())
+            .addSortOrder(SortOrder.unsorted())
+            .discardChanges()
+            .withMetadataLocation("metadata-location")
+            .build();
+
+    LoadTableResponse response = LoadTableResponse.builder().withTableMetadata(metadata).build();
+
+    String expectedJson =
+        String.format(
+            "{\n"
+                + "  \"metadata-location\" : \"metadata-location\",\n"
+                + "  \"metadata\" : {\n"
+                + "    \"format-version\" : 1,\n"
+                + "    \"table-uuid\" : \"386b9f01-002b-4d8c-b77f-42c3fd3b7c9b\",\n"
+                + "    \"location\" : \"location\",\n"
+                + "    \"last-updated-ms\" : %s,\n"
+                + "    \"last-column-id\" : 1,\n"
+                + "    \"schema\" : {\n"
+                + "      \"type\" : \"struct\",\n"
+                + "      \"schema-id\" : 0,\n"
+                + "      \"fields\" : [ {\n"
+                + "        \"id\" : 1,\n"
+                + "        \"name\" : \"x\",\n"
+                + "        \"required\" : true,\n"
+                + "        \"type\" : \"long\"\n"
+                + "      } ]\n"
+                + "    },\n"
+                + "    \"current-schema-id\" : 0,\n"
+                + "    \"schemas\" : [ {\n"
+                + "      \"type\" : \"struct\",\n"
+                + "      \"schema-id\" : 0,\n"
+                + "      \"fields\" : [ {\n"
+                + "        \"id\" : 1,\n"
+                + "        \"name\" : \"x\",\n"
+                + "        \"required\" : true,\n"
+                + "        \"type\" : \"long\"\n"
+                + "      } ]\n"
+                + "    } ],\n"
+                + "    \"partition-spec\" : [ ],\n"
+                + "    \"default-spec-id\" : 0,\n"
+                + "    \"partition-specs\" : [ {\n"
+                + "      \"spec-id\" : 0,\n"
+                + "      \"fields\" : [ ]\n"
+                + "    } ],\n"
+                + "    \"last-partition-id\" : 999,\n"
+                + "    \"default-sort-order-id\" : 0,\n"
+                + "    \"sort-orders\" : [ {\n"
+                + "      \"order-id\" : 0,\n"
+                + "      \"fields\" : [ ]\n"
+                + "    } ],\n"
+                + "    \"properties\" : { },\n"
+                + "    \"current-snapshot-id\" : -1,\n"
+                + "    \"refs\" : { },\n"
+                + "    \"snapshots\" : [ ],\n"
+                + "    \"statistics\" : [ ],\n"
+                + "    \"partition-statistics\" : [ ],\n"
+                + "    \"snapshot-log\" : [ ],\n"
+                + "    \"metadata-log\" : [ ]\n"
+                + "  }\n"
+                + "}",
+            metadata.lastUpdatedMillis());
+    String json = LoadTableResponseParser.toJson(response, true);
+    assertThat(json).isEqualTo(expectedJson);
+    // can't do an equality comparison because Schema doesn't implement equals/hashCode
+    assertThat(LoadTableResponseParser.toJson(LoadTableResponseParser.fromJson(json), true))
+        .isEqualTo(expectedJson);
+  }
+
   @ParameterizedTest
-  @ValueSource(ints = {FORMAT_V2, MAX_FORMAT_VERSION})
-  public void roundTripSerde(int formatVersion) {
+  @ValueSource(ints = {2, MAX_FORMAT_VERSION})
+  public void roundTripSerdeV2andHigher(int formatVersion) {
     String uuid = "386b9f01-002b-4d8c-b77f-42c3fd3b7c9b";
     TableMetadata metadata =
         TableMetadata.buildFromEmpty(formatVersion)
