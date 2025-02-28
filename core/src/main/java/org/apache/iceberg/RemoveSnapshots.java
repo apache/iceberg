03723f487e34a0a097f242d556b9657dd4083bab
Laszlo Bodor <bodorlaszlo0202@gmail.com>
2025-02-28 17:45:52 +0100

    Core: lazy init workerPool

--- core/src/main/java/org/apache/iceberg/RemoveSnapshots.java
+++ core/src/main/java/org/apache/iceberg/RemoveSnapshots.java
@@ -38,6 +38,7 @@ import static org.apache.iceberg.TableProperties.MIN_SNAPSHOTS_TO_KEEP_DEFAULT;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.function.Consumer;
@@ -83,7 +84,7 @@ class RemoveSnapshots implements ExpireSnapshots {
   private int defaultMinNumSnapshots;
   private Consumer<String> deleteFunc = defaultDelete;
   private ExecutorService deleteExecutorService = DEFAULT_DELETE_EXECUTOR_SERVICE;
-  private ExecutorService planExecutorService = ThreadPools.getWorkerPool();
+  private ExecutorService planExecutorService;
   private Boolean incrementalCleanup;
   private boolean specifiedSnapshotId = false;
   private boolean cleanExpiredMetadata = false;
@@ -161,6 +162,10 @@ class RemoveSnapshots implements ExpireSnapshots {
     return this;
   }
 
+  protected ExecutorService planExecutorService() {
+    return Optional.ofNullable(planExecutorService).orElseGet(ThreadPools::getWorkerPool);
+  }
+
   @Override
   public ExpireSnapshots cleanExpiredMetadata(boolean clean) {
     this.cleanExpiredMetadata = clean;
@@ -222,7 +227,7 @@ class RemoveSnapshots implements ExpireSnapshots {
       Set<Integer> reachableSpecs = Sets.newConcurrentHashSet();
       reachableSpecs.add(base.defaultSpecId());
       Tasks.foreach(idsToRetain)
-          .executeWith(planExecutorService)
+          .executeWith(planExecutorService())
           .run(
               snapshot ->
                   base.snapshot(snapshot).allManifests(ops.io()).stream()
@@ -370,9 +375,9 @@ class RemoveSnapshots implements ExpireSnapshots {
     FileCleanupStrategy cleanupStrategy =
         incrementalCleanup
             ? new IncrementalFileCleanup(
-                ops.io(), deleteExecutorService, planExecutorService, deleteFunc)
+                ops.io(), deleteExecutorService, planExecutorService(), deleteFunc)
             : new ReachableFileCleanup(
-                ops.io(), deleteExecutorService, planExecutorService, deleteFunc);
+                ops.io(), deleteExecutorService, planExecutorService(), deleteFunc);
 
     cleanupStrategy.cleanFiles(base, current);
   }
