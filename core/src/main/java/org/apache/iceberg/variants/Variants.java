280e18d172328543a376b6755c3347dcb9aec3d8
Aihua Xu <aihuaxu@gmail.com>
2025-03-12 14:31:13 -0700

    Add variant array reader in Parquet

--- core/src/main/java/org/apache/iceberg/variants/Variants.java
+++ core/src/main/java/org/apache/iceberg/variants/Variants.java
@@ -120,6 +120,10 @@ public class Variants {
     return VariantUtil.readByte(valueBuffer, 0) == 0;
   }
 
+  public static ShreddedArray array() {
+    return new ShreddedArray();
+  }
+
   public static <T> VariantPrimitive<T> of(PhysicalType type, T value) {
     return new PrimitiveWrapper<>(type, value);
   }
