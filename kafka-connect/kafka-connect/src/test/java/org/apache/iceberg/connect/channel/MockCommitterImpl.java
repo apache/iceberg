d0685f03e40110b121d0ec0b75ead3bbe10dc8f3
Pritam Kumar Mishra <kumarpritam863@gmail.com>
2025-03-21 09:24:09 +0530

    added tests for dynamic loading of committer impl class

---
+++ kafka-connect/kafka-connect/src/test/java/org/apache/iceberg/connect/channel/MockCommitterImpl.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.iceberg.connect.channel;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import org.apache.iceberg.catalog.Catalog;
+import org.apache.iceberg.connect.Committer;
+import org.apache.iceberg.connect.IcebergSinkConfig;
+import org.apache.iceberg.relocated.com.google.common.collect.Lists;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.connect.sink.SinkRecord;
+import org.apache.kafka.connect.sink.SinkTaskContext;
+
+public class MockCommitterImpl implements Committer {
+  private boolean isWorkerStarted = false;
+  private boolean isCoordinatorStarted = false;
+  private List<SinkRecord> sinkRecords = Lists.newArrayList();
+
+  @Override
+  public void start(Catalog catalog, IcebergSinkConfig config, SinkTaskContext context) {
+    throw new UnsupportedOperationException(
+        "The method start(Catalog, IcebergSinkConfig, SinkTaskContext) is deprecated and will be removed in 2.0.0. "
+            + "Use start(Catalog, IcebergSinkConfig, SinkTaskContext, Collection<TopicPartition>) instead.");
+  }
+
+  @Override
+  public void open(
+      Catalog catalog,
+      IcebergSinkConfig config,
+      SinkTaskContext context,
+      Collection<TopicPartition> addedPartitions) {
+    if (addedPartitions.contains(new TopicPartition("mock-topic", 0))) {
+      isCoordinatorStarted = true;
+    }
+  }
+
+  @Override
+  public void stop() {
+    throw new UnsupportedOperationException(
+        "The method stop() is deprecated and will be removed in 2.0.0. "
+            + "Use stop(Collection<TopicPartition>) instead.");
+  }
+
+  @Override
+  public void close(Collection<TopicPartition> closedPartitions) {
+    isWorkerStarted = false;
+    if (closedPartitions.contains(new TopicPartition("mock-topic", 0))) {
+      isCoordinatorStarted = false;
+    }
+  }
+
+  @Override
+  public void save(Collection<SinkRecord> sinkRecords) {
+    if (!isWorkerStarted) {
+      isWorkerStarted = true;
+    }
+    sinkRecords.addAll(sinkRecords);
+  }
+
+  @Override
+  public void configure(Map<String, String> config) {}
+}
