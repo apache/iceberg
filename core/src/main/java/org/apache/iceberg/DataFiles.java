455dcd35fda1bc9cbe11b5260d6f7fd9b9b343d4
Eduard Tudenhoefner <etudenhoefner@gmail.com>
2025-04-02 08:09:33 +0200

    Core: Introduce content_stats

--- core/src/main/java/org/apache/iceberg/DataFiles.java
+++ core/src/main/java/org/apache/iceberg/DataFiles.java
@@ -28,6 +28,8 @@ import org.apache.iceberg.hadoop.HadoopInputFile;
 import org.apache.iceberg.io.InputFile;
 import org.apache.iceberg.relocated.com.google.common.base.Preconditions;
 import org.apache.iceberg.relocated.com.google.common.collect.ImmutableList;
+import org.apache.iceberg.stats.BaseContentStats;
+import org.apache.iceberg.stats.ContentStats;
 import org.apache.iceberg.types.Conversions;
 import org.apache.iceberg.util.ByteBuffers;
 
@@ -151,6 +153,7 @@ public class DataFiles {
     private Map<Integer, Long> nanValueCounts = null;
     private Map<Integer, ByteBuffer> lowerBounds = null;
     private Map<Integer, ByteBuffer> upperBounds = null;
+    private ContentStats contentStats = null;
     private ByteBuffer keyMetadata = null;
     private List<Long> splitOffsets = null;
     private Integer sortOrderId = SortOrder.unsorted().orderId();
@@ -177,6 +180,7 @@ public class DataFiles {
       this.nanValueCounts = null;
       this.lowerBounds = null;
       this.upperBounds = null;
+      this.contentStats = null;
       this.splitOffsets = null;
       this.sortOrderId = SortOrder.unsorted().orderId();
       this.firstRowId = null;
@@ -198,6 +202,7 @@ public class DataFiles {
       this.nanValueCounts = toCopy.nanValueCounts();
       this.lowerBounds = toCopy.lowerBounds();
       this.upperBounds = toCopy.upperBounds();
+      this.contentStats = toCopy.contentStats();
       this.keyMetadata =
           toCopy.keyMetadata() == null ? null : ByteBuffers.copy(toCopy.keyMetadata());
       this.splitOffsets =
@@ -281,6 +286,16 @@ public class DataFiles {
       return this;
     }
 
+    public Builder withContentStats(ContentStats newColumnStats) {
+      this.contentStats = BaseContentStats.buildFrom(newColumnStats).build();
+      this.recordCount = contentStats.recordCount();
+      return this;
+    }
+
+    /**
+     * @deprecated since 1.10.0; use {@link Builder#withContentStats(ContentStats)} instead.
+     */
+    @Deprecated
     public Builder withMetrics(Metrics metrics) {
       // check for null to avoid NPE when unboxing
       this.recordCount = metrics.recordCount() == null ? -1 : metrics.recordCount();
@@ -346,6 +361,7 @@ public class DataFiles {
               nanValueCounts,
               lowerBounds,
               upperBounds),
+          contentStats,
           keyMetadata,
           splitOffsets,
           sortOrderId,
