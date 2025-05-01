f0fe78a6cfcbd852d3fba8a5c0f4b90d474fb312
Russell Spitzer <russell.spitzer@gmail.com>
2025-05-01 14:47:43 -0500

    Spotless Apply

--- api/src/test/java/org/apache/iceberg/TestIcebergBuild.java
+++ api/src/test/java/org/apache/iceberg/TestIcebergBuild.java
@@ -47,9 +47,10 @@ public class TestIcebergBuild {
   }
 
   /**
-   * This test is for Source Releases. When we have a source release we use a version.txt
-   * file in the parent directory of this module to actually set the "version" which should
-   * be included in the gradle build properties used by IcebergBuild.
+   * This test is for Source Releases. When we have a source release we use a version.txt file in
+   * the parent directory of this module to actually set the "version" which should be included in
+   * the gradle build properties used by IcebergBuild.
+   *
    * @throws IOException
    */
   @Test
