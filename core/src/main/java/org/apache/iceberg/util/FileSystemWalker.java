dfc2a70bc37ae232695585a388fb71765751217b
Guosmilesmile <511955993@qq.com>
2025-05-08 20:30:04 +0800

    Flink: Supports delete orphan files in TableMaintenance

---
+++ core/src/main/java/org/apache/iceberg/util/FileSystemWalker.java
@@ -0,0 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.iceberg.util;
+
+import java.util.List;
+import java.util.function.Predicate;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.PathFilter;
+import org.apache.iceberg.io.FileInfo;
+import org.apache.iceberg.io.SupportsPrefixOperations;
+import org.apache.iceberg.relocated.com.google.common.collect.Lists;
+
+/**
+ * Utility class for recursively traversing file systems and identifying hidden paths. Provides
+ * methods to list files recursively while filtering out hidden paths based on specified criteria.
+ */
+public class FileSystemWalker {
+
+  /**
+   * Recursively lists files in the specified directory that satisfy the given conditions.
+   *
+   * @param io File system interface supporting prefix operations
+   * @param dir Base directory to start recursive listing
+   * @param predicate Additional filter condition for files
+   * @param pathFilter Filter to identify hidden paths
+   * @return List to collect matching file locations
+   */
+  public static List<String> listDirRecursively(
+      SupportsPrefixOperations io,
+      String dir,
+      Predicate<FileInfo> predicate,
+      PathFilter pathFilter) {
+    List<String> matchingFiles = Lists.newArrayList();
+    String listPath = dir;
+    if (!dir.endsWith("/")) {
+      listPath = dir + "/";
+    }
+
+    Iterable<FileInfo> files = io.listPrefix(listPath);
+    for (org.apache.iceberg.io.FileInfo file : files) {
+      Path path = new Path(file.location());
+      if (!isHiddenPath(dir, path, pathFilter) && predicate.test(file)) {
+        matchingFiles.add(file.location());
+      }
+    }
+
+    return matchingFiles;
+  }
+
+  /**
+   * Determines if a path is hidden by checking its hierarchy against the base directory.
+   *
+   * @param baseDir The root directory to use as the stopping point for recursion
+   * @param path The path to check for hidden status
+   * @param pathFilter Filter used to evaluate path visibility
+   * @return {@code true} if the path is hidden, {@code false} otherwise
+   */
+  public static boolean isHiddenPath(String baseDir, Path path, PathFilter pathFilter) {
+    boolean isHiddenPath = false;
+    Path currentPath = path;
+    while (currentPath.getParent().toString().contains(baseDir)) {
+      if (!pathFilter.accept(currentPath)) {
+        isHiddenPath = true;
+        break;
+      }
+
+      currentPath = currentPath.getParent();
+    }
+
+    return isHiddenPath;
+  }
+}
---
+++ flink/v2.0/flink/src/test/java/org/apache/iceberg/flink/maintenance/operator/TestAntiJoin.java
@@ -0,0 +1,235 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.iceberg.flink.maintenance.operator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.streaming.runtime.streamrecord.StreamRecord;
+import org.apache.flink.streaming.util.KeyedTwoInputStreamOperatorTestHarness;
+import org.apache.flink.streaming.util.ProcessFunctionTestHarnesses;
+import org.apache.iceberg.exceptions.ValidationException;
+import org.apache.iceberg.flink.maintenance.api.DeleteOrphanFiles;
+import org.apache.iceberg.relocated.com.google.common.collect.ImmutableList;
+import org.apache.iceberg.relocated.com.google.common.collect.ImmutableMap;
+import org.apache.iceberg.relocated.com.google.common.collect.Maps;
+import org.junit.jupiter.api.Test;
+
+public class TestAntiJoin extends OperatorTestBase {
+  private Map<String, String> equalSchemes =
+      Maps.newHashMap(
+          ImmutableMap.of(
+              "s3n", "s3",
+              "s3a", "s3"));
+  private Map<String, String> equalAuthorities = Maps.newHashMap();
+  private final FileURI SCHEME_FILE_1 =
+      new FileURI("s3:/fileName1", equalSchemes, equalAuthorities);
+  private final FileURI SCHEME_FILE_2 =
+      new FileURI("s3:/fileName2", equalSchemes, equalAuthorities);
+  private final FileURI AUTHORITY_FILE_1 =
+      new FileURI("s3://HDFS1002060/fileName1", equalSchemes, equalAuthorities);
+  private final FileURI S3A_SCHEME_FILE_1 =
+      new FileURI("s3a://HDFS1002060/fileName1", equalSchemes, equalAuthorities);
+  private final FileURI S3B_SCHEME_FILE_1 =
+      new FileURI("s3b://HDFS1002060/fileName1", equalSchemes, equalAuthorities);
+
+  @Test
+  void testFileSystemFirst() throws Exception {
+    try (KeyedTwoInputStreamOperatorTestHarness<String, FileURI, FileURI, String> testHarness =
+        testHarness()) {
+      testHarness.open();
+
+      testHarness.processElement2(SCHEME_FILE_1, EVENT_TIME);
+      testHarness.processElement2(SCHEME_FILE_1, EVENT_TIME);
+      testHarness.processWatermark1(WATERMARK);
+      testHarness.processElement1(SCHEME_FILE_1, EVENT_TIME);
+      assertThat(testHarness.extractOutputValues()).isEmpty();
+      testHarness.processWatermark2(WATERMARK);
+      assertThat(testHarness.extractOutputValues()).isEmpty();
+      assertThat(testHarness.getSideOutput(DeleteOrphanFiles.ERROR_STREAM)).isNull();
+    }
+  }
+
+  @Test
+  void testTableFirst() throws Exception {
+    try (KeyedTwoInputStreamOperatorTestHarness<String, FileURI, FileURI, String> testHarness =
+        testHarness()) {
+      testHarness.open();
+
+      testHarness.processElement1(SCHEME_FILE_1, EVENT_TIME);
+      testHarness.processElement2(SCHEME_FILE_1, EVENT_TIME);
+      testHarness.processWatermark1(WATERMARK);
+      testHarness.processElement2(SCHEME_FILE_1, EVENT_TIME);
+      assertThat(testHarness.extractOutputValues()).isEmpty();
+      testHarness.processWatermark2(WATERMARK);
+      assertThat(testHarness.extractOutputValues()).isEmpty();
+      assertThat(testHarness.getSideOutput(DeleteOrphanFiles.ERROR_STREAM)).isNull();
+    }
+  }
+
+  @Test
+  void testOnlyFileSystem() throws Exception {
+    try (KeyedTwoInputStreamOperatorTestHarness<String, FileURI, FileURI, String> testHarness =
+        testHarness()) {
+      testHarness.open();
+
+      testHarness.processElement2(SCHEME_FILE_1, EVENT_TIME);
+      testHarness.processElement2(SCHEME_FILE_1, EVENT_TIME);
+      assertThat(testHarness.extractOutputValues()).isEmpty();
+      testHarness.processBothWatermarks(WATERMARK);
+      assertThat(testHarness.extractOutputValues())
+          .isEqualTo(ImmutableList.of(SCHEME_FILE_1.uriAsString()));
+      assertThat(testHarness.getSideOutput(DeleteOrphanFiles.ERROR_STREAM)).isNull();
+    }
+  }
+
+  @Test
+  void testOnlyTable() throws Exception {
+    try (KeyedTwoInputStreamOperatorTestHarness<String, FileURI, FileURI, String> testHarness =
+        testHarness()) {
+      testHarness.open();
+
+      testHarness.processElement1(SCHEME_FILE_1, EVENT_TIME);
+      assertThat(testHarness.extractOutputValues()).isEmpty();
+      testHarness.processBothWatermarks(WATERMARK);
+      assertThat(testHarness.extractOutputValues()).isEmpty();
+      assertThat(testHarness.getSideOutput(DeleteOrphanFiles.ERROR_STREAM)).isNull();
+    }
+  }
+
+  @Test
+  void testFileSystemWithAuthority() throws Exception {
+    try (KeyedTwoInputStreamOperatorTestHarness<String, FileURI, FileURI, String> testHarness =
+        testHarness()) {
+      testHarness.open();
+
+      testHarness.processElement1(SCHEME_FILE_1, EVENT_TIME);
+      testHarness.processElement2(AUTHORITY_FILE_1, EVENT_TIME);
+      assertThat(testHarness.extractOutputValues()).isEmpty();
+      testHarness.processBothWatermarks(WATERMARK);
+      assertThat(testHarness.extractOutputValues()).isEmpty();
+      assertThat(testHarness.getSideOutput(DeleteOrphanFiles.ERROR_STREAM)).isNull();
+    }
+  }
+
+  @Test
+  void testTableWithAuthority() throws Exception {
+    try (KeyedTwoInputStreamOperatorTestHarness<String, FileURI, FileURI, String> testHarness =
+        testHarness()) {
+      testHarness.open();
+
+      testHarness.processElement1(AUTHORITY_FILE_1, EVENT_TIME);
+      testHarness.processElement2(SCHEME_FILE_1, EVENT_TIME);
+      assertThat(testHarness.extractOutputValues()).isEmpty();
+      testHarness.processBothWatermarks(WATERMARK);
+      ConcurrentLinkedQueue<StreamRecord<Exception>> errorList =
+          testHarness.getSideOutput(DeleteOrphanFiles.ERROR_STREAM);
+      assertThat(errorList).hasSize(1);
+      assertThat(errorList.stream().findFirst().get().getValue())
+          .isInstanceOf(ValidationException.class);
+
+      assertThat(testHarness.extractOutputValues()).isEmpty();
+    }
+  }
+
+  @Test
+  void testDiffScheme() throws Exception {
+    try (KeyedTwoInputStreamOperatorTestHarness<String, FileURI, FileURI, String> testHarness =
+        testHarness()) {
+      testHarness.open();
+
+      testHarness.processElement1(AUTHORITY_FILE_1, EVENT_TIME);
+      testHarness.processElement2(S3A_SCHEME_FILE_1, EVENT_TIME);
+      assertThat(testHarness.extractOutputValues()).isEmpty();
+      testHarness.processBothWatermarks(WATERMARK);
+      assertThat(testHarness.extractOutputValues()).isEmpty();
+      assertThat(testHarness.getSideOutput(DeleteOrphanFiles.ERROR_STREAM)).isNull();
+    }
+  }
+
+  @Test
+  void testUnRegisterScheme() throws Exception {
+    try (KeyedTwoInputStreamOperatorTestHarness<String, FileURI, FileURI, String> testHarness =
+        testHarness()) {
+      testHarness.open();
+
+      testHarness.processElement1(AUTHORITY_FILE_1, EVENT_TIME);
+      testHarness.processElement2(S3B_SCHEME_FILE_1, EVENT_TIME);
+      assertThat(testHarness.extractOutputValues()).isEmpty();
+      testHarness.processBothWatermarks(WATERMARK);
+      ConcurrentLinkedQueue<StreamRecord<Exception>> errorList =
+          testHarness.getSideOutput(DeleteOrphanFiles.ERROR_STREAM);
+      assertThat(errorList).hasSize(1);
+      assertThat(errorList.stream().findFirst().get().getValue())
+          .isInstanceOf(ValidationException.class);
+
+      assertThat(testHarness.extractOutputValues()).isEmpty();
+    }
+  }
+
+  @Test
+  void testPrefixMismatchModeDelete() throws Exception {
+    try (KeyedTwoInputStreamOperatorTestHarness<String, FileURI, FileURI, String> testHarness =
+        testHarness(org.apache.iceberg.actions.DeleteOrphanFiles.PrefixMismatchMode.DELETE)) {
+      testHarness.open();
+
+      testHarness.processElement1(AUTHORITY_FILE_1, EVENT_TIME);
+      testHarness.processElement2(SCHEME_FILE_1, EVENT_TIME);
+      assertThat(testHarness.extractOutputValues()).isEmpty();
+      testHarness.processBothWatermarks(WATERMARK);
+      assertThat(testHarness.extractOutputValues())
+          .isEqualTo(ImmutableList.of(SCHEME_FILE_1.uriAsString()));
+      assertThat(testHarness.getSideOutput(DeleteOrphanFiles.ERROR_STREAM)).isNull();
+    }
+  }
+
+  @Test
+  void testPrefixMismatchModeIgnore() throws Exception {
+    try (KeyedTwoInputStreamOperatorTestHarness<String, FileURI, FileURI, String> testHarness =
+        testHarness(org.apache.iceberg.actions.DeleteOrphanFiles.PrefixMismatchMode.IGNORE)) {
+      testHarness.open();
+
+      testHarness.processElement1(AUTHORITY_FILE_1, EVENT_TIME);
+      testHarness.processElement2(SCHEME_FILE_1, EVENT_TIME);
+      assertThat(testHarness.extractOutputValues()).isEmpty();
+      testHarness.processBothWatermarks(WATERMARK);
+      assertThat(testHarness.extractOutputValues()).isEmpty();
+      assertThat(testHarness.getSideOutput(DeleteOrphanFiles.ERROR_STREAM)).isNull();
+    }
+  }
+
+  private static KeyedTwoInputStreamOperatorTestHarness<String, FileURI, FileURI, String>
+      testHarness(
+          org.apache.iceberg.actions.DeleteOrphanFiles.PrefixMismatchMode prefixMismatchMode)
+          throws Exception {
+    return ProcessFunctionTestHarnesses.forKeyedCoProcessFunction(
+        new AntiJoin(prefixMismatchMode),
+        (KeySelector<FileURI, String>) t -> t.path(),
+        (KeySelector<FileURI, String>) t -> t.path(),
+        BasicTypeInfo.STRING_TYPE_INFO);
+  }
+
+  private static KeyedTwoInputStreamOperatorTestHarness<String, FileURI, FileURI, String>
+      testHarness() throws Exception {
+    return testHarness(org.apache.iceberg.actions.DeleteOrphanFiles.PrefixMismatchMode.ERROR);
+  }
+}
