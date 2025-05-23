d015cd47627ad2aa410ef8931fef4425a5ef4832
Guosmilesmile <511955993@qq.com>
2025-05-23 22:49:33 +0800

    Flink: Backport fix npe in TaskResultAggregator when job recovery

--- flink/v1.20/flink/src/test/java/org/apache/iceberg/flink/maintenance/operator/TestTaskResultAggregator.java
+++ flink/v1.20/flink/src/test/java/org/apache/iceberg/flink/maintenance/operator/TestTaskResultAggregator.java
@@ -20,8 +20,9 @@ package org.apache.iceberg.flink.maintenance.operator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import org.apache.flink.streaming.api.watermark.Watermark;
+import org.apache.flink.streaming.runtime.streamrecord.StreamRecord;
 import org.apache.flink.streaming.util.TwoInputStreamOperatorTestHarness;
 import org.apache.iceberg.flink.maintenance.api.TaskResult;
 import org.apache.iceberg.flink.maintenance.api.Trigger;
@@ -36,10 +37,42 @@ class TestTaskResultAggregator extends OperatorTestBase {
     try (TwoInputStreamOperatorTestHarness<Trigger, Exception, TaskResult> testHarness =
         new TwoInputStreamOperatorTestHarness<>(taskResultAggregator)) {
       testHarness.open();
-      testHarness.processWatermark1(new Watermark(EVENT_TIME));
-      testHarness.processWatermark2(new Watermark(EVENT_TIME));
+      testHarness.processBothWatermarks(WATERMARK);
       ConcurrentLinkedQueue<Object> output = testHarness.getOutput();
-      assertThat(output).containsOnlyOnce(new Watermark(EVENT_TIME));
+      assertThat(output).containsOnlyOnce(WATERMARK);
+    }
+  }
+
+  @Test
+  void testProcessWatermarkWithoutElement() throws Exception {
+    TaskResultAggregator taskResultAggregator =
+        new TaskResultAggregator("table-name", "task-name", 0);
+    try (TwoInputStreamOperatorTestHarness<Trigger, Exception, TaskResult> testHarness =
+        new TwoInputStreamOperatorTestHarness<>(taskResultAggregator)) {
+      testHarness.open();
+      testHarness.processBothWatermarks(WATERMARK);
+      List<TaskResult> taskResults = testHarness.extractOutputValues();
+      assertThat(taskResults).hasSize(0);
+    }
+  }
+
+  @Test
+  void testProcessWatermark() throws Exception {
+    TaskResultAggregator taskResultAggregator =
+        new TaskResultAggregator("table-name", "task-name", 0);
+    try (TwoInputStreamOperatorTestHarness<Trigger, Exception, TaskResult> testHarness =
+        new TwoInputStreamOperatorTestHarness<>(taskResultAggregator)) {
+      testHarness.open();
+
+      testHarness.processElement1(new StreamRecord<>(Trigger.create(EVENT_TIME, 0)));
+      testHarness.processBothWatermarks(WATERMARK);
+      List<TaskResult> taskResults = testHarness.extractOutputValues();
+      assertThat(taskResults).hasSize(1);
+      TaskResult taskResult = taskResults.get(0);
+      assertThat(taskResult.taskIndex()).isEqualTo(0);
+      assertThat(taskResult.startEpoch()).isEqualTo(EVENT_TIME);
+      assertThat(taskResult.success()).isEqualTo(true);
+      assertThat(taskResult.exceptions()).hasSize(0);
     }
   }
 }
