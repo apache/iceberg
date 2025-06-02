5719b2c494a18f587254af65ba55486f0bdde8fb
Ning Kang <ningk@google.com>
2025-06-02 10:17:36 -0700

    Fixed test following comments

--- core/src/test/java/org/apache/iceberg/rest/auth/TestOAuth2Util.java
+++ core/src/test/java/org/apache/iceberg/rest/auth/TestOAuth2Util.java
@@ -73,10 +73,7 @@ public class TestOAuth2Util {
   public void testExpiresAt() {
     assertThat(OAuth2Util.expiresAtMillis(null)).isNull();
     assertThat(OAuth2Util.expiresAtMillis("not a token")).isNull();
-    assertThat(
-            OAuth2Util.expiresAtMillis(
-                "a.b.c token looks like jwt but not jwt and too short per section"))
-        .isNull();
+    assertThat(OAuth2Util.expiresAtMillis("a.b.c")).isNull();
 
     // expires at epoch second = 1
     String token =
