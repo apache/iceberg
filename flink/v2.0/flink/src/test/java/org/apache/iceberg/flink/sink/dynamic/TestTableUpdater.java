bdd4b21e12e6012944606837e058f9b5875000e3
Max Michels <max_michels@apple.com>
2025-06-03 16:58:12 +0200

    Add more tests

--- flink/v2.0/flink/src/test/java/org/apache/iceberg/flink/sink/dynamic/TestTableUpdater.java
+++ flink/v2.0/flink/src/test/java/org/apache/iceberg/flink/sink/dynamic/TestTableUpdater.java
@@ -20,9 +20,11 @@ package org.apache.iceberg.flink.sink.dynamic;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.iceberg.PartitionSpec;
 import org.apache.iceberg.Schema;
+import org.apache.iceberg.Table;
 import org.apache.iceberg.catalog.Catalog;
 import org.apache.iceberg.catalog.TableIdentifier;
 import org.apache.iceberg.flink.sink.TestFlinkIcebergSinkBase;
@@ -42,6 +44,39 @@ public class TestTableUpdater extends TestFlinkIcebergSinkBase {
           Types.NestedField.optional(2, "data", Types.StringType.get()),
           Types.NestedField.optional(3, "extra", Types.StringType.get()));
 
+  @Test
+  void testTableCreation() {
+    Catalog catalog = CATALOG_EXTENSION.catalog();
+    TableIdentifier tableIdentifier = TableIdentifier.parse("myTable");
+    TableMetadataCache cache = new TableMetadataCache(catalog, 10, Long.MAX_VALUE);
+    TableUpdater tableUpdater = new TableUpdater(cache, catalog);
+
+    tableUpdater.update(tableIdentifier, "main", SCHEMA, PartitionSpec.unpartitioned());
+    assertThat(catalog.tableExists(tableIdentifier)).isTrue();
+
+    Tuple2<Schema, CompareSchemasVisitor.Result> cachedSchema =
+        cache.schema(tableIdentifier, SCHEMA);
+    assertThat(cachedSchema.f0.sameSchema(SCHEMA)).isTrue();
+  }
+
+  @Test
+  void testTableAlreadyExists() {
+    Catalog catalog = CATALOG_EXTENSION.catalog();
+    TableIdentifier tableIdentifier = TableIdentifier.parse("myTable");
+    TableMetadataCache cache = new TableMetadataCache(catalog, 10, Long.MAX_VALUE);
+    TableUpdater tableUpdater = new TableUpdater(cache, catalog);
+
+    // Make the table non-existent in cache
+    cache.exists(tableIdentifier);
+    // Create the table
+    catalog.createTable(tableIdentifier, SCHEMA);
+    // Make sure that the cache is invalidated and the table refreshed without an error
+    Tuple3<Schema, CompareSchemasVisitor.Result, PartitionSpec> result =
+        tableUpdater.update(tableIdentifier, "main", SCHEMA, PartitionSpec.unpartitioned());
+    assertThat(result.f0.sameSchema(SCHEMA)).isTrue();
+    assertThat(result.f1).isEqualTo(CompareSchemasVisitor.Result.SAME);
+    assertThat(result.f2).isEqualTo(PartitionSpec.unpartitioned());
+  }
 
   @Test
   void testBranchCreationAndCaching() {
@@ -57,6 +92,22 @@ public class TestTableUpdater extends TestFlinkIcebergSinkBase {
 
     tableUpdater.update(tableIdentifier, "myBranch", SCHEMA, PartitionSpec.unpartitioned());
     assertThat(cache.getInternalCache().getIfPresent(tableIdentifier)).isEqualTo(cacheItem);
+  }
+
+  @Test
+  void testSpecCreation() {
+    Catalog catalog = CATALOG_EXTENSION.catalog();
+    TableIdentifier tableIdentifier = TableIdentifier.parse("myTable");
+    TableMetadataCache cache = new TableMetadataCache(catalog, 10, Long.MAX_VALUE);
+    TableUpdater tableUpdater = new TableUpdater(cache, catalog);
+
+    PartitionSpec spec = PartitionSpec.builderFor(SCHEMA).bucket("data", 10).build();
+    Tuple3<Schema, CompareSchemasVisitor.Result, PartitionSpec> result =
+        tableUpdater.update(tableIdentifier, "main", SCHEMA, spec);
+
+    Table table = catalog.loadTable(tableIdentifier);
+    assertThat(table).isNotNull();
+    assertThat(table.spec()).isEqualTo(spec);
   }
 
   @Test
@@ -105,24 +156,5 @@ public class TestTableUpdater extends TestFlinkIcebergSinkBase {
                 .getSchemaInfo()
                 .getLastResult(SCHEMA2))
         .isNull();
-  }
-
-  @Test
-  void testTableAlreadyExists() {
-    Catalog catalog = CATALOG_EXTENSION.catalog();
-    TableIdentifier tableIdentifier = TableIdentifier.parse("myTable");
-    TableMetadataCache cache = new TableMetadataCache(catalog, 10, Long.MAX_VALUE);
-    TableUpdater tableUpdater = new TableUpdater(cache, catalog);
-
-    // Make the table non-existent in cache
-    cache.exists(tableIdentifier);
-    // Create the table
-    catalog.createTable(tableIdentifier, SCHEMA);
-    // Make sure that the cache is invalidated and the table refreshed without an error
-    Tuple3<Schema, CompareSchemasVisitor.Result, PartitionSpec> result =
-        tableUpdater.update(tableIdentifier, "main", SCHEMA, PartitionSpec.unpartitioned());
-    assertThat(result.f0.sameSchema(SCHEMA)).isTrue();
-    assertThat(result.f1).isEqualTo(CompareSchemasVisitor.Result.SAME);
-    assertThat(result.f2).isEqualTo(PartitionSpec.unpartitioned());
   }
 }
