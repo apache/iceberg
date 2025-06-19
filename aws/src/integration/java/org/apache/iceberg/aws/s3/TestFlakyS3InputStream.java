a092ab2d513bc1e45df21684a1aa9681f501e667
SanjayMarreddi <sanjay.mareddi@gmail.com>
2025-06-19 17:41:00 +0100

    AWS: Support metrics tracking when using Analytics Accelerator stream

--- aws/src/integration/java/org/apache/iceberg/aws/s3/TestFlakyS3InputStream.java
+++ aws/src/integration/java/org/apache/iceberg/aws/s3/TestFlakyS3InputStream.java
@@ -69,14 +69,15 @@ public class TestFlakyS3InputStream extends TestS3InputStream {
   }
 
   @Override
-  SeekableInputStream newInputStream(S3Client s3Client, S3AsyncClient s3AsyncClient, S3URI uri) {
+  SeekableInputStream newInputStream(
+      S3Client s3Client, S3AsyncClient s3AsyncClient, S3URI uri, MetricsContext metricsContext) {
     if (s3FileIOProperties.isS3AnalyticsAcceleratorEnabled()) {
       PrefixedS3Client client =
           new PrefixedS3Client("s3", Map.of(), () -> s3Client, () -> s3AsyncClient);
       return AnalyticsAcceleratorUtil.newStream(
-          S3InputFile.fromLocation(uri.location(), client, MetricsContext.nullMetrics()));
+          S3InputFile.fromLocation(uri.location(), client, metricsContext));
     }
-    return new S3InputStream(s3Client, uri) {
+    return new S3InputStream(s3Client, uri, s3FileIOProperties, metricsContext) {
       @Override
       void resetForRetry() throws IOException {
         resetForRetryCounter.incrementAndGet();
