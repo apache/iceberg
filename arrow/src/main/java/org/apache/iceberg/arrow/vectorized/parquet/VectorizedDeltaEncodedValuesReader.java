9fe0bba8e1f58d277bf83b1bedb4f33906c34256
Eric Maynard <emaynard@apache.org>
2025-06-23 10:48:08 -0700

    create class

---
+++ arrow/src/main/java/org/apache/iceberg/arrow/vectorized/parquet/VectorizedDeltaEncodedValuesReader.java
@@ -0,0 +1,69 @@
+/*
+ *
+ *  * Licensed to the Apache Software Foundation (ASF) under one
+ *  * or more contributor license agreements.  See the NOTICE file
+ *  * distributed with this work for additional information
+ *  * regarding copyright ownership.  The ASF licenses this file
+ *  * to you under the Apache License, Version 2.0 (the
+ *  * "License"); you may not use this file except in compliance
+ *  * with the License.  You may obtain a copy of the License at
+ *  *
+ *  *   http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing,
+ *  * software distributed under the License is distributed on an
+ *  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  * KIND, either express or implied.  See the License for the
+ *  * specific language governing permissions and limitations
+ *  * under the License.
+ *
+ */
+
+package org.apache.iceberg.arrow.vectorized.parquet;
+
+import org.apache.arrow.vector.FieldVector;
+import org.apache.parquet.column.values.ValuesReader;
+import org.apache.parquet.io.api.Binary;
+
+public class VectorizedDeltaEncodedValuesReader extends ValuesReader implements VectorizedValuesReader {
+
+    @Override
+    public void skip() {
+
+    }
+
+    @Override
+    public byte readByte() {
+        return 0;
+    }
+
+    @Override
+    public short readShort() {
+        return 0;
+    }
+
+    @Override
+    public Binary readBinary(int len) {
+        return null;
+    }
+
+    @Override
+    public void readIntegers(int total, FieldVector vec, int rowId) {
+
+    }
+
+    @Override
+    public void readLongs(int total, FieldVector vec, int rowId) {
+
+    }
+
+    @Override
+    public void readFloats(int total, FieldVector vec, int rowId) {
+
+    }
+
+    @Override
+    public void readDoubles(int total, FieldVector vec, int rowId) {
+
+    }
+}
