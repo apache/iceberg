fb0af7e5fcb45fe69a80ae8cb11208fb7b36a956
Yuya Ebihara <ebyhry@gmail.com>
2025-07-04 16:57:39 +0900

    S3: Add LegacyMd5Plugin to S3 client builder (#12264)

--- aws/src/integration/java/org/apache/iceberg/aws/s3/TestS3FileIO.java
+++ aws/src/integration/java/org/apache/iceberg/aws/s3/TestS3FileIO.java
@@ -115,9 +115,10 @@ import software.amazon.awssdk.services.s3.paginators.ListObjectsV2Iterable;
 
 @Testcontainers
 public class TestS3FileIO {
-  @Container private static final MinIOContainer MINIO = MinioUtil.createContainer();
+  @Container private final MinIOContainer minio = createMinIOContainer();
 
-  private final SerializableSupplier<S3Client> s3 = () -> MinioUtil.createS3Client(MINIO);
+  private final SerializableSupplier<S3Client> s3 =
+      () -> MinioUtil.createS3Client(minio, legacyMd5PluginEnabled());
   private final S3Client s3mock = mock(S3Client.class, delegatesTo(s3.get()));
   private final Random random = new Random(1);
   private final int numBucketsForBatchDeletion = 3;
@@ -134,6 +135,16 @@ public class TestS3FileIO {
           "TagValue1",
           "s3.delete.batch-size",
           Integer.toString(batchDeletionSize));
+
+  protected MinIOContainer createMinIOContainer() {
+    MinIOContainer container = MinioUtil.createContainer();
+    container.start();
+    return container;
+  }
+
+  protected boolean legacyMd5PluginEnabled() {
+    return false;
+  }
 
   @BeforeEach
   public void before() {
