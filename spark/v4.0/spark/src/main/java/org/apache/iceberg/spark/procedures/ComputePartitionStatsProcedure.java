32b5362c892d96a044426d7c34673e068bb2b020
ajantha-bhat <ajanthabhat@gmail.com>
2025-07-11 11:07:30 +0530

    Adopt to built-in procedure style

--- spark/v4.0/spark/src/main/java/org/apache/iceberg/spark/procedures/ComputePartitionStatsProcedure.java
+++ spark/v4.0/spark/src/main/java/org/apache/iceberg/spark/procedures/ComputePartitionStatsProcedure.java
@@ -1,25 +1,24 @@
 /*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- *  * Licensed to the Apache Software Foundation (ASF) under one
- *  * or more contributor license agreements.  See the NOTICE file
- *  * distributed with this work for additional information
- *  * regarding copyright ownership.  The ASF licenses this file
- *  * to you under the Apache License, Version 2.0 (the
- *  * "License"); you may not use this file except in compliance
- *  * with the License.  You may obtain a copy of the License at
- *  *
- *  *   http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  * Unless required by applicable law or agreed to in writing,
- *  * software distributed under the License is distributed on an
- *  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  * KIND, either express or implied.  See the License for the
- *  * specific language governing permissions and limitations
- *  * under the License.
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.apache.iceberg.spark.procedures;
 
+import java.util.Iterator;
 import org.apache.iceberg.PartitionStatisticsFile;
 import org.apache.iceberg.Table;
 import org.apache.iceberg.actions.ComputePartitionStats;
@@ -28,7 +27,9 @@ import org.apache.iceberg.spark.procedures.SparkProcedures.ProcedureBuilder;
 import org.apache.spark.sql.catalyst.InternalRow;
 import org.apache.spark.sql.connector.catalog.Identifier;
 import org.apache.spark.sql.connector.catalog.TableCatalog;
-import org.apache.spark.sql.connector.iceberg.catalog.ProcedureParameter;
+import org.apache.spark.sql.connector.catalog.procedures.BoundProcedure;
+import org.apache.spark.sql.connector.catalog.procedures.ProcedureParameter;
+import org.apache.spark.sql.connector.read.Scan;
 import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.Metadata;
 import org.apache.spark.sql.types.StructField;
@@ -46,20 +47,22 @@ import org.apache.spark.unsafe.types.UTF8String;
  */
 public class ComputePartitionStatsProcedure extends BaseProcedure {
 
+  static final String NAME = "compute_partition_stats";
+
   private static final ProcedureParameter TABLE_PARAM =
-          ProcedureParameter.required("table", DataTypes.StringType);
+      requiredInParameter("table", DataTypes.StringType);
   private static final ProcedureParameter SNAPSHOT_ID_PARAM =
-          ProcedureParameter.optional("snapshot_id", DataTypes.LongType);
+      optionalInParameter("snapshot_id", DataTypes.LongType);
 
   private static final ProcedureParameter[] PARAMETERS =
-          new ProcedureParameter[] {TABLE_PARAM, SNAPSHOT_ID_PARAM};
+      new ProcedureParameter[] {TABLE_PARAM, SNAPSHOT_ID_PARAM};
 
   private static final StructType OUTPUT_TYPE =
-          new StructType(
-                  new StructField[] {
-                          new StructField(
-                                  "partition_statistics_file", DataTypes.StringType, true, Metadata.empty())
-                  });
+      new StructType(
+          new StructField[] {
+            new StructField(
+                "partition_statistics_file", DataTypes.StringType, true, Metadata.empty())
+          });
 
   public static ProcedureBuilder builder() {
     return new Builder<ComputePartitionStatsProcedure>() {
@@ -75,34 +78,33 @@ public class ComputePartitionStatsProcedure extends BaseProcedure {
   }
 
   @Override
+  public BoundProcedure bind(StructType inputType) {
+    return this;
+  }
+
   public ProcedureParameter[] parameters() {
     return PARAMETERS;
   }
 
   @Override
-  public StructType outputType() {
-    return OUTPUT_TYPE;
-  }
-
-  @Override
-  public InternalRow[] call(InternalRow args) {
+  public Iterator<Scan> call(InternalRow args) {
     ProcedureInput input = new ProcedureInput(spark(), tableCatalog(), PARAMETERS, args);
     Identifier tableIdent = input.ident(TABLE_PARAM);
     Long snapshotId = input.asLong(SNAPSHOT_ID_PARAM, null);
 
     return modifyIcebergTable(
-            tableIdent,
-            table -> {
-              ComputePartitionStats action = actions().computePartitionStats(table);
-              if (snapshotId != null) {
-                action.snapshot(snapshotId);
-              }
+        tableIdent,
+        table -> {
+          ComputePartitionStats action = actions().computePartitionStats(table);
+          if (snapshotId != null) {
+            action.snapshot(snapshotId);
+          }
 
-              return toOutputRows(action.execute());
-            });
+          return asScanIterator(OUTPUT_TYPE, toOutputRows(action.execute()));
+        });
   }
 
-  private InternalRow[] toOutputRows(Result result) {
+  private InternalRow[] toOutputRows(ComputePartitionStats.Result result) {
     PartitionStatisticsFile statisticsFile = result.statisticsFile();
     if (statisticsFile != null) {
       InternalRow row = newInternalRow(UTF8String.fromString(statisticsFile.path()));
@@ -110,6 +112,11 @@ public class ComputePartitionStatsProcedure extends BaseProcedure {
     } else {
       return new InternalRow[0];
     }
+  }
+
+  @Override
+  public String name() {
+    return NAME;
   }
 
   @Override
