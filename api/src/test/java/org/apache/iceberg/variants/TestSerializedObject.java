a9a950d08523e9f37a47ec9ae8408cc0ce6ba19d
Manikandan R <manirajv06@gmail.com>
2025-07-22 22:44:52 +0530

    Improve test coverage for 1-5 byte header string primitive in Variant

--- api/src/test/java/org/apache/iceberg/variants/TestSerializedObject.java
+++ api/src/test/java/org/apache/iceberg/variants/TestSerializedObject.java
@@ -24,6 +24,7 @@ import java.nio.ByteBuffer;
 import java.util.Map;
 import java.util.Random;
 import java.util.Set;
+import java.util.stream.Stream;
 import org.apache.iceberg.relocated.com.google.common.collect.ImmutableList;
 import org.apache.iceberg.relocated.com.google.common.collect.ImmutableMap;
 import org.apache.iceberg.relocated.com.google.common.collect.Maps;
@@ -31,6 +32,8 @@ import org.apache.iceberg.relocated.com.google.common.collect.Sets;
 import org.apache.iceberg.util.RandomUtil;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class TestSerializedObject {
@@ -182,15 +185,29 @@ public class TestSerializedObject {
     assertThat(actualInner.get("f").asPrimitive().get()).isEqualTo((byte) 3);
   }
 
-  @ParameterizedTest
-  @ValueSource(
-      ints = {
-        300, // a big string larger than 255 bytes to push the value offset size above 1 byte to
+  static Stream<Arguments> provideInputsForTestMultiByteOffsets() {
+    return Stream.of(
+        Arguments.of(
+            10, 1), // a string lesser than 255 bytes to push the value offset size of 1 byte to
+        // test OneByteOffsets
+        Arguments.of(
+            300,
+            2), // a big string larger than 255 bytes to push the value offset size above 1 byte to
         // test TwoByteOffsets
-        70_000 // a really-big string larger than 65535 bytes to push the value offset size above 1
+        Arguments.of(
+            70_000,
+            3), // a really-big string larger than 65535 bytes to push the value offset size above 2
         // byte to test ThreeByteOffsets
-      })
-  public void testMultiByteOffsets(int multiByteOffset) {
+        Arguments.of(
+            16_800_000,
+            4) // a really very big string larger than 1677216 bytes to push the value offset size
+        // above 3 byte to test FourByteOffsets
+        );
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideInputsForTestMultiByteOffsets")
+  public void testMultiByteOffsets(int multiByteOffset, int offsetSize) {
     String randomString = RandomUtil.generateString(multiByteOffset, random);
     SerializedPrimitive bigString = VariantTestUtil.createString(randomString);
 
@@ -211,7 +228,7 @@ public class TestSerializedObject {
     assertThat(object.get("b").asPrimitive().get()).isEqualTo((byte) 2);
     assertThat(object.get("c").type()).isEqualTo(PhysicalType.INT8);
     assertThat(object.get("c").asPrimitive().get()).isEqualTo((byte) 3);
-    VariantTestUtil.assertVariantString(object.get("big"), randomString);
+    VariantTestUtil.assertVariantString(object.get("big"), randomString, offsetSize);
   }
 
   @ParameterizedTest
