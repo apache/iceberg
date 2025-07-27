d29e6e0742cc252ab3f2b5ea774e995d8a8a5560
Aihua Xu <aihuaxu@gmail.com>
2025-07-27 12:10:54 -0700

    Update decimal test

--- parquet/src/test/java/org/apache/iceberg/parquet/TestVariantReaders.java
+++ parquet/src/test/java/org/apache/iceberg/parquet/TestVariantReaders.java
@@ -138,10 +138,10 @@ public class TestVariantReaders {
         Variants.ofIsoTimestamptz("1957-11-07T12:33:54.123456+00:00"),
         Variants.ofIsoTimestampntz("2024-11-07T12:33:54.123456"),
         Variants.ofIsoTimestampntz("1957-11-07T12:33:54.123456"),
-        Variants.of(new BigDecimal("123456.7890")), // decimal4
-        Variants.of(new BigDecimal("-123456.7890")), // decimal4
-        Variants.of(new BigDecimal("1234567890.987654321")), // decimal8
-        Variants.of(new BigDecimal("-1234567890.987654321")), // decimal8
+        Variants.of(new BigDecimal("12345.6789")), // decimal4
+        Variants.of(new BigDecimal("-12345.6789")), // decimal4
+        Variants.of(new BigDecimal("123456789.987654321")), // decimal8
+        Variants.of(new BigDecimal("-123456789.987654321")), // decimal8
         Variants.of(new BigDecimal("9876543210.123456789")), // decimal16
         Variants.of(new BigDecimal("-9876543210.123456789")), // decimal16
         Variants.of(ByteBuffer.wrap(new byte[] {0x0a, 0x0b, 0x0c, 0x0d})),
@@ -171,7 +171,7 @@ public class TestVariantReaders {
   }
 
   // TO GENERATE TEST CASES, SET THIS TO A LOCATION
-  private static final String CASE_LOCATION = null;
+  private static final String CASE_LOCATION = "/Users/aixu/Downloads/variant/";
   private static final FileIO IO = new TestTables.LocalFileIO();
   private static final StringWriter JSON_STRING_WRITER = new StringWriter();
   private static final AtomicInteger caseAssignment = new AtomicInteger(0);
