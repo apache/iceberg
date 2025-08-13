ddf7511bb841e66d8eeb8065329bb3408a5272b3
Parth Chandra <parthc@apple.com>
2025-08-12 17:28:58 -0700

    better message

--- parquet/src/main/java/org/apache/iceberg/parquet/Parquet.java
+++ parquet/src/main/java/org/apache/iceberg/parquet/Parquet.java
@@ -1359,7 +1359,7 @@ public class Parquet {
 
         if (batchedReaderFunc != null) {
           if (isComet) {
-            LOG.info("Comet enabled");
+            LOG.info("Comet vectorized reader enabled");
             return new CometVectorizedParquetReader<>(
                 file,
                 schema,
