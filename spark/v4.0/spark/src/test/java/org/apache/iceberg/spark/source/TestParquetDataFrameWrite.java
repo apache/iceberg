8b7bf744ce4a8a7ffcbc4e933170ce8564e1c60c
Fokko Driesprong <fokko@apache.org>
2025-08-17 23:49:09 +0200

    Cleanup

--- spark/v4.0/spark/src/test/java/org/apache/iceberg/spark/source/TestParquetDataFrameWrite.java
+++ spark/v4.0/spark/src/test/java/org/apache/iceberg/spark/source/TestParquetDataFrameWrite.java
@@ -18,23 +18,13 @@
  */
 package org.apache.iceberg.spark.source;
 
-import static org.apache.iceberg.types.Types.NestedField.optional;
-import static org.apache.iceberg.types.Types.NestedField.required;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.Assumptions.assumeThat;
 
-import java.io.IOException;
 import org.apache.iceberg.FileFormat;
-import org.apache.iceberg.Schema;
 import org.apache.iceberg.Table;
 import org.apache.iceberg.TableProperties;
-import org.apache.iceberg.types.Type;
-import org.apache.iceberg.types.TypeUtil;
-import org.apache.iceberg.types.Types;
 import org.apache.spark.SparkException;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.FieldSource;
 
 public class TestParquetDataFrameWrite extends DataFrameWriteTestBase {
   @Override
@@ -68,37 +58,5 @@ public class TestParquetDataFrameWrite extends DataFrameWriteTestBase {
         .cause()
         .isInstanceOf(IllegalArgumentException.class)
         .hasMessageStartingWith("Cannot convert value Parquet: unknown");
-  }
-
-  @ParameterizedTest
-  @FieldSource("SIMPLE_TYPES")
-  public void testTypeSchema(Type type) throws IOException {
-    assumeThat(supportsTime() || TypeUtil.find(type, t -> t.typeId() == Type.TypeID.TIME) == null)
-        .as("Spark does not support time fields")
-        .isTrue();
-    assumeThat(
-            supportsTimestampNanos()
-                || TypeUtil.find(type, t -> t.typeId() == Type.TypeID.TIMESTAMP_NANO) == null)
-        .as("timestamp_ns is not yet implemented")
-        .isTrue();
-    assumeThat(
-            supportsVariant()
-                || TypeUtil.find(type, t -> t.typeId() == Type.TypeID.VARIANT) == null)
-        .as("variant is not yet implemented")
-        .isTrue();
-    if (!supportsGeospatial()) {
-      assumeThat(TypeUtil.find(type, t -> t.typeId() == Type.TypeID.GEOMETRY) == null)
-          .as("geometry is not yet implemented")
-          .isTrue();
-      assumeThat(TypeUtil.find(type, t -> t.typeId() == Type.TypeID.GEOGRAPHY) == null)
-          .as("geography is not yet implemented")
-          .isTrue();
-    }
-
-    writeAndValidate(
-        new Schema(
-            required(1, "id", Types.LongType.get()),
-            optional(2, "test_type", type),
-            required(3, "trailing_data", Types.StringType.get())));
   }
 }
