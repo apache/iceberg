90b4b4fa17a2a403260a6731c8772fdb5341dedf
hsiang-c <huangc@apple.com>
2025-09-17 07:54:35 +0800

    Use Comet 0.10.0 w/ deletion support

--- spark/v4.0/spark/src/main/java/org/apache/iceberg/spark/source/SparkBatch.java
+++ spark/v4.0/spark/src/main/java/org/apache/iceberg/spark/source/SparkBatch.java
@@ -176,7 +176,7 @@ class SparkBatch implements Batch {
     return readConf.parquetVectorizationEnabled()
         && readConf.parquetReaderType() == ParquetReaderType.COMET
         && expectedSchema.columns().stream().allMatch(this::supportsCometBatchReads)
-        && taskGroups.stream().allMatch(this::supportsCometBatchReads);
+        && taskGroups.stream().allMatch(this::supportsParquetBatchReads);
   }
 
   private boolean supportsCometBatchReads(Types.NestedField field) {
@@ -184,21 +184,6 @@ class SparkBatch implements Batch {
         && !field.type().typeId().equals(Type.TypeID.UUID)
         && field.fieldId() != MetadataColumns.ROW_ID.fieldId()
         && field.fieldId() != MetadataColumns.LAST_UPDATED_SEQUENCE_NUMBER.fieldId();
-  }
-
-  private boolean supportsCometBatchReads(ScanTask task) {
-    if (task instanceof ScanTaskGroup) {
-      ScanTaskGroup<?> taskGroup = (ScanTaskGroup<?>) task;
-      return taskGroup.tasks().stream().allMatch(this::supportsCometBatchReads);
-
-    } else if (task.isFileScanTask() && !task.isDataTask()) {
-      FileScanTask fileScanTask = task.asFileScanTask();
-      // Comet can't handle delete files for now
-      return fileScanTask.file().format() == FileFormat.PARQUET && fileScanTask.deletes().isEmpty();
-
-    } else {
-      return false;
-    }
   }
 
   // conditions for using ORC batch reads:
