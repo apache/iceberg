1794f082de35c836e106ea23bdeb140d11d3c106
Tobias Pütz <tobias@minio.io>
2025-09-22 16:43:28 +0200

    fix test

--- spark/v4.0/spark/src/test/java/org/apache/iceberg/spark/source/ScanTestBase.java
+++ spark/v4.0/spark/src/test/java/org/apache/iceberg/spark/source/ScanTestBase.java
@@ -116,7 +116,7 @@ public abstract class ScanTestBase extends AvroDataTestBase {
       TableMetadata builder =
           TableMetadata.buildFrom(ops.current())
               .upgradeFormatVersion(3)
-              .setCurrentSchema(expectedSchemaWithTableIds, highestFieldId)
+              .setCurrentSchema(expectedSchemaWithTableIds)
               .build();
       ops.commit(ops.current(), builder);
     }
