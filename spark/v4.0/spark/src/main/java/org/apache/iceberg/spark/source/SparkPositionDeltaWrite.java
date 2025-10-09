79f4e2f740b3bfe43142568f0855be866876de8e
Lee Oren <lee@ryft.io>
2025-10-09 18:57:08 +0300

    spotlessApply

--- spark/v4.0/spark/src/main/java/org/apache/iceberg/spark/source/SparkPositionDeltaWrite.java
+++ spark/v4.0/spark/src/main/java/org/apache/iceberg/spark/source/SparkPositionDeltaWrite.java
@@ -285,7 +285,8 @@ class SparkPositionDeltaWrite implements DeltaWrite, RequiresDistributionAndOrde
 
     @Override
     public String toString() {
-      return String.format("IcebergPositionDeltaWrite(table=%s, format=%s)", table, context.dataFileFormat());
+      return String.format(
+          "IcebergPositionDeltaWrite(table=%s, format=%s)", table, context.dataFileFormat());
     }
 
     private Expression conflictDetectionFilter(SparkBatchQueryScan queryScan) {
