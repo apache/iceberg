d8783cc791e121bd2fbcd5804fb9ef7350c89726
geruh <dru@amazon.com>
2025-10-15 19:28:03 -0700

    Update comments and update Result interface
    Co-authored-by: Amogh Jahagirdar <2amogh@gmail.com>
    Co-authored-by: Mathew Fournier <160646114+tabmatfournier@users.noreply.github.com>

--- api/src/main/java/org/apache/iceberg/actions/RepairManifests.java
+++ api/src/main/java/org/apache/iceberg/actions/RepairManifests.java
@@ -23,35 +23,42 @@ import org.apache.iceberg.ManifestFile;
 /** An action that will repair manifests. Implementations should produce a new set of manifests. */
 public interface RepairManifests extends SnapshotUpdate<RepairManifests, RepairManifests.Result> {
 
-  /** Configuration method for repairing manifest entry statistics */
+  /**
+   * Configuration method for repairing manifest entry statistics
+   *
+   * @return this for method chaining
+   */
   RepairManifests repairEntryStats();
 
   /**
    * Configuration method for removing duplicate file entries and removing files which no longer
    * exist in storage
+   *
+   * @return this for method chaining
    */
   RepairManifests repairFileEntries();
 
   /**
-   * Configuration option for determining the rewritten and added manifests without actually
-   * committing the operation to the table
+   * Configuration option to preview repair manifest operation without actually committing the
+   * operation to the table
    *
    * @return this for method chaining
    */
   RepairManifests dryRun();
 
+  /** The action result that contains a summary of the execution. */
   interface Result {
     /** Returns rewritten manifests. */
     Iterable<ManifestFile> rewrittenManifests();
 
-    /** Returns the duplicate file paths removed */
-    Iterable<String> duplicateFilesRemoved();
+    /** Returns the number of duplicate file entries that were removed from manifests */
+    long duplicateFilesRemoved();
 
-    /** Returns the paths of the missing files which were removed */
-    Iterable<String> missingFilesRemoved();
+    /** Returns the number of missing file references that were removed from manifests */
+    long missingFilesRemoved();
 
-    /** Returns the paths of the missing files which were recovered */
-    Iterable<String> missingFilesRecovered();
+    /** Returns the number of missing files that were successfully recovered. */
+    long missingFilesRecovered();
 
     /** Returns the number of manifest entries for which stats were incorrect */
     long entryStatsIncorrectCount();
