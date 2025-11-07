8a8cbfe73d047fadc316b9b2c199748522115bfd
Peter Vary <peter.vary.apache@gmail.com>
2025-11-07 14:30:59 +0100

    Core, Parquet: Fix Parquet to work OutputFiles generated by StandardEncryptionManager.ecrypt().encryptingOutputFile()

--- core/src/main/java/org/apache/iceberg/encryption/StandardEncryptionManager.java
+++ core/src/main/java/org/apache/iceberg/encryption/StandardEncryptionManager.java
@@ -29,6 +29,7 @@ import java.util.concurrent.TimeUnit;
 import org.apache.iceberg.TableProperties;
 import org.apache.iceberg.io.InputFile;
 import org.apache.iceberg.io.OutputFile;
+import org.apache.iceberg.io.PositionOutputStream;
 import org.apache.iceberg.io.SeekableInputStream;
 import org.apache.iceberg.relocated.com.google.common.base.Preconditions;
 import org.apache.iceberg.relocated.com.google.common.collect.Iterables;
@@ -289,6 +290,35 @@ public class StandardEncryptionManager implements EncryptionManager {
 
     @Override
     public OutputFile encryptingOutputFile() {
+      return this;
+    }
+
+    @Override
+    public OutputFile plainOutputFile() {
+      return plainOutputFile;
+    }
+
+    @Override
+    public PositionOutputStream create() {
+      return lazyEncryptingOutputFile().create();
+    }
+
+    @Override
+    public PositionOutputStream createOrOverwrite() {
+      return lazyEncryptingOutputFile().createOrOverwrite();
+    }
+
+    @Override
+    public String location() {
+      return lazyEncryptingOutputFile().location();
+    }
+
+    @Override
+    public InputFile toInputFile() {
+      return lazyEncryptingOutputFile().toInputFile();
+    }
+
+    private OutputFile lazyEncryptingOutputFile() {
       if (null == lazyEncryptingOutputFile) {
         this.lazyEncryptingOutputFile =
             new AesGcmOutputFile(
@@ -298,11 +328,6 @@ public class StandardEncryptionManager implements EncryptionManager {
       }
 
       return lazyEncryptingOutputFile;
-    }
-
-    @Override
-    public OutputFile plainOutputFile() {
-      return plainOutputFile;
     }
   }
 
