a81000b07cba1614ccb341fde06ec098a949d7d4
Eduard Tudenhoefner <etudenhoefner@gmail.com>
2025-11-26 13:10:00 +0100

    add test

--- spark/v4.0/spark/src/test/java/org/apache/iceberg/spark/sql/TestSelect.java
+++ spark/v4.0/spark/src/test/java/org/apache/iceberg/spark/sql/TestSelect.java
@@ -155,6 +155,9 @@ public class TestSelect extends CatalogTestBase {
     Object[] first = row(1L, "a", 1.0F);
     Object[] second = row(2L, "b", 2.0F);
     Object[] third = row(3L, "c", Float.NaN);
+
+    // verify that LIMIT is properly applied in case SupportsPushDownLimit.isPartiallyPushed() is
+    // ever overridden in SparkScanBuilder
     assertThat(sql("SELECT * FROM %s LIMIT 1", tableName)).containsExactly(first);
     assertThat(sql("SELECT * FROM %s LIMIT 2", tableName)).containsExactly(first, second);
     assertThat(sql("SELECT * FROM %s LIMIT 3", tableName)).containsExactly(first, second, third);
