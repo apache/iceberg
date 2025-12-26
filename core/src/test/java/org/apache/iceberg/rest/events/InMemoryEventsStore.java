6b5ef7006223b862d2a283584b165116909ffc8e
Jayesh45-master <jc1450020@gmail.com>
2025-12-26 23:49:10 +0530

    iceberg issue

---
+++ core/src/test/java/org/apache/iceberg/rest/events/InMemoryEventsStore.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.iceberg.rest.events;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/** Simple in-memory events store for test harness use. */
+public class InMemoryEventsStore {
+  private final List<Map<String, Object>> events = new CopyOnWriteArrayList<>();
+
+  public void postEvents(List<Map<String, Object>> evts) {
+    if (evts != null && !evts.isEmpty()) {
+      events.addAll(evts);
+    }
+  }
+
+  public List<Map<String, Object>> getEvents() {
+    return Collections.unmodifiableList(new ArrayList<>(events));
+  }
+}
