b5af1d4fced16fe91fbe2d0487f7489267c7b7a4
peach12345 <danielweyck1990@gmail.com>
2026-01-18 18:19:04 +0100

    style: please linter

--- flink/v2.1/flink/src/test/java/org/apache/iceberg/flink/CatalogTableLoaderTest.java
+++ flink/v2.1/flink/src/test/java/org/apache/iceberg/flink/CatalogTableLoaderTest.java
@@ -16,7 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 package org.apache.iceberg.flink;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
