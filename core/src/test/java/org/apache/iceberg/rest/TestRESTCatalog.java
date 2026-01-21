03e3a69673eea5412249c654ecebd6d365dc5056
Gabor Kaszab <gaborkaszab@gmail.com>
2026-01-21 12:38:55 +0100

    Fix interference between different reqMatcher functions

--- core/src/test/java/org/apache/iceberg/rest/TestRESTCatalog.java
+++ core/src/test/java/org/apache/iceberg/rest/TestRESTCatalog.java
@@ -3451,8 +3451,7 @@ public class TestRESTCatalog extends CatalogTests<RESTCatalog> {
                     "parent",
                     RESTUtil.namespaceToQueryParam(parentNamespace, expectedSeparator),
                     "pageToken",
-                    ""),
-                null),
+                    "")),
             eq(ListNamespacesResponse.class),
             any(),
             any());
