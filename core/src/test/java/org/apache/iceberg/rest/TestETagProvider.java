d23b0d39bfd0a1f914aaa6d24d65906fc99200a3
gaborkaszab <gaborkaszab@gmail.com>
2026-02-10 11:22:18 +0100

    Core: Include query params into ETag calculation in reference IRC (#15057)

--- core/src/test/java/org/apache/iceberg/rest/TestETagProvider.java
+++ core/src/test/java/org/apache/iceberg/rest/TestETagProvider.java
@@ -21,30 +21,49 @@ package org.apache.iceberg.rest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 public class TestETagProvider {
+  static final String METADATA_LOCATION =
+      "/var/folders/20/290st0_52y5fyjcj2mlg49500000gn/T/junit-3064022805908958416/db_name/tbl_name/metadata/00000-f7a7956e-61d0-499b-be60-b141283f8229.metadata.json";
+
   @Test
   public void testNullInput() {
-    assertThatThrownBy(() -> ETagProvider.of(null))
+    assertThatThrownBy(() -> ETagProvider.of(null, null))
         .isInstanceOf(IllegalArgumentException.class)
         .hasMessageContaining("Invalid metadata location: null");
   }
 
   @Test
   public void testEmptyInput() {
-    assertThatThrownBy(() -> ETagProvider.of(""))
+    assertThatThrownBy(() -> ETagProvider.of("", null))
         .isInstanceOf(IllegalArgumentException.class)
         .hasMessageContaining("Invalid metadata location: empty");
   }
 
   @Test
   public void testETagContent() {
-    assertThat("1f865717")
+    assertThat("90b8ad4e")
+        .isEqualTo(
+            ETagProvider.of(METADATA_LOCATION, Map.of("param1", "value1", "param2", "value2")));
+
+    assertThat("cb787e6a")
         .isEqualTo(
             ETagProvider.of(
-                "/var/folders/20/290st0_52y5fyjcj2mlg49500000gn/T/junit-3064022805908958416/db_name/tbl_name/metadata/00000-f7a7956e-61d0-499b-be60-b141283f8229.metadata.json"));
+                METADATA_LOCATION, Map.of("param1", "other_value1", "param2", "other_value2")));
 
-    assertThat("55faa5d9").isEqualTo(ETagProvider.of("/short/path"));
+    assertThat("55faa5d9").isEqualTo(ETagProvider.of("/short/path", null));
+
+    assertThat("55faa5d9").isEqualTo(ETagProvider.of("/short/path", Map.of()));
+
+    assertThat("8adf3766").isEqualTo(ETagProvider.of("/short/path", Map.of("param", "some_value")));
+  }
+
+  @Test
+  public void testDifferentParameterOrderGiveSameETag() {
+    assertThat(ETagProvider.of(METADATA_LOCATION, Map.of("param1", "value1", "param2", "value2")))
+        .isEqualTo(
+            ETagProvider.of(METADATA_LOCATION, Map.of("param2", "value2", "param1", "value1")));
   }
 }
