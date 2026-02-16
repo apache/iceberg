6b15becc976a8353c48b9a8a6c75c7dc06cad0a3
pvary <peter.vary.apache@gmail.com>
2026-02-16 20:22:26 +0100

    Core, Arrow: Implementation of ArrowFormatModel (#15258)

--- core/src/main/java/org/apache/iceberg/formats/FormatModelRegistry.java
+++ core/src/main/java/org/apache/iceberg/formats/FormatModelRegistry.java
@@ -55,7 +55,9 @@ public final class FormatModelRegistry {
   private static final Logger LOG = LoggerFactory.getLogger(FormatModelRegistry.class);
   // The list of classes which are used for registering the reader and writer builders
   private static final List<String> CLASSES_TO_REGISTER =
-      ImmutableList.of("org.apache.iceberg.data.GenericFormatModels");
+      ImmutableList.of(
+          "org.apache.iceberg.data.GenericFormatModels",
+          "org.apache.iceberg.arrow.vectorized.ArrowFormatModels");
 
   // Format models indexed by file format and object model class
   private static final Map<Pair<FileFormat, Class<?>>, FormatModel<?, ?>> MODELS =
