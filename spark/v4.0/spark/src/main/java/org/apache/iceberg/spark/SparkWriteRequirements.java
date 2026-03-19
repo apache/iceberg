a441cab8b0dbb41ebd3a718d507da7acf0eb44e6
Becker Ewing <jbewing@live.com>
2026-03-18 23:47:47 -0400

    Add named constant for `NO_ADVISORY_PARTITION_SIZE`

--- spark/v4.0/spark/src/main/java/org/apache/iceberg/spark/SparkWriteRequirements.java
+++ spark/v4.0/spark/src/main/java/org/apache/iceberg/spark/SparkWriteRequirements.java
@@ -26,8 +26,10 @@ import org.apache.spark.sql.connector.expressions.SortOrder;
 /** A set of requirements such as distribution and ordering reported to Spark during writes. */
 public class SparkWriteRequirements {
 
+  public static final long NO_ADVISORY_PARTITION_SIZE = 0;
   public static final SparkWriteRequirements EMPTY =
-      new SparkWriteRequirements(Distributions.unspecified(), new SortOrder[0], 0);
+      new SparkWriteRequirements(
+          Distributions.unspecified(), new SortOrder[0], NO_ADVISORY_PARTITION_SIZE);
 
   private final Distribution distribution;
   private final SortOrder[] ordering;
@@ -54,6 +56,8 @@ public class SparkWriteRequirements {
 
   public long advisoryPartitionSize() {
     // Spark prohibits requesting a particular advisory partition size without distribution
-    return distribution instanceof UnspecifiedDistribution ? 0 : advisoryPartitionSize;
+    return distribution instanceof UnspecifiedDistribution
+        ? NO_ADVISORY_PARTITION_SIZE
+        : advisoryPartitionSize;
   }
 }
