5c833f5c3cbec6870d59401eb0b78d4e16ea83aa
GovindarajanD <rdgovindarajan@gmail.com>
2026-04-13 00:35:39 +0530

    API: Fix TableIdentifier.toLowerCase to use Locale.ROOT for namespace levels

--- api/src/test/java/org/apache/iceberg/catalog/TestTableIdentifier.java
+++ api/src/test/java/org/apache/iceberg/catalog/TestTableIdentifier.java
@@ -21,8 +21,8 @@ package org.apache.iceberg.catalog;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.junit.jupiter.api.Test;
 import java.util.Locale;
+import org.junit.jupiter.api.Test;
 
 public class TestTableIdentifier {
 
