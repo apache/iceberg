cd054ad22c1236caa56bb5e062dea49e4fa1437c
Sreesh Maheshwar <maheshwarsreesh@gmail.com>
2026-05-06 00:41:26 +0100

    Encrypting IO as a `DelegateFileIO`

--- api/src/test/java/org/apache/iceberg/encryption/TestEncryptingFileIO.java
+++ api/src/test/java/org/apache/iceberg/encryption/TestEncryptingFileIO.java
@@ -24,9 +24,12 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.withSettings;
 
+import java.util.List;
 import java.util.Map;
+import org.apache.iceberg.io.DelegateFileIO;
 import org.apache.iceberg.io.FileIO;
 import org.apache.iceberg.io.FileInfo;
+import org.apache.iceberg.io.SupportsBulkOperations;
 import org.apache.iceberg.io.SupportsPrefixOperations;
 import org.junit.jupiter.api.Test;
 
@@ -39,13 +42,23 @@ public class TestEncryptingFileIO {
     FileIO fileIONoMixins = mock(FileIO.class);
     assertThat(EncryptingFileIO.combine(fileIONoMixins, em))
         .isInstanceOf(EncryptingFileIO.class)
+        .isNotInstanceOf(EncryptingFileIO.WithSupportsPrefixOperations.class)
+        .isNotInstanceOf(EncryptingFileIO.WithDelegateFileIO.class)
         .extracting(EncryptingFileIO::encryptionManager)
         .isEqualTo(em);
 
-    FileIO fileIOWithMixins =
+    FileIO fileIOWithPrefixOps =
         mock(FileIO.class, withSettings().extraInterfaces(SupportsPrefixOperations.class));
-    assertThat(EncryptingFileIO.combine(fileIOWithMixins, em))
+    assertThat(EncryptingFileIO.combine(fileIOWithPrefixOps, em))
         .isInstanceOf(EncryptingFileIO.WithSupportsPrefixOperations.class)
+        .extracting(EncryptingFileIO::encryptionManager)
+        .isEqualTo(em);
+
+    DelegateFileIO delegateFileIO = mock(DelegateFileIO.class);
+    assertThat(EncryptingFileIO.combine(delegateFileIO, em))
+        .isInstanceOf(EncryptingFileIO.WithDelegateFileIO.class)
+        .isInstanceOf(DelegateFileIO.class)
+        .isInstanceOf(SupportsBulkOperations.class)
         .extracting(EncryptingFileIO::encryptionManager)
         .isEqualTo(em);
   }
@@ -65,6 +78,41 @@ public class TestEncryptingFileIO {
 
     fileIO.deletePrefix(prefix);
     verify(delegate).deletePrefix(prefix);
+  }
+
+  @Test
+  public void reWrappingDelegateFileIOPreservesType() {
+    EncryptionManager em1 = mock(EncryptionManager.class);
+    EncryptionManager em2 = mock(EncryptionManager.class);
+    DelegateFileIO delegate = mock(DelegateFileIO.class);
+
+    EncryptingFileIO first = EncryptingFileIO.combine(delegate, em1);
+    EncryptingFileIO second = EncryptingFileIO.combine(first, em2);
+    assertThat(second)
+        .isInstanceOf(EncryptingFileIO.WithDelegateFileIO.class)
+        .extracting(EncryptingFileIO::encryptionManager)
+        .isEqualTo(em2);
+  }
+
+  @Test
+  public void delegateFileIODelegation() {
+    EncryptionManager em = mock(EncryptionManager.class);
+    DelegateFileIO delegate = mock(DelegateFileIO.class);
+
+    EncryptingFileIO.WithDelegateFileIO fileIO =
+        (EncryptingFileIO.WithDelegateFileIO) EncryptingFileIO.combine(delegate, em);
+
+    String prefix = "prefix";
+    Iterable<FileInfo> fileInfos = mock(Iterable.class);
+    when(delegate.listPrefix(prefix)).thenReturn(fileInfos);
+    assertThat(fileIO.listPrefix(prefix)).isEqualTo(fileInfos);
+
+    fileIO.deletePrefix(prefix);
+    verify(delegate).deletePrefix(prefix);
+
+    List<String> pathsToDelete = List.of("path1", "path2");
+    fileIO.deleteFiles(pathsToDelete);
+    verify(delegate).deleteFiles(pathsToDelete);
   }
 
   @Test
