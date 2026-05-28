789b3b010791e2f371362d6d2fa7818e7de7126d
Ammar Chalifah <ammar.chalifah@gmail.com>
2026-05-28 12:34:01 +0300

    Revert the fix until the issue is reproducibe from Iceberg repo

--- spark/v4.1/spark/src/main/java/org/apache/iceberg/spark/source/StructInternalRow.java
+++ spark/v4.1/spark/src/main/java/org/apache/iceberg/spark/source/StructInternalRow.java
@@ -180,8 +180,8 @@ class StructInternalRow extends InternalRow {
   }
 
   private UTF8String getUTF8StringInternal(int ordinal) {
-    Object value = struct.get(ordinal, Object.class);
-    return UTF8String.fromString(value.toString());
+    CharSequence seq = struct.get(ordinal, CharSequence.class);
+    return UTF8String.fromString(seq.toString());
   }
 
   @Override
