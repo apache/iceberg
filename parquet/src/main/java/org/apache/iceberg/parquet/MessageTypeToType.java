792514e03455169c810454e0c39d39b4766b4b88
Xin Huang <42597328+huan233usc@users.noreply.github.com>
2026-06-26 16:43:56 -0700

    API, Parquet: Map geometry and geography to Parquet logical types (#16765)

--- parquet/src/main/java/org/apache/iceberg/parquet/MessageTypeToType.java
+++ parquet/src/main/java/org/apache/iceberg/parquet/MessageTypeToType.java
@@ -29,9 +29,11 @@ import org.apache.iceberg.relocated.com.google.common.base.Joiner;
 import org.apache.iceberg.relocated.com.google.common.base.Preconditions;
 import org.apache.iceberg.relocated.com.google.common.collect.Lists;
 import org.apache.iceberg.relocated.com.google.common.collect.Maps;
+import org.apache.iceberg.types.EdgeAlgorithm;
 import org.apache.iceberg.types.Type;
 import org.apache.iceberg.types.Types;
 import org.apache.iceberg.types.Types.TimestampType;
+import org.apache.parquet.column.schema.EdgeInterpolationAlgorithm;
 import org.apache.parquet.schema.GroupType;
 import org.apache.parquet.schema.LogicalTypeAnnotation;
 import org.apache.parquet.schema.MessageType;
@@ -253,6 +255,23 @@ class MessageTypeToType extends ParquetTypeVisitor<Type> {
     @Override
     public Optional<Type> visit(LogicalTypeAnnotation.BsonLogicalTypeAnnotation bsonType) {
       return Optional.of(Types.BinaryType.get());
+    }
+
+    @Override
+    public Optional<Type> visit(LogicalTypeAnnotation.GeometryLogicalTypeAnnotation geometryType) {
+      // a null crs resolves to the Iceberg default in GeometryType.of
+      return Optional.of(Types.GeometryType.of(geometryType.getCrs()));
+    }
+
+    @Override
+    public Optional<Type> visit(
+        LogicalTypeAnnotation.GeographyLogicalTypeAnnotation geographyType) {
+      // a null crs / algorithm resolves to the Iceberg default in GeographyType.of
+      EdgeInterpolationAlgorithm algorithm = geographyType.getAlgorithm();
+      return Optional.of(
+          Types.GeographyType.of(
+              geographyType.getCrs(),
+              algorithm != null ? EdgeAlgorithm.fromName(algorithm.name()) : null));
     }
   }
 
