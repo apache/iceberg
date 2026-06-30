173594a86fe582a68385ff98f45ff9784ad54cce
Ryan Blue <blue@apache.org>
2026-06-30 11:02:37 -0700

    Apply spotless.

--- core/src/main/java/org/apache/iceberg/MetricsConfig.java
+++ core/src/main/java/org/apache/iceberg/MetricsConfig.java
@@ -26,7 +26,6 @@ import static org.apache.iceberg.TableProperties.METRICS_MODE_COLUMN_CONF_PREFIX
 
 import java.io.Serializable;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Supplier;
