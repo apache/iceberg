7e0f82289481bb4916af26ccb14ae793e56ba2ef
goingforstudying-ctrl <goingforstudying-ctrl@users.noreply.github.com>
2026-07-02 22:23:59 -0400

    fix: remove unused ConcurrentHashMap import to pass spotless check

--- core/src/main/java/org/apache/iceberg/ManifestFilterManager.java
+++ core/src/main/java/org/apache/iceberg/ManifestFilterManager.java
@@ -23,7 +23,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.atomic.AtomicInteger;
