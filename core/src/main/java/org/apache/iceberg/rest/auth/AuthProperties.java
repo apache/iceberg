843787217fdc9dd47c83fbc0885179c0eb6b5aef
Alexandre Dutra <adutra@apache.org>
2026-07-27 21:04:52 +0200

    remove TODO

--- core/src/main/java/org/apache/iceberg/rest/auth/AuthProperties.java
+++ core/src/main/java/org/apache/iceberg/rest/auth/AuthProperties.java
@@ -38,8 +38,7 @@ public final class AuthProperties {
       "org.apache.iceberg.rest.auth.OAuth2Manager";
   static final String AUTH_MANAGER_IMPL_OAUTH2_NEW =
       "org.apache.iceberg.rest.auth.oauth2.OAuth2Manager";
-  public static final String AUTH_MANAGER_IMPL_OAUTH2 =
-      AUTH_MANAGER_IMPL_OAUTH2_LEGACY; // TODO switch to new manager
+  public static final String AUTH_MANAGER_IMPL_OAUTH2 = AUTH_MANAGER_IMPL_OAUTH2_LEGACY;
   public static final String AUTH_MANAGER_IMPL_SIGV4 =
       "org.apache.iceberg.aws.RESTSigV4AuthManager";
   public static final String AUTH_MANAGER_IMPL_GOOGLE =
