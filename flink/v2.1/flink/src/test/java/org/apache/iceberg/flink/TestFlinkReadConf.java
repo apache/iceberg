e2500088c716115bd5c3c6f2da7b01fa351c56d0
vishnuprakaz <nvishnuprakash02@gmail.com>
2026-08-06 20:40:24 +0530

    Use real tables instead of mocks in TestFlinkReadConf

--- flink/v2.1/flink/src/test/java/org/apache/iceberg/flink/TestFlinkReadConf.java
+++ flink/v2.1/flink/src/test/java/org/apache/iceberg/flink/TestFlinkReadConf.java
@@ -19,22 +19,40 @@
 package org.apache.iceberg.flink;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
 import org.apache.flink.configuration.Configuration;
 import org.apache.iceberg.Table;
 import org.apache.iceberg.TableProperties;
 import org.apache.iceberg.relocated.com.google.common.collect.ImmutableMap;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 class TestFlinkReadConf {
 
+  @TempDir private Path temporaryFolder;
+
+  private Table table;
+  private Table tableWithoutProperties;
+
+  @BeforeEach
+  void before() throws IOException {
+    table =
+        createTable(
+            ImmutableMap.of(
+                TableProperties.SPLIT_SIZE, "111",
+                TableProperties.SPLIT_LOOKBACK, "5",
+                TableProperties.SPLIT_OPEN_FILE_COST, "333"));
+    tableWithoutProperties = createTable(ImmutableMap.of());
+  }
+
   @Test
   void splitSizePrecedence() {
-    Table table = mock(Table.class);
-    when(table.properties()).thenReturn(ImmutableMap.of(TableProperties.SPLIT_SIZE, "111"));
-
     // table property is used when the flink config does not set the option
     FlinkReadConf conf = new FlinkReadConf(table, ImmutableMap.of(), new Configuration());
     assertThat(conf.splitSize()).isEqualTo(111L);
@@ -50,16 +68,12 @@ class TestFlinkReadConf {
     assertThat(conf.splitSize()).isEqualTo(999L);
 
     // default is used when neither is set
-    when(table.properties()).thenReturn(ImmutableMap.of());
-    conf = new FlinkReadConf(table, ImmutableMap.of(), new Configuration());
+    conf = new FlinkReadConf(tableWithoutProperties, ImmutableMap.of(), new Configuration());
     assertThat(conf.splitSize()).isEqualTo(TableProperties.SPLIT_SIZE_DEFAULT);
   }
 
   @Test
   void splitLookbackPrecedence() {
-    Table table = mock(Table.class);
-    when(table.properties()).thenReturn(ImmutableMap.of(TableProperties.SPLIT_LOOKBACK, "5"));
-
     FlinkReadConf conf = new FlinkReadConf(table, ImmutableMap.of(), new Configuration());
     assertThat(conf.splitLookback()).isEqualTo(5);
 
@@ -72,17 +86,12 @@ class TestFlinkReadConf {
         new FlinkReadConf(table, ImmutableMap.of(FlinkReadOptions.SPLIT_LOOKBACK, "9"), flinkConf);
     assertThat(conf.splitLookback()).isEqualTo(9);
 
-    when(table.properties()).thenReturn(ImmutableMap.of());
-    conf = new FlinkReadConf(table, ImmutableMap.of(), new Configuration());
+    conf = new FlinkReadConf(tableWithoutProperties, ImmutableMap.of(), new Configuration());
     assertThat(conf.splitLookback()).isEqualTo(TableProperties.SPLIT_LOOKBACK_DEFAULT);
   }
 
   @Test
   void splitOpenFileCostPrecedence() {
-    Table table = mock(Table.class);
-    when(table.properties())
-        .thenReturn(ImmutableMap.of(TableProperties.SPLIT_OPEN_FILE_COST, "333"));
-
     FlinkReadConf conf = new FlinkReadConf(table, ImmutableMap.of(), new Configuration());
     assertThat(conf.splitFileOpenCost()).isEqualTo(333L);
 
@@ -96,8 +105,12 @@ class TestFlinkReadConf {
             table, ImmutableMap.of(FlinkReadOptions.SPLIT_FILE_OPEN_COST, "999"), flinkConf);
     assertThat(conf.splitFileOpenCost()).isEqualTo(999L);
 
-    when(table.properties()).thenReturn(ImmutableMap.of());
-    conf = new FlinkReadConf(table, ImmutableMap.of(), new Configuration());
+    conf = new FlinkReadConf(tableWithoutProperties, ImmutableMap.of(), new Configuration());
     assertThat(conf.splitFileOpenCost()).isEqualTo(TableProperties.SPLIT_OPEN_FILE_COST_DEFAULT);
+  }
+
+  private Table createTable(Map<String, String> properties) throws IOException {
+    File folder = Files.createTempDirectory(temporaryFolder, "junit").toFile();
+    return SimpleDataUtil.createTable(folder.getAbsolutePath(), properties, false);
   }
 }
