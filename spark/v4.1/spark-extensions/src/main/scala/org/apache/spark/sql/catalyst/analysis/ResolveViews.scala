b0e9a6c3d2d063787999fb21a479b6bde97fc87a
Bobby Morck <bmorck@netflix.com>
2026-08-26 17:05:12 -0400

    Add braces to satisfy scalastyle

--- spark/v4.1/spark-extensions/src/main/scala/org/apache/spark/sql/catalyst/analysis/ResolveViews.scala
+++ spark/v4.1/spark-extensions/src/main/scala/org/apache/spark/sql/catalyst/analysis/ResolveViews.scala
@@ -117,9 +117,11 @@ case class ResolveViews(spark: SparkSession) extends Rule[LogicalPlan] with Look
     val compensate = viewSchemaMode == SparkSQLProperties.VIEW_SCHEMA_MODE_COMPENSATION
     val aliases = view.schema.fields.zipWithIndex.map { case (expected, pos) =>
       val attr = GetColumnByOrdinal(pos, expected.dataType)
-      val coerced =
-        if (compensate) Cast(attr, expected.dataType, ansiEnabled = true)
-        else UpCast(attr, expected.dataType)
+      val coerced = if (compensate) {
+        Cast(attr, expected.dataType, ansiEnabled = true)
+      } else {
+        UpCast(attr, expected.dataType)
+      }
       Alias(coerced, expected.name)(explicitMetadata = Some(expected.metadata))
     }.toIndexedSeq
 
