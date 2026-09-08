cddf72685334b1f725e2175e1ad0c99d3c57e2ea
Thomas Powell <tpowell@palantir.com>
2026-09-08 13:47:10 +0100

    fix

--- core/src/test/java/org/apache/iceberg/actions/TestFileIdentifier.java
+++ core/src/test/java/org/apache/iceberg/actions/TestFileIdentifier.java
@@ -22,7 +22,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
-class FileIdentifierTest {
+class TestFileIdentifier {
 
   @Test
   void matchesFileURIByNormalizedComponents() {
