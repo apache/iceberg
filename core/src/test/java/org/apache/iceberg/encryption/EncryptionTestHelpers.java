e74344ca56029b9af5211d6a474fd1f553170757
Sreesh Maheshwar <maheshwarsreesh@gmail.com>
2026-09-16 21:51:54 +0100

    Core, Hive: Commit manifest list encryption keys with the snapshot that uses them (#17984)

--- core/src/test/java/org/apache/iceberg/encryption/EncryptionTestHelpers.java
+++ core/src/test/java/org/apache/iceberg/encryption/EncryptionTestHelpers.java
@@ -30,6 +30,10 @@ public class EncryptionTestHelpers {
   private EncryptionTestHelpers() {}
 
   public static EncryptionManager createEncryptionManager() {
+    return createEncryptionManager(List.of());
+  }
+
+  public static EncryptionManager createEncryptionManager(List<EncryptedKey> keys) {
     Map<String, String> catalogProperties = Maps.newHashMap();
     catalogProperties.put(
         CatalogProperties.ENCRYPTION_KMS_IMPL, UnitestKMS.class.getCanonicalName());
@@ -37,7 +41,7 @@ public class EncryptionTestHelpers {
     tableProperties.put(TableProperties.ENCRYPTION_TABLE_KEY, UnitestKMS.MASTER_KEY_NAME1);
 
     return EncryptionUtil.createEncryptionManager(
-        List.of(), tableProperties, EncryptionUtil.createKmsClient(catalogProperties));
+        keys, tableProperties, EncryptionUtil.createKmsClient(catalogProperties));
   }
 
   public static String keyEncryptionKeyID(EncryptionManager em) {
