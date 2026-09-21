2ffead0eb00d427628205e082cd6cd50951d9852
Lucas Gameiro <lucas.gameiro@datadoghq.com>
2026-09-21 18:08:58 +0200

    Flink: Backport RowKind fix in DynamicIcebergSink's DataConverter to 1.20/2.1/2.2 (#18196)
    Backports #18101

--- flink/v2.1/flink/src/test/java/org/apache/iceberg/flink/sink/dynamic/TestRowDataConverter.java
+++ flink/v2.1/flink/src/test/java/org/apache/iceberg/flink/sink/dynamic/TestRowDataConverter.java
@@ -33,6 +33,7 @@ import org.apache.flink.table.data.GenericRowData;
 import org.apache.flink.table.data.RowData;
 import org.apache.flink.table.data.StringData;
 import org.apache.flink.table.data.TimestampData;
+import org.apache.flink.types.RowKind;
 import org.apache.iceberg.Schema;
 import org.apache.iceberg.flink.DataGenerator;
 import org.apache.iceberg.flink.DataGenerators;
@@ -74,6 +75,14 @@ class TestRowDataConverter {
   void testAddColumn() {
     assertThat(convert(SimpleDataUtil.createRowData(1, "a"), SCHEMA, SCHEMA2))
         .isEqualTo(GenericRowData.of(1, StringData.fromString("a"), null));
+  }
+
+  @Test
+  void testPreservesRowKind() {
+    RowData deleteRow = SimpleDataUtil.createRowData(1, "a");
+    deleteRow.setRowKind(RowKind.DELETE);
+
+    assertThat(convert(deleteRow, SCHEMA, SCHEMA2).getRowKind()).isEqualTo(RowKind.DELETE);
   }
 
   @Test
