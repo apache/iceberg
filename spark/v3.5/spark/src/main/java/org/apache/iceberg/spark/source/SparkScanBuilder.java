2fe250bf73fb67e8b81333eeecc17d226ccc2467
Manish Malhotra <osscm.bunny@gmail.com>
2026-09-22 16:38:17 -0700

    Spark: Fix missing ImmutableList import in SparkScanBuilder

--- spark/v3.5/spark/src/main/java/org/apache/iceberg/spark/source/SparkScanBuilder.java
+++ spark/v3.5/spark/src/main/java/org/apache/iceberg/spark/source/SparkScanBuilder.java
@@ -62,6 +62,7 @@ import org.apache.iceberg.index.TrackingFileReader;
 import org.apache.iceberg.io.CloseableIterable;
 import org.apache.iceberg.metrics.InMemoryMetricsReporter;
 import org.apache.iceberg.relocated.com.google.common.base.Preconditions;
+import org.apache.iceberg.relocated.com.google.common.collect.ImmutableList;
 import org.apache.iceberg.relocated.com.google.common.collect.ImmutableSet;
 import org.apache.iceberg.relocated.com.google.common.collect.Lists;
 import org.apache.iceberg.relocated.com.google.common.collect.Maps;
